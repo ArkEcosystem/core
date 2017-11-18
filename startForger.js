219b94135c18a0c667096dc7b6dfefbd80c9aba3
fixcrypt <fx.thoorens@gmail.com>
2017-11-18 15:25:26 +0100

    cleanup

--- startForger.js
+++ startForger.js
@@ -1,18 +1,16 @@
-const commander = require('commander');
-const packageJson = require('./package.json');
-const path = require('path');
-const config = require('./core/config');
-const ForgerManager = require('./core/forgerManager');
-const db = require('./core/db'); 
+const commander = require('commander')
+const packageJson = require('./package.json')
+const path = require('path')
+const config = require('./core/config')
+const ForgerManager = require('./core/forgerManager')
 
-
-let forgerManager = null, p2p = null;
+let forgerManager = null
 
 commander
   .version(packageJson.version)
   .option('-c, --config <path>', 'config files path')
   .option('-i, --interactive', 'launch cli')
-  .parse(process.argv);
+  .parse(process.argv)
 
 if (commander.config) {
   config.init({
@@ -20,21 +18,20 @@ if (commander.config) {
     genesisBlock: require(path.resolve(commander.config, 'genesisBlock.json')),
     network: require(path.resolve(commander.config, 'network.json')),
     delegates: require(path.resolve(commander.config, 'delegate.json'))
-  });
+  })
 }
 
-const logger = require('./core/logger');
-logger.init(config.server.fileLogLevel, config.network.name + '-forger');
+const logger = require('./core/logger')
+logger.init(config.server.fileLogLevel, config.network.name + '-forger')
 
-forgerManager = new ForgerManager(config);
+forgerManager = new ForgerManager(config)
 
 process.on('unhandledRejection', (reason, p) => {
-  logger.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
-});
+  logger.error('Unhandled Rejection at: Promise', p, 'reason:', reason)
+})
 
 forgerManager
   .loadDelegates()
   .then((forgers) => logger.info('ForgerManager started with', forgers.length, 'forgers'))
   .then(() => forgerManager.startForging('http://127.0.0.1:4000'))
-  .catch((fatal) => logger.error('fatal error', fatal));
-
+  .catch((fatal) => logger.error('fatal error', fatal))
