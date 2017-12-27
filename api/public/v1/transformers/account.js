fb3022ce3cf3bab4bcd665e996e3e9ad7439d553
chris <kristjan.kosic@gmail.com>
2017-12-27 17:08:05 +0100

    local mappings

--- api/public/v1/transformers/account.js
+++ api/public/v1/transformers/account.js
@@ -2,7 +2,13 @@ class AccountTransformer {
   constructor(model) {
     return {
       address: model.address,
-    };
+      publicKey: model.publicKey,
+      secondPublicKey: model.secondPublicKey,
+      vote: model.vote,
+      username: model.username,
+      balance: model.balance,
+      votebalance: model.votebalance
+    }
   }
 }
 
