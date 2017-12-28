a9e1575f373afb79e9e5120d5b2af12cf3dee055
fix <fx.thoorens@gmail.com>
2017-12-28 10:58:17 +0100

    improved tests

--- test/unit/model/account.spec.js
+++ test/unit/model/account.spec.js
@@ -3,15 +3,16 @@ const sinon = require('sinon')
 
 const Account = require('model/account')
 
-describe('Model | Account', () => {
-  describe('toString', () => {
+describe('Model | Account', () =>
+  describe('toString', () =>
     // TODO implementation is right?
-    xit('returns the address and the balance', () => {
+    it('returns the address and the balance', () => {
       const address = 'Abcde'
       const account = new Account(address)
-      account.balance = Math.pow(10, 8)
+      const balance = parseFloat((Math.random() * 1000).toFixed(8))
+      account.balance = balance * Math.pow(10, 8)
 
-      expect(account.toString()).to.eql(`${address}=1`)
+      expect(account.toString()).to.eql(`${address}=${balance}`)
     })
-  })
-})
+  )
+)
