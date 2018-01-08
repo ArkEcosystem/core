d178786167c75c6bec4c2396ab0d2d7bb1a53b2f
chris <kristjan.kosic@gmail.com>
2018-01-08 10:00:32 +0100

    basic tests for services

--- test/api/v1/accounts.service.spec.js
+++ test/api/v1/accounts.service.spec.js
@@ -1,31 +1,17 @@
 const chai = require('chai')
-const sinonChai = require('sinon-chai')
-
-// Chai plugins
-chai.use(sinonChai)
+const { expect } = require('chai')
 
 const base = 'http://localhost:4003'
 
-describe('GET /api/accounts', () => {
-  /*it('should return account information', (done) => {
-    request.get(`${base}/api/accounts/?address=DRac35wghMcmUSe5jDMLBDLWkVVjyKZFxK`, (err, res, body) => {
-      res.statusCode.should.eql(200)
-      res.headers['content-type'].should.contain('application/json')
-      body = JSON.parse(body)
-      body.status.should.eql('success')
-      body.data[0].should.include.keys(
-        'address','publicKey','secondPublicKey','vote','username','balance','votebalance'
-      )
-      done()
-    })
-  })*/
-
+describe('GET DEVNET /api/accounts', () => {
   it('should return account/wallet information', function(done) {
-    chai.request(`${base}/api/accounts/?address=DRac35wghMcmUSe5jDMLBDLWkVVjyKZFxK`)
-      .get('/')
-      .end(function(err, res) {
-        expect(res).to.have.status(123);
-        done();                               // <= Call done to signal callback end
-      });
-  });
+    chai.request(base)
+      .get('/api/accounts/?address=DRac35wghMcmUSe5jDMLBDLWkVVjyKZFxK')
+      .end((err, res)  => {
+        expect(res).to.have.status(200)
+        expect(res).to.be.json
+        expect(res.body.account).to.have.all.keys(['address','publicKey','secondPublicKey','vote','username','balance','votebalance'])
+        done()
+      })
+  })
 })
