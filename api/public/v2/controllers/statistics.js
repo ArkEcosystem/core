37896ed94ac0a92409d2c221239a3a1130b518f4
Brian Faust <faustbrian@users.noreply.github.com>
2018-01-22 07:45:22 +0200

    improve blockchain stats endpoint

--- api/public/v2/controllers/statistics.js
+++ api/public/v2/controllers/statistics.js
@@ -6,21 +6,49 @@ const _ = require('lodash')
 
 class StatisticsController {
   blockchain (req, res, next) {
+    const lastBlock = blockchain.status.lastBlock
+
     const height = blockchain.status.lastBlock.data.height
     const initialSupply = config.genesisBlock.totalAmount / 10 ** 8
-    const rewardPerBlock = config.getConstants(height).reward / 10 ** 8
 
-    helpers.respondWith('ok', {
-      data: {
-        supply: {
-          initial: initialSupply * 10 ** 8,
-          current: (initialSupply + ((height - config.getConstants(height).height) * rewardPerBlock)) * 10 ** 8
-        },
-        blocks: {
-          forged: height,
-          reward: height * rewardPerBlock
-        }
-      }
+    const constants = config.getConstants(height)
+    const rewardPerBlock = constants.reward / 10 ** 8
+
+    const totalSupply = config.genesisBlock.totalAmount + (lastBlock.data.height - constants.height) * constants.reward
+
+    db.getActiveDelegates(height).then(delegates => {
+      Promise.all(delegates.map(d => {
+        return db.accounts.findById(d.publicKey).then(account => {
+          return {
+            username: account.username,
+            approval: ((d.balance / totalSupply) * 100).toFixed(2),
+            productivity: (100 - (account.missedBlocks / ((account.producedBlocks + account.missedBlocks) / 100))).toFixed(2)
+          }
+        })
+      })).then((accounts) => {
+        const walletsByProductivity = _.sortBy(accounts, 'productivity')
+
+        helpers.respondWith('ok', {
+          data: {
+            supply: {
+              initial: initialSupply * 10 ** 8,
+              current: (initialSupply + ((height - config.getConstants(height).height) * rewardPerBlock)) * 10 ** 8
+            },
+            blocks: {
+              forged: height,
+              rewards: height * rewardPerBlock
+            },
+            rewards: {
+              start: constants.height,
+              total: height * rewardPerBlock
+            },
+            productivity: {
+              best: walletsByProductivity[0],
+              worst: walletsByProductivity.reverse()[0]
+            }
+          }
+        })
+      })
     })
   }
 
