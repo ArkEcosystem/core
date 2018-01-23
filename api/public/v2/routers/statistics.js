3932317c14d3d88988ea9924720eb725047da85b
Brian Faust <hello@brianfaust.me>
2018-01-23 11:35:33 +0200

    add vote/unvote stat endpoints
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- api/public/v2/routers/statistics.js
+++ api/public/v2/routers/statistics.js
@@ -4,15 +4,6 @@ module.exports = (registrar) => {
   registrar.get('stats/blockchain', controller.blockchain)
   registrar.get('stats/transactions', controller.transactions)
   registrar.get('stats/blocks', controller.blocks)
-
-  // tx per block
-  // tx per hour
-  // tx per day
-  // tx per week
-  // tx per month
-
-  // votes
-
-  // incomming ark (from exchanges)
-  // selling ark (to exchanges)
+  registrar.get('stats/votes', controller.votes)
+  registrar.get('stats/unvotes', controller.unvotes)
 }
