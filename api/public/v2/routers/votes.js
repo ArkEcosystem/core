d76f4fe1be527112e121fb1d36ba8a40e44d4eee
Brian Faust <faustbrian@users.noreply.github.com>
2018-01-23 12:32:53 +0200

    wrong method registered in router

--- api/public/v2/routers/votes.js
+++ api/public/v2/routers/votes.js
@@ -1,6 +1,6 @@
 const controllers = require('../controllers/votes')
 
 module.exports = (registrar) => {
-  registrar.get('votes', controllers.votes)
+  registrar.get('votes', controllers.index)
   registrar.get('votes/:id', controllers.show)
 }
