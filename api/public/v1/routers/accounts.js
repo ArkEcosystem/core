3a82b68e1d83260e36c9b6f9ebb4c69587e06d10
Brian Faust <faustbrian@users.noreply.github.com>
2018-01-25 18:51:59 +0200

    adjust route config

--- api/public/v1/routers/accounts.js
+++ api/public/v1/routers/accounts.js
@@ -3,11 +3,11 @@ const schema = require('../schemas/accounts')
 
 module.exports = (registrar) => {
   registrar.get('accounts/getAllAccounts', controllers.index)
-  registrar.get('accounts', controllers.show, schema.getAccount)
-  registrar.get('accounts/getBalance', controllers.balance, schema.getBalance)
-  registrar.get('accounts/getPublicKey', controllers.publicKey, schema.getPublicKey)
+  registrar.get('accounts', controllers.show, {schema: schema.getAccount})
+  registrar.get('accounts/getBalance', controllers.balance, {schema: schema.getBalance})
+  registrar.get('accounts/getPublicKey', controllers.publicKey, {schema: schema.getPublicKey})
   registrar.get('accounts/delegates/fee', controllers.fee)
-  registrar.get('accounts/delegates', controllers.delegates, schema.getDelegates)
+  registrar.get('accounts/delegates', controllers.delegates, {schema: schema.getDelegates})
   registrar.get('accounts/top', controllers.top)
   registrar.get('accounts/count', controllers.count)
 }
