cb74f29b2d91dd80344f0c3e64520a8623910842
Brian Faust <hello@brianfaust.me>
2018-01-29 14:56:30 +0200

    slip
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- api/public/v1/routers/wallets.js
+++ api/public/v1/routers/wallets.js
@@ -2,7 +2,7 @@ const controllers = require('../controllers/wallets')
 const schema = require('../schemas/wallets')
 
 module.exports = (registrar) => {
-  registrar.get('accounts/getAllWallets', controllers.index)
+  registrar.get('accounts/getAllAccounts', controllers.index)
   registrar.get('accounts', controllers.show, {schema: schema.getWallet})
   registrar.get('accounts/getBalance', controllers.balance, {schema: schema.getBalance})
   registrar.get('accounts/getPublicKey', controllers.publicKey, {schema: schema.getPublicKey})
