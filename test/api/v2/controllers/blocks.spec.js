1fac50bfc1d5227b68024fe400dae8b1ca5cadb0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-01-29 15:40:04 +0000

    Block Unit Tests for Search endpoint

--- test/api/v2/controllers/blocks.spec.js
+++ test/api/v2/controllers/blocks.spec.js
@@ -1,6 +1,33 @@
 const Helpers = require('../helpers')
 
 const blockId = '6995950265304491676'
+const version = 0
+const wrongVersion = 1
+const previousBlock = '16934430250273572763'
+const payloadHash = '705a0c79c30269f013bf4b6aec260f848eead57e036e730654b6d6acd09ba3d0'
+const generatorPublicKey = '0326580718fc86ba609799ac95fcd2721af259beb5afa81bfce0ab7d9fe95de991'
+const blockSignature = '304402202c2fec8a929d05e2913d11fe942ed3c4b9a84c92d5676e0ae30d49cba66129cb022027a2e13dfab6297673a3f969fbdaa077807e850d0cebb7235e9ab81f4715c30f'
+const timestamp = 26680224
+const timestampFrom = 26680223
+const timestampTo = 26680225
+const height = 2390645
+const heightFrom = 2390644
+const heightTo = 2390646
+const numberOfTransactions = 5
+const numberOfTransactionsFrom = 4
+const numberOfTransactionsTo = 6
+const totalAmount = 29811060
+const totalAmountFrom = 29811059
+const totalAmountTo = 29811061
+const totalFee = 50000000
+const totalFeeFrom = 40000000
+const totalFeeTo = 60000000
+const reward = 200000000
+const rewardFrom = 100000000
+const rewardTo = 300000000
+const payloadLength = 160
+const payloadLengthFrom = 159
+const payloadLengthTo = 161
 
 describe('API 2.0 - Blocks', () => {
   describe('GET /api/blocks', () => {
@@ -11,24 +38,7 @@ describe('API 2.0 - Blocks', () => {
         Helpers.assertPaginator(res)
 
         const block = res.body.data[1]
-        block.should.have.property('id').which.is.a('string')
-        block.should.have.property('version').which.is.a('number')
-        block.should.have.property('height').which.is.a('number')
-        block.should.have.property('previous').which.is.a('string')
-
-        block.should.have.property('forged').which.is.an('object')
-        block.forged.should.have.property('reward').which.is.an('number')
-        block.forged.should.have.property('fee').which.is.an('number')
-
-        block.should.have.property('payload').which.is.an('object')
-        block.payload.should.have.property('length').which.is.an('number')
-        block.payload.should.have.property('hash').which.is.an('string')
-
-        block.should.have.property('generator').which.is.an('object')
-        block.generator.should.have.property('public_key').which.is.an('string')
-
-        block.should.have.property('signature').which.is.an('string')
-        block.should.have.property('transactions').which.is.an('number')
+        Helpers.assertBlock(block)
 
         done()
       })
@@ -41,24 +51,9 @@ describe('API 2.0 - Blocks', () => {
         Helpers.assertSuccessful(err, res)
         Helpers.assertResource(res)
 
-        res.body.data.should.have.property('id').which.is.a('string')
-        res.body.data.should.have.property('version').which.is.a('number')
-        res.body.data.should.have.property('height').which.is.a('number')
-        res.body.data.should.have.property('previous').which.is.a('string')
-
-        res.body.data.should.have.property('forged').which.is.an('object')
-        res.body.data.forged.should.have.property('reward').which.is.an('number')
-        res.body.data.forged.should.have.property('fee').which.is.an('number')
-
-        res.body.data.should.have.property('payload').which.is.an('object')
-        res.body.data.payload.should.have.property('length').which.is.an('number')
-        res.body.data.payload.should.have.property('hash').which.is.an('string')
-
-        res.body.data.should.have.property('generator').which.is.an('object')
-        res.body.data.generator.should.have.property('public_key').which.is.an('string')
-
-        res.body.data.should.have.property('signature').which.is.an('string')
-        res.body.data.should.have.property('transactions').which.is.an('number')
+        const block = res.body.data;
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
 
         done()
       })
@@ -72,15 +67,8 @@ describe('API 2.0 - Blocks', () => {
         Helpers.assertCollection(res)
 
         const transaction = res.body.data[0]
-        transaction.should.have.property('id').which.is.a('string')
-        transaction.should.have.property('block_id').which.is.a('string').and.equals(blockId)
-        transaction.should.have.property('type').which.is.a('number')
-        transaction.should.have.property('amount').which.is.a('number')
-        transaction.should.have.property('fee').which.is.a('number')
-        transaction.should.have.property('sender').which.is.a('string')
-        transaction.should.have.property('recipient').which.is.a('string')
-        transaction.should.have.property('signature').which.is.a('string')
-        transaction.should.have.property('confirmations').which.is.a('number')
+        Helpers.assertTransaction(transaction)
+        transaction.block_id.should.equal(blockId)
 
         done()
       })
@@ -88,7 +76,7 @@ describe('API 2.0 - Blocks', () => {
   })
 
   describe('POST /api/blocks/search', () => {
-    it('should POST a search for blocks with the specified criteria', (done) => {
+    it('should POST a search for blocks with the exact specified blockId', (done) => {
       Helpers.request('POST', 'blocks/search', { id: blockId }).end((err, res) => {
         Helpers.assertSuccessful(err, res)
         Helpers.assertCollection(res)
@@ -96,24 +84,363 @@ describe('API 2.0 - Blocks', () => {
         res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
 
         const block = res.body.data[0]
-        block.should.have.property('id').which.is.a('string').and.equals(blockId)
-        block.should.have.property('version').which.is.a('number')
-        block.should.have.property('height').which.is.a('number')
-        block.should.have.property('previous').which.is.a('string')
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
 
-        block.should.have.property('forged').which.is.an('object')
-        block.forged.should.have.property('reward').which.is.an('number')
-        block.forged.should.have.property('fee').which.is.an('number')
+        done()
+      })
+    })
 
-        block.should.have.property('payload').which.is.an('object')
-        block.payload.should.have.property('length').which.is.an('number')
-        block.payload.should.have.property('hash').which.is.an('string')
+    it('should POST a search for blocks with the exact specified version', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, version }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
 
-        block.should.have.property('generator').which.is.an('object')
-        block.generator.should.have.property('public_key').which.is.an('string')
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
 
-        block.should.have.property('signature').which.is.an('string')
-        block.should.have.property('transactions').which.is.an('number')
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.version.should.equal(version)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified previousBlock', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, previousBlock }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.previous.should.equal(previousBlock)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified payloadHash', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, payloadHash }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.payload.length.should.equal(payloadLength)
+        block.payload.hash.should.equal(payloadHash)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified generatorPublicKey', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, generatorPublicKey }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.generator.public_key.should.equal(generatorPublicKey)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified blockSignature', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, blockSignature }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.signature.should.equal(blockSignature)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified timestamp', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, timestamp }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified height', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, height }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.height.should.equal(height)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specified height range', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        id: blockId,
+        height: {
+          from: heightFrom,
+          to: heightTo
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.height.should.equal(height)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified numberOfTransactions', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, numberOfTransactions }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.transactions.should.equal(numberOfTransactions)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specified numberOfTransactions range', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        id: blockId,
+        numberOfTransactions: {
+          from: numberOfTransactionsFrom,
+          to: numberOfTransactionsTo
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.transactions.should.equal(numberOfTransactions)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified totalAmount', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, totalAmount }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specified totalAmount range', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        id: blockId,
+        totalAmount: {
+          from: totalAmountFrom,
+          to: totalAmountTo
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified totalFee', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, totalFee }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.forged.fee.should.equal(totalFee)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specified totalFee range', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        id: blockId,
+        totalFee: {
+          from: totalFeeFrom,
+          to: totalFeeTo
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.forged.fee.should.equal(totalFee)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified reward', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, reward }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.forged.reward.should.equal(reward)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specified reward range', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        id: blockId,
+        reward: {
+          from: rewardFrom,
+          to: rewardTo
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.forged.reward.should.equal(reward)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the exact specified payloadLength', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, payloadLength }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.payload.length.should.equal(payloadLength)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specified payloadLength range', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        id: blockId,
+        payloadLength: {
+          from: payloadLengthFrom,
+          to: payloadLengthTo
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
+        block.payload.length.should.equal(payloadLength)
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the wrong specified version', (done) => {
+      Helpers.request('POST', 'blocks/search', { id: blockId, version: wrongVersion }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').that.is.empty
+
+        done()
+      })
+    })
+
+    it('should POST a search for blocks with the specific criteria', (done) => {
+      Helpers.request('POST', 'blocks/search', {
+        generatorPublicKey,
+        version,
+        timestamp: {
+          from: timestampFrom,
+          to: timestampTo,
+        }
+      }).end((err, res) => {
+        Helpers.assertSuccessful(err, res)
+        Helpers.assertCollection(res)
+
+        res.body.should.have.property('data').which.is.an('array').with.lengthOf(1)
+
+        const block = res.body.data[0]
+        Helpers.assertBlock(block)
+        block.id.should.equal(blockId)
 
         done()
       })
--- test/api/v2/helpers.js
+++ test/api/v2/helpers.js
@@ -72,6 +72,28 @@ class Helpers {
     transaction.should.have.property('signature').which.is.a('string')
     transaction.should.have.property('confirmations').which.is.a('number')
   }
+
+  assertBlock (block) {
+    block.should.be.an('object')
+    block.should.have.property('id').which.is.a('string')
+    block.should.have.property('version').which.is.a('number')
+    block.should.have.property('height').which.is.a('number')
+    block.should.have.property('previous').which.is.a('string')
+
+    block.should.have.property('forged').which.is.an('object')
+    block.forged.should.have.property('reward').which.is.an('number')
+    block.forged.should.have.property('fee').which.is.an('number')
+
+    block.should.have.property('payload').which.is.an('object')
+    block.payload.should.have.property('length').which.is.an('number')
+    block.payload.should.have.property('hash').which.is.an('string')
+
+    block.should.have.property('generator').which.is.an('object')
+    block.generator.should.have.property('public_key').which.is.an('string')
+
+    block.should.have.property('signature').which.is.an('string')
+    block.should.have.property('transactions').which.is.an('number')
+  }
 }
 
 module.exports = new Helpers()
