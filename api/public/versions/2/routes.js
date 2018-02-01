d046718f20aa8eadb1c84702a3ade8a967e7084e
Brian Faust <hello@brianfaust.me>
2018-02-01 15:57:38 +0200

    setup API with hapi/glue
    Signed-off-by: Brian Faust <hello@brianfaust.me>

--- api/public/versions/2/routes.js
+++ api/public/versions/2/routes.js
@@ -12,55 +12,55 @@ const wallets = require('./handlers/wallets')
 
 const register = async (server, options) => {
   server.route([
-    { method: 'GET', path: '/v2/blockchain', ...blockchain.index },
+    { method: 'GET', path: '/blockchain', ...blockchain.index },
 
-    { method: 'GET', path: '/v2/blocks', ...blocks.index },
-    { method: 'GET', path: '/v2/blocks/{id}', ...blocks.show },
-    { method: 'GET', path: '/v2/blocks/{id}/transactions', ...blocks.transactions },
-    { method: 'GET', path: '/v2/blocks/search', ...blocks.search },
+    { method: 'GET', path: '/blocks', ...blocks.index },
+    { method: 'GET', path: '/blocks/{id}', ...blocks.show },
+    { method: 'GET', path: '/blocks/{id}/transactions', ...blocks.transactions },
+    { method: 'GET', path: '/blocks/search', ...blocks.search },
 
-    { method: 'GET', path: '/v2/delegates', ...delegates.index },
-    { method: 'GET', path: '/v2/delegates/{id}', ...delegates.show },
-    { method: 'GET', path: '/v2/delegates/{id}/blocks', ...delegates.blocks },
-    { method: 'GET', path: '/v2/delegates/{id}/voters', ...delegates.voters },
+    { method: 'GET', path: '/delegates', ...delegates.index },
+    { method: 'GET', path: '/delegates/{id}', ...delegates.show },
+    { method: 'GET', path: '/delegates/{id}/blocks', ...delegates.blocks },
+    { method: 'GET', path: '/delegates/{id}/voters', ...delegates.voters },
 
-    { method: 'GET', path: '/v2/loader/status', ...loader.status },
-    { method: 'GET', path: '/v2/loader/syncing', ...loader.syncing },
-    { method: 'GET', path: '/v2/loader/configuration', ...loader.configuration },
+    { method: 'GET', path: '/loader/status', ...loader.status },
+    { method: 'GET', path: '/loader/syncing', ...loader.syncing },
+    { method: 'GET', path: '/loader/configuration', ...loader.configuration },
 
-    { method: 'GET', path: '/v2/multisignatures', ...multisignatures.index },
-    { method: 'GET', path: '/v2/multisignatures/pending', ...multisignatures.pending },
-    { method: 'GET', path: '/v2/multisignatures/wallets', ...multisignatures.wallets },
+    { method: 'GET', path: '/multisignatures', ...multisignatures.index },
+    { method: 'GET', path: '/multisignatures/pending', ...multisignatures.pending },
+    { method: 'GET', path: '/multisignatures/wallets', ...multisignatures.wallets },
 
-    { method: 'GET', path: '/v2/peers', ...peers.index },
-    { method: 'GET', path: '/v2/peers/me', ...peers.me },
-    { method: 'GET', path: '/v2/peers/{ip}', ...peers.show },
+    { method: 'GET', path: '/peers', ...peers.index },
+    { method: 'GET', path: '/peers/me', ...peers.me },
+    { method: 'GET', path: '/peers/{ip}', ...peers.show },
 
-    { method: 'GET', path: '/v2/signatures', ...signatures.index },
+    { method: 'GET', path: '/signatures', ...signatures.index },
 
-    { method: 'GET', path: '/v2/statistics/blockchain', ...statistics.blockchain },
-    { method: 'GET', path: '/v2/statistics/transactions', ...statistics.transactions },
-    { method: 'GET', path: '/v2/statistics/blocks', ...statistics.blocks },
-    { method: 'GET', path: '/v2/statistics/votes', ...statistics.votes },
-    { method: 'GET', path: '/v2/statistics/unvotes', ...statistics.unvotes },
+    { method: 'GET', path: '/statistics/blockchain', ...statistics.blockchain },
+    { method: 'GET', path: '/statistics/transactions', ...statistics.transactions },
+    { method: 'GET', path: '/statistics/blocks', ...statistics.blocks },
+    { method: 'GET', path: '/statistics/votes', ...statistics.votes },
+    { method: 'GET', path: '/statistics/unvotes', ...statistics.unvotes },
 
-    { method: 'GET', path: '/v2/transactions', ...transactions.index },
-    { method: 'GET', path: '/v2/transactions/{id}', ...transactions.show },
-    { method: 'GET', path: '/v2/transactions/unconfirmed', ...transactions.unconfirmed },
-    { method: 'GET', path: '/v2/transactions/unconfirmed/{id}', ...transactions.showUnconfirmed },
-    { method: 'GET', path: '/v2/transactions/search', ...transactions.search },
+    { method: 'GET', path: '/transactions', ...transactions.index },
+    { method: 'GET', path: '/transactions/{id}', ...transactions.show },
+    { method: 'GET', path: '/transactions/unconfirmed', ...transactions.unconfirmed },
+    { method: 'GET', path: '/transactions/unconfirmed/{id}', ...transactions.showUnconfirmed },
+    { method: 'GET', path: '/transactions/search', ...transactions.search },
 
-    { method: 'GET', path: '/v2/votes', ...votes.index },
-    { method: 'GET', path: '/v2/votes/{id}', ...votes.show },
+    { method: 'GET', path: '/votes', ...votes.index },
+    { method: 'GET', path: '/votes/{id}', ...votes.show },
 
-    { method: 'GET', path: '/v2/wallets', ...wallets.index },
-    { method: 'GET', path: '/v2/wallets/top', ...wallets.top },
-    { method: 'GET', path: '/v2/wallets/{id}', ...wallets.show },
-    { method: 'GET', path: '/v2/wallets/{id}/transactions', ...wallets.transactions },
-    { method: 'GET', path: '/v2/wallets/{id}/transactions/send', ...wallets.transactionsSend },
-    { method: 'GET', path: '/v2/wallets/{id}/transactions/received', ...wallets.transactionsReceived },
-    { method: 'GET', path: '/v2/wallets/{id}/votes', ...wallets.votes },
-    { method: 'GET', path: '/v2/wallets/search', ...wallets.search }
+    { method: 'GET', path: '/wallets', ...wallets.index },
+    { method: 'GET', path: '/wallets/top', ...wallets.top },
+    { method: 'GET', path: '/wallets/{id}', ...wallets.show },
+    { method: 'GET', path: '/wallets/{id}/transactions', ...wallets.transactions },
+    { method: 'GET', path: '/wallets/{id}/transactions/send', ...wallets.transactionsSend },
+    { method: 'GET', path: '/wallets/{id}/transactions/received', ...wallets.transactionsReceived },
+    { method: 'GET', path: '/wallets/{id}/votes', ...wallets.votes },
+    { method: 'GET', path: '/wallets/search', ...wallets.search }
   ])
 }
 
