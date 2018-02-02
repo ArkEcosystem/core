f5c012d5e7a571c7d980c9e4725b65a811f104de
Chris - Kristjan Kosic <chris@ark.io>
2018-02-02 21:16:46 +0100

    ou yeah - no events, no promises - just Jest doing power work :)

--- test/api/versions/1/handlers/signatures.spec.js
+++ test/api/versions/1/handlers/signatures.spec.js
@@ -1,13 +1,4 @@
 const utils = require('../utils')
-const setupUtils = require('../../../../support/setup-utils')
-
-beforeAll((done) => {
-  console.log('before promise')
-  setupUtils.ensurePublicAPI().then((data) => {
-    console.log('after promise', data)
-    done()
-  })
-}, 60000)
 
 describe('API 1.0 - Signatures', () => {
   describe('GET /api/signatures/fee', () => {
