994a5bc5bb9e1a2b81b01d5ca54145f08551abc7
Chris - Kristjan Kosic <chris@ark.io>
2018-02-02 09:24:31 +0100

    forger promises ... and goofy-fy

--- startForger.js
+++ startForger.js
@@ -18,23 +18,23 @@ if (!fs.existsSync(path.resolve(commander.config))) {
   throw new Error('The directory does not exist or is not accessible because of security settings.')
 }
 
-require('./core/config').init({
+const config = require('./core/config')
+let forgerManager = null
+let forgers = null
+
+process.on('unhandledRejection', (reason, p) => {
+  goofy.error('Unhandled Rejection at: Promise', p, 'reason:', reason)
+})
+
+config.init({
   server: require(path.resolve(commander.config, 'server.json')),
   genesisBlock: require(path.resolve(commander.config, 'genesisBlock.json')),
   network: require(path.resolve(commander.config, 'network.json')),
   delegates: require(path.resolve(commander.config, 'delegate.json'))
-}).then(config => {
-  goofy.init(config.server.fileLogLevel, config.network.name + '-forger')
-
-  let forgerManager = new ForgerManager(config)
-
-  process.on('unhandledRejection', (reason, p) => {
-    goofy.error('Unhandled Rejection at: Promise', p, 'reason:', reason)
-  })
-
-  forgerManager
-    .loadDelegates()
-    .then((forgers) => goofy.info('ForgerManager started with', forgers.length, 'forgers'))
-    .then(() => forgerManager.startForging('http://127.0.0.1:4000'))
-    .catch((fatal) => goofy.error('fatal error', fatal))
 })
+  .then(() => goofy.init(config.server.fileLogLevel, config.network.name + '-forger'))
+  .then(() => (forgerManager = new ForgerManager(config)))
+  .then(() => (forgers = forgerManager.loadDelegates()))
+  .then(() => goofy.info('ForgerManager started with', forgers.length, 'forgers'))
+  .then(() => forgerManager.startForging('http://127.0.0.1:4000'))
+  .catch((fatal) => goofy.error('fatal error', fatal))
