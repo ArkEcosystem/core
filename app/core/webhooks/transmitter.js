a3db0616a553f27a62e4f086c978b4762d1f4d44
Brian Faust <hello@brianfaust.me>
2018-02-09 18:58:51 +0200

    add target to webhooks

--- app/core/webhooks/transmitter.js
+++ app/core/webhooks/transmitter.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 
 module.exports = (webhook, payload) => {
-  return axios.post(webhook.options.hook.url, {
+  return axios.post(webhook.target, {
     formParams: payload,
-    headers: { 'X-Hook-Secret': webhook.options.hook.authToken }
+    headers: { 'X-Hook-Secret': webhook.options.secret }
   })
 }
