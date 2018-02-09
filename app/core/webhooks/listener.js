e9df63abe5df82ab214e7b18b5cbe5cf5125d302
Brian Faust <hello@brianfaust.me>
2018-02-09 20:30:34 +0200

    file based webhook events

--- app/core/webhooks/listener.js
+++ app/core/webhooks/listener.js
@@ -1,7 +1,6 @@
 const EventEmitter = require('events').EventEmitter
-const goofy = require('app/core/goofy')
-const db = require('app/core/dbinterface').getInstance()
-const WebhookTransmitter = require('./transmitter')
+
+const BlockForgedEvent = require('./events/blocks/forged')
 
 let instance
 
@@ -21,17 +20,7 @@ module.exports = class WebhookListener {
   }
 
   subscribe () {
-    this.emitter.on('block:forged', (block) => {
-      goofy.debug('Event [block:forged] fired.')
-
-      db.webhooks.findByEvent('block:forged').then(webhooks => {
-        webhooks.forEach((webhook) => {
-          WebhookTransmitter(webhook, block)
-          .then((r) => goofy.info(`Event [block:forged] transmitted to [${webhook.options.hook.url}] with Status [${r.status}].`))
-          .catch((e) => goofy.error(e.message))
-        })
-      })
-    })
+    this.emitter.on('block:forged', (block) => BlockForgedEvent(block))
 
     return Promise.resolve(instance)
   }
