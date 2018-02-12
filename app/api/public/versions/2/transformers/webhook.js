512f80e1c347c5f4d870d8971789567cc1466a78
Brian Faust <hello@brianfaust.me>
2018-02-12 12:20:03 +0200

    generate webhook secret

--- app/api/public/versions/2/transformers/webhook.js
+++ app/api/public/versions/2/transformers/webhook.js
@@ -2,6 +2,8 @@ module.exports = (model) => {
   return {
     id: model.id,
     event: model.event,
+    target: model.target,
+    secret: model.secret,
     enabled: model.enabled,
     conditions: model.conditions
   }
