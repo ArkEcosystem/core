829ad1dac9e91b721cb60299759a81e8e75b7a87
Brian Faust <hello@brianfaust.me>
2018-02-17 19:48:07 +0200

    add coverage to base config

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,13 +1,26 @@
 const path = require('path')
 
 module.exports = {
+  rootDir: path.resolve(__dirname, '../'),
+
   globalSetup: 'tests/support/tearup-full.js',
   // globalTeardown: 'tests/support/teardown.js',
+
   testEnvironment: 'node',
   bail: true,
   verbose: true,
+
   setupFiles: ['<rootDir>/tests/support/setup-files'],
   setupTestFrameworkScriptFile: '<rootDir>/tests/support/setup-framework',
+
   collectCoverage: false,
-  rootDir: path.resolve(__dirname, '../')
+  coverageDirectory: 'tests/coverage/all',
+  collectCoverageFrom: [
+    'app/api/**/*.js',
+    'app/config/**/*.js',
+    'app/core/**/*.js',
+    'app/database/**/*.js',
+    'app/model/**/*.js',
+    'app/utils/**/*.js'
+  ]
 }
