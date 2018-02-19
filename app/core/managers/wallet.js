91f89182e01a546bc077db485fae111d27818a59
Brian Faust <faustbrian@users.noreply.github.com>
2018-02-19 18:38:13 +0200

    properly log sender in walletmanager

--- app/core/managers/wallet.js
+++ app/core/managers/wallet.js
@@ -89,19 +89,19 @@ module.exports = class WalletManager {
         this.walletsByAddress[recipientId] = recipient
       }
       if (datatx.type === 2 && this.delegatesByUsername[datatx.asset.delegate.username.toLowerCase()]) {
-        logger.error(sender)
+        logger.error(JSON.stringify(sender))
         logger.error(JSON.stringify(datatx))
         return reject(new Error(`Can't apply transaction ${datatx.id}: delegate name already taken`))
       } else if (datatx.type === 3 && !this.walletsByPublicKey[datatx.asset.votes[0].slice(1)].username) {
-        logger.error(sender)
+        logger.error(JSON.stringify(sender))
         logger.error(JSON.stringify(datatx))
         return reject(new Error(`Can't apply transaction ${datatx.id}: voted delegate does not exist`))
       }
       if (config.network.exceptions[datatx.id]) {
         logger.warn('Transaction is forced to be applied because it has been added as an exception:')
-        logger.warn(JSON.stringify(datatx))
+        logger.warn(datatx)
       } else if (!sender.canApply(datatx)) {
-        logger.error(sender)
+        logger.error(JSON.stringify(sender))
         logger.error(JSON.stringify(datatx))
         return reject(new Error(`Can't apply transaction ${datatx.id}`))
       }
