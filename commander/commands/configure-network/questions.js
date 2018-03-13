05cd352f0d4d24a0da9da296b2ee1cec28903f15
Brian Faust <hello@brianfaust.me>
2018-03-13 13:55:17 +0200

    config warning

--- commander/commands/configure-network/questions.js
+++ commander/commands/configure-network/questions.js
@@ -1,7 +1,9 @@
+const chalk = require('chalk')
+
 module.exports = [{
   type: 'select',
   name: 'network',
-  message: 'What network are you on?',
+  message: `What network are you on? ${chalk.bgRed('Make sure you created a backup of your previous configuration if there is any!')}`,
   choices: [
     { title: 'Main / Production', value: 'mainnet' },
     { title: 'Development', value: 'devnet' },
