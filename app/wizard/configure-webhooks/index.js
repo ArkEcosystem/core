ee06e08d82c12c7fd112f2c6e2e5421211c9eb75
Brian Faust <hello@brianfaust.me>
2018-03-13 11:46:24 +0200

    write webhook config to file

--- app/wizard/configure-webhooks/index.js
+++ app/wizard/configure-webhooks/index.js
@@ -2,13 +2,14 @@ const prompts = require('prompts')
 const argon2 = require('argon2')
 const questions = require('./questions')
 const onCancel = require('../cancel')
-const config = require(`config/${process.env.NETWORK}/webhooks.json`)
+const utils = require('../utils')
 
 module.exports = async (answers) => {
   const response = await prompts(questions, { onCancel })
 
-  config.webhooks.enabled = response.enabled
+  let config = utils.readConfig('webhooks')
+  config.enabled = response.enabled
+  config.password = await argon2.hash(response.password, { type: argon2.argon2id })
 
-  const password = await argon2.hash(response.password, { type: argon2.argon2id })
-  config.webhooks.password = password
+  return utils.writeConfig('webhooks', config)
 }
--- app/wizard/utils.js
+++ app/wizard/utils.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
+const util = require('util')
+const writeFile = util.promisify(fs.writeFile)
 
-exports.readConfig = (file) => require(file)
+exports.readConfig = (file) => require(`config/${process.env.NETWORK}/${file}.json`)
 
-exports.writeConfig = (file, data) => fs.writeFile(file, JSON.stringify(data))
+exports.writeConfig = async (file, data) => writeFile(`config/${process.env.NETWORK}/${file}.json`, JSON.stringify(data, null, 2))
