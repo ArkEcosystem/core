a1f124e748b44b3bd0938c0910c74a7e91a2ad43
Brian Faust <hello@brianfaust.me>
2018-03-13 09:47:51 +0200

    logs command

---
+++ app/wizard/show-logs/index.js
@@ -0,0 +1,19 @@
+const prompts = require('prompts')
+const fg = require('fast-glob')
+const Tail = require('tail').Tail
+const path = require('path')
+const onCancel = require('../cancel')
+
+module.exports = async () => {
+  const choices = await fg(path.resolve(__dirname, '../../../storage/logs/*.log'))
+
+  const response = await prompts([{
+    type: 'select',
+    name: 'file',
+    message: 'Pick a log',
+    choices: choices.map(f => ({ title: path.basename(f), value: f }))
+  }], { onCancel })
+
+  const tail = new Tail(response.file)
+  tail.on('line', (data) => console.log(data))
+}
