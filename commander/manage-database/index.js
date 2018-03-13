80f053c2c4e90601dc0f86d46446ff2df1c7cd6d
Brian Faust <hello@brianfaust.me>
2018-03-13 12:07:38 +0200

    wtf

--- commander/manage-database/index.js
+++
@@ -1,9 +0,0 @@
-const prompts = require('prompts')
-const questions = require('./questions')
-const { onCancel } = require('commander/utils')
-
-module.exports = async (answers) => {
-  const response = await prompts(questions, { onCancel })
-
-  if (response.action) require(`./handlers/${response.action}`)()
-}
