aea9c81b41a0376d0e7c7ec81e1bc74371ed98df
fix <fx.thoorens@gmail.com>
2018-03-28 16:14:32 +0200

    comments

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -26,6 +26,7 @@ module.exports = class Transaction {
     return true
   }
 
+  // AIP11 serialization
   static serialize (transaction) {
     const bb = new ByteBuffer(512, true)
     bb.writeByte(0xff) // fill, to disambiguate from v1
