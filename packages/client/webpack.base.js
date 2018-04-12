19c70332e9ded510a1bffbf127478261b60dac17
Brian Faust <hello@brianfaust.me>
2018-04-12 07:42:26 +0300

    update config paths

--- packages/client/webpack.base.js
+++ packages/client/webpack.base.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
-function resolve (dir) {
-  return path.join(__dirname, dir)
-}
+// function resolve (dir) {
+//   return path.join(__dirname, dir)
+// }
 
 module.exports = {
   module: {
