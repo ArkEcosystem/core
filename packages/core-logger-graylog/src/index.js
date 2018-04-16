cd483b5c43bda69184a6a3c7c7b270d0addbfdd8
Brian Faust <hello@brianfaust.me>
2018-04-16 14:13:09 +0300

    fix core-logger-graylog

--- packages/core-logger-graylog/src/index.js
+++ packages/core-logger-graylog/src/index.js
@@ -1,28 +1,6 @@
-const graylog2 = require('graylog2')
+const logger = require('./logger')
 
-class Logger {
-  init (config, network) {
-    this.graylog = new graylog2.graylog(config.options) // eslint-disable-line new-cap
-  }
-
-  error (message) {
-    return this.graylog.error(message)
-  }
-
-  warning (message) {
-    return this.graylog.warning(message)
-  }
-
-  info (message) {
-    return this.graylog.info(message)
-  }
-
-  debug (message) {
-    return this.graylog.debug(message)
-  }
-
-  printTracker (title, current, max, posttitle, figures = 0) {}
-  stopTracker (title, current, max) {}
+exports.plugin = {
+  pkg: require('../package.json'),
+  boot: async(config) => logger.boot(config)
 }
-
-module.exports = new Logger()
---
+++ packages/core-logger-graylog/src/logger.js
@@ -0,0 +1,28 @@
+const graylog2 = require('graylog2')
+
+class Logger {
+  boot (config) {
+    this.graylog = new graylog2.graylog(config) // eslint-disable-line new-cap
+  }
+
+  error (message) {
+    return this.graylog.error(message)
+  }
+
+  warning (message) {
+    return this.graylog.warning(message)
+  }
+
+  info (message) {
+    return this.graylog.info(message)
+  }
+
+  debug (message) {
+    return this.graylog.debug(message)
+  }
+
+  printTracker (title, current, max, posttitle, figures = 0) {}
+  stopTracker (title, current, max) {}
+}
+
+module.exports = new Logger()
