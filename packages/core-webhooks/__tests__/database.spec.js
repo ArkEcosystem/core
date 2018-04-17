cf1fefa5a073c53d080e1ae0065ea387645a96c4
Brian Faust <hello@brianfaust.me>
2018-04-17 13:15:05 +0300

    move things

--- packages/core-webhooks/__tests__/database.spec.js
+++
@@ -1,7 +0,0 @@
-const database = require('../src/database')
-
-describe('Connection Manager', () => {
-  it('should respond with 200 with valid event and token', async () => {
-    console.log(database)
-  })
-})
