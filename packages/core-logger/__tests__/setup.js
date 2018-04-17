5013b6e8eee9be5543c02b665e44795860c05e17
Brian Faust <hello@brianfaust.me>
2018-04-17 08:13:20 +0300

    organise test files

---
+++ packages/core-logger/__tests__/setup.js
@@ -0,0 +1,3 @@
+module.exports = async () => {
+  // Mount plugin here...
+}
--- packages/core/__tests__/api/jest.config.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require('../jest.config.js')
-
-module.exports = Object.assign(baseConfig, {
-  globalSetup: './__tests__/support/tearup-full.js',
-  testMatch: ['**/__tests__/api/**/*.spec.js'],
-  coverageDirectory: 'tests/coverage/api',
-  collectCoverageFrom: ['api/**/*.js'],
-  verbose: true
-})
