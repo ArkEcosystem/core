d71c42d45ed24a04696b7ccb12d4f96fb56f3266
Brian Faust <hello@brianfaust.me>
2018-04-19 09:44:35 +0300

    state machine refactor

---
+++ packages/core-blockchain/lib/machines/blockchain.js
@@ -0,0 +1,65 @@
+const Machine = require('xstate').Machine
+const syncWithNetwork = require('./actions/sync-with-network')
+const rebuildFromNetwork = require('./actions/rebuild-from-network')
+const fork = require('./actions/fork')
+
+module.exports = Machine({
+  key: 'blockchain',
+  initial: 'uninitialised',
+  states: {
+    uninitialised: {
+      on: {
+        START: 'init'
+      }
+    },
+    init: {
+      onEntry: ['init'],
+      on: {
+        REBUILD: 'rebuild',
+        NETWORKSTART: 'idle',
+        STARTED: 'syncWithNetwork',
+        FAILURE: 'exit'
+      }
+    },
+    rebuild: {
+      on: {
+        PROCESSFINISHED: 'syncWithNetwork',
+        FORK: 'fork'
+      },
+      ...rebuildFromNetwork
+    },
+    syncWithNetwork: {
+      on: {
+        TEST: 'idle',
+        SYNCFINISHED: 'idle',
+        FORK: 'fork'
+      },
+      ...syncWithNetwork
+    },
+    idle: {
+      onEntry: ['checkLater', 'blockchainReady'],
+      on: {
+        WAKEUP: 'syncWithNetwork',
+        NEWBLOCK: 'processingBlock'
+      }
+    },
+    processingBlock: {
+      onEntry: ['processBlock'],
+      on: {
+        SUCCESS: 'idle',
+        FAILURE: 'fork'
+      }
+    },
+    fork: {
+      onEntry: ['startForkRecovery'],
+      on: {
+        SUCCESS: 'syncWithNetwork',
+        FAILURE: 'exit'
+      },
+      ...fork
+    },
+    exit: {
+      onEntry: ['exitApp']
+    }
+  }
+})
--- packages/core-blockchain/lib/state-machine.js
+++ packages/core-blockchain/lib/state-machine.js
@@ -7,13 +7,7 @@ const { slots } = require('@arkecosystem/client')
 const { Block } = require('@arkecosystem/client').models
 const config = require('@arkecosystem/core-plugin-manager').get('config')
 const sleep = require('./utils/sleep')
-
-/**
- * State Machine actions.
- */
-const syncWithNetwork = require('./actions/sync-with-networks')
-const rebuildFromNetwork = require('./actions/rebuild-from-network')
-const fork = require('./actions/fork')
+const blockchainMachine = require('./machines/blockchain')
 
 let synctracker = null
 
@@ -39,100 +33,6 @@ const tickSyncTracker = (block) => {
     logger.stopTracker(title, slots.getTime(), slots.getTime())
   }
 }
-
-// const init = Machine({
-//   initial: 'unitisalised',
-//   states: {
-//     unitisalised: {
-//       on: {
-//         START: 'mountDatabase'
-//       }
-//     },
-//     mountDatabase: {
-//       onEntry: ['mountDatabase'],
-//       on: {
-//         SUCCESS: 'mountNetwork',
-//         FAILURE: 'failed'
-//       }
-//     },
-//     mountNetwork: {
-//       onEntry: ['mountNetwork'],
-//       on: {
-//         SUCCESS: 'initialised',
-//         FAILURE: 'failed'
-//       }
-//     },
-//     initialised: {
-//     },
-//     failed: {
-//     }
-//   }
-// })
-
-/**
- * [blockchainMachine description]
- * @type {[type]}
- */
-const blockchainMachine = Machine({
-  key: 'blockchain',
-  initial: 'uninitialised',
-  states: {
-    uninitialised: {
-      on: {
-        START: 'init'
-      }
-    },
-    init: {
-      onEntry: ['init'],
-      on: {
-        REBUILD: 'rebuild',
-        NETWORKSTART: 'idle',
-        STARTED: 'syncWithNetwork',
-        FAILURE: 'exit'
-      }
-    },
-    rebuild: {
-      on: {
-        PROCESSFINISHED: 'syncWithNetwork',
-        FORK: 'fork'
-      },
-      ...rebuildFromNetwork
-    },
-    syncWithNetwork: {
-      on: {
-        TEST: 'idle',
-        SYNCFINISHED: 'idle',
-        FORK: 'fork'
-      },
-      ...syncWithNetwork
-    },
-    idle: {
-      onEntry: ['checkLater', 'blockchainReady'],
-      on: {
-        WAKEUP: 'syncWithNetwork',
-        NEWBLOCK: 'processingBlock'
-      }
-    },
-    processingBlock: {
-      onEntry: ['processBlock'],
-      on: {
-        SUCCESS: 'idle',
-        FAILURE: 'fork'
-      }
-    },
-    fork: {
-      onEntry: ['startForkRecovery'],
-      on: {
-        SUCCESS: 'syncWithNetwork',
-        FAILURE: 'exit'
-      },
-      ...fork
-    },
-    exit: {
-      onEntry: ['exitApp']
-    }
-  }
-})
 
 /**
  * [state description]
