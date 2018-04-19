a58c0b146da92a9a02f4ec396c6a13f4884ec583
Brian Faust <hello@brianfaust.me>
2018-04-19 14:20:58 +0300

    update dev deps for core

--- packages/core-config/__tests__/manager.spec.js
+++ packages/core-config/__tests__/manager.spec.js
@@ -1,34 +1,33 @@
 'use strict';
 
-const path = require('path')
 const configManager = require('../lib/manager')
 
 class FakeDriver {
-  make() {
+  make () {
     return this
   }
 }
 
 describe('Config Manager', () => {
-  it('should be an object', async() => {
+  it('should be an object', async () => {
     expect(configManager).toBeObject()
     expect(configManager.drivers).toBeDefined()
   })
 
-  describe('driver', async() => {
-    it('should be a function', async() => {
+  describe('driver', async () => {
+    it('should be a function', async () => {
       expect(configManager.driver).toBeFunction()
     })
 
-    it('should return the driver', async() => {
+    it('should return the driver', async () => {
       await configManager.makeDriver(new FakeDriver())
 
       expect(configManager.driver()).toBeInstanceOf(FakeDriver)
     })
   })
 
-  describe('makeDriver', async() => {
-    it('should be a function', async() => {
+  describe('makeDriver', async () => {
+    it('should be a function', async () => {
       expect(configManager.makeDriver).toBeFunction()
     })
   })
