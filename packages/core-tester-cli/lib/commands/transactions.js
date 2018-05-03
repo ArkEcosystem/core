793c50b35006a864c168fb1e2264cff6baad9f22
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-03 16:50:15 +0100

    Command for generating transfer txs

--- packages/core-tester-cli/lib/commands/transactions.js
+++ packages/core-tester-cli/lib/commands/transactions.js
@@ -1,5 +1,18 @@
 'use strict'
 
-module.exports = async () => {
-  // Mount command here...
+const path = require('path')
+const client = require(path.resolve('../../client/lib'))
+const genesisPassphrase = 'peace vanish bleak box tuna woman rally manage undo royal lucky since'
+
+module.exports = async (options) => {
+  // for (let i = 0; i < options.number; i++) {
+    const transaction = client.transactionBuilder.transfer()
+    transaction.amount = 1
+    transaction.recipientId = 'AHXtmB84sTZ9Zd35h9Y1vfFvPE2Xzqj8ri'
+    transaction.senderPublicKey = 'AHXtmB84sTZ9Zd35h9Y1vfFvPE2Xzqj8ri'
+    const keys = client.crypto.getKeys(genesisPassphrase)
+    const signature = client.crypto.sign(transaction, keys)
+    transaction.signature = signature
+  // }
+  console.log(transaction)
 }
