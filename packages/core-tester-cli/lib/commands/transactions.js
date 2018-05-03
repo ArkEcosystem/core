6f760b9c161ddbe88415e32708912ba69f580d12
Brian Faust <hello@brianfaust.me>
2018-05-03 19:28:18 +0300

    add placeholder commands

--- packages/core-tester-cli/lib/commands/transactions.js
+++ packages/core-tester-cli/lib/commands/transactions.js
@@ -1,18 +1,18 @@
 'use strict'
 
-const path = require('path')
-const client = require(path.resolve('../../client/lib'))
+const ark = require('arkjs')
+
+const generateWallet = require('../utils/generate-wallet')
+
 const genesisPassphrase = 'peace vanish bleak box tuna woman rally manage undo royal lucky since'
 
 module.exports = async (options) => {
-  // for (let i = 0; i < options.number; i++) {
-    const transaction = client.transactionBuilder.transfer()
-    transaction.amount = 1
-    transaction.recipientId = 'AHXtmB84sTZ9Zd35h9Y1vfFvPE2Xzqj8ri'
-    transaction.senderPublicKey = 'AHXtmB84sTZ9Zd35h9Y1vfFvPE2Xzqj8ri'
-    const keys = client.crypto.getKeys(genesisPassphrase)
-    const signature = client.crypto.sign(transaction, keys)
-    transaction.signature = signature
-  // }
-  console.log(transaction)
+  for (let i = 0; i < options.number; i++) {
+    const amount = 1 * Math.pow(10, 8)
+    const transaction = ark.transaction.createTransaction('AHXtmB84sTZ9Zd35h9Y1vfFvPE2Xzqj8ri', amount, 'test', genesisPassphrase)
+
+    console.log(`TX${i}: ${transaction.id}`)
+
+    // send request with axios...
+  }
 }
