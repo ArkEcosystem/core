b95fa2a6bfe28688f1d1b74974f6ed39668c5ce6
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-03 22:49:56 +0100

    Send txs to local testnet, signed by genesis

--- packages/core-tester-cli/lib/commands/transactions.js
+++ packages/core-tester-cli/lib/commands/transactions.js
@@ -1,18 +1,26 @@
 'use strict'
 
 const ark = require('arkjs')
-
+const axios = require('axios')
 const generateWallet = require('../utils/generate-wallet')
+const genesisPassphrase = 'peace vanish bleak box tuna woman rally manage undo royal lucky since'
 
 module.exports = async (options) => {
+  const transactions = []
   for (let i = 0; i < options.number; i++) {
     const wallet = generateWallet()
 
     const amount = 1 * Math.pow(10, 8)
-    const transaction = ark.transaction.createTransaction(wallet.address, amount, `TID: ${i}`, wallet.passphrase)
-
-    console.log(`${i} ==> ${transaction.id}`)
-
-    // send request with axios...
+    const transaction = ark.transaction.createTransaction(wallet.address, amount, `TID: ${i}`, genesisPassphrase)
+    transactions.push(transaction)
+    console.log(`${i} ==> ${transaction.id}, ${wallet.address}`)
   }
+
+  axios.post('http://localhost:4102/api/v2/transactions', {transactions})
+    .then(response => {
+      console.log('All transactions have been sent')
+    })
+    .catch(error => {
+      console.log(`There was a problem sending transactions: ${error.message}`)
+    })
 }
