e47442aca3151274a9742f3fb0fa4053c3323bb0
Brian Faust <faustbrian@users.noreply.github.com>
2018-05-04 11:08:22 +0300

    feat: shutdown server

--- packages/core-api-graphql/lib/index.js
+++ packages/core-api-graphql/lib/index.js
@@ -7,9 +7,15 @@
 exports.plugin = {
   pkg: require('../package.json'),
   defaults: require('./defaults'),
+  alias: 'api-graphql',
   register: async (manager, options) => {
     manager.get('logger').info('Starting GraphQL API...')
 
     return require('./server')(options)
+  },
+  deregister: async (manager) => {
+    manager.get('logger').info('Stopping GraphQL API...')
+ 
+    return manager.get('api-graphql').stop()
   }
 }
