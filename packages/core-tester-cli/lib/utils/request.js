ef9f770181cb343eac44ca7d051729712a35db3c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-07 19:44:43 +0100

    Store nethash, etc in config for CLI tester

--- packages/core-tester-cli/lib/utils/request.js
+++ packages/core-tester-cli/lib/utils/request.js
@@ -5,21 +5,13 @@ module.exports = {
   get: (endpoint, isTransport) => {
     const baseUrl = isTransport ? config.baseUrlTransport : config.baseUrlApi
     return axios.get(baseUrl + endpoint, {
-      headers: {
-        nethash: 'd9acd04bde4234a81addb8482333b4ac906bed7be5a9970ce8ada428bd083192',
-        version: '1.0.1',
-        port: 4000
-      }
+      headers: config.requestHeaders
     })
   },
   post: (endpoint, data, isTransport) => {
     const baseUrl = isTransport ? config.baseUrlTransport : config.baseUrlApi
     return axios.post(baseUrl + endpoint, data, {
-      headers: {
-        nethash: 'd9acd04bde4234a81addb8482333b4ac906bed7be5a9970ce8ada428bd083192',
-        version: '1.0.1',
-        port: 4000
-      }
+      headers: config.requestHeaders
     })
   }
 }
