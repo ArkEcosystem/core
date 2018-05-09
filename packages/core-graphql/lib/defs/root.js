7728497fe2ecbfc83ac5897c3f5a5f4046ead8ef
Lucio Rubens <luciorubeens@gmail.com>
2018-05-09 13:58:17 -0300

    Remove round type

--- packages/core-graphql/lib/defs/root.js
+++ packages/core-graphql/lib/defs/root.js
@@ -6,8 +6,6 @@ module.exports = `
     blocks(limit: Limit, orderBy: OrderByInput): [Block]
     transaction(id: String): Transaction
     transactions(limit: Limit, orderBy: OrderByInput, filter: TransactionFilter): [Transaction]
-    round(id: Int!): Round
-    rounds(limit: Limit, orderBy: OrderByInput): [Round]
     wallet(address: Address, publicKey: string, username: String): Wallet
     wallets(limit: Limit, orderBy: OrderByInput, vote: String): [Wallet]
   }
--- packages/core-graphql/lib/resolvers/queries/round/round.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const database = require('@arkecosystem/core-container').resolvePlugin('database')
-
-module.exports = async (_, { id }) => {
-  return database.connection.models.round.findById(id)
-}
