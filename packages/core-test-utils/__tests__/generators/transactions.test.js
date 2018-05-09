15059af361352c10be20c99b7bb67d1feae9019d
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-09 11:52:20 +0100

    Remove transfer tx and add devnet test

--- packages/core-test-utils/__tests__/generators/transactions.test.js
+++ packages/core-test-utils/__tests__/generators/transactions.test.js
@@ -1,32 +1,23 @@
 'use strict'
 
 const generateTransactions = require('../../lib/generators/transactions')
-const transactionMethods = require('../../lib/generators/transactions/index')
 const { TRANSACTION_TYPES } = require('../../../client/lib/constants')
-
 
 describe('generateTransactions', () => {
   it('should be function', async () => {
     await expect(generateTransactions).toBeFunction()
   })
-})
 
-describe('Transfer transaction', () => {
-  it('should be function', async () => {
-    await expect(transactionMethods.transfer).toBeFunction()
-  })
-
-  const quantity = 4
-  const transactions = transactionMethods.transfer(null, null, null, 2, quantity)
-
-  it('should return an array', async () => {
-    await expect(transactions).toBeArrayOfSize(quantity)
-  })
-
-  it('should return an array transfer objects', async () => {
-    const quantity = 4
+  it('should create transfer transactions for devnet', async () => {
+    const devnetAddress = 'DJQL8LWj81nRJNv9bbUgNXXELcB3q5qjZH'
+    const transactions = generateTransactions(
+      'devnet',
+      TRANSACTION_TYPES.TRANSFER,
+      undefined,
+      devnetAddress
+    )
     for (let i = 0; i < transactions.length; i++) {
-      await expect(transactions[i]).toMatchObject({type: TRANSACTION_TYPES.TRANSFER})
+      await expect(transactions[i]).toMatchObject({recipientId: devnetAddress})
     }
   })
 })
