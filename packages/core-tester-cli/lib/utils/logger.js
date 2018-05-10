e9d222a5ed4e7fb86caa4d89c85649874241cdd0
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-10 19:56:34 +0100

    Format logging better in CLI tester

--- packages/core-tester-cli/lib/utils/logger.js
+++ packages/core-tester-cli/lib/utils/logger.js
@@ -1,5 +1,9 @@
 const pino = require('pino')
-const pretty = pino.pretty()
+const pretty = pino.pretty({
+  formatter: (data, util) => {
+    return `${util.prefix}: ${util.asColoredText(data, data.msg)}`
+  },
+})
 pretty.pipe(process.stdout)
 
 const logger = pino({
