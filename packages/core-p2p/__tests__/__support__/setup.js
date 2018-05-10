e028ef80552bc6c3eb568f195bd4f0cfb8a53e9d
Brian Faust <hello@brianfaust.me>
2018-05-10 15:33:30 +0300

    more core-p2p tests

--- packages/core-p2p/__tests__/__support__/setup.js
+++ packages/core-p2p/__tests__/__support__/setup.js
@@ -8,11 +8,7 @@ jest.setTimeout(60000)
 exports.setUp = async () => {
   const config = path.resolve(__dirname, '../../../core-config/lib/networks/devnet')
 
-  container.init({ data: '~/.ark', config }, {
-    exclude: [
-      '@arkecosystem/core-p2p'
-    ]
-  })
+  container.init({ data: '~/.ark', config })
 
   await container.plugins.registerGroup('init', {config})
   await container.plugins.registerGroup('beforeCreate')
--- packages/core-p2p/__tests__/server/internal.test.js
+++ packages/core-p2p/__tests__/server/internal.test.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const axios = require('axios')
+
 const app = require('../__support__/setup')
 
 beforeAll(async (done) => {
@@ -14,6 +16,43 @@ afterAll(async (done) => {
   done()
 })
 
+const sendGET = async (endpoint, params = {}) => axios.get(`http://127.0.0.1:4002/internal/${endpoint}`, { params })
+const sendPOST = async (endpoint, params) => axios.post(`http://127.0.0.1:4002/internal/${endpoint}`, params)
+
 describe('API - Internal', () => {
-  //
+  describe('GET /round', async () => {
+    it('should be ok', async () => {
+      const response = await sendGET('round')
+
+      await expect(response.status).toBe(200)
+      await expect(response.data).toBeObject()
+    })
+  })
+
+  describe('POST /block', async () => {
+    it('should be ok', async () => {
+      const response = await sendPOST('block')
+
+      await expect(response.status).toBe(200)
+      await expect(response.data).toBeObject()
+    })
+  })
+
+  describe('POST /verifyTransaction', async () => {
+    it('should be ok', async () => {
+      const response = await sendPOST('verifyTransaction')
+
+      await expect(response.status).toBe(200)
+      await expect(response.data).toBeObject()
+    })
+  })
+
+  describe('GET /forgingTransactions', async () => {
+    it('should be ok', async () => {
+      const response = await sendGET('forgingTransactions')
+
+      await expect(response.status).toBe(200)
+      await expect(response.data).toBeObject()
+    })
+  })
 })
