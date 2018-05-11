567da122979524449899e779bbe4a1b67d911ecc
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-11 13:16:48 +0100

    Additional deployer tests

--- packages/core-deployer/__tests__/builder/genesis-block.test.js
+++ packages/core-deployer/__tests__/builder/genesis-block.test.js
@@ -5,13 +5,31 @@ const network = require('../../../core-config/lib/networks/testnet/network')
 
 let builder
 let genesis
+let wallet
+let delegateWallet
+let delegateWallets
+let delegateTransactions
+const transactionAmount = 10
+let transferTransaction
+let delegateTransaction
+let formattedTransaction
+let genesisBlock
 
 beforeAll(() => {
-    builder = new GenesisBlockBuilder(network, {
-      totalPremine: 2100000000000000,
-      activeDelegates: 51
-    })
-    genesis = builder.generate()
+  builder = new GenesisBlockBuilder(network, {
+    totalPremine: 2100000000000000,
+    activeDelegates: 3
+  })
+  genesis = builder.generate()
+  wallet = builder.__createWallet()
+  delegateWallet = builder.__createDelegateWallet('testing')
+  delegateWallets = builder.__buildDelegates()
+  delegateTransactions = builder.__buildDelegateTransactions(delegateWallets)
+
+  transferTransaction = builder.__createTransferTransaction(delegateWallet, wallet, transactionAmount)
+  delegateTransaction = builder.__createDelegateTransaction(delegateWallet)
+  // formattedTransaction = builder.__formatGenesisTransaction({}, delegateWallets)
+  // genesisBlock = builder.__createGenesisBlock(delegateWallets)
 })
 
 describe('Genesis Block Builder', () => {
@@ -24,16 +42,96 @@ describe('Genesis Block Builder', () => {
       await expect(builder.generate).toBeFunction()
     })
 
-    it('should return a genesis block', async () => {
-      await expect(genesis).toHaveProperty('genesisBlock')
+    it('should return a genesis object', async () => {
+      await expect(genesis).toContainAllKeys([
+        'genesisBlock',
+        'genesisWallet',
+        'delegatePassphrases'
+      ])
+    })
+  })
+
+  describe('__createWallet', async () => {
+    it('should be a function', async () => {
+      await expect(builder.__createWallet).toBeFunction()
     })
 
-    it('should return a genesis wallet', async () => {
-      await expect(genesis).toHaveProperty('genesisWallet')
+    it('should return an object', async () => {
+      await expect(wallet).toBeObject()
     })
 
-    it('should return delegate passphrases', async () => {
-      await expect(genesis).toHaveProperty(['delegatePassphrases'])
+    it('should return a wallet object', async () => {
+      await expect(wallet).toContainAllKeys([
+        'address',
+        'keys',
+        'passphrase'
+      ])
+    })
+
+    it('should have a valid address', async () => {
+      await expect(wallet.address).toEqual(expect.stringMatching(/^A/))
+    })
+  })
+
+  describe('__createDelegateWallet', async () => {
+    it('should be a function', async () => {
+      await expect(builder.__createDelegateWallet).toBeFunction()
+    })
+
+    it('should return an object', async () => {
+      await expect(delegateWallet).toBeObject()
+    })
+
+    it('should return a delegate wallet object', async () => {
+      await expect(delegateWallet).toContainAllKeys([
+        'address',
+        'keys',
+        'passphrase',
+        'username'
+      ])
+    })
+
+    it('should have a valid address', async () => {
+      await expect(delegateWallet.address).toEqual(expect.stringMatching(/^A/))
+    })
+
+    it('should have a valid username', async () => {
+      await expect(delegateWallet.username).toEqual(expect.stringMatching(/^[a-z0-9!@$&_.]+$/))
+    })
+  })
+
+  describe('__buildDelegates', async () => {
+    it('should be a function', async () => {
+      await expect(builder.__buildDelegates).toBeFunction()
+    })
+
+    it('should return an array of 3', async () => {
+      await expect(delegateWallets).toBeArrayOfSize(3)
+    })
+  })
+
+  describe('__buildDelegateTransactions', async () => {
+    it('should be a function', async () => {
+      await expect(builder.__buildDelegateTransactions).toBeFunction()
+    })
+
+    it('should return an array of 3', async () => {
+      await expect(delegateTransactions).toBeArrayOfSize(3)
+    })
+  })
+
+  describe('__createTransferTransaction', async () => {
+    it('should be a function', async () => {
+      await expect(builder.__createTransferTransaction).toBeFunction()
+    })
+
+    xit('should return a transaction object', async () => {
+      await expect(transferTransaction).toContainAllEntries([
+        ['type', 0],
+        ['amount', transactionAmount],
+        ['fee', 0],
+        ['recipientId', wallet.address],
+      ])
     })
   })
 })
