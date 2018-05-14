c6ea1c136d4894bcfa4176f81242340533cbb1f9
Kristjan Kosic - Chris <chris@ark.io>
2018-05-14 19:28:37 +0200

    timeout adjust

--- packages/core-p2p/lib/monitor.js
+++ packages/core-p2p/lib/monitor.js
@@ -127,7 +127,7 @@ module.exports = class Monitor {
     const npeer = new Peer(peer.ip, peer.port, this.config)
 
     try {
-      await npeer.ping(2000)
+      await npeer.ping(1500)
 
       this.peers[peer.ip] = npeer
       logger.debug(`Accepted new peer ${npeer}`)
