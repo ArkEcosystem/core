3a33aa7e56ee5f4a70f764d5de6246d3ce2d8860
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-15 12:49:22 +0100

    Initial Commit of transaction rules

---
+++ packages/core-validation/lib/rules/models/transactions.js
@@ -0,0 +1,6 @@
+'use strict'
+
+exports.transfer = require('./transactions/transfer')
+exports.signature = require('./transactions/signature')
+exports.delegate = require('./transactions/delegate')
+exports.vote = require('./transactions/vote')
---
+++ packages/core-validation/lib/rules/models/transactions/vote.js
@@ -0,0 +1,27 @@
+const Joi = require('joi')
+const validate = require('../utils/validate-with-joi')
+
+module.exports = (transaction) => {
+  const { error, value } = validate(transaction, Joi.object({
+    id: Joi.string().alphanum().required(),
+    blockid: Joi.number(),
+    type: Joi.number().valid(3),
+    timestamp: Joi.number().min(0).required(),
+    amount: Joi.number().valid(0),
+    fee: Joi.number().min(1).required(),
+    senderId: Joi.string().alphanum().length(34),
+    recipientId: Joi.string().alphanum().length(34),
+    senderPublicKey: Joi.string().alphanum().length(66).required(),
+    signature: Joi.string().alphanum().required(),
+    asset: Joi.object({
+      votes: Joi.array().required()
+    }).required(),
+    confirmations: Joi.number().min(0)
+  }))
+
+  return {
+    data: value,
+    passes: !error,
+    fails: error
+  }
+}
