6467e747ca6619a28cc75acaeb7d6f1778183f02
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-15 12:48:51 +0100

    Delegate username regex

--- packages/core-validation/lib/rules/username.js
+++ packages/core-validation/lib/rules/username.js
@@ -2,7 +2,7 @@ const Joi = require('joi')
 const validate = require('../utils/validate-with-joi')
 
 module.exports = (attributes) => {
-  const { error, value } = validate(attributes, Joi.string().alphanum())
+  const { error, value } = validate(attributes, Joi.string().regex(/^[a-z0-9!@$&_.]+$/))
 
   return {
     data: value,
