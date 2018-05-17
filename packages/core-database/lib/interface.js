19495f7a4faaa051eb684ce05c863ad037c03d9f
Brian Faust <faustbrian@users.noreply.github.com>
2018-05-17 17:33:57 +0300

    fix undefined property on delegate

--- packages/core-database/lib/interface.js
+++ packages/core-database/lib/interface.js
@@ -246,16 +246,12 @@ module.exports = class ConnectionInterface {
     const slot = slots.getSlotNumber(block.data.timestamp)
     const forgingDelegate = delegates[slot % delegates.length]
 
-    if (forgingDelegate) {
-      logger.debug(`Delegate ${block.data.generatorPublicKey} allowed to forge block ${block.data.height}`)
-    }
-
     if (!forgingDelegate) {
       logger.debug(`Could not decide if delegate ${block.data.generatorPublicKey} is allowed to forge block ${block.data.height}`)
-    }
-
-    if (forgingDelegate.publicKey !== block.data.generatorPublicKey) {
+    } else if (forgingDelegate.publicKey !== block.data.generatorPublicKey) {
       throw new Error(`Delegate ${block.data.generatorPublicKey} not allowed to forge, should be ${forgingDelegate.publicKey}`)
+    } else {
+      logger.debug(`Delegate ${block.data.generatorPublicKey} allowed to forge block ${block.data.height}`)
     }
 
     return true
