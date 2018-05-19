9fa3ae4751da34c2e64444c0b8e0580c58a9207a
Kristjan Kosic - Chris <chris@ark.io>
2018-05-19 16:14:11 +0200

    feat: adding fee parameter to tester-cli

--- packages/core-tester-cli/lib/config/index.js
+++ packages/core-tester-cli/lib/config/index.js
@@ -4,6 +4,7 @@ const config = {
   baseUrlApi: 'http://localhost:4102',
   baseUrlP2P: 'http://localhost:4000',
   passphrase: 'prison tobacco acquire stone dignity palace note decade they current lesson robot',
+  secondPassPhrase: '',
   publicKeyHash: 23,
   requestHeaders: {
     nethash: 'd9acd04bde4234a81addb8482333b4ac906bed7be5a9970ce8ada428bd083192',
