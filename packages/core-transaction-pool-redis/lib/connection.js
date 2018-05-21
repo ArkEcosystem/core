422c9f8ce72ffcb1d48fe8b81fd79d02a309d896
Kristjan Kosic - Chris <chris@ark.io>
2018-05-21 13:34:01 +0200

    fix: filering and handling only ark expiration messages..

--- packages/core-transaction-pool-redis/lib/connection.js
+++ packages/core-transaction-pool-redis/lib/connection.js
@@ -25,7 +25,6 @@ module.exports = class TransactionPool extends TransactionPoolInterface {
     this.pool = null
     this.subscription = null
     this.keyPrefix = this.options.key
-    this.counters = {}
     this.pool = new Redis(this.options.redis)
     this.subscription = new Redis(this.options.redis)
 
@@ -44,12 +43,14 @@ module.exports = class TransactionPool extends TransactionPoolInterface {
 
     this.subscription.on('message', async (channel, message) => {
       logger.debug(`Received expiration message ${message} from channel ${channel}`)
+      if (message.split(':')[0] === this.keyPrefix) {
+        const transactionId = message.split(':')[2]
+        console.log('removing', transactionId)
+        const transaction = await this.getTransaction(transactionId)
+        emitter.emit('transaction.expired', transaction.data)
 
-      const transactionId = message.split(':')[2]
-      const transaction = await this.getTransaction(transactionId)
-      emitter.emit('transaction.expired', transaction.data)
-
-      await this.removeTransaction(transaction)
+        await this.removeTransaction(transaction)
+      }
     })
 
     return this
@@ -310,7 +311,7 @@ module.exports = class TransactionPool extends TransactionPoolInterface {
    * @return {void}
    */
   async flush () {
-    const keys = await this.pool.keys(`${this.keyPrefix}*`)
+    const keys = await this.pool.keys(`${this.keyPrefix}:*`)
 
     keys.forEach(key => this.pool.del(key))
   }
