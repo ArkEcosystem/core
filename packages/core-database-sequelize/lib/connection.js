ee77d9cf619497ec7e7cfc4c98307d9311d1894d
fix <fx.thoorens@gmail.com>
2018-05-27 19:31:59 +0200

    better naming

--- packages/core-database-sequelize/lib/connection.js
+++ packages/core-database-sequelize/lib/connection.js
@@ -161,7 +161,7 @@ module.exports = class SequelizeConnection extends ConnectionInterface {
     if (data.length < maxDelegates) {
       const data2 = await this.models.wallet.findAll({
         attributes: [
-          ['vote', 'publicKey'],
+          ['vote', 'publicKey']
         ],
         group: ['vote'],
         where: {
@@ -275,16 +275,16 @@ module.exports = class SequelizeConnection extends ConnectionInterface {
    * @return {Object}
    */
   async saveWallets (force) {
-    const wallets = Object.values(this.walletManager.walletsByPublicKey || {}).filter(acc => acc.publicKey && (force || acc.dirty))
+    const wallets = Object.values(this.walletManager.walletsByPublicKey || {}).filter(wallet => wallet.publicKey && (force || wallet.dirty))
     const chunk = 5000
 
     // breaking into chunks of 5k wallets, to prevent from loading RAM with GB of SQL data
     for (let i = 0, j = wallets.length; i < j; i += chunk) {
-      await this.connection.transaction(transaction =>
+      await this.connection.transaction(dbtransaction =>
         Promise.all(
           wallets
             .slice(i, i + chunk)
-            .map(acc => this.models.wallet.upsert(acc, { transaction }))
+            .map(wallet => this.models.wallet.upsert(wallet, { dbtransaction }))
         )
       )
     }
@@ -293,7 +293,7 @@ module.exports = class SequelizeConnection extends ConnectionInterface {
 
     this.walletManager.purgeEmptyNonDelegates()
 
-    return Object.values(this.walletManager.walletsByAddress).forEach(acc => (acc.dirty = false))
+    return Object.values(this.walletManager.walletsByAddress).forEach(wallet => (wallet.dirty = false))
   }
 
   /**
