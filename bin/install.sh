7bf2b23b1681a202f2be6f62264ca42e3099481c
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-05-30 16:30:06 +0100

    feat: install script initial commit

---
+++ bin/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# ark_commander=$(basename "$0")
+DIR=$(cd $(dirname "$0") && pwd)
+. "${DIR}/lib/dependencies.sh"
+
+check_program_dependencies
+check_nodejs_dependencies
+install_base_dependencies
