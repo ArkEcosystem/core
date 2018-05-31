1ab9832f38f2e104eeef3825137b1a6c6d05dcf9
Brian Faust <hello@brianfaust.me>
2018-05-31 15:24:28 +0300

    refactor: extract build methods into SqlBuilder

--- packages/core-database-sequelize/lib/query-builder/index.js
+++ packages/core-database-sequelize/lib/query-builder/index.js
@@ -1,8 +1,7 @@
-const { QueryTypes } = require('sequelize')
-const concerns = require('./concerns')
-
 const Sequelize = require('sequelize')
-const Op = Sequelize.Op
+const { QueryTypes, Op } = Sequelize
+const concerns = require('./concerns')
+const SqlBuilder = require('./sql-builder')
 
 module.exports = class QueryBuiler {
   /**
@@ -320,7 +319,9 @@ module.exports = class QueryBuiler {
    * @return {[type]}
    */
   async all () {
-    const { query, replacements } = this.__toSQL()
+    const {
+      query, replacements
+    } = SqlBuilder.build(this.criteria, this.replacements)
 
     return this.connection.query(query, {
       replacements,
@@ -336,166 +337,6 @@ module.exports = class QueryBuiler {
     const data = await this.all()
 
     return data[0]
-  }
-
-  /**
-   * [__toSQL description]
-   * @return {[type]}
-   */
-  __toSQL () {
-    let raw = {
-      query: '',
-      replacements: null
-    }
-
-    if (this.criteria.select) {
-      raw.query += this.__buildSelect()
-    }
-
-    if (this.criteria.from) {
-      raw.query += this.__buildFrom()
-    }
-
-    if (this.criteria.where) {
-      raw.query += this.__buildWhere()
-    }
-
-    if (this.criteria.groupBy) {
-      raw.query += this.__buildGroupBy()
-    }
-
-    if (this.criteria.orderBy) {
-      raw.query += this.__buildOrderBy()
-    }
-
-    if (this.criteria.limit) {
-      raw.query += this.__buildLimit()
-    }
-
-    if (this.criteria.offset) {
-      raw.query += this.__buildOffset()
-    }
-
-    raw.replacements = this.replacements
-
-    this.__reset()
-
-    return raw
-  }
-
-  /**
-   * [__buildSelect description]
-   * @return {String}
-   */
-  __buildSelect () {
-    this.replacements = this.criteria.select.columns
-
-    const criteria = Array(this.criteria.select.columns.length).fill('?').join(',')
-
-    return `SELECT ${criteria} `
-  }
-
-  /**
-   * [__buildFrom description]
-   * @return {String}
-   */
-  __buildFrom () {
-    this.replacements.push(this.criteria.from)
-
-    return 'FROM ? '
-  }
-
-  /**
-   * [__buildWhere description]
-   * @return {String}
-   */
-  __buildWhere () {
-    const map = (item) => {
-      this.replacements.push(item.column)
-
-      if (item.hasOwnProperty('from') && item.hasOwnProperty('to')) {
-        this.replacements.push(item.from)
-        this.replacements.push(item.to)
-
-        return `? ${item.operator} ? AND ?`
-      } else {
-        this.replacements.push(item.value)
-
-        return `? ${item.operator} ?`
-      }
-    }
-
-    const andQuery = Object
-      .values(this.criteria.where.and)
-      .map(item => map(item))
-      .join(' AND ')
-
-    const orQuery = Object
-      .values(this.criteria.where.or)
-      .map(item => map(item))
-      .join(' OR ')
-
-    if (!andQuery && !orQuery) {
-      return ''
-    }
-
-    let query = 'WHERE '
-
-    if (andQuery) {
-      query += andQuery
-    }
-
-    if (orQuery) {
-      query += ` OR ${orQuery}`
-    }
-
-    return `${query} `
-  }
-
-  /**
-   * [__buildGroupBy description]
-   * @return {String}
-   */
-  __buildGroupBy () {
-    this.replacements.push(this.criteria.groupBy)
-
-    return 'GROUP BY ? '
-  }
-
-  /**
-   * [__buildOrderBy description]
-   * @return {String}
-   */
-  __buildOrderBy () {
-    const criteria = []
-
-    Object.values(this.criteria.orderBy).forEach(item => {
-      this.replacements.push(item.column)
-
-      criteria.push(`? ${item.direction.toUpperCase()}`)
-    })
-
-    return `ORDER BY ${criteria.join(',')} `
-  }
-
-  /**
-   * [__buildLimit description]
-   * @return {String}
-   */
-  __buildLimit () {
-    this.replacements.push(this.criteria.limit)
-
-    return 'LIMIT ? '
-  }
-
-  /**
-   * [__buildOffset description]
-   * @return {String}
-   */
-  __buildOffset () {
-    this.replacements.push(this.criteria.offset)
-
-    return 'OFFSET ? '
   }
 
   /**
---
+++ packages/core-database-sequelize/lib/query-builder/sql-builder.js
@@ -0,0 +1,166 @@
+module.exports = class SqlBuilder {
+  /**
+   * [build description]
+   * @param  {[type]} criteria     [description]
+   * @param  {[type]} replacements [description]
+   * @return {[type]}              [description]
+   */
+  static build (criteria, replacements) {
+    this.criteria = criteria
+    this.replacements = replacements
+
+    let raw = {
+      query: '',
+      replacements: null
+    }
+
+    if (this.criteria.select) {
+      raw.query += this.__buildSelect()
+    }
+
+    if (this.criteria.from) {
+      raw.query += this.__buildFrom()
+    }
+
+    if (this.criteria.where) {
+      raw.query += this.__buildWhere()
+    }
+
+    if (this.criteria.groupBy) {
+      raw.query += this.__buildGroupBy()
+    }
+
+    if (this.criteria.orderBy) {
+      raw.query += this.__buildOrderBy()
+    }
+
+    if (this.criteria.limit) {
+      raw.query += this.__buildLimit()
+    }
+
+    if (this.criteria.offset) {
+      raw.query += this.__buildOffset()
+    }
+
+    raw.replacements = this.replacements
+
+    this.__reset()
+
+    return raw
+  }
+
+  /**
+   * [__buildSelect description]
+   * @return {String}
+   */
+  __buildSelect () {
+    this.replacements = this.criteria.select.columns
+
+    const criteria = Array(this.criteria.select.columns.length).fill('?').join(',')
+
+    return `SELECT ${criteria} `
+  }
+
+  /**
+   * [__buildFrom description]
+   * @return {String}
+   */
+  __buildFrom () {
+    this.replacements.push(this.criteria.from)
+
+    return 'FROM ? '
+  }
+
+  /**
+   * [__buildWhere description]
+   * @return {String}
+   */
+  __buildWhere () {
+    const map = (item) => {
+      this.replacements.push(item.column)
+
+      if (item.hasOwnProperty('from') && item.hasOwnProperty('to')) {
+        this.replacements.push(item.from)
+        this.replacements.push(item.to)
+
+        return `? ${item.operator} ? AND ?`
+      } else {
+        this.replacements.push(item.value)
+
+        return `? ${item.operator} ?`
+      }
+    }
+
+    const andQuery = Object
+      .values(this.criteria.where.and)
+      .map(item => map(item))
+      .join(' AND ')
+
+    const orQuery = Object
+      .values(this.criteria.where.or)
+      .map(item => map(item))
+      .join(' OR ')
+
+    if (!andQuery && !orQuery) {
+      return ''
+    }
+
+    let query = 'WHERE '
+
+    if (andQuery) {
+      query += andQuery
+    }
+
+    if (orQuery) {
+      query += ` OR ${orQuery}`
+    }
+
+    return `${query} `
+  }
+
+  /**
+   * [__buildGroupBy description]
+   * @return {String}
+   */
+  __buildGroupBy () {
+    this.replacements.push(this.criteria.groupBy)
+
+    return 'GROUP BY ? '
+  }
+
+  /**
+   * [__buildOrderBy description]
+   * @return {String}
+   */
+  __buildOrderBy () {
+    const criteria = []
+
+    Object.values(this.criteria.orderBy).forEach(item => {
+      this.replacements.push(item.column)
+
+      criteria.push(`? ${item.direction.toUpperCase()}`)
+    })
+
+    return `ORDER BY ${criteria.join(',')} `
+  }
+
+  /**
+   * [__buildLimit description]
+   * @return {String}
+   */
+  __buildLimit () {
+    this.replacements.push(this.criteria.limit)
+
+    return 'LIMIT ? '
+  }
+
+  /**
+   * [__buildOffset description]
+   * @return {String}
+   */
+  __buildOffset () {
+    this.replacements.push(this.criteria.offset)
+
+    return 'OFFSET ? '
+  }
+}
