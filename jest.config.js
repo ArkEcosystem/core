a7abcf674b772b3952a707e15b4e7c4d403e484a
jaml <dev@jaml.pro>
2018-06-03 18:52:21 +0200

    Create a Jest configuration to test all packages

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  bail: false,
+  verbose: true,
+  testMatch: [
+    '**/packages/**/__tests__/**/*.test.js'
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json'
+  ],
+  coverageDirectory: '<rootDir>/.coverage',
+  collectCoverageFrom: [
+    'packages/**/lib/**/*.js',
+    '!**/node_modules/**'
+  ],
+  watchman: false,
+  setupTestFrameworkScriptFile: 'jest-extended'
+}
