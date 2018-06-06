2d586b35b7f6434c160c6de71d80e0766709f33e
jaml <dev@jaml.pro>
2018-06-06 11:42:29 +0200

    Change Delegates V2 API handlers tests to be more strict

--- packages/core-api/__tests__/v2/handlers/delegates.test.js
+++ packages/core-api/__tests__/v2/handlers/delegates.test.js
@@ -4,9 +4,11 @@ require('../../__support__/setup')
 
 const utils = require('../utils')
 
-const delegateUsername = 'genesis_9'
-const delegateAddress = 'AG8kwwk4TsYfA2HdwaWBVAJQBj6VhdcpMo'
-const delegatePublicKey = '0377f81a18d25d77b100cb17e829a72259f08334d064f6c887298917a04df8f647'
+const delegate = {
+  username: 'genesis_9',
+  address: 'AG8kwwk4TsYfA2HdwaWBVAJQBj6VhdcpMo',
+  publicKey: '0377f81a18d25d77b100cb17e829a72259f08334d064f6c887298917a04df8f647'
+}
 
 describe('API 2.0 - Delegates', () => {
   describe('GET /delegates', () => {
@@ -21,56 +23,45 @@ describe('API 2.0 - Delegates', () => {
 
   describe('GET /delegates/:id', () => {
     it('should GET a delegate by the given username', async () => {
-      const response = await utils.request('GET', `delegates/${delegateUsername}`)
+      const response = await utils.request('GET', `delegates/${delegate.username}`)
       utils.expectSuccessful(response)
       utils.expectResource(response)
 
-      utils.expectDelegate(response.data.data)
-      expect(response.data.data).toHaveProperty('username', delegateUsername)
-      expect(response.data.data).toHaveProperty('address', delegateAddress)
-      expect(response.data.data).toHaveProperty('publicKey', delegatePublicKey)
+      utils.expectDelegate(response.data.data, delegate)
     })
 
     it('should GET a delegate by the given address', async () => {
-      const response = await utils.request('GET', `delegates/${delegateAddress}`)
+      const response = await utils.request('GET', `delegates/${delegate.address}`)
       utils.expectSuccessful(response)
       utils.expectResource(response)
 
-      utils.expectDelegate(response.data.data)
-      expect(response.data.data).toHaveProperty('username', delegateUsername)
-      expect(response.data.data).toHaveProperty('address', delegateAddress)
-      expect(response.data.data).toHaveProperty('publicKey', delegatePublicKey)
+      utils.expectDelegate(response.data.data, delegate)
     })
 
     it('should GET a delegate by the given public key', async () => {
-      const response = await utils.request('GET', `delegates/${delegatePublicKey}`)
+      const response = await utils.request('GET', `delegates/${delegate.publicKey}`)
       utils.expectSuccessful(response)
       utils.expectResource(response)
 
-      utils.expectDelegate(response.data.data)
-      expect(response.data.data).toHaveProperty('username', delegateUsername)
-      expect(response.data.data).toHaveProperty('address', delegateAddress)
-      expect(response.data.data).toHaveProperty('publicKey', delegatePublicKey)
+      utils.expectDelegate(response.data.data, delegate)
     })
   })
 
   describe('POST /delegates/search', () => {
     it('should POST a search for delegates with a username that matches the given string', async () => {
-      const response = await utils.request('POST', 'delegates/search', { username: delegateUsername })
+      const response = await utils.request('POST', 'delegates/search', { username: delegate.username })
       utils.expectSuccessful(response)
       utils.expectCollection(response)
 
       expect(response.data.data).toHaveLength(1)
 
-      const delegate = response.data.data[0]
-      utils.expectDelegate(response.data.data[0])
-      expect(delegate).toHaveProperty('username', delegateUsername)
+      utils.expectDelegate(response.data.data[0], delegate)
     })
   })
 
   describe.skip('GET /delegates/:id/blocks', () => {
     it('should GET all blocks for a delegate by the given identifier', async () => {
-      const response = await utils.request('GET', `delegates/${delegatePublicKey}/blocks`)
+      const response = await utils.request('GET', `delegates/${delegate.publicKey}/blocks`)
       utils.expectSuccessful(response)
       utils.expectCollection(response)
       utils.expectBlock(response.data.data[0])
@@ -79,7 +70,7 @@ describe('API 2.0 - Delegates', () => {
 
   describe('GET /delegates/:id/voters', () => {
     it('should GET all voters (wallets) for a delegate by the given identifier', async () => {
-      const response = await utils.request('GET', `delegates/${delegatePublicKey}/voters`)
+      const response = await utils.request('GET', `delegates/${delegate.publicKey}/voters`)
       utils.expectSuccessful(response)
       utils.expectCollection(response)
 
