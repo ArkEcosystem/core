e403ff00271b6626588fe1c8fbff76b30b3b883e
Brian Faust <hello@brianfaust.me>
2018-06-15 08:31:52 +0300

    fix: add missing host to json-rpc

--- packages/core-json-rpc/lib/defaults.js
+++ packages/core-json-rpc/lib/defaults.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   enabled: false,
+  host: process.env.ARK_JSONRPC_HOST || '0.0.0.0',
   port: process.env.ARK_JSONRPC_PORT || 8080,
   allowRemote: true,
   whitelist: ['127.0.0.1', '192.168.*']
