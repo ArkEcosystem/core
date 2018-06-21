33ac5a6257a464e67925aed0eb0d65d0528e480c
Jeremi Gendron <gendronjeremi@gmail.com>
2018-06-21 03:40:15 -0400

    added pretty-ms, required in sync-tracker, time values now remain in ms throughout the module, variable names changed to reflect the last point, logic remains the same, milliseconds left get passed to prettyms in the logged message

--- packages/core-blockchain/lib/utils/tick-sync-tracker.js
+++ packages/core-blockchain/lib/utils/tick-sync-tracker.js
@@ -2,6 +2,8 @@ const container = require('@arkecosystem/core-container')
 const logger = container.resolvePlugin('logger')
 const database = container.resolvePlugin('database')
 
+const prettyMs = require('pretty-ms')
+
 let tracker = null
 
 module.exports = async block => {
@@ -12,7 +14,7 @@ module.exports = async block => {
       start: new Date().getTime(),
       networkHeight: container.resolvePlugin('p2p').getMonitor().getNetworkHeight(),
       downloadedBlocks: height,
-      blockPerSecond: 0,
+      blockPerMs: 0,
       percent: 0,
       secondsLeft: 0
     }
@@ -20,14 +22,14 @@ module.exports = async block => {
 
   tracker.downloadedBlocks += 400
   tracker.percent = (tracker.downloadedBlocks * 100) / tracker.networkHeight
-  tracker.blockPerSecond = ((new Date().getTime()) - tracker.start) / 1000 / tracker.downloadedBlocks
-  tracker.secondsLeft = (tracker.networkHeight - tracker.downloadedBlocks) / tracker.blockPerSecond
+  tracker.blockPerMs = ((new Date().getTime()) - tracker.start) / tracker.downloadedBlocks
+  tracker.msLeft = (tracker.networkHeight - tracker.downloadedBlocks) / tracker.blockPerMs
 
   if (tracker.percent < 100) {
     const downloadedBlocks = tracker.downloadedBlocks.toLocaleString()
     const networkHeight = tracker.networkHeight.toLocaleString()
 
-    logger.printTracker('Fast Sync', tracker.percent, 100, `(${downloadedBlocks} of ${networkHeight} blocks)`)
+    logger.printTracker('Fast Sync', tracker.percent, 100, `(${downloadedBlocks} of ${networkHeight} - ${prettyMs(tracker.msLeft, {secDecimalDigits: 0})})`)
   } else {
     tracker = null
     logger.stopTracker('Fast Sync', 100, 100)
