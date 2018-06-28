117f44d0d76fd91451290ffd241416937e970712
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2018-06-28 06:38:15 +0200

    fix: change delegate "votes" property to "vote" as in v1 (#713)

--- packages/core-api/lib/versions/1/transformers/delegate.js
+++ packages/core-api/lib/versions/1/transformers/delegate.js
@@ -12,7 +12,7 @@ module.exports = (delegate) => {
     username: delegate.username,
     address: delegate.address,
     publicKey: delegate.publicKey,
-    votes: delegate.votebalance,
+    vote: delegate.votebalance + '',
     producedblocks: delegate.producedBlocks,
     missedblocks: delegate.missedBlocks,
     forged: delegate.forged,
