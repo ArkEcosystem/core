70c075eaca51c5efa494f57dea5efbc6c428e4a9
Kristjan Kosic - Chris <chris@ark.io>
2018-07-02 15:44:48 +0200

    fix: correclty returning overheight header

--- packages/core-p2p/lib/utils/calc-quorum.js
+++ packages/core-p2p/lib/utils/calc-quorum.js
@@ -42,7 +42,7 @@ module.exports = (p2pMonitor, lastBlock) => {
   const calculatedQuorum = quorum / (quorum + noquorum)
   const forgingAllowed = !overHeightBlockHeader && calculatedQuorum > 0.66
 
-  logger.debug(`Node height: ${lastBlock.data.height}, CalcQuorum: ${calculatedQuorum}, Quorum: ${quorum}, NQuorum: ${noquorum} Last Block id: ${lastBlock.data.id}`)
+  logger.debug(`Node height: ${lastBlock.data.height}, CalcQuorum: ${calculatedQuorum}, Quorum: ${quorum}, NQuorum: ${noquorum}, OverHeightQuorum: ${overHeightQuorum} Last Block id: ${lastBlock.data.id}`)
 
-  return {quorum: calculatedQuorum, forgingAllowed: forgingAllowed, nodeHeight: lastBlock.data.height, lastBlockId: lastBlock.data.id, overHeightBlockHeader: overHeightQuorum, minimumNetworkReach: true}
+  return {quorum: calculatedQuorum, forgingAllowed: forgingAllowed, nodeHeight: lastBlock.data.height, lastBlockId: lastBlock.data.id, overHeightBlockHeader: overHeightBlockHeader, minimumNetworkReach: true}
 }
