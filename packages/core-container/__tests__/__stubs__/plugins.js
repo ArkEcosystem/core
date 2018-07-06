6d68d0466d78b2692291d564ee4817ece94d924c
Juan <j-a-m-l@users.noreply.github.com>
2018-07-06 10:52:03 +0200

    fix: core-container tests (#727)
    * Refactor PluginRegistrars
    
    * Resolve plugins with relative paths using the configuration file as reference for the path

--- packages/core-container/__tests__/__stubs__/plugins.js
+++ packages/core-container/__tests__/__stubs__/plugins.js
@@ -1,12 +1,12 @@
 module.exports = {
-  './__tests__/__stubs__/plugin-a': {
+  './plugin-a': {
     enabled: true
   },
-  './__tests__/__stubs__/plugin-b': {
+  './plugin-b': {
     enabled: true,
     property: 'value'
   },
-  './__tests__/__stubs__/plugin-c': {
+  './plugin-c': {
     enabled: true
   }
 }
