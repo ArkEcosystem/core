929615a995dd206a35610fab6e87377e97b2dada
jaml <dev@jaml.pro>
2018-07-14 15:11:56 +0200

    Enable the `core-transaction-pool` tests on the root level and Travis CI

--- jest.config.js
+++ jest.config.js
@@ -8,15 +8,19 @@ module.exports = {
     '**/packages/client/**/__tests__/**/*.test.js',
     '**/packages/core-config/**/__tests__/**/*.test.js',
     '**/packages/core-container/**/__tests__/**/*.test.js',
-    // '**/packages/core-database-sequelize/**/__tests__/**/*.test.js'
     '**/packages/core-deployer/**/__tests__/**/*.test.js',
     '**/packages/core-event-emitter/**/__tests__/**/*.test.js',
     '**/packages/core-logger-winston/**/__tests__/**/*.test.js',
     '**/packages/core-logger/**/__tests__/**/*.test.js',
     '**/packages/core-test-utils/**/__tests__/**/*.test.js',
     '**/packages/core-tester-cli/**/__tests__/**/*.test.js',
+    '**/packages/core-transaction-pool-redis/**/__tests__/**/*.test.js',
     '**/packages/core-webhooks/**/__tests__/**/*.test.js',
     '**/packages/validation/**/__tests__/**/*.test.js'
+
+    /* These packages have failing tests */
+
+    // '**/packages/core-database-sequelize/**/__tests__/**/*.test.js'
 
     /* These packages don't have any test yet */
 
@@ -34,9 +38,8 @@ module.exports = {
     // '**/packages/core-forger/**/__tests__/**/*.test.js',
     // '**/packages/core-json-rpc/**/__tests__/**/*.test.js',
     // '**/packages/core-p2p/**/__tests__/**/*.test.js',
-    // '**/packages/core-transaction-pool-redis/**/__tests__/**/*.test.js',
     // '**/packages/core-transaction-pool/**/__tests__/**/*.test.js',
-    // '**/packages/crypto/**/__tests__/**/*.test.js',
+    // '**/packages/crypto/**/__tests__/**/*.test.js'
   ],
   moduleFileExtensions: [
     'js',
