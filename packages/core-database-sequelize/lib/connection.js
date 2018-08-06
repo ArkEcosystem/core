141edbd249c5699d7e081419c74be83293b18185
fix <fx.thoorens@gmail.com>
2018-08-06 13:25:11 +0200

    Removed unused recentBlockIds since implementation is overidding it
    Also there was no garantee this.recentBlockIds is properly in sync with blockcain state. (for instance when a block is reverted)

--- packages/core-database-sequelize/lib/connection.js
+++ packages/core-database-sequelize/lib/connection.js
@@ -588,18 +588,14 @@ module.exports = class SequelizeConnection extends ConnectionInterface {
    * @return {[]String}
    */
   async getRecentBlockIds () {
-    if (!this.recentBlockIds.length) {
-      const blocks = await this.query
-        .select('id')
-        .from('blocks')
-        .orderBy({ timestamp: 'DESC' })
-        .limit(10)
-        .all()
+    const blocks = await this.query
+      .select('id')
+      .from('blocks')
+      .orderBy({ timestamp: 'DESC' })
+      .limit(10)
+      .all()
 
-      this.recentBlockIds = blocks.map(block => block.id)
-    }
-
-    return this.recentBlockIds
+    return blocks.map(block => block.id)
   }
 
   /**
