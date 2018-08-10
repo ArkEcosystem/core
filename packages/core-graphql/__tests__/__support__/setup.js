47da1f663af5a6bc919d47ff8380c1d4819d09e5
jeremiG <gendronjeremi@gmail.com>
2018-08-10 10:49:08 +0000

    test: prepare core-graphql tests (#816)

---
+++ packages/core-graphql/__tests__/__support__/setup.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const path = require('path')
+const container = require('@arkecosystem/core-container')
+
+exports.setUp = async () => {
+
+  jest.setTimeout(60000)
+
+  process.env.ARK_GRAPHQL_ENABLED = true
+  process.env.ARK_GRAPHQL_HOST = 'localhost'
+  process.env.ARK_GRAPHQL_PORT = 4005
+
+  await container.setUp({
+    data: '~/.ark',
+    config: path.resolve(__dirname, '../../../core/lib/config/testnet'),
+    token: 'ark',
+    network: 'testnet'
+  }, {
+  })
+
+  return container
+}
+
+exports.tearDown = async () => container.tearDown()
