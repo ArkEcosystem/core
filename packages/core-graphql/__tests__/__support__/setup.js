6df7526c1bddbef4be24acaf0bb02084efd599cf
jeremi gendron <gendronjeremi@gmail.com>
2018-08-13 01:38:59 +0000

    linted me ;-;

--- packages/core-graphql/__tests__/__support__/setup.js
+++ packages/core-graphql/__tests__/__support__/setup.js
@@ -4,7 +4,6 @@ const path = require('path')
 const container = require('@arkecosystem/core-container')
 
 exports.setUp = async () => {
-
   await container.setUp({
     data: '~/.ark',
     config: path.resolve(__dirname, './config'),
