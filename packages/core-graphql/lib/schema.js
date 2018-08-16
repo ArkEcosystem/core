8391050ee67ec8893c78a3da835fecd05251cc9c
jeremi gendron <gendronjeremi@gmail.com>
2018-08-16 00:17:09 +0000

    schema basic doc

--- packages/core-graphql/lib/schema.js
+++ packages/core-graphql/lib/schema.js
@@ -4,6 +4,13 @@ const { makeExecutableSchema } = require('graphql-tools')
 const resolvers = require('./resolvers')
 const typeDefs = require('./defs')
 
+/**
+ * Schema used by the Apollo GraphQL plugin for the hapi.js server.
+ * NOTE: Concatenated Template Literals following GraphQL syntax consist
+ * the executable schema.
+ * @param {String} defini  tions
+ * @param {String} resolvers
+ */
 module.exports = makeExecutableSchema({
   typeDefs,
   resolvers
