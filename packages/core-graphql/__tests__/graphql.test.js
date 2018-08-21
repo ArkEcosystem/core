e4f8d563f1cd41129b518d7fed6bd3706b6e6a66
jeremi gendron <gendronjeremi@gmail.com>
2018-08-21 19:24:27 +0000

    documentation for tests

--- packages/core-graphql/__tests__/graphql.test.js
+++ packages/core-graphql/__tests__/graphql.test.js
@@ -1,13 +1,25 @@
 const app = require('./__support__/setup')
 
+/**
+ * Core plugins used by the testing suite.
+ */
 let graphql
 let logger
 
+/**
+ * Hard coded queries to run, data to feed into the database
+ * and expected results from the queries.
+ */
 const { testQueries,
         testData,
         testExpected
       } = require('./__fixtures__')
 
+/**
+ * Important elements to setUp and assign before running the tests.
+ * We setup a container (see __support__/) and grant access to
+ * both the GraphQL and Logger Core plugins to the suite.
+ */
 beforeAll(async () => {
   const container = await app.setUp()
   graphql = await container.resolvePlugin('graphql')
@@ -15,26 +27,37 @@ beforeAll(async () => {
   logger.info('Starting GraphQL Tests!')
 })
 
+/**
+ * Tear down the container.
+ */
 afterAll(() => {
   app.tearDown()
 })
 
+/**
+ * Ensure that our graphql plugin has been properly loaded
+ * then run the tests.
+ */
 describe('GraphQL', () => {
   it('should be an object', () => {
     expect(graphql).toBeObject()
   })
 
+  /**
+   * Test queries to be performed on the test data in the database
+   * and matched against expected results
+   */
   describe('test queries', () => {
     it('should be an object', () => {
       expect(testQueries).toBeObject()
-      logger.info(`Processing test queries`)
+      logger.info('Processing test queries')
     })
 
     describe('test query for Block', () => {
       it('Should have queries', () => {
         expect(testQueries.block.length).toBeTruthy()
 
-        logger.debug(`Proceeding with Block test queries`)
+        logger.debug('Proceeding with Block test queries')
 
         testQueries.block.forEach((block) => {
           logger.info(`Processing Block test query with id: ${block.id}`)
@@ -46,7 +69,7 @@ describe('GraphQL', () => {
       it('Should have queries', () => {
         expect(testQueries.blocks.length).toBeTruthy()
 
-        logger.debug(`Proceeding with Blocks test queries`)
+        logger.debug('Proceeding with Blocks test queries')
 
         testQueries.blocks.forEach((blocks) => {
           logger.info(`Processing Blocks test query with limit: ${blocks.limit}, offset: ${blocks.offset}, orderBy: ${blocks.orderBy} and filter: ${blocks.filter}`)
@@ -58,7 +81,7 @@ describe('GraphQL', () => {
       it('should have queries', () => {
         expect(testQueries.transaction.length).toBeTruthy()
 
-        logger.debug(`Proceeding with Transaction test queries`)
+        logger.debug('Proceeding with Transaction test queries')
 
         testQueries.transaction.forEach((transaction) => {
           logger.info(`Processing Transaction test query with id: ${transaction.id}`)
@@ -70,7 +93,7 @@ describe('GraphQL', () => {
       it('should have queries', () => {
         expect(testQueries.transactions.length).toBeTruthy()
 
-        logger.debug(`Proceeding with Transactions test queries`)
+        logger.debug('Proceeding with Transactions test queries')
 
         testQueries.transactions.forEach((transactions) => {
           logger.info(`Processing Transactions test query with limit: ${transactions.limit}, orderBy: ${transactions.orderBy} and filter: ${transactions.filter}`)
@@ -82,7 +105,7 @@ describe('GraphQL', () => {
       it('should have queries', () => {
         expect(testQueries.wallet.length).toBeTruthy()
 
-        logger.debug(`Proceeding with Wallet test queries`)
+        logger.debug('Proceeding with Wallet test queries')
 
         testQueries.wallet.forEach((wallet) => {
           logger.info(`Processing Wallet test query with ${Object.keys(wallet)[0]}: ${wallet[Object.keys(wallet)[0]]}`)
@@ -94,7 +117,7 @@ describe('GraphQL', () => {
       it('should have queries', () => {
         expect(testQueries.wallets.length).toBeTruthy()
 
-        logger.debug(`Proceeding with Wallets test queries`)
+        logger.debug('Proceeding with Wallets test queries')
 
         testQueries.wallets.forEach((wallets) => {
           logger.info(`Processing Wallets test query with limit: ${wallets.limit}, orderBy: ${wallets.orderBy} and filter: ${wallets.filter}`)
@@ -103,12 +126,18 @@ describe('GraphQL', () => {
     })
   })
 
+  /**
+   * Test data to be fed into a database and queried by test queries.
+   */
   describe('test data', () => {
     it('should be an object', () => {
       expect(testData).toBeObject()
     })
   })
 
+  /**
+   * Expected results from running test queries on the test data.
+   */
   describe('test expected', () => {
     it('should be an object', () => {
       expect(testExpected).toBeObject()
