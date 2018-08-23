9972cb3a4771d30f7e74faea8eede7dc3b57c9e4
Kristjan Kosic - Chris <chris@ark.io>
2018-08-23 12:18:09 +0200

    fix: adding whitelist setting back

--- packages/core/lib/config/testnet.live/plugins.js
+++ packages/core/lib/config/testnet.live/plugins.js
@@ -42,7 +42,8 @@ module.exports = {
     enabled: !process.env.ARK_TRANSACTION_POOL_DISABLED,
     key: 'ark1',
     maxTransactionsPerSender: process.env.ARK_TRANSACTION_POOL_MAX_PER_SENDER || 100,
-    allowedSenders: ['127.0.0.1', '::ffff:127.0.0.1', '192.168.*'],
+    whitelist: ['127.0.0.1', '::ffff:127.0.0.1', '192.168.*'],
+    allowedSenders: [],
     maxTransactionsPerRequest: 150,
     redis: {
       host: process.env.ARK_REDIS_HOST || 'localhost',
