3a8cfa3372f07a43ad29c17fa09e8a0719377a43
Air1 <erwann@ark.io>
2018-08-24 16:03:00 +0200

    test: add package core-blockchain to jest config

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,8 @@ module.exports = {
     '**/packages/core-database-sequelize/**/__tests__/**/*.test.js',
     '**/packages/core-database/**/__tests__/**/*.test.js',
     '**/packages/core-forger/**/__tests__/**/*.test.js',
-    '**/packages/core-p2p/**/__tests__/**/*.test.js'
+    '**/packages/core-p2p/**/__tests__/**/*.test.js',
+    '**/packages/core-blockchain/**/__tests__/**/*.test.js'
 
     /* These packages have failing tests */
 
@@ -35,7 +36,6 @@ module.exports = {
 
     /* These packages have very long  timeouts or don't end properly */
 
-    // '**/packages/core-blockchain/**/__tests__/**/*.test.js',
     // '**/packages/core-json-rpc/**/__tests__/**/*.test.js',
     // '**/packages/core-transaction-pool/**/__tests__/**/*.test.js',
     // '**/packages/crypto/**/__tests__/**/*.test.js'
