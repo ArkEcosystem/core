b5fb4bee3e36de0c0551734212029fd5bf0a6d57
Kristjan Kosic - Chris <chris@ark.io>
2018-08-27 11:42:23 +0200

    removing as is in another PR

--- packages/core/lib/config/testnet/plugins.js
+++ packages/core/lib/config/testnet/plugins.js
@@ -43,7 +43,6 @@ module.exports = {
     key: 'ark-testnet',
     maxTransactionsPerSender: process.env.ARK_TRANSACTION_POOL_MAX_PER_SENDER || 100,
     whitelist: [],
-    allowedSenders: [],
     redis: {
       host: process.env.ARK_REDIS_HOST || 'localhost',
       port: process.env.ARK_REDIS_PORT || 6379
