17f705ebe0dbb15bac3a6572983197dea3d1f459
air1one <36802613+air1one@users.noreply.github.com>
2018-08-30 13:12:12 +0200

    test: add crypto package (#951)
    * test: crypto package
    * test: test data.timestamp exists but do not check vs current timestamp

--- jest.config.js
+++ jest.config.js
@@ -23,7 +23,8 @@ module.exports = {
     '**/packages/core-p2p/**/__tests__/**/*.test.js',
     '**/packages/core-blockchain/**/__tests__/**/*.test.js',
     '**/packages/core-transaction-pool/**/__tests__/**/*.test.js',
-    '**/packages/core-json-rpc/**/__tests__/**/*.test.js'
+    '**/packages/core-json-rpc/**/__tests__/**/*.test.js',
+    '**/packages/crypto/**/__tests__/**/*.test.js'
 
     /* These packages have failing tests */
 
@@ -38,7 +39,6 @@ module.exports = {
 
     /* These packages have very long  timeouts or don't end properly */
 
-    // '**/packages/crypto/**/__tests__/**/*.test.js'
   ],
   moduleFileExtensions: [
     'js',
