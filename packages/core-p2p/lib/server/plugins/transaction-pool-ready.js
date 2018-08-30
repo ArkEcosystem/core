9906d2b6cdc4879d2f682115ece3e92d89c3b1fc
Brian Faust <envoyer@pm.me>
2018-08-30 10:26:05 +0300

    feat: transaction pool ready plugin
    Signed-off-by: Brian Faust <envoyer@pm.me>

---
+++ packages/core-p2p/lib/server/plugins/transaction-pool-ready.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const Boom = require('boom')
+const container = require('@arkecosystem/core-container')
+
+/**
+ * The register method used by hapi.js.
+ * @param  {Hapi.Server} server
+ * @param  {Object} options
+ * @return {void}
+ */
+const register = async (server, options) => {
+  server.ext({
+    type: 'onRequest',
+    async method (request, h) {
+      if (!options.routes.includes(request.path)) {
+        return h.continue
+      }
+
+      if (!container.resolvePlugin('transactionPool')) {
+        return Boom.serverUnavailable('Transaction Pool not ready')
+      }
+
+      return h.continue
+    }
+  })
+}
+
+/**
+ * The struct used by hapi.js.
+ * @type {Object}
+ */
+exports.plugin = {
+  name: 'core-p2p-transaction-pool-ready',
+  version: '0.1.0',
+  register
+}
