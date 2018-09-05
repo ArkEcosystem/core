7c6ae7251906a2613ac69c782d579afea558bdf0
supaiku <supaiku@protonmail.com>
2018-09-05 17:17:01 +0200

    fix: core-api tests

--- packages/core-api/__tests__/__support__/utils/generate-round.js
+++ packages/core-api/__tests__/__support__/utils/generate-round.js
@@ -1,7 +1,9 @@
+const { Bignum } = require('@arkecosystem/crypto')
+
 module.exports = (delegates, round) => {
   return delegates.map(delegate => ({
     round,
     publicKey: delegate,
-    balance: '245098000000000'
+    balance: Bignum.from('245098000000000')
   }))
 }
