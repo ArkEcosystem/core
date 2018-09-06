ca5a2a77d750d84b0b96a05845f6ad36aabdd35a
air1one <36802613+air1one@users.noreply.github.com>
2018-09-06 17:32:43 +0200

    test: Initial tests on graphql package + fix wallets query (#973)
    * refactor: migrate to apollo server 2
    * chore: add some usage examples
    * fix: wallets query was not filtering by vote
    * test: initial tests on core-graphql package + add to jest

---
+++ packages/core-graphql/__tests__/api/transactions.test.js
@@ -0,0 +1,64 @@
+const app = require('../__support__/setup')
+const utils = require('../__support__/utils')
+
+beforeAll(async () => {
+  await app.setUp()
+})
+
+afterAll(() => {
+  app.tearDown()
+})
+
+describe('GraphQL API { transactions }', () => {
+  describe('GraphQL queries for Transactions - filter by fee', () => {
+    it('should get all transactions with fee = 0', async () => {
+      const query = '{ transactions(filter: { fee: 0 }) { id } }'
+      const response = await utils.request(query)
+
+      utils.expectSuccessful(response)
+      utils.expectResource(response)
+
+      const data = response.data.data
+      expect(data.transactions.length).toBe(100) // because of default limit = 100
+    })
+
+    it('should get no transaction with fee = 987', async () => {
+      const query = '{ transactions(filter: { fee: 987 }) { id } }'
+      const response = await utils.request(query)
+
+      utils.expectSuccessful(response)
+      utils.expectResource(response)
+
+      const data = response.data.data
+      expect(data.transactions.length).toBe(0)
+    })
+  })
+
+  describe.skip('GraphQL queries for Transactions - filter by blockId', () => {
+
+  })
+
+  describe.skip('GraphQL queries for Transactions - filter by senderPublicKey', () => {
+
+  })
+
+  describe.skip('GraphQL queries for Transactions - filter by recipientId', () => {
+
+  })
+
+  describe.skip('GraphQL queries for Transactions - filter by type', () => {
+
+  })
+
+  describe.skip('GraphQL queries for Transactions - using orderBy, limit', () => {
+
+  })
+
+  describe.skip('GraphQL queries for Transactions - testing relationships', () => {
+
+  })
+
+  describe.skip('GraphQL queries for Transactions - testing api errors', () => {
+    // example: filter by column not defined in WalletFilter (lib/defs/inputs.js)
+  })
+})
