88526c366d499dd31702313c8eda883122e5e4fc
fix <fx.thoorens@gmail.com>
2018-09-14 19:03:13 +0200

    reverted changes

--- packages/core/lib/config/mainnet/plugins.js
+++ packages/core/lib/config/mainnet/plugins.js
@@ -43,8 +43,8 @@ module.exports = {
       host: process.env.ARK_DB_HOST || 'localhost',
       port: process.env.ARK_DB_PORT || 5432,
       database: process.env.ARK_DB_DATABASE || `ark_${process.env.ARK_NETWORK_NAME}`,
-      user: process.env.ARK_DB_USERNAME || 'fixcrypt',
-      password: process.env.ARK_DB_PASSWORD || ''
+      user: process.env.ARK_DB_USERNAME || 'ark',
+      password: process.env.ARK_DB_PASSWORD || 'password'
     },
     redis: {
       host: process.env.ARK_REDIS_HOST || 'localhost',
