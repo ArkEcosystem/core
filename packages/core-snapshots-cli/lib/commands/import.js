8fbf541e92a1ea6cb0983d287148f0ec8cc58de6
Kristjan Kosic - Chris <chris@ark.io>
2018-09-18 15:18:53 +0200

    refactor

--- packages/core-snapshots-cli/lib/commands/import.js
+++ packages/core-snapshots-cli/lib/commands/import.js
@@ -12,19 +12,20 @@ const cliProgress = require('cli-progress')
 const { Block, Transaction } = require('@arkecosystem/crypto').models
 
 module.exports = async (options) => {
-  const sourceStream = fs.createReadStream(`${process.env.ARK_PATH_DATA}/snapshots/${process.env.ARK_NETWORK_NAME}/${options.filename}`)
   const progressBbar = new cliProgress.Bar({}, cliProgress.Presets.shades_classic)
-  progressBbar.start(parseInt(options.filename.split('.')[1]), 0) // getting last height from filename
   const writeInterval = 50000
+  const lastDbBlockHeight = !await database.getLastBlock() ? 0 : (await database.getLastBlock()).data.height
+  progressBbar.start(parseInt(options.filename.split('.')[1]), 0) // getting last height from filename
   let block = {}
+  logger.debug(`Last block in database ${lastDbBlockHeight}.`)
 
+  const sourceStream = fs.createReadStream(`${process.env.ARK_PATH_DATA}/snapshots/${process.env.ARK_NETWORK_NAME}/${options.filename}`)
   const pipeline = sourceStream
     .pipe(zlib.createGunzip())
     .pipe(StreamValues.withParser())
 
   const writeQueue = async.queue(async (data, qcallback) => {
     progressBbar.update(data.value.height)
-
     block.data = Block.deserialize(data.value.blockBuffer)
     block.data.id = Block.getId(block.data)
     block.transactions = []
@@ -32,36 +33,35 @@ module.exports = async (options) => {
     if (block.data.transactions) {
       let sequence = 0
       block.transactions = block.data.transactions.map(transaction => {
-      const stampedTransaction = new Transaction(transaction)
-      stampedTransaction.blockId = block.data.id
-      stampedTransaction.timestamp = block.data.timestamp
-      stampedTransaction.sequence = sequence++
-      return stampedTransaction
+        const stampedTransaction = new Transaction(transaction)
+        stampedTransaction.blockId = block.data.id
+        stampedTransaction.timestamp = block.data.timestamp
+        stampedTransaction.sequence = sequence++
+        return stampedTransaction
       })
     }
-
     database.saveBlockAsync(block)
-
-    // committing to db every writeInterval number of blocks
-    if (block.data.height % writeInterval === 0) {
-      await database.saveBlockCommit()
-      pipeline.resume()
-    }
 
     qcallback()
   }, 1)
 
-   pipeline
-    .on('data', (data) => {
-      if (data.value.height % writeInterval === 0) {
-        pipeline.pause()
-      }
-      writeQueue.push(data)
+  pipeline
+  .on('data', async (data) => {
+    // committing to db every writeInterval number of blocks
+    if (data.value.height % writeInterval === 0) {
+      pipeline.pause()
+      await database.saveBlockCommit()
+      pipeline.resume()
+    }
+    if (data.value.height > lastDbBlockHeight) {
+        writeQueue.push(data)
+    }
     })
     .on('end', async () => {
       progressBbar.stop()
       await database.saveBlockCommit()
 
+      // rolling back current round and saving wallets for last state.
       blockchain.stateMachine.state.lastBlock = block
 
       await blockchain.rollbackCurrentRound()
