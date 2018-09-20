5094b47e9b77933cb10c706340e5fa20394b98d9
jaml <dev@jaml.pro>
2018-09-20 18:11:29 +0200

    test: that the API endpoints use the version in the URL

--- packages/client/__tests__/mocks/v2/transactions.js
+++ packages/client/__tests__/mocks/v2/transactions.js
@@ -1,9 +1,9 @@
 module.exports = (mock, host) => {
-  mock.onGet(`${host}/api/transactions`).reply(200, { data: [] })
-  mock.onPost(`${host}/api/transactions`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/transactions/123`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/transactions/unconfirmed`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/transactions/unconfirmed/123`).reply(200, { data: [] })
-  mock.onPost(`${host}/api/transactions/search`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/transactions/types`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/transactions`).reply(200, { data: [] })
+  mock.onPost(`${host}/api/v2/transactions`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/transactions/123`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/transactions/unconfirmed`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/transactions/unconfirmed/123`).reply(200, { data: [] })
+  mock.onPost(`${host}/api/v2/transactions/search`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/transactions/types`).reply(200, { data: [] })
 }
--- packages/client/__tests__/mocks/v2/votes.js
+++ packages/client/__tests__/mocks/v2/votes.js
@@ -1,4 +1,4 @@
 module.exports = (mock, host) => {
-  mock.onGet(`${host}/api/votes`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/votes/123`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/votes`).reply(200, { data: [] })
+  mock.onGet(`${host}/api/v2/votes/123`).reply(200, { data: [] })
 }
