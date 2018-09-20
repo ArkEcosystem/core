42ea13dd50f6034cf7917b5c844953f2099f19f0
jaml <dev@jaml.pro>
2018-09-20 09:54:59 +0200

    test: that the API client sends the GET params

--- packages/client/__tests__/http.test.js
+++ packages/client/__tests__/http.test.js
@@ -93,6 +93,17 @@ describe('API - HTTP Client', () => {
 
       expect(response.config).toHaveAtLeastHeaders(headers)
     })
+
+    it('should send the request params', async () => {
+      const params = { param1: 'value1', param2: 'value2' }
+
+      mock.reset()
+      mock.onGet(`${host}/api/ENDPOINT`, { params }).reply(200, { data: [] })
+
+      const response = await client.get('ENDPOINT', params)
+
+      expect(response.status).toBe(200)
+    })
   })
 
   describe('post', () => {
