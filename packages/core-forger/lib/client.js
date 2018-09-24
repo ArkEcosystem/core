ArkEcosystem/core

commit 3474e414714711dc9682529b96780b08cf3109cd

test: use a mock server to test forger client http calls (#1014)
* use a mock server to test forger Client http calls
* chore: eslint
* spin up core-blockchain until manager tests are refactored.
* chore: address lint and remove noisy comments.

packages/core-forger/lib/client.js
@@ -26,12 +26,9 @@ module.exports = class Client {
    * @return {Object}
    */
   async broadcast (block) {
-    logger.info(`INTERNAL: Sending forged block ${block.id} at height ${block.height.toLocaleString()} with ${block.numberOfTransactions} transactions to ${this.host} :package:`)
+    logger.debug(`Broadcasting forged block id:${block.id} at height:${block.height.toLocaleString()} with ${block.numberOfTransactions} transactions to ${this.host} :package:`)
 
-    return axios.post(`${this.host}/internal/blocks`, { block }, {
-      headers: this.headers,
-      timeout: 2000
-    })
+    return this.__post(`${this.host}/internal/blocks`, { block })
   }
 
   /**
@@ -127,12 +124,7 @@ module.exports = class Client {
     }
 
     try {
-      await axios.post(`${host}/internal/utils/events`, {
-        event, body
-      }, {
-        headers: this.headers,
-        timeout: 2000
-      })
+      await this.__post(`${host}/internal/utils/events`, {event, data: body})
     } catch (error) {
       logger.error(`Failed to emit "${event}" to "${host}"`)
     }
@@ -158,5 +150,9 @@ module.exports = class Client {
 
   async __get (url) {
     return axios.get(url, { headers: this.headers, timeout: 2000 })
+  }
+
+  async __post (url, body) {
+    return axios.post(url, body, { headers: this.headers, timeout: 2000 })
   }
 }

packages/core-forger/__tests__/__support__/setup.js
@@ -10,7 +10,8 @@ exports.setUp = async () => {
     token: 'ark',
     network: 'testnet'
   }, {
-    exit: '@arkecosystem/core-blockchain'
+    exit: '@arkecosystem/core-blockchain',
+    // exclude: '@arkecosystem/core-blockchain'
   })
 }
 

packages/core-forger/__tests__/client.test.js
@@ -2,6 +2,7 @@
 
 const app = require('./__support__/setup')
 const block = require('./__fixtures__/block')
+const nock = require('nock')
 
 jest.setTimeout(30000)
 
@@ -12,15 +13,23 @@ let client
 
 beforeAll(async () => {
   await app.setUp()
+  nock.disableNetConnect()
 })
 
 afterAll(async () => {
   await app.tearDown()
+  nock.enableNetConnect()
 })
 
 beforeEach(() => {
   Client = require('../lib/client')
   client = new Client(host)
+
+  nock(host).get('/peer/status').reply(200)
+})
+
+afterEach(() => {
+  nock.cleanAll()
 })
 
 describe('Client', () => {
@@ -30,12 +39,11 @@ describe('Client', () => {
 
   describe('constructor', () => {
     it('accepts 1 or more hosts as parameter', () => {
-      client = new Client(host)
-      expect(client.hosts).toEqual([host])
+      expect(new Client(host).hosts).toEqual([host])
 
       const hosts = [host, 'http://localhost:4000']
-      client = new Client(hosts)
-      expect(client.hosts).toEqual(hosts)
+
+      expect(new Client(hosts).hosts).toEqual(hosts)
     })
   })
 
@@ -46,6 +54,8 @@ describe('Client', () => {
 
     describe('when the host is available', () => {
       it('should be truthy if broadcasts', async () => {
+        nock(host).post('/internal/blocks', body => body.block.id === block.data.id).reply(200)
+
         await client.__chooseHost()
 
         const wasBroadcasted = await client.broadcast(block.toRawJson())
@@ -61,16 +71,12 @@ describe('Client', () => {
 
     describe('when the host is available', () => {
       it('should be ok', async () => {
-        const round = await client.getRound()
+        const expectedResponse = {'foo': 'bar'}
+        nock(host).get('/internal/rounds/current').reply(200, {data: expectedResponse})
 
-        expect(round).toHaveProperty('current')
-        expect(round).toHaveProperty('reward')
-        expect(round).toHaveProperty('timestamp')
-        expect(round).toHaveProperty('delegates')
-        // expect(round).toHaveProperty('currentForger')
-        // expect(round).toHaveProperty('nextForger')
-        expect(round).toHaveProperty('lastBlock')
-        expect(round).toHaveProperty('canForge')
+        const response = await client.getRound()
+
+        expect(response).toEqual(expectedResponse)
       })
     })
   })
@@ -82,16 +88,13 @@ describe('Client', () => {
 
     describe('when the host is available', () => {
       it('should be ok', async () => {
-        await client.__chooseHost()
+        const expectedResponse = {'foo': 'bar'}
+        nock(host).get('/internal/transactions/forging').reply(200, {data: expectedResponse})
 
+        await client.__chooseHost()
         const response = await client.getTransactions()
 
-        expect(response).toHaveProperty('count')
-        expect(response.count).toBeNumber()
-        expect(response).toHaveProperty('poolSize')
-        expect(response.poolSize).toBeNumber()
-        expect(response).toHaveProperty('transactions')
-        expect(response.transactions).toBeArray()
+        expect(response).toEqual(expectedResponse)
       })
     })
   })
@@ -103,16 +106,13 @@ describe('Client', () => {
 
     describe('when the host is available', () => {
       it('should be ok', async () => {
+        const expectedResponse = {'foo': 'bar'}
+        nock(host).get('/internal/network/state').reply(200, {data: expectedResponse})
+
         await client.__chooseHost()
+        const response = await client.getNetworkState()
 
-        const networkState = await client.getNetworkState()
-
-        expect(networkState).toHaveProperty('quorum')
-        expect(networkState).toHaveProperty('nodeHeight')
-        expect(networkState).toHaveProperty('lastBlockId')
-        expect(networkState).toHaveProperty('overHeightBlockHeader')
-        expect(networkState).toHaveProperty('minimumNetworkReach')
-        expect(networkState).toHaveProperty('coldStart')
+        expect(response).toEqual(expectedResponse)
       })
     })
   })
@@ -120,6 +120,43 @@ describe('Client', () => {
   describe('syncCheck', () => {
     it('should be a function', () => {
       expect(client.syncCheck).toBeFunction()
+    })
+
+    it('should induce network sync', async () => {
+      const action = nock(host).get('/internal/blockchain/sync').reply(200)
+
+      await client.syncCheck()
+
+      expect(action.done())
+    })
+  })
+
+  describe('getUsernames', () => {
+    it('should be a function', () => {
+      expect(client.getUsernames).toBeFunction()
+    })
+
+    it('should fetch usernames', async () => {
+      const expectedResponse = {'foo': 'bar'}
+      nock(host).get('/internal/utils/usernames').reply(200, {data: expectedResponse})
+
+      const response = await client.getUsernames()
+
+      expect(response).toEqual(expectedResponse)
+    })
+  })
+
+  describe('emitEvent', () => {
+    it('should be a function', () => {
+      expect(client.emitEvent).toBeFunction()
+    })
+    it('should emit events', async () => {
+      const action = nock(host).post('/internal/utils/events', body => body.event === 'foo' && body.data === 'bar').reply(200)
+
+      await client.__chooseHost()
+      await client.emitEvent('foo', 'bar')
+
+      expect(action.done())
     })
   })
 })

commit ee203215c36ce596a2577e9f73a1e7f6416519ec

fix: failed to emit event in forger
This was broken by https://github.com/ArkEcosystem/core/commit/3474e414714711dc9682529b96780b08cf3109cd

packages/core-forger/lib/client.js
@@ -124,7 +124,7 @@ module.exports = class Client {
     }
 
     try {
-      await this.__post(`${host}/internal/utils/events`, {event, data: body})
+      await this.__post(`${host}/internal/utils/events`, {event, body})
     } catch (error) {
       logger.error(`Failed to emit "${event}" to "${host}"`)
     }