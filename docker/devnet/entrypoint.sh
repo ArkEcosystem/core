b2002e563f9aa795308fc4ea902864a1c29a24a6
Kristjan Kosic - Chris <chris@ark.io>
2018-10-06 20:13:19 +0200

    fix: adjusting to recent renamings of docker-compose

--- docker/devnet/entrypoint.sh
+++ docker/devnet/entrypoint.sh
@@ -2,9 +2,9 @@
 
 sysctl -w net.ipv4.conf.all.route_localnet=1
 
-POSTGRES=$(ping -c 1 ark-postgres | awk -F'[()]' '/PING/{print $2}')
-REDIS=$(ping -c 1 ark-redis | awk -F'[()]' '/PING/{print $2}')
-CORE=$(ping -c 1 ark-core | awk -F'[()]' '/PING/{print $2}')
+POSTGRES=$(ping -c 1 ark-devnet-postgres | awk -F'[()]' '/PING/{print $2}')
+REDIS=$(ping -c 1 ark-devnet-redis | awk -F'[()]' '/PING/{print $2}')
+CORE=$(ping -c 1 ark-devnet-core | awk -F'[()]' '/PING/{print $2}')
 
 iptables -I OUTPUT -t nat -o lo -d localhost -p tcp --dport 5432 -j DNAT --to-destination ${POSTGRES}:5432
 iptables -I POSTROUTING -t nat -p tcp --dport 5432 -d ${POSTGRES} -j SNAT --to ${CORE}
