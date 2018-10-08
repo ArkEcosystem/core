912fe4d844b9e19ffd0ab7e81e9819255f13379e
Brian Faust <hello@basecode.sh>
2018-10-08 10:22:23 +0300

    feat: forged rewards and fees for delegate (v2 API)

--- packages/core-api/__tests__/v2/handlers/delegates.test.js
+++ packages/core-api/__tests__/v2/handlers/delegates.test.js
@@ -85,4 +85,29 @@ describe('API 2.0 - Delegates', () => {
       utils.expectWallet(response.data.data[0])
     })
   })
+
+  describe('GET /delegates/:id/forged', () => {
+    const assertSuccess = response => {
+      expect(response).toBeSuccessfulResponse()
+      expect(response.data.data).toBeObject()
+      expect(response.data.data.fees).toBeNumber()
+      expect(response.data.data.rewards).toBeNumber()
+      expect(response.data.data.forged).toBeNumber()
+    }
+
+    it('should GET rewards fees by username', async () => {
+      const response = await utils.request('GET', `delegates/${delegate.username}/forged`)
+      assertSuccess(response)
+    })
+
+    it('should GET rewards fees by address', async () => {
+      const response = await utils.request('GET', `delegates/${delegate.address}/forged`)
+      assertSuccess(response)
+    })
+
+    it('should GET rewards fees by publicKey', async () => {
+      const response = await utils.request('GET', `delegates/${delegate.publicKey}/forged`)
+      assertSuccess(response)
+    })
+  })
 })
