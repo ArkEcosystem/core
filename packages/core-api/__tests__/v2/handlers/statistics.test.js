9baf9a51f5195ed1d54c74e769a7d785ea22ba6e
supaiku <supaiku@protonmail.com>
2018-10-09 02:25:46 +0200

    test: statistics

--- packages/core-api/__tests__/v2/handlers/statistics.test.js
+++ packages/core-api/__tests__/v2/handlers/statistics.test.js
@@ -14,80 +14,105 @@ afterAll(async () => {
 
 describe.skip('API 2.0 - Statistics', () => {
   describe('GET /statistics/blockchain', () => {
-    it('should GET the blockchain statistics', async () => {
-      const response = await utils.request('GET', 'statistics/blockchain')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the blockchain statistics', async () => {
+        const response = await utils[request]('GET', 'statistics/blockchain')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.supply).toBeObject()
-      expect(response.data.data.supply.initial).toBeNumber()
-      expect(response.data.data.supply.current).toBeNumber()
+        expect(response.data.data.supply).toBeObject()
+        expect(response.data.data.supply.initial).toBeNumber()
+        expect(response.data.data.supply.current).toBeNumber()
 
-      expect(response.data.data.blocks).toBeObject()
-      expect(response.data.data.blocks.forged).toBeNumber()
-      expect(response.data.data.blocks.rewards).toBeNumber()
+        expect(response.data.data.blocks).toBeObject()
+        expect(response.data.data.blocks.forged).toBeNumber()
+        expect(response.data.data.blocks.rewards).toBeNumber()
 
-      expect(response.data.data.rewards).toBeObject()
-      expect(response.data.data.rewards.start).toBeNumber()
-      expect(response.data.data.rewards.total).toBeNumber()
+        expect(response.data.data.rewards).toBeObject()
+        expect(response.data.data.rewards.start).toBeNumber()
+        expect(response.data.data.rewards.total).toBeNumber()
 
-      expect(response.data.data.productivity).toBeObject()
-      expect(response.data.data.productivity.best.username).toBeString()
-      expect(response.data.data.productivity.best.productivity).toBeString()
+        expect(response.data.data.productivity).toBeObject()
+        expect(response.data.data.productivity.best.username).toBeString()
+        expect(response.data.data.productivity.best.productivity).toBeString()
 
-      expect(response.data.data.productivity.worst).toBeObject()
-      expect(response.data.data.productivity.worst.username).toBeString()
-      expect(response.data.data.productivity.worst.productivity).toBeString()
+        expect(response.data.data.productivity.worst).toBeObject()
+        expect(response.data.data.productivity.worst.username).toBeString()
+        expect(response.data.data.productivity.worst.productivity).toBeString()
+      })
     })
   })
 
   // TODO: big performance impact, think about storing them in memory on boot
   describe('GET /statistics/transactions', () => {
-    it('should GET the transaction statistics', async () => {
-      const response = await utils.request('GET', 'statistics/transactions')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the transaction statistics', async () => {
+        const response = await utils[request]('GET', 'statistics/transactions')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.count).toBeNumber()
-      expect(response.data.data.amount).toBeNumber()
-      expect(response.data.data.fees).toBeNumber()
+        expect(response.data.data.count).toBeNumber()
+        expect(response.data.data.amount).toBeNumber()
+        expect(response.data.data.fees).toBeNumber()
+      })
     })
   })
 
   // TODO: big performance impact, think about storing them in memory on boot
   describe('GET /statistics/blocks', () => {
-    it('should GET the block statistics', async () => {
-      const response = await utils.request('GET', 'statistics/blocks')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the block statistics', async () => {
+        const response = await utils[request]('GET', 'statistics/blocks')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.count).toBeNumber()
-      expect(response.data.data.rewards).toBeNumber()
-      expect(response.data.data.fees).toBeNumber()
+        expect(response.data.data.count).toBeNumber()
+        expect(response.data.data.rewards).toBeNumber()
+        expect(response.data.data.fees).toBeNumber()
+      })
     })
   })
 
   describe('GET /statistics/votes', () => {
-    it('should GET the vote statistics', async () => {
-      const response = await utils.request('GET', 'statistics/votes')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the vote statistics', async () => {
+        const response = await utils[request]('GET', 'statistics/votes')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.count).toBeNumber()
-      expect(response.data.data.amount).toBeNumber()
-      expect(response.data.data.fees).toBeNumber()
+        expect(response.data.data.count).toBeNumber()
+        expect(response.data.data.amount).toBeNumber()
+        expect(response.data.data.fees).toBeNumber()
+      })
     })
   })
 
   describe('GET /statistics/unvotes', () => {
-    it('should GET the unvote statistics', async () => {
-      const response = await utils.request('GET', 'statistics/unvotes')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the unvote statistics', async () => {
+        const response = await utils[request]('GET', 'statistics/unvotes')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.count).toBeNumber()
-      expect(response.data.data.amount).toBeNumber()
-      expect(response.data.data.fees).toBeNumber()
+        expect(response.data.data.count).toBeNumber()
+        expect(response.data.data.amount).toBeNumber()
+        expect(response.data.data.fees).toBeNumber()
+      })
     })
   })
 })
