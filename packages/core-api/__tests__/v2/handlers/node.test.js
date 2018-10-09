2e5dc4b2b8de12b3186a435e850fb01592848554
supaiku <supaiku@protonmail.com>
2018-10-09 02:19:51 +0200

    test: node

--- packages/core-api/__tests__/v2/handlers/node.test.js
+++ packages/core-api/__tests__/v2/handlers/node.test.js
@@ -14,41 +14,56 @@ afterAll(async () => {
 
 describe('API 2.0 - Loader', () => {
   describe('GET /node/status', () => {
-    it('should GET the node status', async () => {
-      const response = await utils.request('GET', 'node/status')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the node status', async () => {
+        const response = await utils[request]('GET', 'node/status')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.synced).toBeBoolean()
-      expect(response.data.data.now).toBeNumber()
-      expect(response.data.data.blocksCount).toBeNumber()
+        expect(response.data.data.synced).toBeBoolean()
+        expect(response.data.data.now).toBeNumber()
+        expect(response.data.data.blocksCount).toBeNumber()
+      })
     })
   })
 
   describe('GET /node/syncing', () => {
-    it('should GET the node syncing status', async () => {
-      const response = await utils.request('GET', 'node/syncing')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the node syncing status', async () => {
+        const response = await utils[request]('GET', 'node/syncing')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.syncing).toBeBoolean()
-      expect(response.data.data.blocks).toBeNumber()
-      expect(response.data.data.height).toBeNumber()
-      expect(response.data.data.id).toBeString()
+        expect(response.data.data.syncing).toBeBoolean()
+        expect(response.data.data.blocks).toBeNumber()
+        expect(response.data.data.height).toBeNumber()
+        expect(response.data.data.id).toBeString()
+      })
     })
   })
 
   describe('GET /node/configuration', () => {
-    it('should GET the node configuration', async () => {
-      const response = await utils.request('GET', 'node/configuration')
-      expect(response).toBeSuccessfulResponse()
-      expect(response.data.data).toBeObject()
+    describe.each([
+      ['API-Version', 'request'],
+      ['Accept', 'requestWithAcceptHeader']
+    ])('using the %s header', (header, request) => {
+      it('should GET the node configuration', async () => {
+        const response = await utils[request]('GET', 'node/configuration')
+        expect(response).toBeSuccessfulResponse()
+        expect(response.data.data).toBeObject()
 
-      expect(response.data.data.nethash).toBeString()
-      expect(response.data.data.token).toBeString()
-      expect(response.data.data.symbol).toBeString()
-      expect(response.data.data.explorer).toBeString()
-      expect(response.data.data.version).toBeNumber()
+        expect(response.data.data.nethash).toBeString()
+        expect(response.data.data.token).toBeString()
+        expect(response.data.data.symbol).toBeString()
+        expect(response.data.data.explorer).toBeString()
+        expect(response.data.data.version).toBeNumber()
+      })
     })
   })
 })
