7ff4ef587029db2c634d3c91ce552f0b576a2d46
Brian Faust <hello@basecode.sh>
2018-10-11 14:15:51 +0300

    style: fix eslint issues

--- packages/crypto/lib/utils/bignumify.js
+++ packages/crypto/lib/utils/bignumify.js
@@ -4,7 +4,7 @@ const Bignum = require('./bignum')
 
 module.exports = (object, keys) => {
   for (const key of keys) {
-    if(!(object[key] instanceof Bignum)) {
+    if (!(object[key] instanceof Bignum)) {
       object[key] = new Bignum(object[key])
     }
   }
