bbb87cfbaa6aca4c005cda9abcb801586dbc97ef
supaiku <supaiku@protonmail.com>
2018-10-11 18:31:21 +0200

    misc: keep EXPONENTIAL_AT default of 20

--- packages/crypto/lib/utils/bignum.js
+++ packages/crypto/lib/utils/bignum.js
@@ -1,11 +1,8 @@
 const BigNumber = require('bignumber.js')
 
+BigNumber.config({DECIMAL_PLACES: 0})
+
 BigNumber.ZERO = new BigNumber(0)
 BigNumber.ONE = new BigNumber(1)
-
-BigNumber.config({
-    DECIMAL_PLACES: 0,
-    EXPONENTIAL_AT: 1e9
-})
 
 module.exports = BigNumber
