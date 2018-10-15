9aaffec12c36cf83b7840e8f2117434a677837c2
Brian Faust <hello@basecode.sh>
2018-10-15 13:21:43 +0300

    fix: graphql test setup

--- packages/core-graphql/__tests__/__support__/setup.js
+++ packages/core-graphql/__tests__/__support__/setup.js
@@ -6,8 +6,6 @@ const containerHelper = require('@arkecosystem/core-test-utils/lib/helpers/conta
 jest.setTimeout(60000)
 
 exports.setUp = async () => {
-  process.env.ARK_GRAPHQL_DISABLED = false
-
   await containerHelper.setUp({
     exclude: [
       '@arkecosystem/core-api',
