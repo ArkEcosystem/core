4561048190d52daa635c5632e4efebb75465a65f
supaiku <supaiku@protonmail.com>
2018-10-15 13:21:04 +0200

    fix: winston file logger format

--- packages/core-logger-winston/lib/defaults.js
+++ packages/core-logger-winston/lib/defaults.js
@@ -14,6 +14,7 @@ module.exports = {
       constructor: 'DailyRotateFile',
       options: {
         level: process.env.ARK_LOG_LEVEL || 'debug',
+        format: require('./formatter'),
         filename: process.env.ARK_LOG_FILE || `${process.env.ARK_PATH_DATA}/logs/core/${process.env.ARK_NETWORK_NAME}/%DATE%.log`,
         datePattern: 'YYYY-MM-DD',
         zippedArchive: true,
