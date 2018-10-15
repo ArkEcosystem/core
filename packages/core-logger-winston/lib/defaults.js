c5f7f7bcfd0a8514fcd41c6136a8265b0d13aa67
Brian Faust <hello@basecode.sh>
2018-10-15 06:28:38 +0300

    fix: add missing constructor props

--- packages/core-logger-winston/lib/defaults.js
+++ packages/core-logger-winston/lib/defaults.js
@@ -3,6 +3,7 @@
 module.exports = {
   transports: {
     console: {
+      constructor: 'Console',
       options: {
         colorize: true,
         level: process.env.ARK_LOG_LEVEL || 'debug',
@@ -10,6 +11,8 @@ module.exports = {
       }
     },
     dailyRotate: {
+      package: 'winston-daily-rotate-file',
+      constructor: 'DailyRotateFile',
       options: {
         filename: process.env.ARK_LOG_FILE || `${process.env.ARK_PATH_DATA}/logs/core/${process.env.ARK_NETWORK_NAME}/%DATE%.log`,
         datePattern: 'YYYY-MM-DD',
