dbb235a6e0a83b465eb78b0f3b17e385abb1159f
supaiku <supaiku@protonmail.com>
2018-10-15 14:28:22 +0200

    fix: add flag to disable colorized output

--- packages/core-logger-winston/lib/formatter.js
+++ packages/core-logger-winston/lib/formatter.js
@@ -7,31 +7,34 @@ const emoji = require('node-emoji')
 
 const { colorize, combine, timestamp, printf } = format
 
-module.exports = combine(
+module.exports = (colorOutput = true) => combine(
   colorize(),
   timestamp(),
   printf(info => {
     const infoLevel = info[Symbol.for('level')]
 
     let level = infoLevel.toUpperCase()
-    level = {
-      'error': chalk.bold.red(level),
-      'warn': chalk.bold.yellow(level),
-      'info': chalk.bold.blue(level),
-      'verbose': chalk.bold.cyan(level),
-      'debug': chalk.bold.white(level),
-      'silly': chalk.bold.magenta(level)
-    }[infoLevel]
-
     let message = emoji.emojify(info.message) || JSON.stringify(info.meta)
-    message = {
-      'error': chalk.bold.bgRed(message),
-      'warn': chalk.bold.black.bgYellow(message),
-      'info': message,
-      'verbose': chalk.bold.cyan(message),
-      'debug': chalk.black.bgWhite(message),
-      'silly': chalk.bold.black.bgWhite(message)
-    }[infoLevel]
+
+    if (colorOutput) {
+      level = {
+        'error': chalk.bold.red(level),
+        'warn': chalk.bold.yellow(level),
+        'info': chalk.bold.blue(level),
+        'verbose': chalk.bold.cyan(level),
+        'debug': chalk.bold.white(level),
+        'silly': chalk.bold.magenta(level)
+      }[infoLevel]
+
+      message = {
+        'error': chalk.bold.bgRed(message),
+        'warn': chalk.bold.black.bgYellow(message),
+        'info': message,
+        'verbose': chalk.bold.cyan(message),
+        'debug': chalk.black.bgWhite(message),
+        'silly': chalk.bold.black.bgWhite(message)
+      }[infoLevel]
+    }
 
     const timestamp = moment(info.timestamp).format('YYYY-MM-DD HH:mm:ss')
 
