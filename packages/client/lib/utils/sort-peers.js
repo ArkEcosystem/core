faa7ab37dd830c8214141300472f68a6df9a8974
faustbrian <faustbrian@users.noreply.github.com>
2018-10-18 05:34:52 +0300

    refactor: clean up the convoluted logic

--- packages/client/lib/utils/sort-peers.js
+++ packages/client/lib/utils/sort-peers.js
@@ -3,14 +3,6 @@ const orderBy = require('lodash.orderby')
 /**
  * Sorts the peers, in place, by block height and delay
  */
-module.exports = peers => {
-  if(myObj.hasOwnProperty('latency')){
-    return orderBy(peers, ['height', 'latency'], ['desc', 'asc'])
-  }
-  else if(myObj.hasOwnProperty('delay')){
-    return orderBy(peers, ['height', 'delay'], ['desc', 'asc'])
-  }
-  else {
-    return peers
-  }
-}
+module.exports = peers => orderBy(peers, [
+  'height', peers.latency ? 'latency' : 'delay'
+], ['desc', 'asc'])
