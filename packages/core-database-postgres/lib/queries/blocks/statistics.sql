880a5f41efa3b3f18a4594338ce9070392810b9d
Brian Faust <hello@basecode.sh>
2018-10-18 07:31:38 +0300

    refactor: check for negative balances

--- packages/core-database-postgres/lib/queries/blocks/statistics.sql
+++ packages/core-database-postgres/lib/queries/blocks/statistics.sql
@@ -1,4 +1,5 @@
 SELECT SUM ("number_of_transactions") AS "numberOfTransactions",
        SUM ("total_fee") AS "totalFee",
-       SUM ("total_amount") AS "totalAmount"
+       SUM ("total_amount") AS "totalAmount",
+       COUNT (DISTINCT "height") AS "count"
 FROM blocks
