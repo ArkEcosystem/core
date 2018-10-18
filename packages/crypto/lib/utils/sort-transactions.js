ca17dca2dc0bfec52b0d1a6b93292d30d2948d7e
kalgoop <17824093+kalgoop@users.noreply.github.com>
2018-10-18 17:55:23 +0530

    Improved the Implementation
    Rewrote code for sorting

--- packages/crypto/lib/utils/sort-transactions.js
+++ packages/crypto/lib/utils/sort-transactions.js
@@ -7,22 +7,10 @@ module.exports = (transactions) => {
   // Map to create a new array (sort is done in place)
   // TODO does it matter modifying the order of the original array
   return transactions.map(t => t).sort((a, b) => {
-    if (a.type < b.type) {
-      return -1
+    if (a.type != b.type) {
+      return a.type - b.type
     }
 
-    if (a.type > b.type) {
-      return 1
-    }
-
-    if (a.id < b.id) {
-      return -1
-    }
-
-    if (a.id > b.id) {
-      return 1
-    }
-
-    return 0
+    return a.id - b.id
   })
 }
