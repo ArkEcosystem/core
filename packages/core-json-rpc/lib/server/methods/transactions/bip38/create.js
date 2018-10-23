ae8c63fa930b8338a8adcc046c43307439175d1b
Air1 <erwann@ark.io>
2018-10-23 17:40:23 +0400

    refactor: use signWithWif for bip38 transaction create

--- packages/core-json-rpc/lib/server/methods/transactions/bip38/create.js
+++ packages/core-json-rpc/lib/server/methods/transactions/bip38/create.js
@@ -1,5 +1,5 @@
 const Joi = require('joi')
-const { crypto, transactionBuilder } = require('@arkecosystem/crypto')
+const { transactionBuilder } = require('@arkecosystem/crypto')
 const database = require('../../../services/database')
 const getBip38Keys = require('../../../utils/bip38-keys')
 
@@ -7,18 +7,13 @@ module.exports = {
   name: 'transactions.bip38.create',
   async method (params) {
     const account = await getBip38Keys(params.userId, params.bip38)
+
     const transaction = transactionBuilder
       .transfer()
       .recipientId(params.recipientId)
       .amount(params.amount)
-      .sign('dummy')
+      .signWithWif(account.wif)
       .getStruct()
-
-    transaction.senderPublicKey = account.keys.publicKey
-
-    delete transaction.signature
-    crypto.sign(transaction, account.keys)
-    transaction.id = crypto.getId(transaction)
 
     await database.set(transaction.id, transaction)
 
