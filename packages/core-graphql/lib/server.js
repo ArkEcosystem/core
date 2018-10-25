f19aa6076b27280f1ca7e3e2962c16282c7863d9
Brian Faust <hello@basecode.sh>
2018-10-25 13:15:09 +0300

    refactor: move server duplications and plugins to core-http-utils

--- packages/core-graphql/lib/server.js
+++ packages/core-graphql/lib/server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const Hapi = require('hapi')
-const logger = require('@arkecosystem/core-container').resolvePlugin('logger')
-const server = require('./schema')
+const { createServer, mountServer, plugins } = require('@arkecosystem/core-http-utils')
+
+const apolloServer = require('./schema')
 
 /**
  * Create a new hapi.js server.
@@ -10,45 +10,22 @@ const server = require('./schema')
  * @return {Hapi.Server}
  */
 module.exports = async (config) => {
-  const app = new Hapi.Server({
+  const server = await createServer({
     host: config.host,
     port: config.port
   })
 
-  /**
-   * Register useful hapi.js plugins, the same are also found
-   * in @arkecosystem/core-api.
-   */
-  await app.register([require('vision'), require('inert'), require('lout')])
+  await server.register({
+    plugin: plugins.whitelist,
+    options: { whitelist: config.whitelist }
+  })
 
-  /**
-   * Register Apollo GraphQL plugin for hapi.js server with
-   * our own parameters.
-   * The bulk of the Ark logic for GraphQL is rooted in the
-   * schema module.
-   */
-  await server.applyMiddleware({
-    app,
+  await apolloServer.applyMiddleware({
+    server,
     path: config.path
   })
 
-  await server.installSubscriptionHandlers(app.listener)
+  await apolloServer.installSubscriptionHandlers(server.listener)
 
-  /**
-   * Start the hapi.js server and return it, exit process if
-   * errors are caught.
-   * The returned hapi.js instance is accessible through:
-   * (@arkecosystem/core-container).resolvePlugin('graphql')
-   */
-  try {
-    await app.start()
-
-    logger.info(`GraphQL API Server running at: ${app.info.uri}`)
-
-    return app
-  } catch (error) {
-    logger.error(error.stack)
-    // TODO no exit here?
-    process.exit(1)
-  }
+  return mountServer('GraphQL', server)
 }
