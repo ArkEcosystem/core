16ea82070ed626f1d291c5190f7637a2a45c5b15
Brian Faust <hello@basecode.sh>
2018-10-26 13:34:40 +0300

    fix(core-p2p): always require p2p headers to be present

--- packages/core-p2p/lib/server/plugins/validate-headers.js
+++ packages/core-p2p/lib/server/plugins/validate-headers.js
@@ -22,10 +22,6 @@ const register = async (server, options) => {
   server.ext({
     type: 'onRequest',
     async method (request, h) {
-      if (!request.path.startsWith('/peer')) {
-        return h.continue
-      }
-
       const errors = ajv.validate({
         type: 'object',
         properties: {
