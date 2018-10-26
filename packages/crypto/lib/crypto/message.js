d1af8f86f5fec1dedc5a8d9c105bb7edd48b4041
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2018-10-26 12:50:44 +0100

    feat: sign message with wif

--- packages/crypto/lib/crypto/message.js
+++ packages/crypto/lib/crypto/message.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto')
 const arkCrypto = require('./crypto')
+const configManager = require('../managers/config')
 
 const createHash = message => crypto
     .createHash('sha256')
@@ -15,6 +16,27 @@ module.exports = class Message {
    */
   static sign (message, passphrase) {
     const keys = arkCrypto.getKeys(passphrase)
+
+    return {
+      publicKey: keys.publicKey,
+      signature: arkCrypto.signHash(createHash(message), keys),
+      message
+    }
+  }
+
+  /**
+   * Sign the given message using a WIF.
+   * @param  {String} message
+   * @param  {String} wif
+   * @param  {Object} network
+   * @return {Object}
+   */
+  static signWithWif (message, wif, network) {
+    if (!network) {
+      network = configManager.all()
+    }
+
+    const keys = arkCrypto.getKeysFromWIF(wif, network)
 
     return {
       publicKey: keys.publicKey,
