ac59c9638183514214c35b83c3daa3f8f0e3f0d3
Brian Faust <hello@basecode.sh>
2018-10-29 07:11:50 +0200

    refactor(core-json-rpc): only allow localhost by default

--- packages/core-json-rpc/lib/defaults.js
+++ packages/core-json-rpc/lib/defaults.js
@@ -5,7 +5,7 @@ module.exports = {
   host: process.env.ARK_JSON_RPC_HOST || '0.0.0.0',
   port: process.env.ARK_JSON_RPC_PORT || 8080,
   allowRemote: false,
-  whitelist: ['127.0.0.1', '::ffff:127.0.0.1', '192.168.*'],
+  whitelist: ['127.0.0.1', '::ffff:127.0.0.1'],
   database: {
     uri: process.env.ARK_JSON_RPC_DATABASE || `sqlite://${process.env.ARK_PATH_DATA}/database/json-rpc.sqlite`,
     options: {}
