7956125c37b6b882c30d589dd95bab4599db9b5a
Brian Faust <hello@basecode.sh>
2018-10-31 06:22:02 +0200

    refactor(core-api): counts start at 0 - not positive

--- packages/core-api/lib/versions/2/schema/delegates.js
+++ packages/core-api/lib/versions/2/schema/delegates.js
@@ -55,14 +55,14 @@ exports.blocks = {
     ...{
       orderBy: Joi.string(),
       id: Joi.string(),
-      version: Joi.number().integer().positive(),
-      timestamp: Joi.number().integer().positive(),
+      version: Joi.number().integer().min(0),
+      timestamp: Joi.number().integer().min(0),
       previousBlock: Joi.string(),
       height: Joi.number().integer().positive(),
-      numberOfTransactions: Joi.number().integer().positive(),
+      numberOfTransactions: Joi.number().integer().min(0),
       totalAmount: Joi.number().integer().positive(),
-      totalFee: Joi.number().integer().positive(),
-      reward: Joi.number().integer().positive(),
+      totalFee: Joi.number().integer().min(0),
+      reward: Joi.number().integer().min(0),
       payloadLength: Joi.number().integer().positive(),
       payloadHash: Joi.string().hex(),
       generatorPublicKey: Joi.string().hex().length(66),
@@ -87,10 +87,10 @@ exports.voters = {
       secondPublicKey: Joi.string().hex().length(66),
       vote: Joi.string().hex().length(66),
       username: Joi.string(),
-      balance: Joi.number().integer().positive(),
-      voteBalance: Joi.number().integer().positive(),
-      producedBlocks: Joi.number().integer().positive(),
-      missedBlocks: Joi.number().integer().positive()
+      balance: Joi.number().integer().min(0),
+      voteBalance: Joi.number().integer().min(0),
+      producedBlocks: Joi.number().integer().min(0),
+      missedBlocks: Joi.number().integer().min(0)
     }
   }
 }
