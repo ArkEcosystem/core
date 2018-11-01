0cc08301b7cd79928c74beae7b047191eae7ca90
Brian Faust <hello@basecode.sh>
2018-11-01 10:30:05 +0200

    feat: add core-elasticsearch package

---
+++ packages/core-elasticsearch/lib/index/wallet.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const container = require('@arkecosystem/core-container')
+const database = container.resolvePlugin('database')
+const logger = container.resolvePlugin('logger')
+const Index = require('./index')
+const client = require('../services/client')
+
+class WalletIndex extends Index {
+  /**
+     * Index wallets using the specified chunk size.
+     * @return {void}
+     */
+  async index () {
+    const { count } = await this.__count()
+
+    const queries = Math.ceil(count / this.chunkSize)
+
+    for (let i = 0; i < queries; i++) {
+      const modelQuery = this.__createQuery()
+
+      const query = modelQuery
+        .select()
+        .from(modelQuery)
+        .limit(this.chunkSize)
+        .offset(this.chunkSize * i)
+
+      const rows = await database.query.manyOrNone(query.toQuery())
+
+      if (!rows.length) {
+        continue
+      }
+
+      logger.info(`[Elasticsearch] Indexing ${rows.length} wallets :card_index_dividers:`)
+
+      try {
+        const wallets = rows.map(row => {
+          row.id = row.address
+
+          delete row.createdAt
+          delete row.updatedAt
+
+          return row
+        })
+
+        await client.bulk(this._buildBulkUpsert(wallets))
+      } catch (error) {
+        logger.error(`[Elasticsearch] ${error.message} :exclamation:`)
+      }
+    }
+  }
+
+  /**
+     * Register listeners for "wallet.*" events.
+     * @return {void}
+     */
+  listen () {
+    setInterval(() => this.index(), 8000)
+  }
+
+  /**
+     * Get the document index.
+     * @return {String}
+     */
+  getIndex () {
+    return 'wallets'
+  }
+
+  /**
+     * Get the document type.
+     * @return {String}
+     */
+  getType () {
+    return 'wallet'
+  }
+}
+
+module.exports = new WalletIndex()
