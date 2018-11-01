b5e2b899ce56d5b50734dbdc731511586cf65539
faustbrian <faustbrian@users.noreply.github.com>
2018-11-01 20:02:57 +0200

    chore: remove client package (#1261)

--- packages/client/__tests__/mocks/v1/delegates.js
+++
@@ -1,9 +0,0 @@
-module.exports = (mock, host) => {
-  mock.onGet(`${host}/api/delegates`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/delegates/count`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/delegates/fee`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/delegates/forging/getForgedByAccount`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/delegates/get`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/delegates/search`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/delegates/voters`).reply(200, { data: [] })
-}
--- packages/client/__tests__/mocks/v2/node.js
+++
@@ -1,5 +0,0 @@
-module.exports = (mock, host) => {
-  mock.onGet(`${host}/api/node/status`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/node/syncing`).reply(200, { data: [] })
-  mock.onGet(`${host}/api/node/configuration`).reply(200, { data: [] })
-}
