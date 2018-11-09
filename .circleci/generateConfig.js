71447c25f0e283fac38fa8136810674d8a87ffc0
air1one <36802613+air1one@users.noreply.github.com>
2018-11-09 11:10:11 +0400

    build: CircleCI improve caching - generate yml config on pre-commit (#1345)
    * chore: generate circleci config on pre-commit
    
    * chore: dummy change, config.yml should not get committed
    
    * chore: pre commit script message when finished

---
+++ .circleci/generateConfig.js
@@ -0,0 +1,19 @@
+const yaml = require('js-yaml')
+const fs   = require('fs')
+
+const config = require('./configTemplate.json')
+
+generateConfig()
+
+function generateConfig() {
+  fs.readdir('./packages', (err, packages) => genYaml({ packages }))
+}
+
+function genYaml(options) {
+  const saveCacheStep = config.jobs['test-node10'].steps.find(step => typeof step === 'object' && step.save_cache)
+  saveCacheStep.save_cache.paths = options.packages.map(package => `./packages/${package}/node_modules`).concat('./node_modules')
+      
+  fs.writeFile('.circleci/config.yml', yaml.safeDump(config), 'utf8', err => {
+    if (err) console.log(err)
+  })
+}
