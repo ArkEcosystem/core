cb5f6e81e50b2e953344e2905afd4c50740f23ab
faustbrian <faustbrian@users.noreply.github.com>
2018-11-09 16:27:20 +0200

    chore: update dependencies (#1331)
    *     chore: update dependencies
    
    *          fix(core-test-utils): adjust blockchain entry helper to xstate v4
    
    * fix(core-blockchain): replace toArray calls with valueSeq.toArray on OrderedMaps
    
    See https://github.com/facebook/immutable-js/pull/1340 and https://github.com/facebook/immutable-js/releases/tag/v4.0.0-rc.5 for more details
    
    * chore: update yarn.lock

--- packages/core-test-utils/lib/matchers/blockchain/execute-on-entry.js
+++ packages/core-test-utils/lib/matchers/blockchain/execute-on-entry.js
@@ -5,11 +5,12 @@ const { isEqual, get } = require('lodash')
 const toExecuteOnEntry = (machine, transition) => {
   let path = transition.state
 
-  // For nested states, but only works 1 level depth
+  // For nested states, but only works 1 level deep
   if (transition.state.indexOf('.') !== -1) {
     const slugs = path.split('.')
     path = `${slugs[0]}.states.${slugs[1]}`
   }
+
   const state = get(machine.states, path)
 
   const actions = transition.actions.map(action => `"${action}"`).join(', ')
@@ -17,8 +18,8 @@ const toExecuteOnEntry = (machine, transition) => {
   return {
     // FIXME isNot is necessary to write the right message
     // @see https://facebook.github.io/jest/docs/en/expect.html#expectextendmatchers
-    message: () => `Expected machine to ${this.isNot ? 'not ' : ''}call actions ${actions} on state "${transition.state}"`,
-    pass: isEqual(state.onEntry, transition.actions)
+    message: () => `Expected machine to ${this.isNot ? 'not ' : ''} call actions ${actions} on state "${transition.state}"`,
+    pass: isEqual(state.onEntry.map(action => action.type), transition.actions)
   }
 }
 
