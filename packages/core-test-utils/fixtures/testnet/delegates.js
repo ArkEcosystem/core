42439e110d99f8142a4c784f5efb826f777ef90c
Brian Faust <hello@basecode.sh>
2018-11-12 09:45:03 +0200

    fix: export data instead of function

--- packages/core-test-utils/fixtures/testnet/delegates.js
+++ packages/core-test-utils/fixtures/testnet/delegates.js
@@ -4,25 +4,24 @@ const { client, crypto } = require('@arkecosystem/crypto')
  * Get the testnet genesis delegates information
  * @return {Array} array of objects like { secret, publicKey, address, balance }
  */
-module.exports = () => {
-  client.getConfigManager().setFromPreset('ark', 'testnet')
 
-  const delegatesConfig = require('../../config/testnet/delegates.json')
-  const genesisTransactions = require('../../config/testnet/genesisBlock.json')
-    .transactions
+client.getConfigManager().setFromPreset('ark', 'testnet')
 
-  return delegatesConfig.secrets.map(secret => {
-    const publicKey = crypto.getKeys(secret).publicKey
-    const address = crypto.getAddress(publicKey)
-    const balance = genesisTransactions.find(
-      transaction =>
-        transaction.recipientId === address && transaction.type === 0,
-    ).amount
-    return {
-      secret,
-      publicKey,
-      address,
-      balance,
-    }
-  })
-}
+const delegatesConfig = require('../../config/testnet/delegates.json')
+const genesisTransactions = require('../../config/testnet/genesisBlock.json')
+  .transactions
+
+module.exports = delegatesConfig.secrets.map(secret => {
+  const publicKey = crypto.getKeys(secret).publicKey
+  const address = crypto.getAddress(publicKey)
+  const balance = genesisTransactions.find(
+    transaction =>
+      transaction.recipientId === address && transaction.type === 0,
+  ).amount
+  return {
+    secret,
+    publicKey,
+    address,
+    balance,
+  }
+})
