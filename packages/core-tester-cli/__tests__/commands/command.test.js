2f1c5607895710359c9cc825827bdbd92533bfa6
whitehat <34144820+whitehat@users.noreply.github.com>
2018-11-17 05:33:39 -0600

    chore(core-tester-cli): move copy command test (#1414)

--- packages/core-tester-cli/__tests__/commands/command.test.js
+++ packages/core-tester-cli/__tests__/commands/command.test.js
@@ -1,3 +1,4 @@
+const clipboardy = require('clipboardy')
 const Command = require('../../lib/commands/command')
 
 let command
@@ -6,6 +7,28 @@ beforeEach(() => {
 })
 
 describe('Command Base', () => {
+  describe('Copy to Clipboard', () => {
+    it('should be a function', () => {
+      expect(command.copyToClipboard).toBeFunction()
+    })
+
+    it('should contain the copied content', () => {
+      command.copyToClipboard([
+        {
+          key: 'value',
+          serialized: '00',
+        },
+      ])
+
+      expect(JSON.parse(clipboardy.readSync())).toEqual([
+        {
+          key: 'value',
+          serialized: '00',
+        },
+      ])
+    })
+  })
+  
   describe('Run', () => {
     it('should be a function', () => {
       expect(command.run).toBeFunction()
--- packages/core-tester-cli/lib/utils/copy-to-clipboard.js
+++
@@ -1,8 +0,0 @@
-const clipboardy = require('clipboardy')
-const pluralize = require('pluralize')
-const logger = require('./logger')
-
-module.exports = transactions => {
-  clipboardy.writeSync(JSON.stringify(transactions))
-  logger.info(`Copied ${pluralize('transaction', transactions.length, true)}`)
-}
