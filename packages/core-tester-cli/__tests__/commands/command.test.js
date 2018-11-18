59d63d9508e40a0786583dcde26e827d487dbd1a
whitehat <34144820+whitehat@users.noreply.github.com>
2018-11-17 23:16:19 -0600

    test(core-tester-cli): init command (#1415)

--- packages/core-tester-cli/__tests__/commands/command.test.js
+++ packages/core-tester-cli/__tests__/commands/command.test.js
@@ -1,12 +1,45 @@
 const clipboardy = require('clipboardy')
+const axios = require('axios')
+const MockAdapter = require('axios-mock-adapter')
 const Command = require('../../lib/commands/command')
+
+const mockAxios = new MockAdapter(axios)
 
 let command
 beforeEach(() => {
   command = new Command()
+  mockAxios.reset()
 })
 
 describe('Command Base', () => {
+  describe('Init', () => {
+    it('should be a function', () => {
+      expect(Command.init).toBeFunction()
+    })
+    it('init w/ option', async () => {
+      mockAxios
+        .onGet('http://test_baseUrl:1234/api/v2/node/configuration')
+        .reply(200, { data: { constants: {} } })
+      mockAxios
+        .onGet('http://test_baseUrl:4321/config')
+        .reply(200, { data: { network: {} } })
+      command = await Command.init({
+        baseUrl: 'http://test_baseUrl',
+        apiPort: 1234,
+        p2pPort: 4321,
+        passphrase: 'test_passphrase',
+        secondPassphrase: 'test_secondPassphrase',
+      })
+      expect(command.config).toContainEntries([
+        ['baseUrl', 'http://test_baseUrl'],
+        ['apiPort', 1234],
+        ['p2pPort', 4321],
+        ['passphrase', 'test_passphrase'],
+        ['secondPassphrase', 'test_secondPassphrase'],
+      ])
+    })
+  })
+  
   describe('Copy to Clipboard', () => {
     it('should be a function', () => {
       expect(command.copyToClipboard).toBeFunction()
