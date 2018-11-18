445cca67826c6e1db37ccb559ca9330f52975636
whitehat <34144820+whitehat@users.noreply.github.com>
2018-11-17 20:17:28 -0600

    test(core-tester-cli): generate wallet command (#1417)

--- packages/core-tester-cli/__tests__/commands/command.test.js
+++ packages/core-tester-cli/__tests__/commands/command.test.js
@@ -40,4 +40,22 @@ describe('Command Base', () => {
       )
     })
   })
+  
+  describe('Generate Wallets', () => {
+    it('should be a function', () => {
+      expect(command.generateWallets).toBeFunction()
+    })
+    it('generate wallets', () => {
+      command.config = {
+        network: {
+          version: 1,
+        },
+      }
+      const wallets = command.generateWallets(10)
+      expect(wallets).toBeArrayOfSize(10)
+      wallets.forEach(wallet => {
+        expect(wallet).toContainAllKeys(['address', 'keys', 'passphrase'])
+      })
+    })
+  })
 })
