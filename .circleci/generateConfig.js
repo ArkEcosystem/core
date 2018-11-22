9ec6cad2e9e7c7e5374f8a6e2235b5ba04b33b5e
Air1 <erwann@ark.io>
2018-11-23 02:52:55 +0400

    chore: WIP re-enable circleci parallelism

--- .circleci/generateConfig.js
+++ .circleci/generateConfig.js
@@ -10,12 +10,27 @@ function generateConfig() {
 }
 
 function genYaml(options) {
-  const saveCacheStep = config.jobs['test-node10'].steps.find(
+  const saveCacheStep = config.jobs['init'].steps.find(
     step => typeof step === 'object' && step.save_cache,
   )
   saveCacheStep.save_cache.paths = options.packages
     .map(package => `./packages/${package}/node_modules`)
     .concat('./node_modules')
+
+  const testJobs = ['test-node10', 'test-node10-2']
+  testJobs.forEach((job, index) => {
+    const testStep = config.jobs[job].steps.find(
+      step => typeof step === 'object' && step.run.name === 'Test',
+    )
+    const chunkSize = Math.ceil(options.packages.length / 2)
+    testStep.run.command = testStep.run.command.replace(
+      '{{TESTPATHS}}',
+      options.packages
+        .map(package => `./packages/${package}`)
+        .slice(index * chunkSize, (index + 1) * chunkSize)
+        .join(' '),
+    )
+  })
 
   fs.writeFile('.circleci/config.yml', yaml.safeDump(config), 'utf8', err => {
     if (err) console.error(err)
