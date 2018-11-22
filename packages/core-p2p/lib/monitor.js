31b158e0c5297e9569c318ed33e2e9f07275e5ff
Brian Faust <hello@basecode.sh>
2018-11-22 17:32:11 +0200

    fix: proper coldstart check

--- packages/core-p2p/lib/monitor.js
+++ packages/core-p2p/lib/monitor.js
@@ -734,7 +734,7 @@ class Monitor {
    * not all peers are up, or the network is not active
    */
   __isColdStartActive() {
-    return this.startForgers > dayjs().unix()
+    return this.startForgers.isAfter(dayjs())
   }
 
   /**
