e03caa2d5a4b577b4e24469e41cb53e1e558ac30
Air1 <erwann@ark.io>
2018-11-22 18:58:49 +0400

    feat: core-test-utils allow to generate tx with 2nd sign + refactor

--- packages/core-test-utils/lib/matchers/transactions/valid-second-signature.js
+++ packages/core-test-utils/lib/matchers/transactions/valid-second-signature.js
@@ -2,11 +2,7 @@ const { crypto } = require('@arkecosystem/crypto')
 
 const toHaveValidSecondSignature = (actual, expected) => ({
   message: () => 'Expected value to have a valid second signature',
-  pass: crypto.verifySecondSignature(
-    actual,
-    expected.publicKey,
-    expected.network,
-  ),
+  pass: crypto.verifySecondSignature(actual, expected.publicKey),
 })
 
 expect.extend({
