c40678a5c324d1d92c97776ffd9ee3669f850d93
Brian Faust <hello@basecode.sh>
2018-11-23 05:49:11 +0200

    fix: timestamp format

--- packages/core-utils/lib/format-timestamp.js
+++ packages/core-utils/lib/format-timestamp.js
@@ -8,9 +8,7 @@ const container = require('@arkecosystem/core-container')
  */
 module.exports = epochStamp => {
   const constants = container.resolvePlugin('config').getConstants(1)
-  const timestamp = dayjs(constants.epoch)
-    .add(epochStamp, 'seconds')
-    .utc()
+  const timestamp = dayjs(constants.epoch).add(epochStamp, 'seconds')
 
   return {
     epoch: epochStamp,
