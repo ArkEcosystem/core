d394b4ee7056005176557b2f4f3f9416586163bc
Edgar Goetzendorff <hello@dated.fun>
2018-11-27 12:27:15 +0100

    refactor(core-vote-report): configurable row count and formatting (#1517)
    * refactor(core-vote-report): add delegateRows option
    
    * refactor(core-vote-report): number formatting and alignment

--- packages/core-vote-report/lib/defaults.js
+++ packages/core-vote-report/lib/defaults.js
@@ -1,4 +1,5 @@
 module.exports = {
   host: process.env.ARK_VOTE_REPORT_HOST || '0.0.0.0',
   port: process.env.ARK_VOTE_REPORT_PORT || 4006,
+  delegateRows: process.env.ARK_VOTE_REPORT_DELEGATE_ROWS || 80,
 }
