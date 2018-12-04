81f414ae65b6cdab290cae085babba9b4366a7f9
Vasil Dimov <vd@freebsd.org>
2018-12-04 18:34:04 +0100

    perf(core-database-postgres): add sender&receiver indexes (#1638)
    Add indexes on transactions's sender_public_key and recipient_id
    columns.
    
    Impact on queries:
    
    SELECT count(*) AS count FROM transactions WHERE
    sender_public_key = '... many matches ...' OR
    recipient_id = '... many matches ...';
    ~230ms -> 230ms (no change)
    
    SELECT count(*) AS count FROM transactions WHERE
    sender_public_key = '... many matches ...' OR
    recipient_id = '... few matches ...';
    ~230ms -> 90ms
    
    SELECT count(*) AS count FROM transactions WHERE
    sender_public_key = '... few matches ...' OR
    recipient_id = '... many matches ...';
    ~230ms -> 70ms
    
    SELECT count(*) AS count FROM transactions WHERE
    sender_public_key = '... few matches ...' OR
    recipient_id = '... few matches ...';
    ~230ms -> 50ms

--- packages/core-database-postgres/lib/migrations/index.js
+++ packages/core-database-postgres/lib/migrations/index.js
@@ -8,4 +8,5 @@ module.exports = [
   loadQueryFile(__dirname, './20181129400000-add-block_id-index-to-transactions-table.sql'),
   loadQueryFile(__dirname, './20181204100000-blocks-generator_public_key-index.sql'),
   loadQueryFile(__dirname, './20181204200000-transactions-timestamp-index.sql'),
+  loadQueryFile(__dirname, './20181204300000-transactions-sender-and-receiver-indexes.sql'),
 ]
