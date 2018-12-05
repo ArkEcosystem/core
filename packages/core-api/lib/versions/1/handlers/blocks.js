6c8c4429ee19d0335bb9fbf2a0bff5888e35b84b
supaiku <supaiku@protonmail.com>
2018-12-05 04:04:00 +0100

    refactor: use supply calculator vote report

--- packages/core-api/lib/versions/1/handlers/blocks.js
+++ packages/core-api/lib/versions/1/handlers/blocks.js
@@ -1,5 +1,5 @@
 const app = require('@arkecosystem/core-container')
-const { bignumify } = require('@arkecosystem/core-utils')
+const { supplyCalculator } = require('@arkecosystem/core-utils')
 
 const config = app.resolvePlugin('config')
 const blockchain = app.resolvePlugin('blockchain')
@@ -184,16 +184,7 @@ exports.supply = {
    */
   handler(request, h) {
     const lastBlock = blockchain.getLastBlock()
-    const constants = config.getConstants(lastBlock.data.height)
-    const rewards = bignumify(constants.reward).times(
-      lastBlock.data.height - constants.height,
-    )
-
-    return utils.respondWith({
-      supply: +bignumify(config.genesisBlock.totalAmount)
-        .plus(rewards)
-        .toFixed(),
-    })
+    return supplyCalculator(lastBlock.data.height).toFixed()
   },
 }
 
@@ -209,9 +200,6 @@ exports.status = {
   handler(request, h) {
     const lastBlock = blockchain.getLastBlock()
     const constants = config.getConstants(lastBlock.data.height)
-    const rewards = bignumify(constants.reward).times(
-      lastBlock.data.height - constants.height,
-    )
 
     return utils.respondWith({
       epoch: constants.epoch,
@@ -220,9 +208,7 @@ exports.status = {
       milestone: Math.floor(lastBlock.data.height / 3000000),
       nethash: config.network.nethash,
       reward: constants.reward,
-      supply: +bignumify(config.genesisBlock.totalAmount)
-        .plus(rewards)
-        .toFixed(),
+      supply: supplyCalculator(lastBlock.data.height).toFixed(),
     })
   },
 }
