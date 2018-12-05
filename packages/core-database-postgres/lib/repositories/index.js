b4e4d5661d8afd5d743d933a9f636459b52aecb3
Brian Faust <faustbrian@users.noreply.github.com>
2018-12-05 12:51:12 +0200

    feat(core-database-postgres): store executed migrations in the database (#1648)

--- packages/core-database-postgres/lib/repositories/index.js
+++ packages/core-database-postgres/lib/repositories/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   blocks: require('./blocks'),
+  migrations: require('./migrations'),
   rounds: require('./rounds'),
   transactions: require('./transactions'),
   wallets: require('./wallets'),
