fe0f93bfb78cbe66b97fb4a61e29a45036f7fda2
Brian Faust <faustbrian@users.noreply.github.com>
2018-12-05 10:22:44 +0200

    chore: update changelogs and bump versions (#1642)

--- packages/core-api/lib/versions/2/handlers/blockchain.js
+++ packages/core-api/lib/versions/2/handlers/blockchain.js
@@ -1,5 +1,5 @@
 const app = require('@arkecosystem/core-container')
-const { bignumify } = require('@arkecosystem/core-utils')
+const { bignumify, supplyCalculator } = require('@arkecosystem/core-utils')
 
 const config = app.resolvePlugin('config')
 const blockchain = app.resolvePlugin('blockchain')
@@ -16,20 +16,13 @@ exports.index = {
   handler(request, h) {
     const lastBlock = blockchain.getLastBlock()
 
-    const constants = config.getConstants(lastBlock.data.height)
-    const rewards = bignumify(constants.reward).times(
-      lastBlock.data.height - constants.height,
-    )
-
     return {
       data: {
         block: {
           height: lastBlock.data.height,
           id: lastBlock.data.id,
         },
-        supply: +bignumify(config.genesisBlock.totalAmount)
-          .plus(rewards)
-          .toFixed(),
+        supply: supplyCalculator.calculate(lastBlock.data.height),
       },
     }
   },
