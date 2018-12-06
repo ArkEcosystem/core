880d989a82dad299d7cae0dd24532343f67519d8
Brian Faust <hello@basecode.sh>
2018-12-06 10:03:40 +0200

    chore: update root jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,19 @@
 module.exports = {
+  testEnvironment: 'node',
   bail: false,
   verbose: true,
-  testEnvironment: 'node',
-  testMatch: ['**/packages/**/__tests__/**/*.test.js'],
-  moduleFileExtensions: ['js', 'json'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/packages/**/__tests__/**/*.test.(js|ts)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: false,
   coverageDirectory: '<rootDir>/.coverage',
-  collectCoverageFrom: ['packages/**/lib/**/*.js', '!**/node_modules/**'],
+  collectCoverageFrom: [
+    'packages/**/lib/**/*.js',
+    'packages/**/src/**/*.js',
+    '!**/node_modules/**',
+  ],
   watchman: false,
   setupTestFrameworkScriptFile: 'jest-extended',
 }
