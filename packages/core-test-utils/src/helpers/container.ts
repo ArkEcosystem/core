c9fdbaba38b77f502c977cccffdcdd3206fe18d8
supaiku <supaiku@protonmail.com>
2018-12-07 20:43:14 +0100

    fix(core-test-utils): wrong config path

--- packages/core-test-utils/src/helpers/container.ts
+++ packages/core-test-utils/src/helpers/container.ts
@@ -1,17 +1,19 @@
 import { app } from "@arkecosystem/core-container";
 import * as path from "path";
 
-export async function setUpContainer(options: any): Promise<any> {
-  return app.setUp(
-    "2.0.0",
-    {
-      data: options.data || "~/.ark",
-      config: options.config
-        ? options.config
-        : path.resolve(__dirname, "../../config/testnet"),
-      token: options.token || "ark",
-      network: options.network || "testnet"
-    },
-    options
-  );
+export default {
+  setup: async (options: any): Promise<any> => {
+    return app.setUp(
+      "2.0.0",
+      {
+        data: options.data || "~/.ark",
+        config: options.config
+          ? options.config
+          : path.resolve(__dirname, "../config/testnet"),
+        token: options.token || "ark",
+        network: options.network || "testnet"
+      },
+      options
+    );
+  }
 }
