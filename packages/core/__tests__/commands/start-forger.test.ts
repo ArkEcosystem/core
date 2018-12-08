38008d32d7ad0f23b5cfeacf150a982a79b88868
Brian Faust <hello@basecode.sh>
2018-12-08 15:33:22 +0200

    test(core): set timeout and force exit tests

--- packages/core/__tests__/commands/start-forger.test.ts
+++ packages/core/__tests__/commands/start-forger.test.ts
@@ -1,6 +1,9 @@
 import "jest-extended";
+import delay from "delay";
 import { startRelay, startForger } from "../../src/commands";
 import { opts, version } from "../__support__/app";
+
+jest.setTimeout(60000);
 
 describe("Commands - Start Forger", () => {
   it("should be a function", () => {
@@ -19,5 +22,7 @@ describe("Commands - Start Forger", () => {
 
     expect(forger.isReady).toBeFalse();
     expect(relay.isReady).toBeFalse();
+
+    await delay(3000);
   });
 });
--- packages/core/__tests__/commands/start-relay.test.ts
+++ packages/core/__tests__/commands/start-relay.test.ts
@@ -1,6 +1,9 @@
 import "jest-extended";
+import delay from "delay";
 import { startRelay } from "../../src/commands";
 import { opts, version } from "../__support__/app";
+
+jest.setTimeout(60000);
 
 describe("Commands - Start Relay", () => {
   it("should be a function", () => {
@@ -15,5 +18,7 @@ describe("Commands - Start Relay", () => {
     await app.tearDown();
 
     expect(app.isReady).toBeFalse();
+
+    await delay(3000);
   });
 });
