9deafbfec213afdb430c51496ac8237947ff96cd
jeremiG <gendronjeremi@gmail.com>
2018-12-11 11:39:35 -0500

    check contructor errors and bip38 functionality

--- packages/crypto/__tests__/models/delegate.test.js
+++ packages/crypto/__tests__/models/delegate.test.js
@@ -1,38 +1,80 @@
-const Bignum = require('../../lib/utils/bignum')
-const Wallet = require('../../lib/models/wallet')
-const { ARKTOSHI } = require('../../lib/constants')
-const sortTransactions = require('../../lib/utils/sort-transactions')
-const configManager = require('../../lib/managers/config')
+const bip38 = require('bip38')
 
-describe('Models - Delegate', () => {
-  describe('static sortTransactions', () => {
-    it('returns the transactions ordered by type and id', () => {
-      const ordered = [
-        { type: 1, id: 2 },
-        { type: 1, id: 8 },
-        { type: 2, id: 5 },
-        { type: 2, id: 9 },
-      ]
-      const unordered = [ordered[3], ordered[2], ordered[1], ordered[0]]
+const { keys } = require('@arkecosystem/crypto').identities
+const { Delegate } = require('@arkecosystem/crypto').models
+const { TESTNET } = require('@arkecosystem/crypto').constants.CONFIGURATIONS.ARK
 
-      expect(sortTransactions(unordered)).toEqual(ordered)
+const passphrase = 'this is a top secret passphrase'
+const password = '123'
+
+describe('Delegate Model - Crypto', () => {
+  describe('Instantiation scenarios', () => {
+    it('Should throw when creating an empty Delegate', () => {
+      expect(() => {
+        const delegate = new Delegate()
+      }).toThrow()
+    })
+
+    it('Should fail when providing passphrase without network', () => {
+      expect(() => {
+        const delegate = new Delegate(passphrase)
+      }).toThrow()
+    })
+
+    it('Should create a new Delegate when provided a passphrase and network', () => {
+      const delegate = new Delegate(passphrase, TESTNET)
+      expect(delegate.keys).toHaveProperty('privateKey')
+      expect(delegate.network).toHaveProperty('name', 'testnet')
     })
   })
 
-  describe('forge', () => {
-    describe('without version option', () => {
-      it("doesn't sort the transactions", () => {
-        const address = 'Abcde'
-        const wallet = new Wallet(address)
-        wallet.balance = new Bignum(ARKTOSHI)
+  describe('BIP38 Encryption with password', () => {
+    let encryptedPassphrase
+    let decryptedKeys
+    let delegate
 
-        expect(wallet.toString()).toBe(
-          `${address} (1 ${configManager.config.client.symbol})`,
-        )
-      })
+    it('Should return a valid encrypted passphrase', () => {
+      encryptedPassphrase = Delegate.encryptPassphrase(
+        passphrase,
+        TESTNET,
+        password,
+      )
 
-      // TODO probably useful for debugging
-      it('throws an Error', () => {})
+      expect(bip38.verify(encryptedPassphrase)).toBe(true)
+    })
+
+    it('Should decrypt into the original passphrase', () => {
+      decryptedKeys = Delegate.decryptPassphrase(
+        encryptedPassphrase,
+        TESTNET,
+        password,
+      )
+
+      expect(keys.fromPassphrase(passphrase).privateKey).toBe(
+        decryptedKeys.privateKey,
+      )
+    })
+
+    it('Should create a delegate with the encrypted passphrase, network and password', () => {
+      delegate = new Delegate(encryptedPassphrase, TESTNET, password)
+
+      expect(delegate.keys).toBe(null)
+      expect(delegate).toHaveProperty('encryptedKeys')
+      expect(delegate.network).toHaveProperty('name', 'testnet')
+    })
+
+    it('Should decrypt into keys using encryptedKeys and otp', () => {
+      delegate.decryptKeysWithOtp()
+
+      expect(decryptedKeys).toEqual(delegate.keys)
+    })
+
+    it('Should catch missing arguments and set keys to null', () => {
+      const localDelegate = new Delegate(encryptedPassphrase)
+
+      expect(localDelegate.address).toBe(null)
+      expect(localDelegate.publicKey).toBe(null)
+      expect(localDelegate.keys).toBe(null)
     })
   })
 })
