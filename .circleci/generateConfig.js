38bbf2b1096ffef1e44e7154a3c791ce7764a1b8
Brian Faust <hello@basecode.sh>
2018-12-13 12:49:48 +0200

    chore: force exit tests to avoid hanging

--- .circleci/generateConfig.js
+++ .circleci/generateConfig.js
@@ -1,111 +1,109 @@
-const yaml = require('js-yaml')
-const fs = require('fs')
-const path = require('path')
+const yaml = require("js-yaml");
+const fs = require("fs");
+const path = require("path");
 
-const config = require('./configTemplate.json')
+const config = require("./configTemplate.json");
 
-generateConfig()
+generateConfig();
 
 function generateConfig() {
-  fs.readdir('./packages', (err, packages) => genYaml({ packages }))
+    fs.readdir("./packages", (err, packages) => genYaml({ packages }));
 }
 
 function genYaml(options) {
-  // save cache
-  const saveCacheStep = config.jobs['test-node10-0'].steps.find(
-    step => typeof step === 'object' && step.save_cache,
-  )
-  saveCacheStep.save_cache.paths = options.packages
-    .map(package => `./packages/${package}/node_modules`)
-    .concat('./node_modules')
+    // save cache
+    const saveCacheStep = config.jobs["test-node10-0"].steps.find(step => typeof step === "object" && step.save_cache);
+    saveCacheStep.save_cache.paths = options.packages
+        .map(package => `./packages/${package}/node_modules`)
+        .concat("./node_modules");
 
-  // test split
-  const packagesSplit = splitPackagesByTestFiles(options.packages, 3)
+    // test split
+    const packagesSplit = splitPackagesByTestFiles(options.packages, 3);
 
-  const jobs = [
-    config.jobs['test-node10-0'],
-    JSON.parse(JSON.stringify(config.jobs['test-node10-0'])),
-    JSON.parse(JSON.stringify(config.jobs['test-node10-0'])),
-  ]
+    const jobs = [
+        config.jobs["test-node10-0"],
+        JSON.parse(JSON.stringify(config.jobs["test-node10-0"])),
+        JSON.parse(JSON.stringify(config.jobs["test-node10-0"])),
+    ];
 
-  jobs.forEach((job, index) => {
-    const testStepIndex = job.steps.findIndex(step => typeof step === 'object' && step.run && step.run.name === 'Test')
+    jobs.forEach((job, index) => {
+        const testStepIndex = job.steps.findIndex(
+            step => typeof step === "object" && step.run && step.run.name === "Test",
+        );
 
-    const pkgs = packagesSplit[index].map(package => `./packages/${package}/`)
+        const pkgs = packagesSplit[index].map(package => `./packages/${package}/`);
 
-    const steps = pkgs.map(pkg => {
-        const name = path.basename(pkg)
+        const steps = pkgs
+            .map(pkg => {
+                const name = path.basename(pkg);
 
-        return {
-            run: {
-                name,
-                command: `cd ~/ark-core/packages/${name} && yarn test`
-            }
+                return {
+                    run: {
+                        name,
+                        command: `cd ~/ark-core/packages/${name} && yarn test`,
+                    },
+                };
+            })
+            .filter(pkg => {
+                const { scripts } = require(path.resolve(__dirname, `../packages/${pkg.run.name}/package.json`));
+
+                return Object.keys(scripts).includes("test");
+            });
+
+        const stepLog = job.steps[9];
+        const stepCoverage = job.steps[10];
+
+        for (i = 0; i < steps.length; i++) {
+            job.steps[testStepIndex + i] = steps[i];
         }
-    }).filter(pkg => {
-        const { scripts } = require(path.resolve(__dirname, `../packages/${pkg.run.name}/package.json`))
 
-        return Object.keys(scripts).includes("test")
-    })
+        job.steps.push(stepLog);
+        job.steps.push(stepCoverage);
 
-    const stepLog = job.steps[9]
-    const stepCoverage = job.steps[10]
+        config.jobs[`test-node10-${index}`] = job;
+        config.workflows.build_and_test.jobs.push(`test-node10-${index}`);
+    });
 
-    for (i = 0; i < steps.length; i++) {
-        job.steps[testStepIndex + i] = steps[i];
-    }
-
-    job.steps.push(stepLog)
-    job.steps.push(stepCoverage)
-
-    config.jobs[`test-node10-${index}`] = job
-    config.workflows.build_and_test.jobs.push(`test-node10-${index}`)
-  })
-
-  fs.writeFile('.circleci/config.yml', yaml.safeDump(config), 'utf8', err => {
-    if (err) console.error(err)
-  })
+    fs.writeFile(".circleci/config.yml", yaml.safeDump(config), "utf8", err => {
+        if (err) console.error(err);
+    });
 }
 
 function splitPackagesByTestFiles(packages, splitNumber) {
-  /* distribute test packages by test files count : start by most files package,
+    /* distribute test packages by test files count : start by most files package,
      and distribute package by package in each _packagesSplit_ (not the most effective
      distribution but simple and enough for now) */
-  const packagesWithCount = packages.map(package => ({
-    package,
-    count: countFiles(`packages/${package}/__tests__`, '.test.js'),
-  }))
-  const packagesSortedByCount = packagesWithCount.sort(
-    (pkgA, pkgB) => pkgA.count > pkgB.count,
-  )
+    const packagesWithCount = packages.map(package => ({
+        package,
+        count: countFiles(`packages/${package}/__tests__`, ".test.js"),
+    }));
+    const packagesSortedByCount = packagesWithCount.sort((pkgA, pkgB) => pkgA.count > pkgB.count);
 
-  const packagesSplit = new Array(splitNumber)
-  packagesSortedByCount.forEach(
-    (pkg, index) =>
-      (packagesSplit[index % splitNumber] = [pkg.package].concat(
-        packagesSplit[index % splitNumber] || [],
-      )),
-  )
+    const packagesSplit = new Array(splitNumber);
+    packagesSortedByCount.forEach(
+        (pkg, index) =>
+            (packagesSplit[index % splitNumber] = [pkg.package].concat(packagesSplit[index % splitNumber] || [])),
+    );
 
-  return packagesSplit
+    return packagesSplit;
 }
 
 function countFiles(startPath, filter) {
-  let count = 0
-  if (!fs.existsSync(startPath)) {
-    return
-  }
-
-  var files = fs.readdirSync(startPath)
-  for (let i = 0; i < files.length; i++) {
-    const filename = path.join(startPath, files[i])
-    const stat = fs.lstatSync(filename)
-    if (stat.isDirectory()) {
-      count += countFiles(filename, filter)
-    } else if (filename.indexOf(filter) >= 0) {
-      count++
+    let count = 0;
+    if (!fs.existsSync(startPath)) {
+        return;
     }
-  }
 
-  return count
+    var files = fs.readdirSync(startPath);
+    for (let i = 0; i < files.length; i++) {
+        const filename = path.join(startPath, files[i]);
+        const stat = fs.lstatSync(filename);
+        if (stat.isDirectory()) {
+            count += countFiles(filename, filter);
+        } else if (filename.indexOf(filter) >= 0) {
+            count++;
+        }
+    }
+
+    return count;
 }
