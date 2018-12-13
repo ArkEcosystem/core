c2d27c2ac6f82e9945896e71f2470c4a4ab842fa
Brian Faust <hello@basecode.sh>
2018-12-13 13:49:18 +0200

    chore: run crypto heavy tests in their own job on CircleCI

--- .circleci/generateConfig.js
+++ .circleci/generateConfig.js
@@ -4,13 +4,56 @@ const path = require("path");
 
 const config = require("./configTemplate.json");
 
+const slowPerformance = ['core-json-rpc', 'crypto'];
+
 generateConfig();
 
-function generateConfig() {
-    fs.readdir("./packages", (err, packages) => genYaml({ packages }));
+function jason(value) {
+    return JSON.parse(JSON.stringify(value));
 }
 
-function genYaml(options) {
+function generateConfig() {
+    fs.readdir("./packages", (err, packages) => generateYAML({ packages }));
+}
+
+function createJob (name, steps, config) {
+    const job = jason(config.jobs[name]);
+
+    const testStepIndex = job.steps.findIndex(
+        step => typeof step === "object" && step.run && step.run.name === "Test",
+    );
+
+    const stepLog = jason(job.steps[9]);
+    const stepCoverage = jason(job.steps[10]);
+
+    for (i = 0; i < steps.length; i++) {
+        job.steps[testStepIndex + i] = steps[i];
+    }
+
+    job.steps.push(stepLog);
+    job.steps.push(stepCoverage);
+
+    config.jobs[name.slice(0,-1) + 'slow'] = job;
+    config.workflows.build_and_test.jobs.push(name.slice(0,-1) + 'slow');
+}
+
+function createSlowJob (name, config) {
+    const slowPerformanceSteps = slowPerformance.map(pkg => {
+        return {
+            run: {
+                name: pkg,
+                command: `cd ~/ark-core/packages/${pkg} && yarn test:coverage`,
+            },
+        };
+    });
+
+    createJob(name, slowPerformanceSteps, config)
+}
+
+function generateYAML(options) {
+    // test split
+    const packagesSplit = splitPackagesByTestFiles(options.packages, 3);
+
     for(const [name, job] of Object.entries(config.jobs)) {
         // save cache
         const saveCacheStep = config.jobs[name].steps.find(step => typeof step === "object" && step.save_cache);
@@ -18,14 +61,13 @@ function genYaml(options) {
             .map(package => `./packages/${package}/node_modules`)
             .concat("./node_modules");
 
-        // test split
-        const packagesSplit = splitPackagesByTestFiles(options.packages, 3);
-
         const jobs = [
             config.jobs[name],
-            JSON.parse(JSON.stringify(config.jobs[name])),
-            JSON.parse(JSON.stringify(config.jobs[name])),
+            jason(config.jobs[name]),
+            jason(config.jobs[name]),
         ];
+
+        createSlowJob(name, config)
 
         jobs.forEach((job, index) => {
             const testStepIndex = job.steps.findIndex(
@@ -41,14 +83,14 @@ function genYaml(options) {
                     return {
                         run: {
                             name,
-                            command: `cd ~/ark-core/packages/${name} && yarn test`,
+                            command: `cd ~/ark-core/packages/${name} && yarn test:coverage`,
                         },
                     };
                 })
                 .filter(pkg => {
                     const { scripts } = require(path.resolve(__dirname, `../packages/${pkg.run.name}/package.json`));
 
-                    return Object.keys(scripts).includes("test");
+                    return Object.keys(scripts).includes("test:coverage");
                 });
 
             const stepLog = job.steps[9];
@@ -78,14 +120,14 @@ function splitPackagesByTestFiles(packages, splitNumber) {
     const packagesWithCount = packages.map(package => ({
         package,
         count: countFiles(`packages/${package}/__tests__`, ".test.js"),
-    }));
+    })).filter(item => {
+        return !slowPerformance.includes(item.package)
+    })
+
     const packagesSortedByCount = packagesWithCount.sort((pkgA, pkgB) => pkgA.count > pkgB.count);
 
     const packagesSplit = new Array(splitNumber);
-    packagesSortedByCount.forEach(
-        (pkg, index) =>
-            (packagesSplit[index % splitNumber] = [pkg.package].concat(packagesSplit[index % splitNumber] || [])),
-    );
+    packagesSortedByCount.forEach((pkg, index) => (packagesSplit[index % splitNumber] = [pkg.package].concat(packagesSplit[index % splitNumber] || [])));
 
     return packagesSplit;
 }
