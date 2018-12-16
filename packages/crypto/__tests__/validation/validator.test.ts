790642fb16161814b1833b648aa751849c78901c
jeremigendron <gendronjeremi@gmail.com>
2018-12-16 11:38:40 -0500

    rm private methods & unused import from crypto/validation/validator

--- packages/crypto/__tests__/validation/validator.test.ts
+++ packages/crypto/__tests__/validation/validator.test.ts
@@ -1,5 +1,4 @@
 import "jest-extended";
-import Joi from "joi";
 import { validator } from "../../src/validation";
 
 beforeEach(() => {
@@ -72,82 +71,6 @@ describe("Validator", () => {
             validator.extend("fake", "news");
 
             expect(validator.rules).toHaveProperty("fake");
-        });
-    });
-
-    describe("__validateWithRule", () => {
-        it("should be true", () => {
-            validator.__validateWithRule("DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN", "address");
-
-            expect(validator.passes()).toBeTrue();
-        });
-
-        it("should be false", () => {
-            validator.__validateWithRule("_DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN_", "address");
-
-            expect(validator.passes()).toBeFalse();
-        });
-    });
-
-    describe("__validateWithFunction", () => {
-        it("should be true", () => {
-            validator.__validateWithFunction("DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN", value => ({
-                data: value,
-                passes: value.length === 34,
-                fails: value.length !== 34,
-            }));
-
-            expect(validator.passes()).toBeTrue();
-        });
-
-        it("should be false", () => {
-            validator.__validateWithFunction("_DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN_", value => ({
-                data: value,
-                passes: value.length === 34,
-                fails: value.length !== 34,
-            }));
-
-            expect(validator.passes()).toBeFalse();
-        });
-    });
-
-    describe("__validateWithJoi", () => {
-        it("should be true", () => {
-            validator.__validateWithJoi(
-                "DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN",
-                Joi.string()
-                    .alphanum()
-                    .length(34)
-                    .required(),
-            );
-
-            expect(validator.passes()).toBeTrue();
-        });
-
-        it("should be false", () => {
-            validator.__validateWithJoi(
-                "_DARiJqhogp2Lu6bxufUFQQMuMyZbxjCydN_",
-                Joi.string()
-                    .alphanum()
-                    .length(34)
-                    .required(),
-            );
-
-            expect(validator.passes()).toBeFalse();
-        });
-    });
-
-    describe("__reset", () => {
-        it("should be empty", () => {
-            validator.results = {
-                key: "value",
-            };
-
-            expect(validator.results).not.toBeNull();
-
-            validator.__reset();
-
-            expect(validator.results).toBeNull();
         });
     });
 });
