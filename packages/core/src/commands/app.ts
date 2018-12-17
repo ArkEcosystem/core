1ba24500efbc144fb1e3a68b4c61b021ef7fd2a6
Brian Faust <hello@basecode.sh>
2018-12-17 11:17:24 +0200

    refactor(core): check npm for a new package version if on mainnet

--- packages/core/src/commands/app.ts
+++ packages/core/src/commands/app.ts
@@ -1,25 +1,45 @@
 import axios from "axios";
 import dayjs from "dayjs-ext";
 import getRepoInfo from "git-repo-info";
+import latestVersion from "latest-version";
+import prompts from "prompts";
 import semver from "semver";
+
+async function performUpdate(options, callback) {
+    const response = await prompts([
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "A new version is available, would you like to update now?",
+            initial: true,
+        },
+    ]);
+
+    if (response.confirm) {
+        callback(options);
+    }
+}
 
 export async function update(options) {
     const baseUrl = "https://api.github.com/repos/ArkEcosystem/core";
 
     if (options.network === "mainnet") {
-        const response = await axios.get(`${baseUrl}/releases/latest`);
-        const latestRelease = response.data.tag_name;
+        const latestRelease = await latestVersion("@arkecosystem/core");
 
         if (semver.gt(latestRelease, options.parent._version)) {
-            // update available...
+            return performUpdate(options, () => {
+                // update via yarn
+            });
         }
     } else {
-        const response = await axios.get(`${baseUrl}/commits`);
+        const response = await axios.get(`https://api.github.com/repos/ArkEcosystem/core/commits`);
         const lastCommit = dayjs(response.data[0].commit.author.date);
         const currentCommit = dayjs(getRepoInfo().authorDate);
 
         if (lastCommit.isAfter(currentCommit)) {
-            // update available...
+            return performUpdate(options, () => {
+                // update via git
+            });
         }
     }
 }
