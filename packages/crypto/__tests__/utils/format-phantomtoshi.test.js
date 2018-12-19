506fb4448914c1b23b5e3e70c55cb4773b8042cb
PHANTOM-DEV1 <phantomdev1@outlook.com>
2018-12-20 05:26:50 +0800

    changes on ticker

--- packages/crypto/__tests__/utils/format-phantomtoshi.test.js
+++ packages/crypto/__tests__/utils/format-phantomtoshi.test.js
@@ -3,12 +3,12 @@ const { PHANTOMTOSHI } = require('../../lib/constants')
 
 describe('Format Phantomtoshi', () => {
   it('should format phantomtoshis', () => {
-    expect(formatPhantomtoshi(PHANTOMTOSHI)).toBe('1 Ẕ')
-    expect(formatPhantomtoshi(0.1 * PHANTOMTOSHI)).toBe('0.1 Ẕ')
-    expect(formatPhantomtoshi((0.1 * PHANTOMTOSHI).toString())).toBe('0.1 Ẕ')
-    expect(formatPhantomtoshi(new Bignum(10))).toBe('0.0000001 Ẕ')
+    expect(formatPhantomtoshi(PHANTOMTOSHI)).toBe('1 ⓟ')
+    expect(formatPhantomtoshi(0.1 * PHANTOMTOSHI)).toBe('0.1 ⓟ')
+    expect(formatPhantomtoshi((0.1 * PHANTOMTOSHI).toString())).toBe('0.1 ⓟ')
+    expect(formatPhantomtoshi(new Bignum(10))).toBe('0.0000001 ⓟ')
     expect(formatPhantomtoshi(new Bignum(PHANTOMTOSHI + 10012))).toBe(
-      '1.00010012 Ẕ',
+      '1.00010012 ⓟ',
     )
   })
 })
