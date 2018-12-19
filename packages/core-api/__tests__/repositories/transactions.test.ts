e8af07ffa3b291df24b485b2cdbfafa0f5d9fc63
jaml <dev@jaml.pro>
2018-12-19 20:14:46 +0100

    test(core-api): searching transactions by `addresses` on the repository

--- packages/core-api/__tests__/repositories/transactions.test.ts
+++ packages/core-api/__tests__/repositories/transactions.test.ts
@@ -104,6 +104,198 @@ describe("Transaction Repository", () => {
             await expectSearch({ recipientId: genesisTransaction.recipientId }, 2);
         });
 
+        describe("when searching by `senderPublicKey` and `recipientId`", () => {
+            it("should search transactions by sent by `senderPublicKey` to `recipientId`", async () => {
+                const senderPublicKey = genesisTransaction.senderPublicKey;
+                const recipientId = genesisBlock.transactions[2].recipientId;
+
+                let transactions = await repository.search({
+                    recipientId,
+                    senderPublicKey,
+                });
+
+                await expectSearch(transactions, 1);
+
+                for (const row of transactions.rows) {
+                    expect(row.senderPublicKey).toEqual(senderPublicKey);
+                    expect(row.recipientId).toEqual(recipientId);
+                }
+
+                transactions = await repository.search({
+                    recipientId: "unknown",
+                    senderPublicKey,
+                });
+
+                await expectSearch(transactions, 0);
+            });
+        });
+
+        describe("when searching by `senderId` and `recipientId`", () => {
+            it("should search transactions by sent by `senderId` to `recipientId`", async () => {
+                const senderId = crypto.getAddress(genesisTransaction.senderPublicKey, 23);
+                const recipientId = genesisBlock.transactions[2].recipientId;
+
+                let transactions = await repository.search({
+                    recipientId,
+                    senderId,
+                });
+
+                await expectSearch(transactions, 1);
+
+                for (const row of transactions.rows) {
+                    expect(row.senderPublicKey).toEqual(genesisTransaction.senderPublicKey);
+                    expect(row.recipientId).toEqual(recipientId);
+                }
+
+                transactions = await repository.search({
+                    recipientId: "unknown",
+                    senderId,
+                });
+
+                await expectSearch(transactions, 0);
+            });
+        });
+
+        describe("`addresses`", () => {
+            const addresses = [genesisBlock.transactions[1].recipientId, genesisBlock.transactions[4].recipientId];
+
+            it("should search transactions by the specified `addresses` (sender and recipient)", async () => {
+                await expectSearch({ addresses: [addresses[0]] }, 3);
+
+                await expectSearch({ addresses }, 6);
+            });
+
+            describe("when `addresses` is empty", () => {
+                it("should return all transactions", async () => {
+                    await expectSearch({ address: [] }, 153);
+                });
+            });
+
+            describe("when searching by `addresses` and `senderId`", () => {
+                it("should search transactions by the `addresses`, but only include those received from `senderId`", async () => {
+                    const senderId = crypto.getAddress(genesisTransaction.senderPublicKey, 23);
+
+                    let transactions = await repository.search({
+                        senderId,
+                        addresses,
+                    });
+
+                    await expectSearch(transactions, 2);
+
+                    for (const row of transactions.rows) {
+                        expect(row.senderPublicKey).toEqual(genesisTransaction.senderPublicKey);
+                    }
+
+                    transactions = await repository.search({
+                        senderId: "unknown",
+                        addresses,
+                    });
+
+                    await expectSearch(transactions, 0);
+                });
+            });
+
+            describe("when searching by `addresses` and `senderPublicKey`", () => {
+                it("should search transactions by the `addresses`, but only include those received from `senderPublicKey`", async () => {
+                    const { senderPublicKey } = genesisTransaction;
+
+                    let transactions = await repository.search({
+                        senderPublicKey,
+                        addresses,
+                    });
+
+                    await expectSearch(transactions, 2);
+
+                    for (const row of transactions.rows) {
+                        expect(row.senderPublicKey).toEqual(senderPublicKey);
+                    }
+
+                    transactions = await repository.search({
+                        senderPublicKey: "unknown",
+                        addresses,
+                    });
+
+                    await expectSearch(transactions, 0);
+                });
+            });
+
+            describe("when searching by `addresses` and `recipientId`", () => {
+                it("should search transactions by the `addresses`, but only include those sent to `recipientId`", async () => {
+                    const senderId = crypto.getAddress(genesisTransaction.senderPublicKey, 23);
+                    const recipientId = genesisBlock.transactions[2].recipientId;
+
+                    let transactions = await repository.search({
+                        recipientId,
+                        addresses: [senderId],
+                    });
+
+                    await expectSearch(transactions, 1);
+
+                    for (const row of transactions.rows) {
+                        expect(row.recipientId).toEqual(recipientId);
+                    }
+
+                    transactions = await repository.search({
+                        recipientId: "unknown",
+                        addresses,
+                    });
+
+                    await expectSearch(transactions, 0);
+                });
+            });
+
+            describe("when searching by `addresses`, `senderId` and `recipientId`", () => {
+                it("should search transactions by `senderId` and `recipientId` only", async () => {
+                    const senderId = crypto.getAddress(genesisTransaction.senderPublicKey, 23);
+                    const params = {
+                        senderId,
+                        recipientId: genesisTransaction.recipientId,
+                        addresses,
+                    };
+                    const transactions = await repository.search(params);
+
+                    await expectSearch(transactions, 1);
+
+                    const { rows } = transactions;
+                    expect(rows[0].senderPublicKey).toEqual(genesisTransaction.senderPublicKey);
+                    expect(rows[0].recipientId).toEqual(genesisTransaction.recipientId);
+                });
+            });
+
+            describe("when searching by `addresses`, `senderPublicKey` and `recipientId`", () => {
+                it("should search transactions by `senderPublicKey` and `recipientId` only", async () => {
+                    const params = {
+                        senderPublicKey: genesisTransaction.senderPublicKey,
+                        recipientId: genesisTransaction.recipientId,
+                        addresses,
+                    };
+                    const transactions = await repository.search(params);
+
+                    await expectSearch(transactions, 1);
+
+                    const { rows } = transactions;
+                    expect(rows[0].senderPublicKey).toEqual(genesisTransaction.senderPublicKey);
+                    expect(rows[0].recipientId).toEqual(genesisTransaction.recipientId);
+                });
+            });
+
+            describe("when searching by `addresses` and other field`", () => {
+                it("should search transactions by the specified `addresses` and that field", async () => {
+                    const amount = 245098000000000;
+                    const transactions = await repository.search({
+                        amount: { from: amount },
+                        addresses,
+                    });
+
+                    await expectSearch(transactions, 2);
+
+                    for (const row of transactions.rows) {
+                        expect(row.amount).toEqual(amount.toString());
+                    }
+                });
+            });
+        });
+
         it("should search transactions by the specified `timestamp`", async () => {
             await expectSearch(
                 {
@@ -152,8 +344,6 @@ describe("Transaction Repository", () => {
 
         describe("when no results", () => {
             it("should not return them", async () => {
-                // await connection.saveBlock(genesisBlock)
-
                 const transactions = await repository.search({ recipientId: "dummy" });
                 expect(transactions).toBeObject();
 
