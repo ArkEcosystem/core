311c561bebaf9fd337bf1a3e42346434d08d27a1
tekhit <ispoonedi@gmail.com>
2018-12-20 13:30:59 -0600

    refactor: use core-container/logger types in core-tracker-bugsnag

--- packages/core-error-tracker-bugsnag/src/index.ts
+++ packages/core-error-tracker-bugsnag/src/index.ts
@@ -1,3 +1,4 @@
+import { Container } from "@arkecosystem/core-container";
 import bugsnag from "@bugsnag/js";
 import { defaults } from "./defaults";
 
@@ -5,7 +6,7 @@ export const plugin = {
     pkg: require("../package.json"),
     defaults,
     alias: "error-tracker",
-    async register(container, options) {
+    async register(container: Container, options) {
         return bugsnag(options);
     },
 };
