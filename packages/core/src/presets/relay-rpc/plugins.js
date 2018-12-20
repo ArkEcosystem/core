27d1e7e8c0ec1305e5ae4698f41e347fffbea738
Brian Faust <hello@basecode.sh>
2018-12-20 11:17:49 +0200

    feat(core): add basic presets

---
+++ packages/core/src/presets/relay-rpc/plugins.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "@arkecosystem/core-event-emitter": {},
+    "@arkecosystem/core-logger-winston": {},
+    "@arkecosystem/core-database-postgres": {},
+    "@arkecosystem/core-transaction-pool": {},
+    "@arkecosystem/core-p2p": {},
+    "@arkecosystem/core-blockchain": {},
+    "@arkecosystem/core-json-rpc": {},
+    "@arkecosystem/core-snapshots": {}
+};
