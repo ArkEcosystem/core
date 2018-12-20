90d0e438206513d6efbc7c46f369d094bcf2613a
tekhit <ispoonedi@gmail.com>
2018-12-20 14:33:30 -0600

    refactor: use core-container/logger types in core-snapshots

--- packages/core-snapshots/src/db/utils/index.ts
+++ packages/core-snapshots/src/db/utils/index.ts
@@ -1,9 +1,10 @@
+import { AbstractLogger } from "@arkecosystem/core-logger";
 import path from "path";
 import { QueryFile } from "pg-promise";
 
 import { app } from "@arkecosystem/core-container";
 
-const logger = app.resolvePlugin("logger");
+const logger = app.resolvePlugin<AbstractLogger>("logger");
 
 export const loadQueryFile = (directory, file) => {
     const fullPath = path.join(directory, file);
@@ -18,7 +19,7 @@ export const loadQueryFile = (directory, file) => {
     const query = new QueryFile(fullPath, options);
 
     if (query.error) {
-        logger.error(query.error);
+        logger.error(query.error.toString());
     }
 
     return query;
