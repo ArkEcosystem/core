8edc81bb5e92235dbf614c0a1136e8d59502c8dd
zillionn <ts.denev@netsense.bg>
2018-12-22 15:52:26 +0200

    fix: tests

--- packages/crypto/__tests__/utils/format-arktoshi.test.ts
+++ packages/crypto/__tests__/utils/format-arktoshi.test.ts
@@ -5,10 +5,10 @@ import { Bignum, formatArktoshi } from "../../src/utils";
 
 describe("Format Arktoshi", () => {
     it("should format arktoshis", () => {
-        expect(formatArktoshi(ARKTOSHI)).toBe("1 DѦ");
-        expect(formatArktoshi(0.1 * ARKTOSHI)).toBe("0.1 DѦ");
-        expect(formatArktoshi((0.1 * ARKTOSHI).toString())).toBe("0.1 DѦ");
-        expect(formatArktoshi(new Bignum(10))).toBe("0.0000001 DѦ");
-        expect(formatArktoshi(new Bignum(ARKTOSHI + 10012))).toBe("1.00010012 DѦ");
+      expect(formatArktoshi(ARKTOSHI)).toBe("DѦ0.00000001");
+      expect(formatArktoshi(0.1 * ARKTOSHI)).toBe("DѦ0.10000000");
+      expect(formatArktoshi((0.1 * ARKTOSHI).toString())).toBe("DѦ0.10000000");
+      expect(formatArktoshi(new Bignum(10))).toBe("DѦ0.00000010");
+      expect(formatArktoshi(new Bignum(ARKTOSHI + 10012))).toBe("DѦ1.00010012");
     });
 });
