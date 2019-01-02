4d1b1892e0885fe1d01b85de08c9e2fee9dcf256
Brian Faust <hello@basecode.sh>
2019-01-02 15:00:13 +0200

    feat(core): add copyright command

---
+++ packages/core/src/commands/copyright.ts
@@ -0,0 +1,39 @@
+import { flags } from "@oclif/command";
+import { BaseCommand } from "./command";
+
+export class RelayStop extends BaseCommand {
+    public static description: string = "Stop the relay";
+
+    public static examples: string[] = [
+        `Show the copyright notice
+$ ark copyright
+`,
+    ];
+
+    public async run(): Promise<void> {
+        const copyright = `
+The MIT License (MIT)
+
+Copyright (c) Ark Ecosystem <info@ark.io>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+`.trim();
+
+        console.log(copyright);
+    }
+}
