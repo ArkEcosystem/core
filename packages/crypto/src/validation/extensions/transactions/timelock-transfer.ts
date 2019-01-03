f182c95017a1c1e50ae6e42acfa68289fba0b460
Brian Faust <hello@basecode.sh>
2019-01-03 15:06:29 +0200

    fix(crypto): add vendor field to timelock transfer extension

--- packages/crypto/src/validation/extensions/transactions/timelock-transfer.ts
+++ packages/crypto/src/validation/extensions/transactions/timelock-transfer.ts
@@ -13,6 +13,15 @@ export const timelockTransfer = joi => ({
             .try(joi.bignumber().only(0), joi.number().only(0))
             .optional(),
         asset: joi.object().required(),
+        vendorFieldHex: joi
+            .string()
+            .max(64, "hex")
+            .optional(),
+        vendorField: joi
+            .string()
+            .max(64, "utf8")
+            .allow("", null)
+            .optional(), // TODO: remove in 2.1
         recipientId: joi.empty(),
     }),
 });
