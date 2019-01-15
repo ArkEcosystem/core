3a10c9682b4d5f5b13ac6f96ab49f385b73a57d5
jeremiG <gendronjeremi@gmail.com>
2019-01-14 19:56:07 -0500

    add linted file from other package

--- packages/crypto/__tests__/builder/transactions/multi-payment.test.ts
+++ packages/crypto/__tests__/builder/transactions/multi-payment.test.ts
@@ -1,18 +1,18 @@
 import "jest-extended";
+import { MultiPaymentBuilder } from "../../../dist/builder";
 import { client as ark } from "../../../dist/client";
 import { TransactionTypes } from "../../../dist/constants";
 import { feeManager } from "../../../dist/managers/fee";
 import { transactionBuilder } from "./__shared__/transaction-builder";
-import { MultiPaymentBuilder } from "../../../dist/builder";
 
-let builder : MultiPaymentBuilder;
+let builder: MultiPaymentBuilder;
 
 beforeEach(() => {
     builder = ark.getBuilder().multiPayment();
 });
 
 describe("Multi Payment Transaction", () => {
-    transactionBuilder( () => builder);
+    transactionBuilder(() => builder);
 
     it("should have its specific properties", () => {
         expect(builder).toHaveProperty("data.type", TransactionTypes.MultiPayment);
