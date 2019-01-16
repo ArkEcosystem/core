491852e92e5cba2dcbd3a85334a25dee75e09f1a
Brian Faust <faustbrian@users.noreply.github.com>
2019-01-16 14:59:51 +0200

    fix(core): temporarily add data and config flags

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -15,6 +15,8 @@ function registerCommand(name: string, description: string): any {
     return app
         .command(name)
         .description(description)
+        .option("-d, --data <data>", "data directory", "~/.core")
+        .option("-c, --config <config>", "core config", "~/.core/config")
         .option("-t, --token <token>", "token name")
         .option("-n, --network <network>", "token network")
         .option("-r, --remote <remote>", "remote peer for config")
