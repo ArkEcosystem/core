ea8d6e4ff1f906ab4ef77343d0d8f614cad39a51
Air1 <erwann@ark.io>
2019-01-17 15:42:30 +0400

    test: setup core-graphql plugin manually

--- packages/core-graphql/__tests__/__support__/setup.ts
+++ packages/core-graphql/__tests__/__support__/setup.ts
@@ -1,18 +1,30 @@
 import { app } from "@arkecosystem/core-container";
-import { setUpContainer } from "@arkecosystem/core-test-utils/src/helpers/container";
+import { registerWithContainer, setUpContainer } from "../../../core-test-utils/src/helpers/container";
 
 jest.setTimeout(60000);
+
+const options = {
+    enabled: true,
+    host: "0.0.0.0",
+    port: 4005,
+};
 
 export const setUp = async () => {
     process.env.CORE_GRAPHQL_ENABLED = "true";
 
     await setUpContainer({
-        exclude: ["@arkecosystem/core-api", "@arkecosystem/core-forger"],
+        exclude: ["@arkecosystem/core-api", "@arkecosystem/core-forger", "@arkecosystem/core-graphql"],
     });
+
+    const { plugin } = require("../../src");
+    await registerWithContainer(plugin, options);
 
     return app;
 };
 
 export const tearDown = async () => {
     await app.tearDown();
+
+    const { plugin } = require("../../src");
+    await plugin.deregister(app, options);
 };
