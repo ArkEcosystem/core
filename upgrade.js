333535e68f658e130a1f3990666814e920ec5378
Brian Faust <hello@basecode.sh>
2019-01-18 09:55:48 +0200

    feat: delete pm2 processes

--- upgrade.js
+++ upgrade.js
@@ -1,11 +1,35 @@
 const envPaths = require("env-paths");
 const expandHomeDir = require('expand-home-dir');
 const fs = require('fs-extra');
+const pm2 = require('pm2');
 
-// TODO: kill & delete all pm2 processes
+// Delete all pm2 processes created by commander
+function deletePM2(name) {
+    pm2.connect(connectionError => {
+        if (connectionError) {
+            console.error(connectionError);
+            process.exit(2);
+        }
+
+        pm2.delete(processName, deleteError => {
+            pm2.disconnect();
+
+            if (deleteError) {
+                throw deleteError;
+            }
+        });
+    });
+}
+
+deletePM2('ark-core-relay');
+deletePM2('ark-core-forger');
+deletePM2('core-relay');
+deletePM2('core-forger');
 
 // Paths
-const corePaths = envPaths("ark", { suffix: "core" });
+const corePaths = envPaths("ark", {
+    suffix: "core"
+});
 
 const paths = {
     core: {
@@ -35,7 +59,9 @@ for (const value of Object.values(paths)) {
     if (fs.existsSync(value.old)) {
         fs.ensureDirSync(value.new)
 
-        fs.moveSync(value.old, value.new)
+        fs.moveSync(value.old, value.new, {
+            overwrite: true
+        });
     }
 }
 
