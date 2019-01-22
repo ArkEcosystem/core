aad84f6dafb2f10980c04f8ea9561e7dab0699d2
Air1 <erwann@ark.io>
2019-01-22 12:59:08 +0400

    refactor: update crypto transaction validators

--- packages/crypto/src/validation/rules/models/transactions/delegate-registration.ts
+++ packages/crypto/src/validation/rules/models/transactions/delegate-registration.ts
@@ -17,21 +17,23 @@ export const delegateRegistration = transaction => {
                 .integer()
                 .min(0)
                 .required(),
-            amount: Engine.joi.alternatives().try(
-                Engine.joi.bignumber(),
-                Engine.joi
-                    .number()
-                    .valid(0)
-                    .required(),
-            ),
-            fee: Engine.joi.alternatives().try(
-                Engine.joi.bignumber(),
-                Engine.joi
-                    .number()
-                    .integer()
-                    .positive()
-                    .required(),
-            ),
+            amount: Engine.joi
+                .alternatives()
+                .try(Engine.joi.bignumber().only(0), Engine.joi.number().valid(0))
+                .required(),
+            fee: Engine.joi
+                .alternatives()
+                .try(
+                    Engine.joi
+                        .bignumber()
+                        .integer()
+                        .positive(),
+                    Engine.joi
+                        .number()
+                        .integer()
+                        .positive(),
+                )
+                .required(),
             senderId: Engine.joi.address(),
             recipientId: Engine.joi.empty(),
             senderPublicKey: Engine.joi.publicKey().required(),
