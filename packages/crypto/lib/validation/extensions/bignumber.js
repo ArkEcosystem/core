f882207ce407cd3a92e10360b0c0910fffd2022f
Brian Faust <faustbrian@users.noreply.github.com>
2019-01-28 06:38:30 +0200

    fix(crypto): add missing bignumber rules

--- packages/crypto/lib/validation/extensions/bignumber.js
+++ packages/crypto/lib/validation/extensions/bignumber.js
@@ -4,39 +4,72 @@ module.exports = joi => ({
   name: 'bignumber',
   base: joi.object().type(Bignum),
   language: {
-    min: 'is lower than minimum',
+    min: 'is less than minimum',
+    max: 'is greater than maximum',
     only: 'is different from allowed value',
+    integer: 'is not an integer',
+    positive: 'is not positive',
   },
   rules: [
     {
       name: 'min',
       params: {
-        q: joi.number().required(),
+          q: joi.number().required(),
       },
       validate(params, value, state, options) {
-        if (value.isLessThan(params.q)) {
-          return this.createError('bignumber.min', { v: value }, state, options)
-        }
+          if (value.isLessThan(params.q)) {
+              return this.createError('bignumber.min', { v: value }, state, options);
+          }
 
-        return value
+          return value;
       },
     },
     {
-      name: 'only',
+      name: 'max',
       params: {
-        q: joi.number().required(),
+          q: joi.number().required(),
       },
       validate(params, value, state, options) {
-        if (!value.isEqualTo(params.q)) {
-          return this.createError(
-            'bignumber.only',
-            { v: value },
-            state,
-            options,
-          )
-        }
+          if (value.isGreaterThan(params.q)) {
+              return this.createError('bignumber.max', { v: value }, state, options);
+          }
 
-        return value
+          return value;
+      },
+    },
+    {
+        name: 'only',
+        params: {
+            q: joi.number().required(),
+        },
+        validate(params, value, state, options) {
+            if (!value.isEqualTo(params.q)) {
+                return this.createError('bignumber.only', { v: value }, state, options);
+            }
+
+            return value;
+        },
+    },
+    {
+      name: 'integer',
+      params: {},
+      validate(_, value, state, options) {
+          if (!value.isInteger()) {
+              return this.createError('bignumber.integer', { v: value }, state, options);
+          }
+
+          return value;
+      },
+    },
+    {
+      name: 'positive',
+      params: {},
+      validate(_, value, state, options) {
+          if (!value.isPositive() || value.isZero()) {
+              return this.createError('bignumber.positive', { v: value }, state, options);
+          }
+
+          return value;
       },
     },
   ],
