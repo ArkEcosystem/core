36f96dd0efd703d07209d67fb4bb4c6e0cbbd822
Brian Faust <faustbrian@users.noreply.github.com>
2019-01-28 12:44:26 +0200

    chore: delete pm2 processes before upgrading to avoid config issues

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-pm2 stop ark-core > /dev/null 2>&1
-pm2 stop ark-core-relay > /dev/null 2>&1
-pm2 stop ark-core-forger > /dev/null 2>&1
+pm2 delete ark-core > /dev/null 2>&1
+pm2 delete ark-core-relay > /dev/null 2>&1
+pm2 delete ark-core-forger > /dev/null 2>&1
 
-pm2 stop core > /dev/null 2>&1
-pm2 stop core-relay > /dev/null 2>&1
-pm2 stop core-forger > /dev/null 2>&1
+pm2 delete core > /dev/null 2>&1
+pm2 delete core-relay > /dev/null 2>&1
+pm2 delete core-forger > /dev/null 2>&1
 
 node ./scripts/upgrade/upgrade.js
 
