322a755d5424f208d4d04332f175edce02b26651
Adrian Kerchev <akerchev@gmail.com>
2019-01-28 14:29:02 -0500

    db exists detect

--- install.sh
+++ install.sh
@@ -287,7 +287,7 @@ if [[ "$choice" =~ ^(yes|y|Y) ]]; then
     read -p "Enter the database name: " databaseName
 
     userExists=$(sudo -i -u postgres psql -c "SELECT * FROM pg_user WHERE usename = '${databaseUsername}'" | grep -c "1 row")
-    databaseExists=$(sudo -i -u postgres psql -l | grep "${databaseName}" | wc -l)
+    databaseExists=$(sudo -i -u postgres psql -l | grep "^ ${databaseName}" | wc -l)
 
     if [[ $userExists == 1 ]]; then
         read -p "The database user ${databaseUsername} already exists, do you want to overwrite it? [y/N]: " choice
