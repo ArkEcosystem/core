ef563b78a89e0dda15a9827155efa6233be92579
Brian Faust <faustbrian@users.noreply.github.com>
2019-02-01 23:14:52 +0200

    refactor(core-debugger-cli): replace commander.js with @oclif (#2049)
    * refactor(core-debugger-cli): replace commander.js with @oclif
    
    * chore: remove empty dev dependencies
    
    * chore: update yarn.lock
    
    * chore(core-debugger-cli): add @oclif/config dependency
    
    * chore(core-debugger-cli): remove commander.js dependency

--- packages/core-debugger-cli/__tests__/commands/identity.test.ts
+++ packages/core-debugger-cli/__tests__/commands/identity.test.ts
@@ -1,11 +1,11 @@
 import "jest-extended";
 
-import { identity } from "../../src/commands/identity";
+import { IdentityCommand } from "../../src/commands/identity";
 
-describe("Commands - Identity", () => {
+describe("Commands - Identity", async () => {
     const fixtureIdentities = require("../__fixtures__/identities.json");
 
-    it("should return identities from passphrase", () => {
+    it("should return identities from passphrase", async () => {
         const expected = {
             passphrase: "this is a top secret passphrase",
             publicKey: "034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192",
@@ -13,44 +13,31 @@ describe("Commands - Identity", () => {
             address: "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib",
         };
 
-        expect(
-            identity({
-                data: fixtureIdentities.passphrase,
-                type: "passphrase",
-            }),
-        ).toEqual(expected);
+        expect(await IdentityCommand.run(["--data", fixtureIdentities.passphrase, "--type", "passphrase"])).toEqual(
+            expected,
+        );
     });
 
-    it("should return identities from privateKey", () => {
+    it("should return identities from privateKey", async () => {
         const expected = {
             publicKey: "034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192",
             privateKey: "d8839c2432bfd0a67ef10a804ba991eabba19f154a3d707917681d45822a5712",
             address: "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib",
         };
 
-        expect(
-            identity({
-                data: fixtureIdentities.privateKey,
-                type: "privateKey",
-            }),
-        ).toEqual(expected);
+        expect(await IdentityCommand.run(["--data", fixtureIdentities.privateKey, "--type", "privateKey"])).toEqual(
+            expected,
+        );
     });
 
-    it("should return identities from publicKey", () => {
+    it("should return identities from publicKey", async () => {
         const expected = {
             publicKey: "034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192",
             address: "D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib",
         };
 
-        expect(
-            identity({
-                data: fixtureIdentities.publicKey,
-                type: "publicKey",
-            }),
-        ).toEqual(expected);
-    });
-
-    it("should not return anything for empty input", () => {
-        expect(identity({})).toEqual(undefined);
+        expect(await IdentityCommand.run(["--data", fixtureIdentities.publicKey, "--type", "publicKey"])).toEqual(
+            expected,
+        );
     });
 });
--- packages/core-debugger-cli/__tests__/commands/verify-second.test.ts
+++ packages/core-debugger-cli/__tests__/commands/verify-second.test.ts
@@ -1,16 +1,18 @@
 import "jest-extended";
 
-import { verifySecondSignature } from "../../src/commands/verify-second";
+import { VerifySecondSignatureCommand } from "../../src/commands/verify-second";
 
 describe("Commands - Verify Second", () => {
     const fixtureTransaction = require("../__fixtures__/transaction-second.json");
 
-    it("should verify a second signature", () => {
+    it("should verify a second signature", async () => {
         expect(
-            verifySecondSignature({
-                data: fixtureTransaction.serialized,
-                publicKey: "03699e966b2525f9088a6941d8d94f7869964a000efe65783d78ac82e1199fe609",
-            }),
+            await VerifySecondSignatureCommand.run([
+                "--data",
+                fixtureTransaction.serialized,
+                "--publicKey",
+                "03699e966b2525f9088a6941d8d94f7869964a000efe65783d78ac82e1199fe609",
+            ]),
         ).toBeTrue();
     });
 });
