2537d44fcf9c18aa4ad0f162e566b02568535d00
Brian Faust <hello@basecode.sh>
2019-02-01 10:29:08 +0200

    refactor(core-debugger-cli): replace commander.js with @oclif

--- packages/core-debugger-cli/src/commands/identity.ts
+++ packages/core-debugger-cli/src/commands/identity.ts
@@ -1,32 +1,56 @@
 import { crypto } from "@arkecosystem/crypto";
+import { flags } from "@oclif/command";
 import { handleOutput } from "../utils";
+import { BaseCommand } from "./command";
 
-function identity(opts) {
-    let output;
+export class IdentityCommand extends BaseCommand {
+    public static description: string = "Get identities from the given input";
 
-    if (opts.type === "passphrase") {
-        const keys = crypto.getKeys(opts.data);
-        output = {
-            passphrase: opts.data,
-            publicKey: keys.publicKey,
-            privateKey: keys.privateKey,
-            address: crypto.getAddress(keys.publicKey, opts.network),
-        };
-    } else if (opts.type === "privateKey") {
-        const keys = crypto.getKeysByPrivateKey(opts.data);
-        output = {
-            publicKey: keys.publicKey,
-            privateKey: keys.privateKey,
-            address: crypto.getAddress(keys.publicKey, opts.network),
-        };
-    } else if (opts.type === "publicKey") {
-        output = {
-            publicKey: opts.data,
-            address: crypto.getAddress(opts.data, opts.network),
-        };
+    public static flags = {
+        ...BaseCommand.flags,
+        data: flags.string({
+            description: "the data to get the identities from",
+            required: true,
+        }),
+        network: flags.integer({
+            description: "the network version used for calculating the address.",
+            required: true,
+            default: 30,
+        }),
+        type: flags.string({
+            description: "the input type is either of passphrase, privateKey or publicKey",
+            required: true,
+        }),
+    };
+
+    public async run(): Promise<void> {
+        // tslint:disable-next-line:no-shadowed-variable
+        const { flags } = this.parse(IdentityCommand);
+
+        let output;
+
+        if (flags.type === "passphrase") {
+            const keys = crypto.getKeys(flags.data);
+            output = {
+                passphrase: flags.data,
+                publicKey: keys.publicKey,
+                privateKey: keys.privateKey,
+                address: crypto.getAddress(keys.publicKey, flags.network),
+            };
+        } else if (flags.type === "privateKey") {
+            const keys = crypto.getKeysByPrivateKey(flags.data);
+            output = {
+                publicKey: keys.publicKey,
+                privateKey: keys.privateKey,
+                address: crypto.getAddress(keys.publicKey, flags.network),
+            };
+        } else if (flags.type === "publicKey") {
+            output = {
+                publicKey: flags.data,
+                address: crypto.getAddress(flags.data, flags.network),
+            };
+        }
+
+        return handleOutput(flags, output);
     }
-
-    return handleOutput(opts, output);
 }
-
-export { identity };
