9a53684fdd278a9802f3a93bc61e53322cf9d368
Air1 <erwann@ark.io>
2019-02-06 12:44:40 +0400

    test: core-jest-matchers dispatch tests

--- packages/core-jest-matchers/src/blockchain/dispatch.ts
+++ packages/core-jest-matchers/src/blockchain/dispatch.ts
@@ -19,13 +19,15 @@ expect.extend({
         const calls = dispatcher.dispatch.mock.calls;
         const pass = calls && calls[0] ? Object.is(calls[0][0], expected) : false;
 
+        const messageStr = `Expected "${expected}" to ${this.isNot ? "not" : ""} be dispatched, received ${
+            calls && calls[0] ? calls[0][0] : ""
+        }`;
+        const message = () => messageStr;
+
         return {
             // FIXME isNot is necessary to write the right message
             // @see https://facebook.github.io/jest/docs/en/expect.html#expectextendmatchers
-            message: () =>
-                `Expected "${expected}" to ${this.isNot ? "not" : ""} be dispatched, received ${
-                    calls && calls[0] ? calls[0][0] : ""
-                }`,
+            message,
             pass,
         };
     },
