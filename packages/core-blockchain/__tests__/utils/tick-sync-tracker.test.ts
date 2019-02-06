d8aabc3699447c82036a8c6a63b4a4f331334d48
Air1 <erwann@ark.io>
2019-02-06 11:51:31 +0400

    test: fix tick-sync-tracker test

--- packages/core-blockchain/__tests__/utils/tick-sync-tracker.test.ts
+++ packages/core-blockchain/__tests__/utils/tick-sync-tracker.test.ts
@@ -1,13 +1,11 @@
 import "jest-extended";
 
-const printTracker = jest.fn();
-const stopTracker = jest.fn();
+const info = jest.fn();
 jest.mock("@arkecosystem/core-container", () => {
     return {
         app: {
             resolvePlugin: name => ({
-                printTracker,
-                stopTracker,
+                info,
                 getNetworkHeight: () => 120,
             }),
         },
@@ -39,12 +37,27 @@ describe("tickSyncTracker", () => {
 
         tickSyncTracker(8, 52);
 
-        expect(printTracker).toHaveBeenCalledWith("Fast Sync", 50, 100, "(60 of 120 blocks - Est. 60ms)");
+        expect(info).toHaveBeenLastCalledWith("Synchronising In Progress (60 of 120 blocks - Est. 60ms)");
     });
 
     it("should stop tracker when percent == 100", () => {
-        tickSyncTracker(60, 60);
+        tickSyncTracker(60, 60); // should set tracker = null, so when we call again tickSyncTracker it is reset
 
-        expect(stopTracker).toHaveBeenCalledWith("Fast Sync", 100, 100);
+        const now = new Date();
+        const nowMinus8Ms = new Date();
+        nowMinus8Ms.setMilliseconds(now.getMilliseconds() - 8);
+
+        // mocking Date to return nowMinus8Ms then now
+        global.Date = jest
+            .fn()
+            .mockImplementationOnce(() => nowMinus8Ms)
+            .mockImplementationOnce(() => now) as any;
+        global.Date.UTC = DateBackup.UTC;
+        global.Date.parse = DateBackup.parse;
+        global.Date.now = DateBackup.now;
+
+        tickSyncTracker(8, 12);
+
+        expect(info).toHaveBeenLastCalledWith("Synchronising In Progress (20 of 120 blocks - Est. 100ms)");
     });
 });
