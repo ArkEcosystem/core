4608df4cb758477f5bb107ed6ec61967117f3f28
Brian Faust <hello@basecode.sh>
2019-02-13 07:58:01 +0200

    feat(core): check updates from npm

--- packages/core/src/hooks/init.ts
+++ packages/core/src/hooks/init.ts
@@ -3,12 +3,22 @@ import got from "got";
 import semver from "semver";
 import { logger } from "../logger";
 
+async function getVersionFromNode(name: string): Promise<string> {
+    const { body } = await got(`https://registry.npmjs.org/${name}`);
+
+    return JSON.parse(body)["dist-tags"].latest;
+}
+
+async function getVersionFromGithub(name: string): Promise<string> {
+    const { body } = await got(`https://api.github.com/repos/${name.substr(1)}/releases/latest`);
+
+    return JSON.parse(body).tag_name;
+}
+
 // tslint:disable-next-line:only-arrow-functions
 export const init: Hook<"init"> = async function(opts) {
     try {
-        const { body } = await got("https://api.github.com/repos/ArkEcosystem/core/releases/latest");
-
-        const remoteVersion: string = JSON.parse(body).tag_name;
+        const remoteVersion = await getVersionFromNode(opts.config.name);
 
         if (semver.gt(remoteVersion, opts.config.version)) {
             logger.info(
--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,6 +1,21 @@
-import Pino from "pino";
+import * as logSymbols from "log-symbols";
 
-export const logger = Pino({
-    name: "core",
-    prettyPrint: true,
-});
+class Logger {
+    public info(message: string): void {
+        console.log(logSymbols.info, message);
+    }
+
+    public success(message: string): void {
+        console.log(logSymbols.success, message);
+    }
+
+    public warn(message: string): void {
+        console.log(logSymbols.warning, message);
+    }
+
+    public error(message: string): void {
+        console.log(logSymbols.error, message);
+    }
+}
+
+export const logger = new Logger();
