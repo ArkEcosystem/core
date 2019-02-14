bb81b4a09077bc13b7d5235443459add81fb0b93
supaiku <supaiku@protonmail.com>
2019-02-14 19:14:47 +0100

    wip: ajv tx schema

--- packages/crypto/src/transactions/types/schemas_ajv.ts
+++ packages/crypto/src/transactions/types/schemas_ajv.ts
@@ -21,6 +21,17 @@ const schemas = {
         pattern: "^[0-9]+$",
     },
 
+    alphanumeric: {
+        $id: "alphanumeric",
+        type: "string",
+        pattern: "^[a-zA-Z0-9]+$",
+    },
+
+    transactionId: {
+        $id: "transactionId",
+        allOf: [{ minLength: 64 }, { maxLength: 64 }, { $ref: "alphanumeric" }],
+    },
+
     address: {
         $id: "address",
         allOf: [{ $ref: "base58" }, { minLength: 34, maxLength: 34 }],
@@ -60,18 +71,42 @@ const schemas = {
         ],
     },
 
-    test: {
+    baseTx: {
         type: "object",
+        required: ["id", "signature", "senderPublicKey", "recipientId", "amount", "fee"],
         properties: {
-            bla: { type: "string", cast: { to: "bignumber" } },
-            publicKey: { $ref: "publicKey" },
-            delegate: { $ref: "delegateUsername" },
+            id: { $ref: "transactionId" },
+            version: { enum: [1, 2] },
+            type: { type: "integer", minimum: 0, maximum: 255 },
+            timestamp: { type: "integer", minimum: 0 },
             amount: { $ref: "bignumber" },
+            fee: { $ref: "bignumber" },
+            recipientId: { $ref: "address" },
+            senderPublicKey: { $ref: "publicKey" },
+            signature: { $ref: "alphanumeric" },
+            secondSignature: { $ref: "alphanumeric" },
         },
     },
 };
 
-const ajv = new Ajv({ $data: true, schemas });
+const blblbbl = {
+    version: 221,
+    network: 23,
+    type: 0,
+    timestamp: 58126413,
+    senderPublicKey: "03d7dfe44e771039334f4712fb95ad355254f674c8f5d286503199157b7bf7c357",
+    fee: "10000000",
+    amount: "200000000",
+    vendorFieldHex: "5472616e73616374696f6e2037",
+    expiration: 0,
+    recipientId: "APyFYXxXtUrvZFnEuwLopfst94GMY5Zkeq",
+    signature:
+        "3045022100bac5b7699748a891b39ff5439e16ea1a694e93954b248be6b8082da01e5386310220129eb06a58b9f80d36ea3cdc903e6cc0240bbe1d371339ffe15c87742af1427d",
+    vendorField: "Transaction 7",
+    id: "00d2025f7914a8e794bdaea404a579840cf71402cef312d2080c7ecd86177e5f",
+};
+
+const ajv = new Ajv({ $data: true, schemas, removeAdditional: "all" });
 ajvKeywords(ajv);
 
 ajv.addKeyword("cast", {
@@ -93,8 +128,6 @@ ajv.addKeyword("cast", {
 const instanceOf = ajvKeywords.get("instanceof").definition;
 instanceOf.CONSTRUCTORS.Bignum = Bignum;
 
-const x = { bla: "1", publicKey: "A".repeat(66), delegate: "1", amount: 0.00001 };
-console.log(ajv.validate("test", x));
+console.log(ajv.validate("baseTx", blblbbl));
 console.log(ajv.errors);
-console.log(typeof x.amount);
-console.log(x);
+console.log(blblbbl);
