41be5770744a26d6b0fcfe2fc53d578f6deb1d68
supaiku <supaiku@protonmail.com>
2019-02-15 02:43:30 +0100

    test: replace fixture

--- packages/crypto/__tests__/fixtures/transaction.ts
+++ packages/crypto/__tests__/fixtures/transaction.ts
@@ -1,29 +1,13 @@
 export const transaction = {
+    id: "0db225779a7da86bdeaf3a24494fcf3cae7bfe74451fdc3bcb866f62319badeb",
+    timestamp: 60093626,
     version: 1,
-    network: 30,
-    type: 4,
-    timestamp: 48069935,
-    senderPublicKey: "023b31dd57d9025a538ed6433c8e0d339abba3084612e8bbbe09a240e0208b33c9",
-    fee: 2000000000,
-    asset: {
-        multisignature: {
-            keysgroup: [
-                "+02db1d199f20038e569500895b3521a453b2924e4a07c75aa9f7bf2aa4ad71392d",
-                "+02a7442df1f6cbef57d84c9c0eff248f9af48370384987de90bdcebd000feccdb6",
-                "+037a9458c87080768f79c4320941fdc64c9fe580673f17358125b93e80bd0b1d27",
-            ],
-            min: 3,
-            lifetime: 72,
-        },
-    },
-    signature:
-        "3045022100874eed54c9b4e8e41e47cfa340b38b87d6a16469d67462b066aea51fb3a8918d02205d812e19f382cfee5f9ce3bc98abc050f5aaa8262d9a2bf24d5cb117840d2948",
-    signatures: [
-        "3044022022fb3b1d48d9e4905ab566949d637f0832dd0ab6f2cb67a620496e23e83a86d902203182ad967d22db258f97f9fab6d3856c29738ae745eb2f40eb5d472722b794b9",
-        "3045022100aef482ecaea6ecaf8e6f86bd7ac474458e657614b3eb9e440789549d1ea85f6002205c75763411e0febb7d11a7ccf7cb826fc11ddbe3722b73f77e22e9f0919e179d",
-        "3045022100e1dff5c0a4289ffee8caa79fd25fe86f0ded4daaeb9f25e123ea327b01fdb9710220476da4d177652fe4a375e414089ce8c86800bcc4ca6ce0b6d974ef98d8c9d4cf",
-    ],
-    amount: 0,
+    type: 0,
+    fee: 10000000,
+    amount: 1000,
     recipientId: "DJLxkgm7JMortrGVh1ZrvDH39XALWLa83e",
-    id: "115d68cd8a8a9e589ddd51b721085831ce9472273b64b14e195e31dfbab4bf4e",
+    senderPublicKey: "03aacac6c98daaf3d433fe90e9295ce380916946f850bcdc6f6880ae6503ca1e40",
+    vendorField: "Test",
+    signature:
+        "304402203f6d69c0d066c6392c397ae6570f2e0296bfd3e6087f11b7243a71c18436eaa90220201e6ed6cc7e6e2a176ba92d8cabf239d0d87b22597cd6d7b90d9003379cb436",
 };
