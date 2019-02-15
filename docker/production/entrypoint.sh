d795ab0b50aebaaf3f3dd763d6b0bfea9033104d
Brian Faust <hello@basecode.sh>
2019-02-15 09:37:02 +0200

    chore: update docker installation steps

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -4,8 +4,7 @@ sudo /usr/sbin/ntpd -s
 sudo rm -rf /home/node/.config/ark-core/*
 sudo rm -rf /home/node/.local/state/ark-core/*
 sudo chown node:node -R /home/node
-cp -r /home/node/core/packages/core/src/config/$NETWORK /home/node/.config/ark-core/$NETWORK
-cd /home/node/core/packages/core
+ark config:publish --network=$NETWORK
 
 CONFIG=/home/node/.config/ark-core/$NETWORK
 SECRET=`openssl rsautl -decrypt -inkey /run/secrets/secret.key -in /run/secrets/secret.dat`
@@ -13,39 +12,39 @@ CORE_FORGER_PASSWORD=`openssl rsautl -decrypt -inkey /run/secrets/bip.key -in /r
 
 #startup functions
 
-config_plain ()
-{
-    ark config:forger:bip39 --bip39 "$SECRET"
-}
-
-config_bip ()
-{
-    ark config:forger:bip38 --bip38 "$SECRET" --password "$CORE_FORGER_PASSWORD"
-}
-
 start_relay ()
 {
     ark relay:start
 }
 
-start_forger ()
+config_bip38 ()
 {
-    ark forger:start --bip39 "$SECRET"
+    ark config:forger:bip38 --bip38 "$SECRET" --password "$CORE_FORGER_PASSWORD"
 }
 
-start_bip ()
+config_bip39 ()
+{
+    ark config:forger:bip39 --bip39 "$SECRET"
+}
+
+start_forger_with_bip38 ()
 {
     ark forger:start --bip38 "$SECRET" --password "$CORE_FORGER_PASSWORD"
 }
 
+start_forger_with_bip39 ()
+{
+    ark forger:start --bip39 "$SECRET"
+}
+
 #configure
 if [ -n "$SECRET" ] && [ -n "$CORE_FORGER_PASSWORD" ]; then
-        config_bip
+        config_bip38
 elif [ "$MODE" = "forger" ] && [ -z "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
         echo "set SECRET and/or CORE_FORGER_PASWORD if you want to run a forger"
         exit
 elif [ -n "$CORE_FORGER_PASSWORD" ]; then
-        config_plain
+        config_bip39
 fi
 
 #relay
@@ -55,10 +54,10 @@ fi
 
 #forging
 if [ "$MODE" = "forger" ] && [ -n "$SECRET" ] && [ -n "$CORE_FORGER_PASSWORD" ]; then
-        export CORE_FORGER_BIP38=$(grep bip38 /home/node/.config/ark-core/$NETWORK/delegates.json | awk '{print $2}' | tr -d '"') && export CORE_FORGER_PASSWORD && start_bip && sudo rm -rf /run/secrets/*
+        export CORE_FORGER_BIP38=$(grep bip38 /home/node/.config/ark-core/$NETWORK/delegates.json | awk '{print $2}' | tr -d '"') && export CORE_FORGER_PASSWORD && start_forger_with_bip38 && sudo rm -rf /run/secrets/*
 elif [ "$MODE" = "forger" ] && [ -z "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
         echo "set SECRET and/or CORE_FORGER_PASWORD if you want to run a forger"
         exit
 elif [ "$MODE" = "forger" ] && [ -n "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-        start_forger
+        start_forger_with_bip39
 fi
