837d39c57eafaa19437e0eb9e995817f4bcd4723
Brian Faust <faustbrian@users.noreply.github.com>
2019-02-15 16:47:28 +0200

    Update entrypoint.sh

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -7,59 +7,31 @@ sudo chown node:node -R /home/node
 ark config:publish --network=$NETWORK --force
 sudo rm -f /home/node/.config/ark-core/$NETWORK/.env
 
-
 SECRET=`openssl rsautl -decrypt -inkey /run/secrets/secret.key -in /run/secrets/secret.dat`
 CORE_FORGER_PASSWORD=`openssl rsautl -decrypt -inkey /run/secrets/bip.key -in /run/secrets/bip.dat`
 
-#startup functions
-
-start_relay ()
-{
-    ark relay:start --no-daemon
-}
-
-config_bip38 ()
-{
-    ark config:forger:bip38 --bip39 "$SECRET" --password "$CORE_FORGER_PASSWORD"
-}
-
-config_bip39 ()
-{
-    ark config:forger:bip39 --bip39 "$SECRET"
-}
-
-start_forger_with_bip38 ()
-{
-    ark core:start --no-daemon
-}
-
-start_forger_with_bip39 ()
-{
-    ark core:start --no-daemon
-}
-
-#configure
+# configure
 if [ -n "$SECRET" ] && [ -n "$CORE_FORGER_PASSWORD" ]; then
-        config_bip38
+         ark config:forger:bip38 --bip39 "$SECRET" --password "$CORE_FORGER_PASSWORD"
 elif [ "$MODE" = "forger" ] && [ -z "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
         echo "set SECRET and/or CORE_FORGER_PASWORD if you want to run a forger"
         exit
 elif [ -n "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-        config_bip39
+        ark config:forger:bip39 --bip39 "$SECRET"
 fi
 
-#relay
+# relay
 if [[ "$MODE" = "relay" ]]; then
-        start_relay
+        ark relay:start --no-daemon
 fi
 
-#forging
+# forging
 if [ "$MODE" = "forger" ] && [ -n "$SECRET" ] && [ -n "$CORE_FORGER_PASSWORD" ]; then
         export CORE_FORGER_BIP38=$(grep bip38 /home/node/.config/ark-core/$NETWORK/delegates.json | awk '{print $2}' | tr -d '"') && export CORE_FORGER_PASSWORD && start_forger_with_bip38 && sudo rm -rf /run/secrets/*
 elif [ "$MODE" = "forger" ] && [ -z "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
         echo "set SECRET and/or CORE_FORGER_PASWORD if you want to run a forger"
         exit
 elif [ "$MODE" = "forger" ] && [ -n "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-        start_forger_with_bip39
+        ark core:start --no-daemon
 fi
 
