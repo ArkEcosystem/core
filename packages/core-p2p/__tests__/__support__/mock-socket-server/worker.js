b0612bfaf512df64f9bdc9698c9ca41314471e29
Air1 <erwann@ark.io>
2019-02-22 22:32:10 +0400

    test: peer tests with peer socket mocking

--- packages/core-p2p/__tests__/__support__/mock-socket-server/worker.js
+++ packages/core-p2p/__tests__/__support__/mock-socket-server/worker.js
@@ -1,69 +1,47 @@
+const delay = require("delay")
 const SCWorker = require('socketcluster/scworker');
 
 class Worker extends SCWorker {
     run() {
         console.log(`   >> Mock Worker PID: ${process.pid}`);
         
-        const scServer = this.scServer;
-
-        const self = this
+        this.mocks = [];
 
         /*
         In here we handle our incoming realtime connections and listen for events.
         */
-        scServer.on('connection', function (socket) {
-
-            self.registerEndpoints(socket)
-
-            
-            /*socket.on('disconnect', function () {
-                    clearInterval(interval);
-                });*/
+        this.scServer.on('connection', (socket) => {
+            this.registerEndpoints(socket)
         });
-
     }
 
     registerEndpoints(socket) {
-        const self = this
+        socket.on(`mock.add`, async (data, res) => {
+            this.mocks.push(data.endpoint)
+            socket.on(data.endpoint, async (d, r) => r(null, data.value))
+            res()
+        })
 
-        const peerHandlers = {
-            getPeers: true,
-            getHeight: true,
-            getCommonBlocks: true,
-            getTransactions: true,
-            getStatus: true,
-            postBlock: true,
-            postTransactions: true,
-            getBlocks: true,
-        }
-        for (const name of Object.keys(peerHandlers)) {
-            socket.on(`p2p.peer.getStatus`, async (data, res) => res(null, {
-                
-                    "currentSlot": 7473975,
-                    "forgingAllowed": true,
-                    "header": {
-                      "blockSignature": "304402202fe5de5697fa25d3d3c0cb24617ac02ddfb1c915ee9194a89f8392f948c6076402200d07c5244642fe36afa53fb2d048735f1adfa623e8fa4760487e5f72e17d253b",
-                      "generatorPublicKey": "03b47f6b6719c76bad46a302d9cff7be9b1c2b2a20602a0d880f139b5b8901f068",
-                      "height": 1,
-                      "id": "17184958558311101492",
-                      "idHex": "ee7d3cc24bf13434",
-                      "numberOfTransactions": 153,
-                      "payloadHash": "d9acd04bde4234a81addb8482333b4ac906bed7be5a9970ce8ada428bd083192",
-                      "payloadLength": 35960,
-                      "previousBlockHex": "0000000000000000",
-                      "reward": "0",
-                      "timestamp": 0,
-                      "totalAmount": "12500000000000000",
-                      "totalFee": "0",
-                      "version": 0,
-                    },
-                    "height": 1,
-                    "success": true,
-                 
-            }) )
-            socket.on(`p2p.peer.${name}`, async (data, res) => {} )
-                //self.forwardToMaster(Object.assign(data, { endpoint: `p2p.peer.${name}` }), res))
-        }
+        socket.on(`mock.timeout`, async (data, res) => {
+            socket.on(data.endpoint, async (d, r) => {
+                setTimeout(() => r(new Error("Timeout")), data.timeout)
+            })
+            res()
+        })
+
+        socket.on(`mock.reset`, async (data, res) => {
+            socket.off(data.endpoint)
+            res()
+        })
+
+        socket.on(`mock.resetAll`, async (data, res) => {
+            for (const mockEndpoint of this.mocks) {
+                socket.off(mockEndpoint)
+            }
+            this.mocks = []
+            res()
+        })
+
     }
 
 }
--- packages/core-p2p/__tests__/peer.test.ts
+++ packages/core-p2p/__tests__/peer.test.ts
@@ -1,5 +1,5 @@
 import { generators } from "@arkecosystem/core-test-utils";
-import { models } from "@arkecosystem/crypto";
+import socketCluster from "socketcluster-client";
 import { delegates } from "../../core-test-utils/src/fixtures";
 
 import { setUpFull, tearDownFull } from "./__support__/setup";
@@ -7,25 +7,53 @@ import { setUpFull, tearDownFull } from "./__support__/setup";
 import { fork } from "child_process";
 import delay from "delay";
 
-const { Block, Transaction } = models;
+let genesisBlockJSON;
 
-let genesisBlock;
-let genesisTransaction;
-
-let peer;
+let peerMock;
 let localConfig;
 let mockServer;
+let socket;
+let emit;
+let addMock;
+let resetAllMocks;
 
 beforeAll(async () => {
     await setUpFull();
 
-    genesisBlock = new Block(require("@arkecosystem/core-test-utils/src/config/testnet/genesisBlock.json"));
-    genesisTransaction = new Transaction(genesisBlock.transactions[0]);
+    genesisBlockJSON = require("@arkecosystem/core-test-utils/src/config/testnet/genesisBlock.json");
 
     // launching a "mock socket server" so that we can mock a peer
-    // In order for all this mocking to work, we must disable (in the code, didn't manage otherwise)
-    // the isLocalHost() check in is-valid-peer.ts so that we can have local peers
     mockServer = fork(__dirname + "/__support__/mock-socket-server/index.js");
+
+    await delay(2000);
+
+    // client socket so we can send mocking instructions to our mock server
+    socket = socketCluster.create({
+        port: 4009,
+        hostname: "127.0.0.1",
+    });
+
+    emit = async (event, data) =>
+        new Promise((resolve, reject) => {
+            socket.emit(event, data, (err, val) => (err ? reject(err) : resolve(val)));
+        });
+
+    addMock = async (endpoint, mockData, headers?) =>
+        emit("mock.add", {
+            endpoint: `p2p.peer.${endpoint}`,
+            value: {
+                data: mockData,
+                headers: headers || {
+                    version: "2.2.0-beta.4",
+                    port: 4000,
+                    nethash: "27acac9ce53a648f05ba43cdee17454ebb891f205a98196ad6a0ed761abc8e48",
+                    height: 1,
+                    "Content-Type": "application/json",
+                    hashid: "a4e0e642",
+                },
+            },
+        });
+    resetAllMocks = async () => emit("mock.resetAll", {});
 
     await delay(2000);
 });
@@ -43,14 +71,16 @@ beforeEach(() => {
     localConfig.set("port", 4009); // we mock a peer on localhost:4009
 
     const { Peer } = require("../src/peer");
-    peer = new Peer("127.0.0.1", 4000);
-    // the "real" peer (the app we launched with setupFull) is listening on port 4000
+    peerMock = new Peer("127.0.0.1", 4009);
 });
 
 describe("Peer", () => {
+    afterEach(async () => resetAllMocks());
+
     describe("postBlock", () => {
         it("should get back success when posting genesis block", async () => {
-            const response = await peer.postBlock(genesisBlock.toJson());
+            await addMock("postBlock", { success: true });
+            const response = await peerMock.postBlock(genesisBlockJSON);
 
             expect(response).toBeObject();
             expect(response).toHaveProperty("success");
@@ -60,6 +90,7 @@ describe("Peer", () => {
 
     describe("postTransactions", () => {
         it("should be ok", async () => {
+            await addMock("postTransactions", { success: true });
             const transactions = generators.generateTransfers(
                 "testnet",
                 delegates[1].passphrase,
@@ -68,7 +99,7 @@ describe("Peer", () => {
                 1,
                 true,
             );
-            const response = await peer.postTransactions(transactions);
+            const response = await peerMock.postTransactions(transactions);
 
             expect(response).toBeObject();
             expect(response).toHaveProperty("success");
@@ -78,77 +109,83 @@ describe("Peer", () => {
 
     describe("downloadBlocks", () => {
         it("should return the blocks with status 200", async () => {
-            const response = await peer.downloadBlocks(1);
+            await addMock("getBlocks", { blocks: [genesisBlockJSON] });
+            const response = await peerMock.downloadBlocks(1);
 
             expect(response).toBeArrayOfSize(1);
-            expect(response[0].id).toBe(genesisBlock.id);
+            expect(response[0].id).toBe(genesisBlockJSON.id);
         });
     });
 
     describe("ping", () => {
         it("should be ok", async () => {
-            // we ping the peer on port 4000.
-            // we do this with headers sets with port = 4009
-            // so the app will attempt to ping us back on 4009 where our socket server mock is listening
-            // => 127.0.0.1:4009 should be added as a peer in the app (real peer - 4000)
+            const mockStatus = {
+                success: true,
+                height: 1,
+                forgingAllowed: true,
+                currentSlot: 1,
+                header: {},
+            };
+            await addMock("getStatus", mockStatus);
+            process.env.CORE_SKIP_PEER_STATE_VERIFICATION = "true";
 
-            const status = await peer.ping(1000);
+            const status = await peerMock.ping(1000);
 
-            expect(status.success).toBeTrue();
-
-            const peers = await peer.getPeers();
-            expect(peers).toHaveLength(0);
+            expect(status).toEqual(mockStatus);
         });
 
-        it.skip("when localhost check is disabled it should add 127.0.0.1 as a peer", async () => {
-            // we ping the peer on port 4000.
-            // we do this with headers sets with port = 4009
-            // so the app will attempt to ping us back on 4009 where our socket server mock is listening
-            // => 127.0.0.1:4009 should be added as a peer in the app (real peer - 4000)
-
-            const status = await peer.ping(1000);
-
-            expect(status.success).toBeTrue();
-
-            const peers = await peer.getPeers();
-            expect(peers).toHaveLength(1);
-            expect(peers[0].ip).toBe("127.0.0.1");
-            expect(peers[0].port).toBe(4009);
+        it.skip("when ping request timeouts", async () => {
+            await resetAllMocks();
+            process.env.CORE_SKIP_PEER_STATE_VERIFICATION = "true";
+            await peerMock.ping(400);
         });
     });
 
     describe("recentlyPinged", () => {
         it("should return true after a ping", async () => {
-            peer.lastPinged = null;
+            const mockStatus = {
+                success: true,
+                height: 1,
+                forgingAllowed: true,
+                currentSlot: 1,
+                header: {},
+            };
+            await addMock("getStatus", mockStatus);
 
-            expect(peer.recentlyPinged()).toBeFalse();
+            peerMock.lastPinged = null;
 
-            const response = await peer.ping(5000);
+            expect(peerMock.recentlyPinged()).toBeFalse();
+
+            const response = await peerMock.ping(5000);
 
             expect(response).toBeObject();
             expect(response).toHaveProperty("success");
             expect(response.success).toBeTrue();
-            expect(peer.recentlyPinged()).toBeTrue();
+            expect(peerMock.recentlyPinged()).toBeTrue();
         });
     });
 
     describe("getPeers", () => {
         it("should return the list of peers", async () => {
-            const peers = await peer.getPeers();
-            expect(peers).toHaveLength(0);
-            // TODO test when peer has peers
+            const peersMock = [{ ip: "1.1.1.1" }];
+            await addMock("getPeers", { peers: peersMock });
+            const peers = await peerMock.getPeers();
+            expect(peers).toEqual(peersMock);
         });
     });
 
     describe("hasCommonBlocks", () => {
-        it("should return true when peer has a common block", async () => {
-            const commonBlocks = await peer.hasCommonBlocks([genesisBlock.data.id]);
-            expect(commonBlocks).toBeTrue();
+        it("should return false when peer has no common block", async () => {
+            await addMock("getCommonBlocks", { success: true, common: null });
+            const commonBlocks = await peerMock.hasCommonBlocks([genesisBlockJSON.id]);
+            expect(commonBlocks).toBeFalse();
         });
 
-        it("should return false when peer has no common block", async () => {
-            const commonBlocks = await peer.hasCommonBlocks(["456321789"]);
-            expect(commonBlocks).toBeFalse();
+        it("should return true when peer has common block", async () => {
+            await resetAllMocks();
+            await addMock("getCommonBlocks", { success: true, common: genesisBlockJSON });
+            const commonBlocks = await peerMock.hasCommonBlocks([genesisBlockJSON.id]);
+            expect(commonBlocks).toEqual(genesisBlockJSON);
         });
     });
 });
