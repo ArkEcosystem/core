b4749f2f0424c86a474ada002a3db7126466ac12
Brian Faust <faustbrian@users.noreply.github.com>
2019-02-22 18:04:02 +0200

    fix(core): do not ignore the network flag in parseWithNetwork (#2138)

--- packages/core/src/commands/command.ts
+++ packages/core/src/commands/command.ts
@@ -125,15 +125,23 @@ export abstract class BaseCommand extends Command {
     protected async parseWithNetwork(command: any): Promise<any> {
         const { args, flags } = this.parse(command);
 
-        if (process.env.CORE_PATH_CONFIG) {
-            const network: string = process.env.CORE_PATH_CONFIG.split("/").pop();
+        if (process.env.CORE_PATH_CONFIG && !flags.network) {
+            let config: string = process.env.CORE_PATH_CONFIG;
+
+            if (config.endsWith("/")) {
+                config = config.slice(0, -1);
+            }
+
+            const network: string = config.split("/").pop();
 
             if (!this.isValidNetwork(network)) {
                 this.error(`The given network "${flags.network}" is not valid.`);
             }
 
             flags.network = network;
-        } else {
+        }
+
+        if (!flags.network) {
             const { config } = this.getEnvPaths(flags);
 
             try {
