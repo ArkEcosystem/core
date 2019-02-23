aa4ab786e31bc0d9df76e2a1541cd245f5b0c0be
Brian Faust <faustbrian@users.noreply.github.com>
2019-02-23 15:57:22 +0200

    Update sort-entries.ts

--- packages/core-database/src/repositories/utils/sort-entries.ts
+++ packages/core-database/src/repositories/utils/sort-entries.ts
@@ -4,9 +4,13 @@ import { orderBy } from "@arkecosystem/utils";
 export function sortEntries(params: Database.IParameters, entries: any[], defaultValue) {
     const [iteratee, order] = params.orderBy ? params.orderBy.split(":") : defaultValue;
 
-    const properties = ["balance", "fee", "amount", "reward", "voteBalance", "totalFee", "totalAmount"];
+    if (["balance", "voteBalance"].includes(iteratee)) {
+        return Object.values(entries).sort((a: any, b: any) => {
+            return order === "asc"
+                ? +a[iteratee].minus(b[iteratee]).toFixed()
+                : +b[iteratee].minus(a[iteratee]).toFixed();
+        });
+    }
 
-    const iteratees = properties.includes(iteratee) ? value => value[iteratee].toFixed() : [iteratee];
-
-    return orderBy(entries, iteratees, [order as "desc" | "asc"]);
+    return orderBy(entries, [iteratee], [order as "desc" | "asc"]);
 }
