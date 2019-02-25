b41e5763c377c0e3f7f3149dbaf0d4fb3864bff9
supaiku <supaiku@protonmail.com>
2019-02-26 00:04:15 +0100

    refactor crypto hash usages

--- packages/core-database/src/database-service.ts
+++ packages/core-database/src/database-service.ts
@@ -1,9 +1,8 @@
 import { app } from "@arkecosystem/core-container";
 import { Blockchain, Database, EventEmitter, Logger } from "@arkecosystem/core-interfaces";
 import { roundCalculator } from "@arkecosystem/core-utils";
-import { Bignum, constants, crypto as arkCrypto, models, Transaction } from "@arkecosystem/crypto";
+import { Bignum, constants, crypto, HashAlgorithms, models, Transaction } from "@arkecosystem/crypto";
 import assert from "assert";
-import crypto from "crypto";
 import cloneDeep from "lodash/cloneDeep";
 import pluralize from "pluralize";
 import { WalletManager } from "./wallet-manager";
@@ -154,10 +153,7 @@ export class DatabaseService implements Database.IDatabaseService {
         }
 
         const seedSource = round.toString();
-        let currentSeed = crypto
-            .createHash("sha256")
-            .update(seedSource, "utf8")
-            .digest();
+        let currentSeed = HashAlgorithms.sha256(seedSource);
 
         for (let i = 0, delCount = delegates.length; i < delCount; i++) {
             for (let x = 0; x < 4 && i < delCount; i++, x++) {
@@ -166,10 +162,7 @@ export class DatabaseService implements Database.IDatabaseService {
                 delegates[newIndex] = delegates[i];
                 delegates[i] = b;
             }
-            currentSeed = crypto
-                .createHash("sha256")
-                .update(currentSeed)
-                .digest();
+            currentSeed = HashAlgorithms.sha256(currentSeed);
         }
 
         this.forgingDelegates = delegates.map(delegate => {
@@ -520,7 +513,7 @@ export class DatabaseService implements Database.IDatabaseService {
     }
 
     public async verifyTransaction(transaction: Transaction): Promise<boolean> {
-        const senderId = arkCrypto.getAddress(transaction.data.senderPublicKey, this.config.get("network.pubKeyHash"));
+        const senderId = crypto.getAddress(transaction.data.senderPublicKey, this.config.get("network.pubKeyHash"));
 
         const sender = this.walletManager.findByAddress(senderId); // should exist
 
--- packages/crypto/src/crypto/hash-algorithms.ts
+++ packages/crypto/src/crypto/hash-algorithms.ts
@@ -5,6 +5,7 @@ export class HashAlgorithms {
      * Create a "ripemd160" buffer.
      */
     public static ripemd160(buffer: Buffer | string): Buffer {
+        buffer = buffer instanceof Buffer ? buffer : Buffer.from(buffer);
         return RIPEMD160.digest(buffer);
     }
 
@@ -12,6 +13,7 @@ export class HashAlgorithms {
      * Create a "sha1" buffer.
      */
     public static sha1(buffer: Buffer | string): Buffer {
+        buffer = buffer instanceof Buffer ? buffer : Buffer.from(buffer);
         return SHA1.digest(buffer);
     }
 
@@ -20,7 +22,18 @@ export class HashAlgorithms {
      * @param  {Buffer} buffer
      * @return {Buffer}
      */
-    public static sha256(buffer: Buffer | string): Buffer {
+    public static sha256(buffer: Buffer | string | Buffer[]): Buffer {
+        if (Array.isArray(buffer)) {
+            let sha256 = SHA256.ctx;
+            sha256.init();
+            buffer.forEach(element => {
+                sha256 = sha256.update(element);
+            });
+
+            return sha256.final();
+        }
+
+        buffer = buffer instanceof Buffer ? buffer : Buffer.from(buffer);
         return SHA256.digest(buffer);
     }
 
@@ -28,6 +41,7 @@ export class HashAlgorithms {
      * Create a "hash160" buffer.
      */
     public static hash160(buffer: Buffer | string): Buffer {
+        buffer = buffer instanceof Buffer ? buffer : Buffer.from(buffer);
         return Hash160.digest(buffer);
     }
 
@@ -35,6 +49,7 @@ export class HashAlgorithms {
      * Create a "hash256" buffer.
      */
     public static hash256(buffer: Buffer | string): Buffer {
+        buffer = buffer instanceof Buffer ? buffer : Buffer.from(buffer);
         return Hash256.digest(buffer);
     }
 }
