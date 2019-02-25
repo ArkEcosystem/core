262b37e0014d4d11eff00c419bdc7522135f1cf9
Brian Faust <hello@basecode.sh>
2019-02-25 12:42:30 +0200

    refactor(core): require the user to take action for updates

--- packages/core/src/commands/update.ts
+++ packages/core/src/commands/update.ts
@@ -1,10 +1,58 @@
-import { checkForUpdates } from "../helpers/update";
+import Chalk from "chalk";
+import cli from "cli-ux";
+import { removeSync } from "fs-extra";
+import { requestConfirmation } from "../helpers/prompts";
+import { checkForUpdates, installFromChannel } from "../helpers/update";
 import { BaseCommand } from "./command";
 
 export class UpdateCommand extends BaseCommand {
     public static description: string = "Update the core installation";
 
     public async run(): Promise<void> {
-        await checkForUpdates(this);
+        const state = await checkForUpdates(this);
+
+        if (!state.ready) {
+            this.log(`You already have the latest version (${state.currentVersion})`);
+
+            return;
+        }
+
+        try {
+            const currentVersion = state.currentVersion;
+            const newVersion = state.updateVersion;
+
+            this.warn(
+                `${state.name} update available from ${Chalk.greenBright(currentVersion)} to ${Chalk.greenBright(
+                    newVersion,
+                )}.`,
+            );
+
+            await requestConfirmation("Would you like to update?", async () => {
+                try {
+                    cli.action.start(`Updating from ${currentVersion} to ${newVersion}`);
+
+                    await installFromChannel(state.name, state.channel);
+
+                    cli.action.stop();
+
+                    removeSync(state.cache);
+
+                    this.warn(`Version ${newVersion} has been installed.`);
+                    this.warn(
+                        'Respectively run "ark relay:restart", "ark forger:restart" or "ark core:restart" to restart your processes.',
+                    );
+
+                    // @TODO ask the user if he wants to restart the core
+                    // @TODO ask the user if he wants to restart the relay
+                    // @TODO ask the user if he wants to restart the forger
+
+                    process.exit();
+                } catch (err) {
+                    this.error(err.message);
+                }
+            });
+        } catch (err) {
+            this.error(err.message);
+        }
     }
 }
--- packages/core/src/helpers/update.ts
+++ packages/core/src/helpers/update.ts
@@ -1,17 +1,15 @@
 import { IConfig } from "@oclif/config";
-import Chalk from "chalk";
 import cli from "cli-ux";
 import { shell } from "execa";
 import { closeSync, openSync, statSync } from "fs";
 import { existsSync } from "fs-extra";
-import { ensureDirSync, removeSync } from "fs-extra";
+import { ensureDirSync } from "fs-extra";
 import latestVersion from "latest-version";
 import { join } from "path";
-import prompts from "prompts";
 import semver from "semver";
 import { configManager } from "./config";
 
-async function getVersionFromNode(name: string, channel: string): Promise<string> {
+async function getLatestVersion(name: string, channel: string): Promise<string> {
     try {
         const version = await latestVersion(name, { version: channel });
 
@@ -24,7 +22,21 @@ async function getVersionFromNode(name: string, channel: string): Promise<string
 function ensureCacheFile(config: IConfig): string {
     ensureDirSync(config.cacheDir);
 
-    return join(config.cacheDir, "update");
+    const fileName = join(config.cacheDir, "update");
+
+    closeSync(openSync(fileName, "w"));
+
+    return fileName;
+}
+
+export async function installFromChannel(pkg, channel) {
+    const { stdout, stderr } = await shell(`yarn global add ${pkg}@${channel}`);
+
+    if (stderr) {
+        console.error(stderr);
+    }
+
+    console.log(stdout);
 }
 
 export function getRegistryChannel(config: IConfig): string {
@@ -53,22 +65,19 @@ export function needsRefresh(config: IConfig): boolean {
     }
 }
 
-export async function installFromChannel(pkg, channel) {
-    const { stdout, stderr } = await shell(`yarn global add ${pkg}@${channel}`);
+export async function checkForUpdates({ config, error, warn }): Promise<any> {
+    const initialState = {
+        ready: false,
+        name: config.name,
+        currentVersion: config.version,
+    };
 
-    if (stderr) {
-        console.error(stderr);
-    }
-
-    console.log(stdout);
-}
-
-export async function checkForUpdates({ config, error, log, warn }): Promise<void> {
     if (existsSync(join(__dirname, "../../../..", ".git"))) {
         if (!process.env.CORE_DEVELOPER_MODE) {
             warn(`You are using a git clone for developers. Please install core via yarn for auto-updates.`);
         }
-        return;
+
+        return initialState;
     }
 
     try {
@@ -76,49 +85,28 @@ export async function checkForUpdates({ config, error, log, warn }): Promise<voi
         const cacheFile = ensureCacheFile(config);
 
         cli.action.start(`Checking for updates`);
-        const remoteVersion = await getVersionFromNode(config.name, channel);
+        const latestVersion = await getLatestVersion(config.name, channel);
         cli.action.stop();
 
-        closeSync(openSync(cacheFile, "w"));
-
-        if (remoteVersion === undefined) {
+        if (latestVersion === undefined) {
             error(`We were unable to find any releases for the "${channel}" channel.`);
+
+            return initialState;
         }
 
-        if (semver.gt(remoteVersion, config.version)) {
-            warn(
-                `${config.name} update available from ${Chalk.greenBright(config.version)} to ${Chalk.greenBright(
-                    remoteVersion,
-                )}.`,
-            );
-
-            const response = await prompts([
-                {
-                    type: "confirm",
-                    name: "confirm",
-                    message: `Would you like to update?`,
+        if (semver.gt(latestVersion, config.version)) {
+            return {
+                ...initialState,
+                ...{
+                    ready: true,
+                    updateVersion: latestVersion,
+                    cache: cacheFile,
                 },
-            ]);
-
-            if (response.confirm) {
-                try {
-                    cli.action.start(`Updating from ${config.version} to ${remoteVersion}`);
-
-                    await installFromChannel(config.name, channel);
-
-                    removeSync(cacheFile);
-
-                    cli.action.stop();
-
-                    warn(`Version ${remoteVersion} has been installed. Please restart your relay and forger.`);
-                } catch (err) {
-                    error(err.message);
-                }
-            }
-        } else {
-            log(`You already have the latest version (${config.version})`);
+            };
         }
     } catch (err) {
         error(err.message);
     }
+
+    return initialState;
 }
