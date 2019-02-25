6a914c2885accf6cd61d1ebd67a392464cb68e94
paroxysm <ispoonedi@gmail.com>
2019-02-24 22:01:51 -0600

    test(core-database): add tests to blocks & transactions business repositories (#2147)

--- packages/core-interfaces/src/core-database/business-repository/transactions-business-repository.ts
+++ packages/core-interfaces/src/core-database/business-repository/transactions-business-repository.ts
@@ -1,22 +1,24 @@
+import { IParameters } from "./parameters";
+
 export interface ITransactionsBusinessRepository {
 
-    findAll(params: any, sequenceOrder?: "asc" | "desc"): Promise<any>;
+    findAll(params: IParameters, sequenceOrder?: "asc" | "desc"): Promise<any>;
 
-    findAllLegacy(parameters: any): Promise<any>;
+    findAllLegacy(parameters: IParameters): Promise<any>;
 
     findWithVendorField(): Promise<any>;
 
-    findAllByWallet(wallet, parameters: any): Promise<any>;
+    findAllByWallet(wallet, parameters?: IParameters): Promise<any>;
 
-    findAllBySender(senderPublicKey, parameters: any): Promise<any>;
+    findAllBySender(senderPublicKey, parameters?: IParameters): Promise<any>;
 
-    findAllByRecipient(recipientId, parameters: any): Promise<any>;
+    findAllByRecipient(recipientId, parameters?: IParameters): Promise<any>;
 
-    allVotesBySender(senderPublicKey, parameters: any): Promise<any>;
+    allVotesBySender(senderPublicKey, parameters?: IParameters): Promise<any>;
 
-    findAllByBlock(blockId, parameters: any): Promise<any>;
+    findAllByBlock(blockId, parameters?: IParameters): Promise<any>;
 
-    findAllByType(type, parameters: any): Promise<any>;
+    findAllByType(type, parameters?: IParameters): Promise<any>;
 
     findById(id: string): Promise<any>;
 
@@ -24,5 +26,5 @@ export interface ITransactionsBusinessRepository {
 
     getFeeStatistics(): Promise<any>;
 
-    search(parameters): Promise<any>;
+    search(params: IParameters): Promise<any>;
 }
