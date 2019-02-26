8b4c5b8a1f37c92abe9e8f743e3e67465b700bb4
Brian Faust <hello@basecode.sh>
2019-02-26 15:59:51 +0200

    refactor: replace micromatch with the more lightweight nanomatch

--- packages/core-http-utils/src/plugins/whitelist.ts
+++ packages/core-http-utils/src/plugins/whitelist.ts
@@ -1,6 +1,6 @@
 import { app } from "@arkecosystem/core-container";
 import Boom from "boom";
-import mm from "micromatch";
+import nm from "nanomatch";
 
 export const whitelist = {
     name: "whitelist",
@@ -14,7 +14,7 @@ export const whitelist = {
                 if (Array.isArray(options.whitelist)) {
                     for (const ip of options.whitelist) {
                         try {
-                            if (mm.isMatch(remoteAddress, ip)) {
+                            if (nm.isMatch(remoteAddress, ip)) {
                                 return h.continue;
                             }
                         } catch {
