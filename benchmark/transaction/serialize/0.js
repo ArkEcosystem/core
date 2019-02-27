3449c6fc6e9473ded4dac3eaf10276243706b301
supaiku <1311798+supaiku0@users.noreply.github.com>
2019-02-27 04:23:12 +0100

    refactor(crypto): benchmarks (#2167)

--- benchmark/transaction/serialize/0.js
+++ benchmark/transaction/serialize/0.js
@@ -1,9 +1,9 @@
 const {
-    Transaction
+    TransactionSerializer
 } = require('@arkecosystem/crypto')
 
 const data = require('../../helpers').getJSONFixture('transaction/deserialized/0');
 
 exports['core'] = () => {
-    return Transaction.fromData(data);
+    return TransactionSerializer.getBytes(data);
 };
