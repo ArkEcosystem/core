4e53bc1b67421a4ff10f5c26670adccc76245dcd
Brian Faust <hello@basecode.sh>
2019-02-28 06:48:10 +0200

    unused

--- packages/core/src/enums.ts
+++ packages/core/src/enums.ts
@@ -1,9 +1,5 @@
 export enum ProcessState {
     Errored = "errored",
-    Launching = "launching",
-    OneLaunch = "one-launch-status",
     Online = "online",
     Stopped = "stopped",
-    Stopping = "stopping",
-    WaitingRestarting = "waiting restart",
 }
