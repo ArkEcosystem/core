8fa0a5d8c2730049716360006b4b62736838c32b
Brian Faust <faustbrian@users.noreply.github.com>
2019-03-02 09:17:38 +0200

    Update command.ts

--- packages/core-tester-cli/src/commands/command.ts
+++ packages/core-tester-cli/src/commands/command.ts
@@ -99,7 +99,7 @@ export abstract class BaseCommand extends Command {
 
         return this.api.post("transactions", { transactions });
     }
-    
+
     protected async knockTransaction(id: string): Promise<boolean> {
         try {
             const { data } = await this.api.get(`transactions/${id}`);
@@ -115,7 +115,7 @@ export abstract class BaseCommand extends Command {
             return false;
         }
     }
-    
+
     protected async knockBalance(address: string, expected: Bignum): Promise<void> {
         const actual = await this.getWalletBalance(address);
 
