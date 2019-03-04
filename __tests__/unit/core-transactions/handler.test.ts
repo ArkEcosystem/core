5f04cadba1c52af088a3cfd3130e0145dc6ddd05
supaiku <supaiku@protonmail.com>
2019-03-04 13:44:17 +0100

    test: fix imports

--- __tests__/unit/core-transactions/handler.test.ts
+++ __tests__/unit/core-transactions/handler.test.ts
@@ -1,5 +1,6 @@
 import "jest-extended";
 
+import { Bignum, configManager, constants, crypto, ITransactionData, models, Transaction } from "@arkecosystem/crypto";
 import {
     AlreadyVotedError,
     InsufficientBalanceError,
@@ -18,13 +19,10 @@ import {
 } from "../../../packages/core-transactions/src/errors";
 import { TransactionServiceRegistry } from "../../../packages/core-transactions/src/index";
 import { TransactionService } from "../../../packages/core-transactions/src/services/transaction";
-import { crypto, ITransactionData, Transaction } from "../../../packages/crypto/src";
-import { ARKTOSHI } from "../../../packages/crypto/src/constants";
-import { configManager } from "../../../packages/crypto/src/managers/config";
-import { Wallet } from "../../../packages/crypto/src/models";
-import { Bignum } from "../../../packages/crypto/src/utils";
 import { transaction as transactionFixture } from "../crypto/transactions/__fixtures__/transaction";
 import { wallet as walletFixture } from "../crypto/transactions/__fixtures__/wallet";
+
+const { ARKTOSHI } = constants;
 
 let wallet;
 let transaction: ITransactionData;
@@ -88,8 +86,7 @@ describe("General Tests", () => {
             expect(() => service.canBeApplied(instance, wallet)).toThrow(SenderWalletMismatchError);
         });
 
-        // FIXME: the config manager on service side is a different one for some reason...
-        it.skip("should be false if the transaction has a second signature but wallet does not", () => {
+        it("should be false if the transaction has a second signature but wallet does not", () => {
             delete configManager.getMilestone().ignoreInvalidSecondSignatureField;
             instance = Transaction.fromData(transactionWithSecondSignature);
             expect(() => service.canBeApplied(instance, wallet)).toThrow(UnexpectedSecondSignatureError);
@@ -490,7 +487,7 @@ describe.skip("MultiSignatureRegistrationTransaction", () => {
     let multisignatureTest;
 
     beforeEach(() => {
-        wallet = new Wallet("D61xc3yoBQDitwjqUspMPx1ooET6r1XLt7");
+        wallet = new models.Wallet("D61xc3yoBQDitwjqUspMPx1ooET6r1XLt7");
         wallet.balance = new Bignum(100390000000);
         wallet.publicKey = "026f717e50bf3dbb9d8593996df5435ba22217410fc7a132f3d2c942a01a00a202";
         wallet.secondPublicKey = "0380728436880a0a11eadf608c4d4e7f793719e044ee5151074a5f2d5d43cb9066";
