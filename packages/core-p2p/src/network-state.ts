c79369a6b60da4764f453d1c9cde3251d5eb50ca
Vasil Dimov <vd@FreeBSD.org>
2019-03-08 15:41:30 +0100

    feat(core-p2p): Don't spoil the quorum if the peer has !forgingAllowed
    We need >66% quorum to forge and thus we should not count peers that
    report "forgingAllowed: false" towards no-quorum ones, because too many
    such peers could trip the forger to skip a block.

--- packages/core-p2p/src/network-state.ts
+++ packages/core-p2p/src/network-state.ts
@@ -170,7 +170,6 @@ export class NetworkState {
             }
 
             if (!peer.state.forgingAllowed) {
-                quorum = false;
                 this.quorumDetails.peersForgingNotAllowed++;
             }
 
