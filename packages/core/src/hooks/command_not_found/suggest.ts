79420a031a6b43f17896377420768fac0364f652
Brian Faust <hello@basecode.sh>
2019-03-11 05:59:01 +0200

    refactor(core): allow to provide yes/no callbacks for confirm

--- packages/core/src/hooks/command_not_found/suggest.ts
+++ packages/core/src/hooks/command_not_found/suggest.ts
@@ -2,7 +2,6 @@
 
 import { Hook } from "@oclif/config";
 import Chalk from "chalk";
-import cli from "cli-ux";
 import * as Levenshtein from "fast-levenshtein";
 import minBy from "lodash/minBy";
 import { confirm } from "../../helpers/prompts";
@@ -28,15 +27,16 @@ export const init: Hook<"init"> = async function(opts) {
     const suggestion: string = closest(commandIDs, opts.id);
     this.warn(`${Chalk.redBright(opts.id)} is not a ${opts.config.bin} command.`);
 
-    await confirm(`Did you mean ${Chalk.blueBright(suggestion)}?`, async () => {
-        try {
-            const argv = process.argv;
-            await this.config.runCommand(suggestion, argv.slice(3, argv.length));
-        } catch (err) {
-            this.error(err.message);
-            this.error(`Run ${Chalk.blueBright(binHelp)} for a list of available commands.`, { exit: 127 });
-        } finally {
-            cli.action.stop();
-        }
-    });
+    await confirm(
+        `Did you mean ${Chalk.blueBright(suggestion)}?`,
+        async () => {
+            try {
+                const argv = process.argv;
+                await this.config.runCommand(suggestion, argv.slice(3, argv.length));
+            } catch (err) {
+                this.error(err.message);
+            }
+        },
+        this.error(`Run ${Chalk.blueBright(binHelp)} for a list of available commands.`, { exit: 127 }),
+    );
 };
