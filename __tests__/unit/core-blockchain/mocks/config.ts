96b92179bec0507d42f9ecde277e8dbe9c85edd6
Brian Faust <hello@basecode.sh>
2019-03-14 11:07:12 +0200

    chore: setup linting for tests

--- __tests__/unit/core-blockchain/mocks/config.ts
+++ __tests__/unit/core-blockchain/mocks/config.ts
@@ -2,6 +2,6 @@ import genesisBlock from "../../../utils/config/testnet/genesisBlock.json";
 
 export const config = {
     "network.nethash": genesisBlock.payloadHash,
-    genesisBlock: genesisBlock,
+    genesisBlock,
     "state.maxLastBlocks": 50,
 };
