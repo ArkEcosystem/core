fc7e82c25a1ee2faa4d83741573db90e0ee613b9
supaiku <supaiku@protonmail.com>
2019-03-15 00:39:45 +0100

    refactor: update exports

--- packages/core-transactions/src/index.ts
+++ packages/core-transactions/src/index.ts
@@ -1,2 +1,7 @@
-export * from "./errors";
+import * as errors from "./errors";
+
+export * from "./services/transaction";
+export * from "./interfaces";
+
+export { errors };
 export { transactionServiceRegistry as TransactionServiceRegistry } from "./service-registry";
