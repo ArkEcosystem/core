5292929029ff8b168ac079e9fec499c7e82c71b2
Brian Faust <hello@basecode.sh>
2019-03-19 08:27:57 +0200

    feat(core-logger-pino): set different log levels for console and file

--- packages/core-logger-pino/src/defaults.ts
+++ packages/core-logger-pino/src/defaults.ts
@@ -1,3 +1,6 @@
 export const defaults = {
-    level: process.env.CORE_LOG_LEVEL || "debug",
+    levels: {
+        console: process.env.CORE_LOG_LEVEL || "debug",
+        file: process.env.CORE_LOG_LEVEL_FILE || "trace",
+    },
 };
