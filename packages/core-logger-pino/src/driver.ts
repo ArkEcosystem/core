8ab492eb0637a5dc7a78493e06736b9dce6d7149
supaiku <supaiku@protonmail.com>
2019-03-23 01:45:51 +0100

    fix: prettify filestream

--- packages/core-logger-pino/src/driver.ts
+++ packages/core-logger-pino/src/driver.ts
@@ -1,28 +1,39 @@
 import { AbstractLogger } from "@arkecosystem/core-logger";
+import { WriteStream } from "fs";
 import isEmpty from "lodash/isEmpty";
-import pino from "pino";
-import { multistream } from "pino-multi-stream";
+import pino, { PrettyOptions } from "pino";
 import PinoPretty from "pino-pretty";
-import { getPrettyStream } from "pino/lib/tools";
+import pump from "pump";
+import { Transform } from "readable-stream";
 import rfs from "rotating-file-stream";
+import split from "split2";
+import { PassThrough } from "stream";
 import { inspect } from "util";
 
 export class PinoLogger extends AbstractLogger {
     public logger: pino.Logger;
     public silent: boolean = false;
 
+    private fileStream: WriteStream;
+
     public make() {
+        const stream = new PassThrough();
         this.logger = pino(
             {
                 base: null,
                 safe: true,
                 level: this.options.levels.console,
             },
-            multistream([
-                { level: this.options.levels.console, stream: this.getConsoleStream() },
-                { level: this.options.levels.file, stream: this.getFileStream() },
-            ]),
+            stream,
         );
+
+        this.fileStream = this.getFileStream();
+
+        const consoleTransport = this.createPrettyTransport({ colorize: true });
+        const fileTransport = this.createPrettyTransport({ colorize: false });
+
+        pump(stream, split(), consoleTransport, process.stdout);
+        pump(stream, split(), fileTransport, this.fileStream);
 
         return this;
     }
@@ -67,19 +78,27 @@ export class PinoLogger extends AbstractLogger {
         this.logger[method](message);
     }
 
-    private getConsoleStream() {
-        return getPrettyStream(
-            {
+    private createPrettyTransport(prettyOptions?: PrettyOptions): Transform {
+        const pinoPretty = PinoPretty({
+            ...{
                 levelFirst: false,
                 translateTime: "yyyy-mm-dd HH:MM:ss.l",
-                colorize: true,
             },
-            PinoPretty,
-            process.stdout,
-        );
+            ...prettyOptions,
+        });
+
+        return new Transform({
+            transform(chunk, enc, cb) {
+                const line = pinoPretty(chunk.toString());
+                if (line === undefined) {
+                    return cb();
+                }
+                cb(null, line);
+            },
+        });
     }
 
-    private getFileStream() {
+    private getFileStream(): WriteStream {
         const createFileName = (time: Date, index: number) => {
             if (!time) {
                 return new Date().toISOString().slice(0, 10) + ".log";
