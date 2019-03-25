2036f5b3ad31721b982855dc823b4a6f822cf9f4
Andrej Novikov <jazzaiman@gmail.com>
2019-03-25 20:49:29 +0200

    refactor: replace any with proper types (#2276)

--- packages/core/src/helpers/snapshot.ts
+++ packages/core/src/helpers/snapshot.ts
@@ -1,9 +1,10 @@
 import { app } from "@arkecosystem/core-container";
+import { Container } from "@arkecosystem/core-interfaces";
 
 // tslint:disable-next-line:no-var-requires
 const { version } = require("../../package.json");
 
-export async function setUpLite(options) {
+export async function setUpLite(options): Promise<Container.IContainer> {
     process.env.CORE_SKIP_BLOCKCHAIN = "true";
 
     await app.setUp(version, options, {
