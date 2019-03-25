83a460c881cad1ea485f345778ee593457c9b5ae
Brian Faust <hello@basecode.sh>
2019-03-25 07:20:25 +0200

    release: 2.3.0-next.0

--- scripts/publish/alpha.sh
+++ scripts/publish/alpha.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-for dir in `find packages -mindepth 1 -maxdepth 1 -type d`; do
+for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
     cd $dir
     echo $PWD
-    cd ../..
     npm publish --tag alpha
+    cd ../..
 done
