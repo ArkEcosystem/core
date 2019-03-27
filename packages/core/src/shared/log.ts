af9eabea2a9c56e521d09d861de82539ba8d6842
Brian Faust <hello@basecode.sh>
2019-03-27 14:22:50 +0200

    refactor(core): combine tail and read last lines

--- packages/core/src/shared/log.ts
+++ packages/core/src/shared/log.ts
@@ -1,16 +1,8 @@
 import clear from "clear";
-import cli from "cli-ux";
-import nsfw from "nsfw";
+import Tail from "nodejs-tail";
 import readLastLines from "read-last-lines";
 import { BaseCommand } from "../commands/command";
 import { processManager } from "../process-manager";
-import { CommandFlags } from "../types";
-
-interface FileEvent {
-    action: number;
-    directory: string;
-    file: string;
-}
 
 export abstract class AbstractLogCommand extends BaseCommand {
     public async run(): Promise<void> {
@@ -30,30 +22,18 @@ export abstract class AbstractLogCommand extends BaseCommand {
             `Tailing last ${flags.lines} lines for [${processName}] process (change the value with --lines option)`,
         );
 
-        await this.readLines(file, flags.lines);
+        this.log((await readLastLines.read(file, flags.lines)).trim());
 
-        const watcher = await nsfw(
-            file,
-            async (events: FileEvent[]) => {
-                for (const event of events) {
-                    if (event.action === nsfw.actions.MODIFIED) {
-                        await this.readLines(`${event.directory}/${event.file}`, 1);
-                    }
-                }
-            },
-            {
-                debounceMS: 250,
-            },
-        );
+        const log = new Tail(file, {
+            useFsEvents: false,
+        });
 
-        await watcher.start();
+        log.on("line", this.log);
+
+        log.watch();
     }
 
     public abstract getClass();
 
     public abstract getSuffix(): string;
-
-    private async readLines(file: string, lines: number): Promise<void> {
-        this.log((await readLastLines.read(file, lines)).trim());
-    }
 }
