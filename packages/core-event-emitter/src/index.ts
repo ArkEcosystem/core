68fa2cc5eb46d73d4a9af3968b26007e3162d1a0
Brian Faust <hello@basecode.sh>
2019-03-31 17:36:12 +0300

    refactor(core-event-emitter): expose all application events through an enum

--- packages/core-event-emitter/src/index.ts
+++ packages/core-event-emitter/src/index.ts
@@ -7,3 +7,30 @@ export const plugin = {
         return new EventEmitter();
     },
 };
+
+export enum ApplicationEvents {
+    BlockApplied = "block.applied",
+    BlockDisregarded = "block.disregarded",
+    BlockForged = "block.forged",
+    BlockReceived = "block.received",
+    BlockReverted = "block.reverted",
+    DelegateRegistered = "delegate.registered",
+    DelegateResigned = "delegate.resigned",
+    ForgerFailed = "forger.failed",
+    ForgerMissing = "forger.missing",
+    ForgerStarted = "forger.started",
+    PeerAdded = "peer.added",
+    PeerRemoved = "peer.removed",
+    RoundApplied = "round.applied",
+    RoundCreated = "round.created",
+    StateStarted = "state.started",
+    TransactionApplied = "transaction.applied",
+    TransactionExpired = "transaction.expired",
+    TransactionForged = "transaction.forged",
+    TransactionPoolAdded = "transaction.pool.added",
+    TransactionPoolRejected = "transaction.pool.rejected",
+    TransactionPoolRemoved = "transaction.pool.removed",
+    TransactionReverted = "transaction.reverted",
+    WalletColdCreated = "wallet.created.cold",
+    WalletSaved = "wallet.saved",
+}
