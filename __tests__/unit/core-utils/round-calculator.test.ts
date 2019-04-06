049db8dbcedccd51030567e6a8a0f346c18f2da0
supaiku <supaiku@protonmail.com>
2019-04-06 04:17:24 +0200

    refactor: simplify everything

--- __tests__/unit/core-utils/round-calculator.test.ts
+++ __tests__/unit/core-utils/round-calculator.test.ts
@@ -37,31 +37,12 @@ describe("Round calculator", () => {
             it("should calculate the correct with dynamic delegate count", () => {
                 const testVector = [
                     { height: 1, round: 1, roundHeight: 1, nextRound: 1, activeDelegates: 2 },
-                    { height: 2, round: 1, roundHeight: 1, nextRound: 2, activeDelegates: 2 },
                     { height: 3, round: 2, roundHeight: 3, nextRound: 2, activeDelegates: 3 },
-                    { height: 4, round: 2, roundHeight: 3, nextRound: 2, activeDelegates: 3 },
-                    { height: 5, round: 2, roundHeight: 3, nextRound: 3, activeDelegates: 3 },
                     { height: 6, round: 3, roundHeight: 6, nextRound: 4, activeDelegates: 1 },
-                    { height: 7, round: 4, roundHeight: 7, nextRound: 5, activeDelegates: 1 },
-                    { height: 8, round: 5, roundHeight: 8, nextRound: 6, activeDelegates: 1 },
-                    { height: 9, round: 6, roundHeight: 9, nextRound: 7, activeDelegates: 1 },
                     { height: 10, round: 7, roundHeight: 10, nextRound: 7, activeDelegates: 51 },
-                    { height: 11, round: 7, roundHeight: 10, nextRound: 7, activeDelegates: 51 },
-                    { height: 61, round: 8, roundHeight: 61, nextRound: 8, activeDelegates: 51 },
-                    { height: 62, round: 8, roundHeight: 61, nextRound: 8, activeDelegates: 51 },
                     { height: 112, round: 9, roundHeight: 112, nextRound: 10, activeDelegates: 1 },
-                    { height: 113, round: 10, roundHeight: 113, nextRound: 11, activeDelegates: 1 },
-                    { height: 114, round: 11, roundHeight: 114, nextRound: 12, activeDelegates: 1 },
                     { height: 115, round: 12, roundHeight: 115, nextRound: 12, activeDelegates: 2 },
-                    { height: 116, round: 12, roundHeight: 115, nextRound: 13, activeDelegates: 2 },
-                    { height: 117, round: 13, roundHeight: 117, nextRound: 13, activeDelegates: 2 },
-                    { height: 118, round: 13, roundHeight: 117, nextRound: 14, activeDelegates: 2 },
-                    { height: 119, round: 14, roundHeight: 119, nextRound: 14, activeDelegates: 2 },
-                    { height: 120, round: 14, roundHeight: 119, nextRound: 15, activeDelegates: 2 },
                     { height: 131, round: 20, roundHeight: 131, nextRound: 20, activeDelegates: 51 },
-                    { height: 180, round: 20, roundHeight: 131, nextRound: 20, activeDelegates: 51 },
-                    { height: 181, round: 20, roundHeight: 131, nextRound: 21, activeDelegates: 51 },
-                    { height: 182, round: 21, roundHeight: 182, nextRound: 21, activeDelegates: 51 },
                 ];
 
                 const milestones = testVector.reduce((acc, vector) => acc.set(vector.height, vector), new Map());
@@ -93,6 +74,25 @@ describe("Round calculator", () => {
     });
 
     describe("isNewRound", () => {
+        const setMilestones = milestones => {
+            app.getConfig = jest.fn(() => {
+                return {
+                    config: {
+                        milestones,
+                    },
+                    getMilestone: height => {
+                        for (let i = milestones.length - 1; i >= 0; i--) {
+                            if (milestones[i].height <= height) {
+                                return milestones[i];
+                            }
+                        }
+
+                        return milestones[0];
+                    },
+                };
+            });
+        };
+
         it("should determine the beginning of a new round", () => {
             expect(isNewRound(1)).toBeTrue();
             expect(isNewRound(2)).toBeFalse();
@@ -105,33 +105,16 @@ describe("Round calculator", () => {
         });
 
         it("should be ok when changing delegate count", () => {
-            const milestones = {
-                "1": { height: 1, activeDelegates: 2 }, // R1
-                "2": { height: 2, activeDelegates: 2 }, // R1
-                "3": { height: 3, activeDelegates: 3 }, // R2
-                "4": { height: 4, activeDelegates: 3 }, // R2
-                "5": { height: 5, activeDelegates: 3 }, // R2
-                "6": { height: 6, activeDelegates: 1 }, // R3
-                "7": { height: 7, activeDelegates: 1 }, // R4
-                "8": { height: 8, activeDelegates: 1 }, // R5
-                "9": { height: 9, activeDelegates: 1 }, // R6
-                "10": { height: 10, activeDelegates: 51 }, // R7
-                "11": { height: 11, activeDelegates: 51 }, // R7
-                "61": { height: 61, activeDelegates: 51 }, // R7
-                "62": { height: 62, activeDelegates: 51 }, // R8
-            };
+            const milestones = [
+                { height: 1, activeDelegates: 2 }, // R1
+                { height: 3, activeDelegates: 3 }, // R2
+                { height: 6, activeDelegates: 1 }, // R3
+                { height: 10, activeDelegates: 51 }, // R7
+                { height: 62, activeDelegates: 51 }, // R8
+            ];
 
             const backup = app.getConfig;
-            app.getConfig = jest.fn(() => {
-                return {
-                    config: {
-                        milestones: Object.values(milestones),
-                    },
-                    getMilestone: height => {
-                        return milestones[height];
-                    },
-                };
-            });
+            setMilestones(milestones);
 
             // 2 Delegates
             expect(isNewRound(1)).toBeTrue();
--- packages/core-utils/src/round-calculator.ts
+++ packages/core-utils/src/round-calculator.ts
@@ -1,11 +1,6 @@
 import { app } from "@arkecosystem/core-container";
 import { Shared } from "@arkecosystem/core-interfaces";
 
-interface IActiveDelegateMilestone {
-    activeDelegates: number;
-    height: number;
-}
-
 export const calculateRound = (height: number): Shared.IRoundInfo => {
     const config = app.getConfig();
     const { milestones } = config.config;
@@ -70,95 +65,6 @@ export const calculateRound = (height: number): Shared.IRoundInfo => {
 
 export const isNewRound = (height: number): boolean => {
     const config = app.getConfig();
-
-    const nextMilestone = config.getMilestone(height);
-    const milestone = getPreviousDelegateMilestone(height);
-
-    // The delegate count can only change at the beginning of a new round.
-    if (
-        height === 1 ||
-        (height === nextMilestone.height && nextMilestone.activeDelegates !== milestone.activeDelegates)
-    ) {
-        return true;
-    }
-
-    // Offset height relative to previous milestone and delegate count
-    if (height > milestone.activeDelegates && milestone.height !== nextMilestone.height) {
-        height -= nextMilestone.activeDelegates;
-        if (height >= milestone.height) {
-            height -= milestone.height;
-        }
-        //   console.info(`Normalizing height to: ${height}`);
-    } else {
-        height -= 1; // The first round is special, because the height is initially below the delegate count.
-    }
-
-    // console.info(`${height}%${nextMilestone.activeDelegates} === ${height % nextMilestone.activeDelegates}`);
-    return height % nextMilestone.activeDelegates === 0;
-};
-
-/**
- * Given the following milestones:
- * [
- *   { height: 1, activeDelegates: 2 }, // M1
- *   { height: 2, activeDelegates: 2 }, // M2
- *   { height: 3, activeDelegates: 3 }, // M3
- *   { height: 4, activeDelegates: 3 }, // M4
- *   { height: 5, activeDelegates: 3 }, // M5
- *   { height: 6, activeDelegates: 9 }, // M6
- *   { height: 15, activeDelegates: 51 }, // M15
- *   { height: 66, activeDelegates: 1 }, // M66
- * ]
- *
- * For `height = 6` the active milestone will be `M6`.
- * In order to correctly determine if it's the beginning of a new round
- * the previous milestone is required.
- *
- * By just looking at `height = 6` and `activeDelegates = 9` it would not
- * be possible to say if it's the beginning of a round without knowing the active
- * delegate count prior `M6`.
- *
- * The loop basically goes through all milestones in ascending order and looks for
- * the last active milestone with at least 1 round distance to the current milestone
- * for the given height. In this case the previous delegate count was `3`.
- *
- * Out of `M3`, `M4`and `M5` the loop returns `M3` since there's exactly 1 round distance.
- *
- * Or in other words:
- * From `M3`'s point of view `M6` is the beginning of a new round:
- * `M6.height - M3.height % M3.activeDelegates === 0`
- */
-const getPreviousDelegateMilestone = (height: number): IActiveDelegateMilestone => {
-    const config = app.getConfig();
-    const milestones: IActiveDelegateMilestone[] = config.config.milestones.filter(
-        ({ activeDelegates, height }: IActiveDelegateMilestone) => {
-            return activeDelegates !== undefined && height !== undefined;
-        },
-    );
-
-    if (milestones.length === 0) {
-        throw new Error("Missing delegate milestone configuration.");
-    }
-
-    let previousMilestone = milestones[0];
-    //  console.info(`Previous milestone ${JSON.stringify(previousMilestone)}`);
-
-    for (;;) {
-        const milestone = milestones.find(
-            milestone =>
-                milestone.height > previousMilestone.height &&
-                milestone.height < height &&
-                milestone.activeDelegates !== previousMilestone.activeDelegates &&
-                (milestone.height - previousMilestone.height) % previousMilestone.activeDelegates === 0,
-        );
-
-        //    console.info(`${JSON.stringify(milestone)}`);
-        if (milestone) {
-            previousMilestone = milestone;
-        } else {
-            break;
-        }
-    }
-
-    return previousMilestone as IActiveDelegateMilestone;
+    const milestone = config.getMilestone(height);
+    return height === 1 || (height - milestone.height) % milestone.activeDelegates === 0;
 };
