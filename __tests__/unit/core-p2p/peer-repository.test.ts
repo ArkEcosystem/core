04f392dead1bbab4fc6ee26bece9c28904aff4cc
Brian Faust <hello@basecode.sh>
2019-04-06 18:17:26 +0300

    test(core-p2p): clean up peer repository and storage tests

--- __tests__/unit/core-p2p/peer-repository.test.ts
+++ __tests__/unit/core-p2p/peer-repository.test.ts
@@ -6,139 +6,116 @@ import { P2P } from "@arkecosystem/core-interfaces";
 import { PeerRepository } from "../../../packages/core-p2p/src/peer-repository";
 import { stubPeer } from "../../helpers/peers";
 
-let repo: PeerRepository<P2P.IPeer>;
+const repository: PeerRepository<P2P.IPeer> = new PeerRepository<P2P.IPeer>();
 
 beforeEach(() => {
-    repo = new PeerRepository<P2P.IPeer>();
+    repository.flush();
+    repository.set(stubPeer.ip, stubPeer);
 });
 
 describe("PeerRepository", () => {
     it("should return the underlying repository", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.all()).toEqual(new Map().set(stubPeer.ip, stubPeer));
+        expect(repository.all()).toEqual(new Map().set(stubPeer.ip, stubPeer));
     });
 
     it("should return all entries", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.entries()).toEqual([[stubPeer.ip, stubPeer]]);
+        expect(repository.entries()).toEqual([[stubPeer.ip, stubPeer]]);
     });
 
     it("should return all keys", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.keys()).toEqual([stubPeer.ip]);
+        expect(repository.keys()).toEqual([stubPeer.ip]);
     });
 
     it("should return all values", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.values()).toEqual([stubPeer]);
+        expect(repository.values()).toEqual([stubPeer]);
     });
 
     it("should get an item and remove it", () => {
-        repo.set(stubPeer.ip, stubPeer);
+        expect(repository.pull(stubPeer.ip)).toEqual(stubPeer);
 
-        expect(repo.pull(stubPeer.ip)).toEqual(stubPeer);
-
-        expect(repo.isEmpty()).toBeTrue();
+        expect(repository.isEmpty()).toBeTrue();
     });
 
     it("should get an item", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.get(stubPeer.ip)).toEqual(stubPeer);
+        expect(repository.get(stubPeer.ip)).toEqual(stubPeer);
     });
     it("should set an item", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.has(stubPeer.ip)).toBeTrue();
+        expect(repository.has(stubPeer.ip)).toBeTrue();
     });
 
     it("should forget an item", () => {
-        repo.set(stubPeer.ip, stubPeer);
+        expect(repository.isEmpty()).toBeFalse();
 
-        expect(repo.isEmpty()).toBeFalse();
+        repository.forget(stubPeer.ip);
 
-        repo.forget(stubPeer.ip);
-
-        expect(repo.isEmpty()).toBeTrue();
+        expect(repository.isEmpty()).toBeTrue();
     });
 
     it("should flush all items", () => {
-        repo.set(stubPeer.ip, stubPeer);
+        expect(repository.isEmpty()).toBeFalse();
 
-        expect(repo.isEmpty()).toBeFalse();
+        repository.flush();
 
-        repo.flush();
-
-        expect(repo.isEmpty()).toBeTrue();
+        expect(repository.isEmpty()).toBeTrue();
     });
 
     describe("has", () => {
         it("should return true if an item exists", () => {
-            repo.set(stubPeer.ip, stubPeer);
-
-            expect(repo.has(stubPeer.ip)).toBeTrue();
+            expect(repository.has(stubPeer.ip)).toBeTrue();
         });
 
         it("should return false if an item doesn't exist", () => {
-            expect(repo.has(stubPeer.ip)).toBeFalse();
+            repository.flush();
+
+            expect(repository.has(stubPeer.ip)).toBeFalse();
         });
     });
 
     describe("missing", () => {
-        it("should return false if an item is missing", () => {
-            repo.set(stubPeer.ip, stubPeer);
-
-            expect(repo.missing(stubPeer.ip)).toBeFalse();
+        it("should return false if an item isn't missing", () => {
+            expect(repository.missing(stubPeer.ip)).toBeFalse();
         });
 
-        it("should return true if an item isn't missing", () => {
-            expect(repo.missing(stubPeer.ip)).toBeTrue();
+        it("should return true if an item is missing", () => {
+            repository.flush();
+
+            expect(repository.missing(stubPeer.ip)).toBeTrue();
         });
     });
 
     it("should count all items", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.count()).toBe(1);
+        expect(repository.count()).toBe(1);
     });
 
     describe("isEmpty", () => {
         it("should return false if there are items", () => {
-            repo.set(stubPeer.ip, stubPeer);
-
-            expect(repo.isEmpty()).toBeFalse();
+            expect(repository.isEmpty()).toBeFalse();
         });
 
         it("should return true if there are no items", () => {
-            expect(repo.isEmpty()).toBeTrue();
+            repository.flush();
+
+            expect(repository.isEmpty()).toBeTrue();
         });
     });
 
     describe("isNotEmpty", () => {
         it("should return true if there are items", () => {
-            repo.set(stubPeer.ip, stubPeer);
-
-            expect(repo.isNotEmpty()).toBeTrue();
+            expect(repository.isNotEmpty()).toBeTrue();
         });
 
         it("should return false if there are no items", () => {
-            expect(repo.isNotEmpty()).toBeFalse();
+            repository.flush();
+
+            expect(repository.isNotEmpty()).toBeFalse();
         });
     });
 
     it("should return a random item", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.random()).toEqual(stubPeer);
+        expect(repository.random()).toEqual(stubPeer);
     });
 
     it("should turn the items into JSON", () => {
-        repo.set(stubPeer.ip, stubPeer);
-
-        expect(repo.toJson()).toEqual(JSON.stringify({ [stubPeer.ip]: stubPeer }));
+        expect(repository.toJson()).toEqual(JSON.stringify({ [stubPeer.ip]: stubPeer }));
     });
 });
--- __tests__/unit/core-p2p/peer-storage.test.ts
+++ __tests__/unit/core-p2p/peer-storage.test.ts
@@ -27,42 +27,136 @@ beforeEach(() => {
 });
 
 describe("PeerStorage", () => {
-    it("should get the peers", () => {
-        storage.setPeer(stubPeer);
+    describe("Real Peers", () => {
+        it("should get the peers", () => {
+            storage.setPeer(stubPeer);
 
-        expect(storage.getPeers()).toEqual([stubPeer]);
+            expect(storage.getPeers()).toEqual([stubPeer]);
+        });
+
+        it("should get the peer", () => {
+            storage.setPeer(stubPeer);
+
+            expect(storage.getPeer(stubPeer.ip)).toEqual(stubPeer);
+        });
+
+        it("should return true when it has a specific peers", () => {
+            storage.setPeer(stubPeer);
+
+            expect(storage.hasPeer(stubPeer.ip)).toBeTrue();
+        });
+
+        it("should return false when it doesn't have a peers", () => {
+            expect(storage.hasPeer(stubPeer.ip)).toBeFalse();
+        });
+
+        it("should return true when it has any peers", () => {
+            storage.setPeer(stubPeer);
+
+            expect(storage.hasPeers()).toBeTrue();
+        });
+
+        it("should return false when it has no peers", () => {
+            expect(storage.hasPeers()).toBeFalse();
+        });
+
+        it("should forgget a peer", () => {
+            storage.setPeer(stubPeer);
+
+            expect(storage.hasPeers()).toBeTrue();
+
+            storage.forgetPeer(stubPeer);
+
+            expect(storage.hasPeers()).toBeFalse();
+        });
     });
 
-    it("should get the peer", () => {
-        storage.setPeer(stubPeer);
+    describe("Pending Peers", () => {
+        it("should get the peers", () => {
+            storage.setPendingPeer(stubPeer);
 
-        expect(storage.getPeer(stubPeer.ip)).toEqual(stubPeer);
+            expect(storage.getPendingPeers()).toEqual([stubPeer]);
+        });
+
+        it("should get the peer", () => {
+            storage.setPendingPeer(stubPeer);
+
+            expect(storage.getPendingPeer(stubPeer.ip)).toEqual(stubPeer);
+        });
+
+        it("should return true when it has a specific peers", () => {
+            storage.setPendingPeer(stubPeer);
+
+            expect(storage.hasPendingPeer(stubPeer.ip)).toBeTrue();
+        });
+
+        it("should return false when it doesn't have a peers", () => {
+            expect(storage.hasPendingPeer(stubPeer.ip)).toBeFalse();
+        });
+
+        it("should return true when it has any peers", () => {
+            storage.setPendingPeer(stubPeer);
+
+            expect(storage.hasPendingPeers()).toBeTrue();
+        });
+
+        it("should return false when it has no peers", () => {
+            expect(storage.hasPendingPeers()).toBeFalse();
+        });
+
+        it("should forgget a peer", () => {
+            storage.setPendingPeer(stubPeer);
+
+            expect(storage.hasPendingPeers()).toBeTrue();
+
+            storage.forgetPendingPeer(stubPeer);
+
+            expect(storage.hasPendingPeers()).toBeFalse();
+        });
     });
 
-    it("should return true when it has peers", () => {
-        storage.setPeer(stubPeer);
+    describe("Suspended Peers", () => {
+        it("should get the peers", () => {
+            storage.setSuspendedPeer(stubSuspension);
 
-        expect(storage.hasPeers()).toBeTrue();
-    });
+            expect(storage.getSuspendedPeers()).toEqual([stubSuspension]);
+        });
 
-    it("should return false when it has no peer", () => {
-        expect(storage.hasPeers()).toBeFalse();
-    });
+        it("should get the peer", () => {
+            storage.setSuspendedPeer(stubSuspension);
 
-    it("should remove the peer", () => {
-        storage.setPeer(stubPeer);
+            expect(storage.getSuspendedPeer(stubPeer.ip)).toEqual(stubSuspension);
+        });
 
-        expect(storage.hasPeers()).toBeTrue();
+        it("should return true when it has a specific peers", () => {
+            storage.setSuspendedPeer(stubSuspension);
 
-        storage.forgetPeer(stubPeer);
+            expect(storage.hasSuspendedPeer(stubPeer.ip)).toBeTrue();
+        });
 
-        expect(storage.hasPeers()).toBeFalse();
-    });
+        it("should return false when it doesn't have a peers", () => {
+            expect(storage.hasSuspendedPeer(stubPeer.ip)).toBeFalse();
+        });
 
-    it("should get the suspended peers from guard.all()", () => {
-        storage.setSuspendedPeer(stubSuspension);
+        it("should return true when it has any peers", () => {
+            storage.setSuspendedPeer(stubSuspension);
 
-        expect(storage.getSuspendedPeers()).toEqual([stubSuspension]);
+            expect(storage.hasSuspendedPeers()).toBeTrue();
+        });
+
+        it("should return false when it has no peers", () => {
+            expect(storage.hasSuspendedPeers()).toBeFalse();
+        });
+
+        it("should forgget a peer", () => {
+            storage.setSuspendedPeer(stubSuspension);
+
+            expect(storage.hasSuspendedPeers()).toBeTrue();
+
+            storage.forgetSuspendedPeer(stubPeer);
+
+            expect(storage.hasSuspendedPeers()).toBeFalse();
+        });
     });
 
     it("should cache the peers into file", () => {
