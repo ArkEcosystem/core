37d9e688b66f560cee88070507b19c94adc5ad80
Edgar Goetzendorff <hello@dated.fun>
2019-04-06 04:07:17 +0200

    misc: rename voteBalance to votes

--- packages/core-api/src/versions/2/rounds/transformer.ts
+++ packages/core-api/src/versions/2/rounds/transformer.ts
@@ -3,6 +3,6 @@ import { bignumify } from "@arkecosystem/core-utils";
 export function transformRoundDelegate(model) {
     return {
         publicKey: model.publicKey,
-        voteBalance: +bignumify(model.balance).toFixed(),
+        votes: +bignumify(model.balance).toFixed(),
     };
 }
