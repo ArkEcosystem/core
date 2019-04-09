f60916fc46b1f66faf70ce6705d599f480538e6b
Brian Faust <faustbrian@users.noreply.github.com>
2019-04-09 05:38:20 +0300

    test(core-logger): unit tests for logger and manager (#2373)

--- __tests__/unit/core-logger/__stubs__/logger.ts
+++ __tests__/unit/core-logger/__stubs__/logger.ts
@@ -1,31 +1,39 @@
 import { AbstractLogger } from "../../../../packages/core-logger/src";
 
 export class Logger extends AbstractLogger {
+    protected logger = {
+        error: jest.fn(),
+        warn: jest.fn(),
+        info: jest.fn(),
+        debug: jest.fn(),
+        verbose: jest.fn(),
+    };
+
     public make(): any {
+        if (this.options.stubLogger) {
+            this.logger = this.options.stubLogger;
+        }
+
         return this;
     }
 
-    public error(message: string): void {
-        //
+    public error(message: string): boolean {
+        return this.log("error", message);
     }
 
-    public warn(message: string): void {
-        //
+    public warn(message: string): boolean {
+        return this.log("warn", message);
     }
 
-    public info(message: string): void {
-        //
+    public info(message: string): boolean {
+        return this.log("info", message);
     }
 
-    public debug(message: string): void {
-        //
+    public debug(message: string): boolean {
+        return this.log("debug", message);
     }
 
-    public verbose(message: string): void {
-        //
-    }
-
-    public suppressConsoleOutput(suppress: boolean): void {
-        //
+    public verbose(message: string): boolean {
+        return this.log("verbose", message);
     }
 }
