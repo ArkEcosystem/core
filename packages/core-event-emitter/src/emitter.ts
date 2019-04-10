085b5a829d338f602300077bc1b2156362a29aed
Brian Faust <faustbrian@users.noreply.github.com>
2019-04-10 13:20:05 +0300

    feat(core-event-emitter): add off method (#2396)

--- packages/core-event-emitter/src/emitter.ts
+++ packages/core-event-emitter/src/emitter.ts
@@ -8,23 +8,29 @@ export class EventEmitter {
     }
 
     public on(event: string | symbol, listener: (...args: any) => void): void {
-        this.ensureMaxListenerCount(event);
+        this.ensureListenerCount(event, maxListeners => maxListeners + 1);
 
         this.emitter.on(event, listener);
     }
 
     public once(event: string | symbol, listener: (...args: any) => void): void {
-        this.ensureMaxListenerCount(event);
+        this.ensureListenerCount(event, maxListeners => maxListeners + 1);
 
         this.emitter.once(event, listener);
     }
 
-    private ensureMaxListenerCount(event): void {
+    public off(event: string | symbol, listener: (...args: any) => void): void {
+        this.emitter.off(event, listener);
+
+        this.ensureListenerCount(event, maxListeners => maxListeners - 1);
+    }
+
+    private ensureListenerCount(event: string | symbol, count: (maxListeners: number) => number): void {
         const maxListeners = this.emitter.getMaxListeners();
         const listenerCount = this.emitter.listenerCount(event);
 
         if (listenerCount >= maxListeners) {
-            this.emitter.setMaxListeners(maxListeners + 1);
+            this.emitter.setMaxListeners(count(maxListeners));
         }
     }
 }
