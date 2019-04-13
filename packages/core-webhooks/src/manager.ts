fa94a17e60b7288af35897e584d8bc084b7ddd03
Brian Faust <hello@basecode.sh>
2019-04-13 18:53:25 +0300

    refactor(core-webhooks): add interfaces and types

--- packages/core-webhooks/src/manager.ts
+++ packages/core-webhooks/src/manager.ts
@@ -4,19 +4,20 @@ import { EventEmitter, Logger } from "@arkecosystem/core-interfaces";
 import { httpie } from "@arkecosystem/core-utils";
 import * as conditions from "./conditions";
 import { database } from "./database";
+import { IWebhook } from "./interfaces";
 
 export class WebhookManager {
     private readonly logger: Logger.ILogger = app.resolvePlugin<Logger.ILogger>("logger");
     private readonly emitter: EventEmitter.EventEmitter = app.resolvePlugin<EventEmitter.EventEmitter>("event-emitter");
 
-    public async setUp() {
+    public async setUp(): Promise<void> {
         for (const event of Object.values(ApplicationEvents)) {
             this.emitter.on(event, async payload => {
                 const { rows } = await database.findByEvent(event);
 
                 for (const webhook of this.getMatchingWebhooks(rows, payload)) {
                     try {
-                        const response = await httpie.post(webhook.target, {
+                        const { status } = await httpie.post(webhook.target, {
                             body: {
                                 timestamp: +new Date(),
                                 data: payload,
@@ -30,7 +31,7 @@ export class WebhookManager {
                         this.logger.debug(
                             `Webhooks Job ${webhook.id} completed! Event [${webhook.event}] has been transmitted to [${
                                 webhook.target
-                            }] with a status of [${response.status}].`,
+                            }] with a status of [${status}].`,
                         );
                     } catch (error) {
                         this.logger.error(`Webhooks Job ${webhook.id} failed: ${error.message}`);
@@ -40,31 +41,25 @@ export class WebhookManager {
         }
     }
 
-    private getMatchingWebhooks(webhooks, payload) {
-        const matches = [];
-
-        for (const webhook of webhooks) {
+    private getMatchingWebhooks(webhooks: IWebhook[], payload): IWebhook[] {
+        return webhooks.filter((webhook: IWebhook) => {
             if (!webhook.enabled) {
-                continue;
+                return false;
             }
 
             if (!webhook.conditions || (Array.isArray(webhook.conditions) && !webhook.conditions.length)) {
-                matches.push(webhook);
-
-                continue;
+                return true;
             }
 
             for (const condition of webhook.conditions) {
                 const satisfies = conditions[condition.condition];
 
-                if (!satisfies(payload[condition.key], condition.value)) {
-                    continue;
+                if (satisfies(payload[condition.key], condition.value)) {
+                    return true;
                 }
-
-                matches.push(webhook);
             }
-        }
 
-        return matches;
+            return false;
+        });
     }
 }
