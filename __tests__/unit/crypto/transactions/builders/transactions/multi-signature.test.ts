968475715308744b052e67d63cd44107b6e610e8
supaiku <supaiku@protonmail.com>
2019-04-17 14:55:46 +0200

    test: add case when inactive

--- __tests__/unit/crypto/transactions/builders/transactions/multi-signature.test.ts
+++ __tests__/unit/crypto/transactions/builders/transactions/multi-signature.test.ts
@@ -1,7 +1,7 @@
 import "jest-extended";
 
-import { slots } from "../../../../../../packages/crypto/src/crypto";
 import { TransactionTypes } from "../../../../../../packages/crypto/src/enums";
+import { TransactionVersionError } from "../../../../../../packages/crypto/src/errors";
 import { configManager } from "../../../../../../packages/crypto/src/managers";
 import { feeManager } from "../../../../../../packages/crypto/src/managers/fee";
 import { BuilderFactory } from "../../../../../../packages/crypto/src/transactions";
@@ -38,6 +38,23 @@ describe("Multi Signature Transaction", () => {
 
             expect(actual.build().verified).toBeTrue();
             expect(actual.verify()).toBeTrue();
+        });
+
+        it("should be invalid when aip11 is not active", () => {
+            configManager.getMilestone().aip11 = false;
+            const actual = builder
+                .multiSignatureAsset({
+                    publicKeys: [
+                        "039180ea4a8a803ee11ecb462bb8f9613fcdb5fe917e292dbcc73409f0e98f8f22",
+                        "028d3611c4f32feca3e6713992ae9387e18a0e01954046511878fe078703324dc0",
+                        "021d3932ab673230486d0f956d05b9e88791ee298d9af2d6df7d9ed5bb861c92dd",
+                    ],
+                    min: 2,
+                })
+                .senderPublicKey("039180ea4a8a803ee11ecb462bb8f9613fcdb5fe917e292dbcc73409f0e98f8f22");
+
+            expect(() => actual.multiSign("secret 1", 0)).toThrowError(TransactionVersionError);
+            configManager.getMilestone().aip11 = true;
         });
     });
 
@@ -89,13 +106,14 @@ describe("Multi Signature Transaction", () => {
                     ],
                     min: 2,
                 })
-                .senderPublicKey("039180ea4a8a803ee11ecb462bb8f9613fcdb5fe917e292dbcc73409f0e98f8f22")
+                .senderPublicKey("039180ea4a8a803ee11ecb462bb8f9613fcdb5fe917e292dbcc73409f0e98f8f22");
+
+            actual.data.timestamp = 0;
+
+            actual
                 .multiSign("secret 1", 0)
                 .multiSign("secret 2", 1)
                 .multiSign("secret 3", 2);
-
-            // @ts-ignore
-            slots.getTime = jest.fn(() => slots.beginEpochTime().toMilliseconds());
 
             expect(actual.data.signature).toBe(
                 "00bab66bbc4a6b9e350b641969c454fa3052ff6511e748aafbbb0511f8178e0039810a336149436d6d1ad407a65fc121e6246c3449086a5d295d868269eceaf62f014f01534036346f7442d6f2b0b88f8325fd296cfa0b521f9a56ba53c4df4718586ed7358a12b4e8943b0e7936f5cbf457b356918dd70b3d644c7fd7820cdbd4fc02c876f3697ffa8df485348c1b5d164e69ff182e98756c527f62711e4fbabc0d19913c0274c9550f07a3cc4ccb213143ee07bf0f8160d01c91301394eda0c458e7",
