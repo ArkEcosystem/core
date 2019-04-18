9aeba81c628162263f3b7b60f94b28669bb01841
Brian Faust <faustbrian@users.noreply.github.com>
2019-04-18 08:07:21 +0300

    refactor(core-event-emitter): extend the node.js event emitter (#2440)

--- packages/core-event-emitter/src/emitter.ts
+++ packages/core-event-emitter/src/emitter.ts
@@ -1,36 +1,38 @@
 import { EventEmitter as NativeEmitter } from "events";
 
-export class EventEmitter {
-    private readonly emitter: NativeEmitter = new NativeEmitter();
+export class EventEmitter extends NativeEmitter {
+    public on(event: string | symbol, listener: (...args: any) => void): this {
+        this.ensureListenerCount(event, (maxListeners: number) => maxListeners + 1);
 
-    public emit(event: string | symbol, args: any): boolean {
-        return this.emitter.emit(event, args);
+        return super.on(event, listener);
     }
 
-    public on(event: string | symbol, listener: (...args: any) => void): void {
-        this.ensureListenerCount(event, maxListeners => maxListeners + 1);
-
-        this.emitter.on(event, listener);
+    public off(event: string | symbol, listener: (...args: any) => void): this {
+        return this.removeListener(event, listener);
     }
 
-    public once(event: string | symbol, listener: (...args: any) => void): void {
-        this.ensureListenerCount(event, maxListeners => maxListeners + 1);
+    public once(event: string | symbol, listener: (...args: any) => void): this {
+        this.ensureListenerCount(event, (maxListeners: number) => maxListeners + 1);
 
-        this.emitter.once(event, listener);
+        return super.once(event, listener);
     }
 
-    public off(event: string | symbol, listener: (...args: any) => void): void {
-        this.emitter.off(event, listener);
+    public addListener(event: string | symbol, listener: (...args: any) => void): this {
+        return this.on(event, listener);
+    }
 
-        this.ensureListenerCount(event, maxListeners => maxListeners - 1);
+    public removeListener(event: string | symbol, listener: (...args: any) => void): this {
+        this.ensureListenerCount(event, (maxListeners: number) => maxListeners - 1);
+
+        return super.removeListener(event, listener);
     }
 
     private ensureListenerCount(event: string | symbol, count: (maxListeners: number) => number): void {
-        const maxListeners = this.emitter.getMaxListeners();
-        const listenerCount = this.emitter.listenerCount(event);
+        const maxListeners: number = this.getMaxListeners();
+        const listenerCount: number = this.listenerCount(event);
 
         if (listenerCount >= maxListeners) {
-            this.emitter.setMaxListeners(count(maxListeners));
+            this.setMaxListeners(count(maxListeners));
         }
     }
 }
