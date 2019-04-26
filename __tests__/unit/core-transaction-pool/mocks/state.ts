111f01c80bbbaa2c8e53bd0f9d8ffbb866d0a4eb
Brian Faust <hello@basecode.sh>
2019-04-26 17:38:46 +0300

    test: fix mocks

--- __tests__/unit/core-transaction-pool/mocks/state.ts
+++ __tests__/unit/core-transaction-pool/mocks/state.ts
@@ -2,6 +2,7 @@ export const state = {
     getStore: () => ({
         cacheTransactions: () => null,
         getLastBlock: () => ({ data: { height: 0 } }),
+        getLastHeight: () => 1,
         removeCachedTransactionIds: () => null,
     }),
 };
