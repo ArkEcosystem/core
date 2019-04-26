cca267895e959b1bb2e3cb1d6cdf4362493892e3
Brian Faust <hello@basecode.sh>
2019-04-26 15:14:00 +0300

    refactor(core-state): store blocks by id and height

--- __tests__/unit/core-state/stores/blocks.test.ts
+++ __tests__/unit/core-state/stores/blocks.test.ts
@@ -4,9 +4,22 @@ import { genesisBlock } from "../../../utils/config/testnet/genesisBlock";
 describe("BlockStore", () => {
     it("should push and get a block", () => {
         const store = new BlockStore(100);
-        store.push(genesisBlock);
+        store.set(genesisBlock);
 
         expect(store.count()).toBe(1);
         expect(store.get(genesisBlock.id)).toEqual(genesisBlock);
+        expect(store.get(genesisBlock.height)).toEqual(genesisBlock);
+    });
+
+    it("should return all ids and heights in the order they were inserted", () => {
+        const store = new BlockStore(5);
+
+        for (let i = 0; i < 5; i++) {
+            store.set({ id: i.toString(), height: i } as any);
+        }
+
+        expect(store.count()).toBe(5);
+        expect(store.getIds()).toEqual(["0", "1", "2", "3", "4"]);
+        expect(store.getHeights()).toEqual([0, 1, 2, 3, 4]);
     });
 });
--- packages/core-state/src/stores/blocks.ts
+++ packages/core-state/src/stores/blocks.ts
@@ -1,8 +1,64 @@
 import { OrderedCappedMap } from "@arkecosystem/core-utils";
 import { Interfaces } from "@arkecosystem/crypto";
 
-export class BlockStore extends OrderedCappedMap<string, Interfaces.IBlockData> {
-    public push(value: Interfaces.IBlockData): void {
-        this.set(value.id, value);
+export class BlockStore {
+    private readonly byId: OrderedCappedMap<string, Interfaces.IBlockData>;
+    private readonly byHeight: OrderedCappedMap<number, Interfaces.IBlockData>;
+
+    public constructor(maxSize: number) {
+        this.byId = new OrderedCappedMap<string, Interfaces.IBlockData>(maxSize);
+        this.byHeight = new OrderedCappedMap<number, Interfaces.IBlockData>(maxSize);
+    }
+
+    public get(key: string | number): Interfaces.IBlockData {
+        return typeof key === "string" ? this.byId.get(key) : this.byHeight.get(key);
+    }
+
+    public set(value: Interfaces.IBlockData): void {
+        this.byId.set(value.id, value);
+        this.byHeight.set(value.height, value);
+    }
+
+    public has(value: Interfaces.IBlockData): boolean {
+        return this.byId.has(value.id) || this.byHeight.has(value.height);
+    }
+
+    public delete(value: Interfaces.IBlockData): void {
+        this.byId.delete(value.id);
+        this.byHeight.delete(value.height);
+    }
+
+    public clear(): void {
+        this.byId.clear();
+        this.byHeight.clear();
+    }
+
+    public resize(maxSize: number): void {
+        this.byId.resize(maxSize);
+        this.byHeight.resize(maxSize);
+    }
+
+    public first(): Interfaces.IBlockData {
+        return this.byId.first();
+    }
+
+    public last(): Interfaces.IBlockData {
+        return this.byId.last();
+    }
+
+    public values(): Interfaces.IBlockData[] {
+        return this.byId.values();
+    }
+
+    public count(): number {
+        return this.byId.count();
+    }
+
+    public getIds(): string[] {
+        return this.byId.keys();
+    }
+
+    public getHeights(): number[] {
+        return this.byHeight.keys();
     }
 }
