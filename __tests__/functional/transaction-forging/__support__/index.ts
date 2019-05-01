3c1c64a4b4db71bfbb04ca112d8fc84f22d6caa6
Brian Faust <hello@basecode.sh>
2019-05-01 08:31:44 +0300

    test: expect undefined instead of null

--- __tests__/functional/transaction-forging/__support__/index.ts
+++ __tests__/functional/transaction-forging/__support__/index.ts
@@ -60,7 +60,7 @@ export async function expectAcceptAndBroadcast(transactions, id): Promise<void> 
         console.log(body.errors);
     }
 
-    expect(body.errors).toBeNull();
+    expect(body.errors).toBeUndefined();
     expect(body.data.accept).toContain(id);
     expect(body.data.broadcast).toContain(id);
 }
