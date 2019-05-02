7396f93a5209493639e28f3785b00656bbb1f292
supaiku <supaiku@protonmail.com>
2019-05-02 21:01:06 +0200

    refactor: process genesis block separately

--- packages/core-blockchain/src/replay/replay-blockchain.ts
+++ packages/core-blockchain/src/replay/replay-blockchain.ts
@@ -2,13 +2,14 @@ import { app } from "@arkecosystem/core-container";
 import { Database, Logger, P2P, State, TransactionPool } from "@arkecosystem/core-interfaces";
 import { Wallets } from "@arkecosystem/core-state";
 import { roundCalculator } from "@arkecosystem/core-utils";
-import { Blocks, Enums, Interfaces, Utils } from "@arkecosystem/crypto";
+import { Blocks, Enums, Interfaces, Managers, Utils } from "@arkecosystem/crypto";
 import { Blockchain } from "../blockchain";
 import { BlockProcessorResult } from "../processor";
 import { MemoryDatabaseService } from "./memory-database-service";
 
 export class ReplayBlockchain extends Blockchain {
     private walletManager: Wallets.WalletManager;
+    private chunkSize: number = 20000;
 
     private memoryDatabase: Database.IDatabaseService;
     public get database(): Database.IDatabaseService {
@@ -44,21 +45,27 @@ export class ReplayBlockchain extends Blockchain {
         return;
     }
 
+    // TODO: support dynamic start height
     public async replay(startHeight = 1, endHeight: number = -1): Promise<void> {
-        const logger = app.resolvePlugin<Logger.ILogger>("logger");
-        const database = app.resolvePlugin<Database.IDatabaseService>("database");
+        const logger: Logger.ILogger = app.resolvePlugin<Logger.ILogger>("logger");
+        const database: Database.IDatabaseService = app.resolvePlugin<Database.IDatabaseService>("database");
 
         logger.info("Starting replay...");
 
-        const lastBlock = await database.getLastBlock();
+        const lastBlock: Interfaces.IBlock = await database.getLastBlock();
+        let targetHeight = lastBlock.data.height;
 
-        const targetHeight = lastBlock.data.height;
-        const chunkSize = 20000;
+        if (endHeight !== -1 && endHeight < lastBlock.data.height) {
+            targetHeight = endHeight;
+        }
+
+        await this.processGenesisBlock();
+        logger.info("Applied geneis block.");
 
         console.time("REPLAY");
-        for (let i = startHeight || 1; i < targetHeight; i += chunkSize) {
+        for (let i = startHeight + 1; i < targetHeight; i += this.chunkSize) {
             console.time("CHUNK");
-            const blocks = await database.getBlocks(i, chunkSize);
+            const blocks = await database.getBlocks(i, this.chunkSize);
             let activeDelegates: State.IDelegateWallet[] = [];
 
             for (const blockData of blocks) {
@@ -69,16 +76,10 @@ export class ReplayBlockchain extends Blockchain {
 
                     console.log(`Processing block ${height}...`);
 
-                    if (height === 1) {
-                        this.processGenesisBlock(blockData);
-                    } else {
-                        const block = Blocks.BlockFactory.fromData(blockData);
-                        const result = await this.blockProcessor.process(block);
-                        if (result !== BlockProcessorResult.Accepted) {
-                            throw new Error("....");
-                        }
-
-                        //  walletManager.applyBlock(block);
+                    const block = Blocks.BlockFactory.fromData(blockData);
+                    const result = await this.blockProcessor.process(block);
+                    if (result !== BlockProcessorResult.Accepted) {
+                        throw new Error("....");
                     }
 
                     if (roundCalculator.isNewRound(nextHeight)) {
@@ -100,29 +101,34 @@ export class ReplayBlockchain extends Blockchain {
         console.timeEnd("REPLAY");
     }
 
-    private processGenesisBlock(block: Interfaces.IBlockData): void {
-        const { transactions } = block;
+    private async processGenesisBlock(): Promise<void> {
+        const genesisBlock = Blocks.BlockFactory.fromJson(Managers.configManager.get("genesisBlock"));
+        const database: Database.IDatabaseService = app.resolvePlugin<Database.IDatabaseService>("database");
+
+        const { transactions } = genesisBlock;
         for (const transaction of transactions) {
             if (transaction.type === Enums.TransactionTypes.Transfer) {
-                const recipient = this.walletManager.findByAddress(transaction.recipientId);
-                recipient.balance = new Utils.BigNumber(transaction.amount);
+                const recipient = this.walletManager.findByAddress(transaction.data.recipientId);
+                recipient.balance = new Utils.BigNumber(transaction.data.amount);
             }
         }
 
         for (const transaction of transactions) {
-            const sender = this.walletManager.findByPublicKey(transaction.senderPublicKey);
-            sender.balance = sender.balance.minus(transaction.amount).minus(transaction.fee);
+            const sender = this.walletManager.findByPublicKey(transaction.data.senderPublicKey);
+            sender.balance = sender.balance.minus(transaction.data.amount).minus(transaction.data.fee);
 
             if (transaction.type === Enums.TransactionTypes.DelegateRegistration) {
-                sender.username = transaction.asset.delegate.username;
+                sender.username = transaction.data.asset.delegate.username;
                 this.walletManager.reindex(sender);
-            }
-
-            if (sender.balance.isLessThan(0)) {
-                //  this.logger.warn(`Negative balance: ${sender}`);
             }
         }
 
-        this.state.setLastBlock(Blocks.BlockFactory.fromData(block));
+        this.state.setLastBlock(genesisBlock);
+
+        // Initialize the very first round
+        const roundInfo = roundCalculator.calculateRound(1);
+        const delegates = this.walletManager.loadActiveDelegateList(roundInfo);
+        const activeDelegates = await database.getActiveDelegates(roundInfo, delegates);
+        (database as any).forgingDelegates = activeDelegates;
     }
 }
