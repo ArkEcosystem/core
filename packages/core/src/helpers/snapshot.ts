f5c10c565a07b62ba187d167c95ed6eebd417828
Brian Faust <faustbrian@users.noreply.github.com>
2019-05-02 11:27:34 +0200

    feat(core): require the user to choose a snapshot if the blocks flag is missing (#2522)

--- packages/core/src/helpers/snapshot.ts
+++ packages/core/src/helpers/snapshot.ts
@@ -1,5 +1,8 @@
 import { app } from "@arkecosystem/core-container";
 import { Container } from "@arkecosystem/core-interfaces";
+import { lstatSync, readdirSync } from "fs";
+import prompts from "prompts";
+import { CommandFlags } from "../types";
 
 // tslint:disable-next-line:no-var-requires
 const { version } = require("../../package.json");
@@ -16,4 +19,41 @@ export const setUpLite = async (options): Promise<Container.IContainer> => {
     });
 
     return app;
+};
+
+export const chooseSnapshot = async (flags: CommandFlags, message: string) => {
+    const source: string = `${process.env.CORE_PATH_DATA}/snapshots`;
+
+    const snapshots: string[] = readdirSync(source).filter((name: string) =>
+        lstatSync(`${source}/${name}`).isDirectory(),
+    );
+
+    if (!snapshots) {
+        throw new Error("Failed to find any snapshots.");
+    }
+
+    if (snapshots.length === 1) {
+        flags.blocks = snapshots[0];
+        return;
+    }
+
+    const response = await prompts([
+        {
+            type: "select",
+            name: "blocks",
+            message,
+            choices: snapshots.map((name: string) => ({ title: name, value: name })),
+        },
+        {
+            type: "confirm",
+            name: "confirm",
+            message: "Can you confirm?",
+        },
+    ]);
+
+    if (!response.blocks) {
+        throw new Error("Please select a snapshot and try again.");
+    }
+
+    flags.blocks = response.blocks;
 };
