8198cbc40ac7a34bb77b58166673510ddcb9f7e0
supaiku <supaiku@protonmail.com>
2019-05-07 15:24:12 +0200

    fix: return empty array instead of undefined

--- packages/core/src/process-manager.ts
+++ packages/core/src/process-manager.ts
@@ -5,6 +5,10 @@ class ProcessManager extends Foreman {
     public restart(id: ProcessIdentifier): ExecaReturns {
         return super.restart(id, ["--update-env"]);
     }
+
+    public list(): Array<Record<string, any>> {
+        return super.list() || [];
+    }
 }
 
 export const processManager = new ProcessManager();
