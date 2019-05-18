f8b52241820d600961a3ffcb5159a180c67fe12f
Zaraki <fpalazo@gmail.com>
2019-05-18 14:32:04 +0200

    Fixed docker configuration for forger

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -8,34 +8,11 @@ yarn --cwd /home/node/core ark config:publish --network=$NETWORK
 sudo rm -f /home/node/.config/ark-core/$NETWORK/.env
 
 if [ "$MODE" = "forger" ]; then
-  SECRET=`openssl rsautl -decrypt -inkey /run/secrets/secret.key -in /run/secrets/secret.dat`
-  CORE_FORGER_PASSWORD=`openssl rsautl -decrypt -inkey /run/secrets/bip.key -in /run/secrets/bip.dat`
-
-  # configure
-  if [ -n "$SECRET" ] && [ -n "$CORE_FORGER_PASSWORD" ]; then
-    yarn --cwd /home/node/core ark config:forger:bip38 --bip39 "$SECRET" --password "$CORE_FORGER_PASSWORD"
-  elif [ "$MODE" = "forger" ] && [ -z "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-    echo "set SECRET and/or CORE_FORGER_PASWORD if you want to run a forger"
-    exit
-  elif [ -n "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-    yarn --cwd /home/node/core ark config:forger:bip39 --bip39 "$SECRET"
-  fi
+    echo "{\"secrets\":[\"$DELEGATE_PASSPHRASE=\"]}" > /home/node/.config/ark-core/$NETWORK/delegates.json
+    yarn --cwd /home/node/core ark core:start --no-daemon
 fi
 
 # relay
 if [[ "$MODE" = "relay" ]]; then
     yarn --cwd /home/node/core ark relay:start --no-daemon
-fi
-
-# forging
-if [ "$MODE" = "forger" ] && [ -n "$SECRET" ] && [ -n "$CORE_FORGER_PASSWORD" ]; then
-    export CORE_FORGER_BIP38=$(grep bip38 /home/node/.config/ark-core/$NETWORK/delegates.json | awk '{print $2}' | tr -d '"')
-    export CORE_FORGER_PASSWORD
-    sudo rm -rf /run/secrets/*
-    yarn --cwd /home/node/core ark core:start --no-daemon
-elif [ "$MODE" = "forger" ] && [ -z "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-    echo "set SECRET and/or CORE_FORGER_PASWORD if you want to run a forger"
-    exit
-elif [ "$MODE" = "forger" ] && [ -n "$SECRET" ] && [ -z "$CORE_FORGER_PASSWORD" ]; then
-    yarn --cwd /home/node/core ark core:start --no-daemon
 fi
