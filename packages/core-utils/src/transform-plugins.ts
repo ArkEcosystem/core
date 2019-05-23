ed00bdccb6bcfe31d1946e6e826c41a6333b9360
Brian Faust <hello@basecode.sh>
2019-05-23 09:55:52 +0300

    fix(core-wallet-api): return plugins with package names

--- packages/core-utils/src/transform-plugins.ts
+++ packages/core-utils/src/transform-plugins.ts
@@ -11,7 +11,7 @@ export const transformPlugins = (plugins): P2P.IPeerPlugins => {
         const port: number = Number(options.port);
         const enabled: boolean = !!options.enabled;
 
-        if (isNaN(port) || name === "p2p") {
+        if (isNaN(port) || name.includes("core-p2p")) {
             continue;
         }
 
