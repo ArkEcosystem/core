21b69f1fa652db485d9125f7750b1d28a7ca2e29
Air1 <erwann@ark.io>
2019-05-28 18:01:01 +0400

    chore: yarn run-sync + update sync.sh

--- __tests__/e2e/lib/utils/sync.sh
+++ __tests__/e2e/lib/utils/sync.sh
@@ -15,6 +15,8 @@ sudo apt-get update && sudo apt-get install yarn
 
 # install needed packages
 sudo apt-get install docker.io python build-essential
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # initialize docker swarm
 hostname -I | awk '{print $1}' | xargs -I {} docker swarm init --advertise-addr {}
