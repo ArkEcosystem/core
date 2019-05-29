1b927903c7ec79100dadaf17b5fa879a137e9337
Brian Faust <hello@basecode.sh>
2019-05-29 18:25:33 +0300

    test(core-webhooks): remove unused helpers

--- __tests__/unit/core-webhooks/__support__/utils.ts
+++ __tests__/unit/core-webhooks/__support__/utils.ts
@@ -6,31 +6,18 @@ export const request = async (server, method, path, payload = {}) => {
     return { body: response.result, status: response.statusCode };
 };
 
-export const expectJson = response => expect(response.body).toBeObject();
 export const expectStatus = (response, code) => expect(response.status).toBe(code);
 export const expectResource = response => expect(response.body.data).toBeObject();
 export const expectCollection = response => expect(Array.isArray(response.body.data)).toBe(true);
 
-export const expectPaginator = response => {
-    expect(response.body.meta).toBeObject();
-    expect(response.body.meta).toHaveProperty("count");
-    expect(response.body.meta).toHaveProperty("pageCount");
-    expect(response.body.meta).toHaveProperty("totalCount");
-    expect(response.body.meta).toHaveProperty("next");
-    expect(response.body.meta).toHaveProperty("previous");
-    expect(response.body.meta).toHaveProperty("self");
-    expect(response.body.meta).toHaveProperty("first");
-    expect(response.body.meta).toHaveProperty("last");
-};
-
 export const expectSuccessful = (response, statusCode = 200) => {
     expectStatus(response, statusCode);
-    expectJson(response);
+    expect(response.body).toBeObject();
 };
 
 export const expectError = (response, statusCode = 404) => {
     expectStatus(response, statusCode);
-    expectJson(response);
+    expect(response.body).toBeObject();
     expect(response.body.statusCode).toBeNumber();
     expect(response.body.error).toBeString();
     expect(response.body.message).toBeString();
--- __tests__/unit/core-webhooks/server.test.ts
+++ __tests__/unit/core-webhooks/server.test.ts
@@ -1,12 +1,27 @@
 import "jest-extended";
 
+import { app } from "@arkecosystem/core-container";
 import { ApplicationEvents } from "@arkecosystem/core-event-emitter";
 import { Server } from "http";
-import { setUp } from "./__support__/setup";
+import { tmpdir } from "os";
+import { database } from "../../../packages/core-webhooks/src/database";
+import { startServer } from "../../../packages/core-webhooks/src/server";
 import * as utils from "./__support__/utils";
 
 let server: Server;
-beforeAll(async () => (server = await setUp()));
+beforeAll(async () => {
+    process.env.CORE_PATH_CACHE = tmpdir();
+
+    app.resolvePlugin = jest.fn().mockReturnValue(console);
+
+    database.make();
+
+    server = await startServer({
+        host: "0.0.0.0",
+        port: 4004,
+        whitelist: ["127.0.0.1", "::ffff:127.0.0.1"],
+    });
+});
 
 const postData = {
     event: ApplicationEvents.BlockForged,
