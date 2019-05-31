c94bdb93c52f6b62149af44d2893e5573b29ee28
Brian Faust <hello@basecode.sh>
2019-05-31 17:30:48 +0300

    fix(core-exchange-json-rpc): remove sqlite:// from database path

--- packages/core-exchange-json-rpc/src/defaults.ts
+++ packages/core-exchange-json-rpc/src/defaults.ts
@@ -4,5 +4,5 @@ export const defaults = {
     port: process.env.CORE_EXCHANGE_JSON_RPC_PORT || 8080,
     allowRemote: false,
     whitelist: ["127.0.0.1", "::ffff:127.0.0.1"],
-    database: process.env.CORE_EXCHANGE_JSON_RPC_DATABASE || `sqlite://${process.env.CORE_PATH_DATA}/json-rpc.sqlite`,
+    database: process.env.CORE_EXCHANGE_JSON_RPC_DATABASE || `${process.env.CORE_PATH_DATA}/json-rpc.sqlite`,
 };
