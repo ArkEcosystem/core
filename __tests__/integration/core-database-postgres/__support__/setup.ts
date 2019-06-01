afc78f57807a54daf5177f300eb4b60f480d2c5b
Brian Faust <hello@basecode.sh>
2019-06-01 08:43:09 +0300

    test: reset database before api and postgres tests

--- __tests__/integration/core-database-postgres/__support__/setup.ts
+++ __tests__/integration/core-database-postgres/__support__/setup.ts
@@ -11,6 +11,8 @@ export const setUp = async () => {
         exclude: ["@arkecosystem/core-database-postgres"],
     });
 
+    process.env.RESET_DATABASE = "1";
+
     await registerWithContainer(pluginDatabase);
 
     await registerWithContainer(plugin, {
