4e23c8aedcf5555fc57ffa8d11f222e87ead75b5
supaiku <supaiku@protonmail.com>
2019-06-08 03:22:50 +0200

    refactor: custom wallet api port

--- packages/core-wallet-api/src/plugin.ts
+++ packages/core-wallet-api/src/plugin.ts
@@ -1,13 +1,19 @@
 import { Container, Logger } from "@arkecosystem/core-interfaces";
+import { defaults } from "./defaults";
 import { startServer } from "./server";
 
 export const plugin: Container.IPluginDescriptor = {
     pkg: require("../package.json"),
+    defaults,
     alias: "wallet-api",
-    async register(container: Container.IContainer) {
-        container.resolvePlugin<Logger.ILogger>("logger").info("Starting Wallet API");
+    async register(container: Container.IContainer, options) {
+        if (!options.enabled) {
+            container.resolvePlugin<Logger.ILogger>("logger").info("Wallet API is disabled");
+            return undefined;
+        }
 
-        return startServer();
+        container.resolvePlugin<Logger.ILogger>("logger").info("Starting Wallet API");
+        return startServer(options.server);
     },
     async deregister(container: Container.IContainer) {
         container.resolvePlugin<Logger.ILogger>("logger").info("Stopping Wallet API");
