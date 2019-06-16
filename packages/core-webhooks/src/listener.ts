b3bafa1ded55a659b0a385c4ff1f3b33f043a3c3
Brian Faust <hello@basecode.sh>
2019-06-16 12:01:29 +0300

    feat(core-webhooks): add option to configure request timeout

--- packages/core-webhooks/src/listener.ts
+++ packages/core-webhooks/src/listener.ts
@@ -44,6 +44,7 @@ export const startListeners = (): void => {
                         headers: {
                             Authorization: webhook.token,
                         },
+                        timeout: app.resolveOptions("event-emitter").timeout,
                     });
 
                     app.resolvePlugin<Logger.ILogger>("logger").debug(
