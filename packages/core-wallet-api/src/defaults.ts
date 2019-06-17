8bf8d207160def41aae9f04ecc164335ae96f44e
Brian Faust <faustbrian@users.noreply.github.com>
2019-06-17 17:56:06 +0300

    Update defaults.ts

--- packages/core-wallet-api/src/defaults.ts
+++ packages/core-wallet-api/src/defaults.ts
@@ -1,5 +1,4 @@
 export const defaults = {
-    enabled: !process.env.CORE_WALLET_API_DISABLED,
     server: {
         host: process.env.CORE_WALLET_API_HOST || "0.0.0.0",
         port: process.env.CORE_WALLET_API_PORT || 4040,
