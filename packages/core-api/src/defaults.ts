8311a01f06a8218ac4f623954cd8c8fe2778cd76
Brian Faust <faustbrian@users.noreply.github.com>
2019-06-17 05:44:06 +0300

    fix(core-api): use CORE_API_DISABLED variable in defaults

--- packages/core-api/src/defaults.ts
+++ packages/core-api/src/defaults.ts
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 
 export const defaults = {
-    enabled: false,
+    enabled: !process.env.CORE_API_DISABLED,
     host: process.env.CORE_API_HOST || "0.0.0.0",
     port: process.env.CORE_API_PORT || 4003,
     cache: {
