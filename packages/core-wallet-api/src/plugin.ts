12cb449c3e204fc0f30fd73d77448e3be894063d
Brian Faust <faustbrian@users.noreply.github.com>
2019-06-17 17:57:12 +0300

    Update plugin.ts

--- packages/core-wallet-api/src/plugin.ts
+++ packages/core-wallet-api/src/plugin.ts
@@ -7,18 +7,9 @@ export const plugin: Container.IPluginDescriptor = {
     defaults,
     alias: "wallet-api",
     async register(container: Container.IContainer, options) {
-        if (!options.enabled) {
-            container.resolvePlugin<Logger.ILogger>("logger").info("Wallet API is disabled");
-            return undefined;
-        }
-
-        container.resolvePlugin<Logger.ILogger>("logger").info("Starting Wallet API");
         return startServer(options.server);
     },
     async deregister(container: Container.IContainer, options) {
-        if (options.enabled) {
-            container.resolvePlugin<Logger.ILogger>("logger").info("Stopping Wallet API");
-            await container.resolvePlugin("wallet-api").stop();
-        }
+        await container.resolvePlugin("wallet-api").stop();
     },
 };
