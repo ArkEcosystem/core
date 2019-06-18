24fa281ab9b952cf30fe18511e7b18f0d91f6f1d
Brian Faust <faustbrian@users.noreply.github.com>
2019-06-18 05:41:22 +0300

    refactor(core-wallet-api): always keep the Wallet API enabled (#2715)

--- packages/core-wallet-api/src/plugin.ts
+++ packages/core-wallet-api/src/plugin.ts
@@ -1,4 +1,4 @@
-import { Container, Logger } from "@arkecosystem/core-interfaces";
+import { Container } from "@arkecosystem/core-interfaces";
 import { defaults } from "./defaults";
 import { startServer } from "./server";
 
@@ -7,18 +7,9 @@ export const plugin: Container.IPluginDescriptor = {
     defaults,
     alias: "wallet-api",
     async register(container: Container.IContainer, options) {
-        if (!options.enabled) {
-            container.resolvePlugin<Logger.ILogger>("logger").info("Wallet API is disabled");
-            return undefined;
-        }
-
-        container.resolvePlugin<Logger.ILogger>("logger").info("Starting Wallet API");
         return startServer(options.server);
     },
     async deregister(container: Container.IContainer, options) {
-        if (options.enabled) {
-            container.resolvePlugin<Logger.ILogger>("logger").info("Stopping Wallet API");
-            await container.resolvePlugin("wallet-api").stop();
-        }
+        await container.resolvePlugin("wallet-api").stop();
     },
 };
