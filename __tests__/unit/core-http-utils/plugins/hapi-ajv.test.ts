b5973cc564cba74ac8310caf3fd1d456441dca35
Darius Roberts <darius.roberts@gmail.com>
2019-06-23 11:32:49 -0700

    #1763 proposed unit test for core-http-utils plugins

--- __tests__/unit/core-http-utils/plugins/hapi-ajv.test.ts
+++ __tests__/unit/core-http-utils/plugins/hapi-ajv.test.ts
@@ -1,24 +1,50 @@
-// import { mocked } from 'ts-jest/utils';
-import "../mocks/boom";
-import "../mocks/core-container";
-import "../mocks/server";
-import "../mocks/validation";
-
+// import "../mocks/validation";
+// import "../mocks/boom";
 import { Validation } from "@arkecosystem/crypto";
-import Boom from "@hapi/boom";
-import { Server } from "@hapi/hapi";
+import Hapi from "@hapi/hapi";
 import { hapiAjv } from "../../../../packages/core-http-utils/src/plugins/hapi-ajv";
 
-describe("Plugins - HAPI AJV", () => {
-    let server;
-    describe("register", () => {
-        beforeAll(async () => {
-            server = new Server();
-            await hapiAjv.register(server, {});
+const FakeServer = { ext: jest.fn() };
+const server: jest.Mocked<Hapi.Server> = FakeServer as any;
+
+describe("register", () => {
+    it("calls 'ext'", async () => {
+        await hapiAjv.register(server, {});
+        expect(hapiAjv.name).toEqual("hapi-ajv");
+        expect(server.ext).toHaveBeenCalled();
+        expect(server.ext.mock.calls[0][0].type).toEqual("onPreHandler");
+    });
+    describe("ext callback", () => {
+        let h;
+        let request;
+        beforeEach(() => {
+            hapiAjv.register(server, {});
         });
-        it("validation", async () => {
-            // mock server implements ext
-            expect(server.ext).toBeCalled();
+        it("should payloadSchema", async () => {
+            h = { continue: "continue" } as any;
+            request = { route: { settings: { plugins: {} } } } as any;
+            const validate = jest.spyOn(Validation.validator, "validate");
+            const actual = server.ext.mock.calls[0][0].method(request, h);
+
+            expect(validate).not.toBeCalled();
+            expect(actual).toEqual("continue");
         });
+        it("should payloadSchema", async () => {
+            h = { continue: "continue" } as any;
+            request = {
+                payload: "bar",
+                route: { settings: { plugins: { "hapi-ajv": { payloadSchema: "foo" } } } },
+            } as any;
+            const rtn = [true, [{ dataPath: "dataPath", message: "message" }]] as any;
+            const validate = jest.spyOn(Validation.validator, "validate").mockReturnValue(rtn);
+            const actual = server.ext.mock.calls[1][0].method(request, h);
+            expect(validate).toHaveBeenCalledWith("foo", "bar");
+            expect(actual).toEqual("continue");
+        });
+        /*
+        it("should querySchema", async () => {
+            
+        });
+*/
     });
 });
