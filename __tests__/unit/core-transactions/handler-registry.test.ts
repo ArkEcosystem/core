7e1c6911bdae598a7bdad17ef2321660c93ce42c
Air1 <erwann@ark.io>
2019-07-04 17:06:24 +0400

    test: fix tests and typescript errors

--- __tests__/unit/core-transactions/handler-registry.test.ts
+++ __tests__/unit/core-transactions/handler-registry.test.ts
@@ -72,7 +72,7 @@ class TestTransactionHandler extends TransactionHandler {
     public apply(transaction: Transactions.Transaction, walletManager: State.IWalletManager): void {
         return;
     }
-    public revert(transaction: Transactions.Transaction, wallet: State.IWalletManager): void {
+    public async revert(transaction: Transactions.Transaction, wallet: State.IWalletManager): Promise<void> {
         return;
     }
 
