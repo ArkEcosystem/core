3a27fab1ebf5c89eb3e10b7d251adb8f08129226
supaiku <supaiku@protonmail.com>
2019-07-05 19:34:04 +0200

    fix: missing recipient_id

--- packages/core-database-postgres/src/repositories/transactions.ts
+++ packages/core-database-postgres/src/repositories/transactions.ts
@@ -1,3 +1,4 @@
+import { app } from "@arkecosystem/core-container";
 import { Database, State } from "@arkecosystem/core-interfaces";
 import { Crypto, Enums, Interfaces, Utils } from "@arkecosystem/crypto";
 import dayjs from "dayjs";
@@ -29,7 +30,7 @@ export class TransactionsRepository extends Repository implements Database.ITran
 
             if (participants.length > 0) {
                 const [first, last] = participants;
-                for (const query of [ selectQuery, selectQueryCount ]) {
+                for (const query of [selectQuery, selectQueryCount]) {
                     query.where(this.query[this.propToColumnName(first.field)][first.operator](first.value));
                 }
 
@@ -39,26 +40,39 @@ export class TransactionsRepository extends Repository implements Database.ITran
                     );
 
                     if (usesInOperator) {
-                        for (const query of [ selectQuery, selectQueryCount ]) {
+                        for (const query of [selectQuery, selectQueryCount]) {
                             query.or(this.query[this.propToColumnName(last.field)][last.operator](last.value));
                         }
                     } else {
                         // This search is 1 `senderPublicKey` and 1 `recipientId`
-                        for (const query of [ selectQuery, selectQueryCount ]) {
+                        for (const query of [selectQuery, selectQueryCount]) {
                             query.and(this.query[this.propToColumnName(last.field)][last.operator](last.value));
                         }
+                    }
+                } else if (first.field === "recipientId" && first.operator === Database.SearchOperator.OP_EQ) {
+                    // Workaround to include transactions (e.g. type 2) where the recipient_id is missing in the database
+                    const walletManager: State.IWalletManager = app.resolvePlugin<Database.IDatabaseService>("database")
+                        .walletManager;
+                    const recipientWallet: State.IWallet = walletManager.findByAddress(first.value);
+
+                    for (const query of [selectQuery, selectQueryCount]) {
+                        query.or(
+                            this.query.sender_public_key
+                                .equals(recipientWallet.publicKey)
+                                .and(this.query.recipient_id.isNull()),
+                        );
                     }
                 }
             } else if (rest.length) {
                 const first = rest.shift();
 
-                for (const query of [ selectQuery, selectQueryCount ]) {
+                for (const query of [selectQuery, selectQueryCount]) {
                     query.where(this.query[this.propToColumnName(first.field)][first.operator](first.value));
                 }
             }
 
             for (const condition of rest) {
-                for (const query of [ selectQuery, selectQueryCount ]) {
+                for (const query of [selectQuery, selectQueryCount]) {
                     query.and(this.query[this.propToColumnName(condition.field)][condition.operator](condition.value));
                 }
             }
@@ -156,9 +170,11 @@ export class TransactionsRepository extends Repository implements Database.ITran
         const selectQuery = this.query.select();
         const selectQueryCount = this.query.select(this.query.count().as("cnt"));
 
-        for (const query of [ selectQuery, selectQueryCount ]) {
-            query.from(this.query).where(this.query.sender_public_key.equals(wallet.publicKey))
-            .or(this.query.recipient_id.equals(wallet.address));
+        for (const query of [selectQuery, selectQueryCount]) {
+            query
+                .from(this.query)
+                .where(this.query.sender_public_key.equals(wallet.publicKey))
+                .or(this.query.recipient_id.equals(wallet.address));
         }
 
         return this.findManyWithCount(selectQuery, selectQueryCount, paginate, orderBy);
