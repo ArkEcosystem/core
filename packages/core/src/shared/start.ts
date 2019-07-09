78098b9ebd1b7875dbf76c8a35fbf773a6f5e253
supaiku <supaiku@protonmail.com>
2019-07-09 03:09:34 +0200

    refactor: take totalmem into account

--- packages/core/src/shared/start.ts
+++ packages/core/src/shared/start.ts
@@ -1,6 +1,6 @@
 import cli from "cli-ux";
 
-import { freemem } from "os";
+import { freemem, totalmem } from "os";
 import { BaseCommand } from "../commands/command";
 import { processManager } from "../process-manager";
 import { CommandFlags, ProcessOptions } from "../types";
@@ -17,7 +17,7 @@ export abstract class AbstractStartCommand extends BaseCommand {
     protected abstract async runProcess(flags: CommandFlags): Promise<void>;
 
     protected async runWithPm2(options: ProcessOptions, flags: CommandFlags) {
-        const processName = options.name;
+        const processName: string = options.name;
 
         try {
             if (processManager.has(processName)) {
@@ -38,8 +38,9 @@ export abstract class AbstractStartCommand extends BaseCommand {
 
             flagsProcess.name = processName;
 
-            const mem = freemem() / Math.pow(1024, 3);
-            const potato = mem < 1;
+            const totalMemGb: number = totalmem() / Math.pow(1024, 3);
+            const freeMemGb: number = freemem() / Math.pow(1024, 3);
+            const potato: boolean = totalMemGb < 2 || freeMemGb < 1.5;
 
             processManager.start(
                 {
