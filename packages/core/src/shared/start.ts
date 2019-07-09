59aed5ddac4b8c4a33ad729b270e6a2bc2ec8336
supaiku <1311798+supaiku0@users.noreply.github.com>
2019-07-09 05:25:53 +0200

    refactor(core): run GC earlier on potatos (#2785)

--- packages/core/src/shared/start.ts
+++ packages/core/src/shared/start.ts
@@ -1,5 +1,6 @@
 import cli from "cli-ux";
 
+import { freemem, totalmem } from "os";
 import { BaseCommand } from "../commands/command";
 import { processManager } from "../process-manager";
 import { CommandFlags, ProcessOptions } from "../types";
@@ -16,7 +17,7 @@ export abstract class AbstractStartCommand extends BaseCommand {
     protected abstract async runProcess(flags: CommandFlags): Promise<void>;
 
     protected async runWithPm2(options: ProcessOptions, flags: CommandFlags) {
-        const processName = options.name;
+        const processName: string = options.name;
 
         try {
             if (processManager.has(processName)) {
@@ -37,14 +38,19 @@ export abstract class AbstractStartCommand extends BaseCommand {
 
             flagsProcess.name = processName;
 
+            const totalMemGb: number = totalmem() / Math.pow(1024, 3);
+            const freeMemGb: number = freemem() / Math.pow(1024, 3);
+            const potato: boolean = totalMemGb < 2 || freeMemGb < 1.5;
+
             processManager.start(
                 {
                     ...options,
                     ...{
                         env: {
-                            NODE_ENV: 'production',
+                            NODE_ENV: "production",
                             CORE_ENV: flags.env,
                         },
+                        node_args: potato ? { max_old_space_size: 500 } : undefined,
                     },
                 },
                 flagsProcess,
