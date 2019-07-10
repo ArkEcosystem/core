7d60c0d6dd867a03ac8f870dc149269bad58f6fe
supaiku <supaiku@protonmail.com>
2019-07-10 14:01:34 +0200

    fix: check current slot

--- packages/core-forger/src/manager.ts
+++ packages/core-forger/src/manager.ts
@@ -155,12 +155,12 @@ export class ForgerManager {
 
         const minimumMs: number = 2000;
         const timeLeftInMs: number = Crypto.Slots.getTimeInMsUntilNextSlot();
-        if (timeLeftInMs >= minimumMs) {
-            this.logger.info(
-                `Forged new block ${block.data.id} by delegate ${this.usernames[delegate.publicKey]} (${
-                    delegate.publicKey
-                })`,
-            );
+        const currentSlot: number = Crypto.Slots.getSlotNumber();
+        const roundSlot: number = Crypto.Slots.getSlotNumber(round.timestamp);
+        const username: string = this.usernames[delegate.publicKey];
+
+        if (timeLeftInMs >= minimumMs && currentSlot === roundSlot) {
+            this.logger.info(`Forged new block ${block.data.id} by delegate ${username} (${delegate.publicKey})`);
 
             await this.client.broadcastBlock(block.toJson());
 
@@ -170,11 +170,15 @@ export class ForgerManager {
                 this.client.emitEvent(ApplicationEvents.TransactionForged, transaction);
             }
         } else {
-            this.logger.warn(
-                `Failed to forge new block by delegate ${this.usernames[delegate.publicKey]} (${
-                    delegate.publicKey
-                }, because there were less than ${minimumMs}ms left`,
-            );
+            if (currentSlot !== roundSlot) {
+                this.logger.warn(
+                    `Failed to forge new block by delegate ${username} (${delegate.publicKey}, because already in next slot.`,
+                );
+            } else {
+                this.logger.warn(
+                    `Failed to forge new block by delegate ${username} (${delegate.publicKey}, because there were less than ${minimumMs}ms left`,
+                );
+            }
         }
     }
 
