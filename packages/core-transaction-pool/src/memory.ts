938944b65f28e420b73a45d992380c73836918ec
Brian Faust <faustbrian@users.noreply.github.com>
2019-07-17 07:21:02 +0300

    style: apply tslint and prettier formatting (#2824)

--- packages/core-transaction-pool/src/memory.ts
+++ packages/core-transaction-pool/src/memory.ts
@@ -34,7 +34,7 @@ export class Memory {
             const expirationContext = {
                 blockTime: Managers.configManager.getMilestone(currentHeight).blocktime,
                 currentHeight,
-                now: Crypto.Slots.getTime()
+                now: Crypto.Slots.getTime(),
             };
 
             this.all.sort((a, b) => {
@@ -70,14 +70,14 @@ export class Memory {
         const expirationContext = {
             blockTime: Managers.configManager.getMilestone(currentHeight).blocktime,
             currentHeight,
-            now: Crypto.Slots.getTime()
+            now: Crypto.Slots.getTime(),
         };
 
         if (!this.byExpirationIsSorted) {
             this.byExpiration.sort(
                 (a, b) =>
                     this.calculateTransactionExpiration(a, expirationContext) -
-                    this.calculateTransactionExpiration(b, expirationContext)
+                    this.calculateTransactionExpiration(b, expirationContext),
             );
             this.byExpirationIsSorted = true;
         }
@@ -164,7 +164,7 @@ export class Memory {
         const expirationContext = {
             blockTime: Managers.configManager.getMilestone(currentHeight).blocktime,
             currentHeight,
-            now: Crypto.Slots.getTime()
+            now: Crypto.Slots.getTime(),
         };
         const expiration: number = this.calculateTransactionExpiration(transaction, expirationContext);
         if (expiration !== null) {
@@ -290,10 +290,10 @@ export class Memory {
     private calculateTransactionExpiration(
         transaction: Interfaces.ITransaction,
         context: {
-            blockTime: number,
-            currentHeight: number,
-            now: number,
-        }
+            blockTime: number;
+            currentHeight: number;
+            now: number;
+        },
     ): number {
         if (transaction.type === Enums.TransactionTypes.TimelockTransfer) {
             // tslint:disable-next-line:no-null-keyword
