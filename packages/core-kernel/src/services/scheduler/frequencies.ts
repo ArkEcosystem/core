a399f1948e0a50c0b8fb1571d430be32bea63e79
Brian Faust <hello@basecode.sh>
2019-08-15 09:35:04 +0300

    feat(core-kernel): initial draft of scheduler

---
+++ packages/core-kernel/src/services/scheduler/frequencies.ts
@@ -0,0 +1,385 @@
+/**
+ * @see https://crontab.guru/
+ * @see https://github.com/kelektiv/node-cron
+ *
+ * @export
+ * @class Frequencies
+ */
+export class Frequencies {
+    /**
+     * @private
+     * @type {string}
+     * @memberof Frequencies
+     */
+    protected expression: string = "* * * * * *";
+
+    /**
+     * The Cron expression representing the job's frequency.
+     *
+     * @param {string} expression
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public cron(expression: string): this {
+        this.expression = expression;
+
+        return this;
+    }
+
+    /**
+     * Schedule the job to run every minute.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public everyMinute(): this {
+        return this.setMinute("*");
+    }
+
+    /**
+     * Schedule the job to run every five minutes.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public everyFiveMinutes(): this {
+        return this.setMinute("*/5");
+    }
+
+    /**
+     * Schedule the job to run every ten minutes.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public everyTenMinutes(): this {
+        return this.setMinute("*/10");
+    }
+
+    /**
+     * Schedule the job to run every fifteen minutes.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public everyFifteenMinutes(): this {
+        return this.setMinute("*/15");
+    }
+
+    /**
+     * Schedule the job to run every thirty minutes.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public everyThirtyMinutes(): this {
+        return this.setMinute("*/30");
+    }
+
+    /**
+     * Schedule the job to run hourly.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public hourly(): this {
+        return this.setMinute("0");
+    }
+
+    /**
+     * Schedule the job to run hourly at a given offset in the hour.
+     *
+     * @param {string} minute
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public hourlyAt(minute: string): this {
+        return this.setMinute(minute);
+    }
+
+    /**
+     * Schedule the job to run daily.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public daily(): this {
+        return this.setMinute("0").setHour("0");
+    }
+
+    /**
+     * Schedule the job to run daily at a given time (10:00, 19:30, etc).
+     *
+     * @param {string} hour
+     * @param {string} minute
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public dailyAt(hour: string, minute: string): this {
+        return this.setMinute(minute).setHour(hour);
+    }
+
+    /**
+     * Schedule the job to run only on weekdays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public weekdays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("1-5");
+    }
+
+    /**
+     * Schedule the job to run only on weekends.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public weekends(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("6,0");
+    }
+
+    /**
+     * Schedule the job to run only on Mondays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public mondays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("MON");
+    }
+
+    /**
+     * Schedule the job to run only on Tuesdays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public tuesdays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("TUE");
+    }
+
+    /**
+     * Schedule the job to run only on Wednesdays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public wednesdays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("WED");
+    }
+
+    /**
+     * Schedule the job to run only on Thursdays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public thursdays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("THU");
+    }
+
+    /**
+     * Schedule the job to run only on Fridays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public fridays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("FRI");
+    }
+
+    /**
+     * Schedule the job to run only on Saturdays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public saturdays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("SAT");
+    }
+
+    /**
+     * Schedule the job to run only on Sundays.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public sundays(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("SUN");
+    }
+
+    /**
+     * Schedule the job to run weekly.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public weekly(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayWeek("0");
+    }
+
+    /**
+     * Schedule the job to run weekly on a given day and time.
+     *
+     * @param {string} day
+     * @param {string} [hour="0"]
+     * @param {string} [minute="0"]
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public weeklyOn(day: string, hour: string = "0", minute: string = "0"): this {
+        return this.setMinute(minute)
+            .setHour(hour)
+            .setDayWeek(day);
+    }
+
+    /**
+     * Schedule the job to run monthly.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public monthly(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayMonth("1");
+    }
+
+    /**
+     * Schedule the job to run monthly on a given day and time.
+     *
+     * @param {string} day
+     * @param {string} [hour="0"]
+     * @param {string} [minute="0"]
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public monthlyOn(day: string, hour: string = "0", minute: string = "0"): this {
+        return this.setMinute(minute)
+            .setHour(hour)
+            .setDayMonth(day);
+    }
+
+    /**
+     * Schedule the job to run quarterly.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public quarterly(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayMonth("1")
+            .setMonth("*/3");
+    }
+
+    /**
+     * Schedule the job to run yearly.
+     *
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    public yearly(): this {
+        return this.setMinute("0")
+            .setHour("0")
+            .setDayMonth("1")
+            .setMonth("1");
+    }
+
+    /**
+     * @private
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private setSeconds(value: string): this {
+        return this.spliceIntoPosition(0, value);
+    }
+
+    /**
+     * @private
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private setMinute(value: string): this {
+        return this.spliceIntoPosition(1, value);
+    }
+
+    /**
+     * @private
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private setHour(value: string): this {
+        return this.spliceIntoPosition(2, value);
+    }
+
+    /**
+     * @private
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private setDayMonth(value: string): this {
+        return this.spliceIntoPosition(3, value);
+    }
+
+    /**
+     * @private
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private setMonth(value: string): this {
+        return this.spliceIntoPosition(4, value);
+    }
+
+    /**
+     * @private
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private setDayWeek(value: string): this {
+        return this.spliceIntoPosition(5, value);
+    }
+
+    /**
+     * Splice the given value into the given position of the expression.
+     *
+     * @private
+     * @param {number} position
+     * @param {string} value
+     * @returns {this}
+     * @memberof Frequencies
+     */
+    private spliceIntoPosition(position: number, value: string): this {
+        const segments: string[] = this.expression.split(" ");
+        segments[position] = value;
+
+        return this.cron(segments.join(" "));
+    }
+}
