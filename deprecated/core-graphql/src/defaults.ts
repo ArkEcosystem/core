6b7c0480f7b4ccd201df1c0b1d2e52cc3529cdcb
Brian Faust <hello@basecode.sh>
2019-09-02 06:02:07 +0300

    chore: move deprecated plugins to ArkEcosystem/core-plugins-deprecated

--- deprecated/core-graphql/src/defaults.ts
+++
@@ -1,6 +0,0 @@
-export const defaults = {
-    enabled: false,
-    host: process.env.CORE_GRAPHQL_HOST || "0.0.0.0",
-    port: process.env.CORE_GRAPHQL_PORT || 4005,
-    path: "/graphql",
-};
--- deprecated/core-graphql/src/defs/index.ts
+++
@@ -1,9 +0,0 @@
-import { inputs } from "./inputs";
-import { root } from "./root";
-import { types } from "./types";
-
-export const typeDefs = `
-  ${inputs}
-  ${root}
-  ${types}
-`;
