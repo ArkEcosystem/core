8df2f120ca1e8d1bf78aedeaaecb57e1e43fa4d7
Brian Faust <hello@basecode.sh>
2019-09-23 08:45:18 +0300

    ci: setup github action workflow for testing

--- __tests__/unit/core/commands/relay/share.test.ts
+++ __tests__/unit/core/commands/relay/share.test.ts
@@ -5,7 +5,7 @@ describe("ShareCommand", () => {
     it("should throw if the process does not exist", async () => {
         const spyConnect = jest.spyOn(ngrok, "connect").mockImplementation(undefined);
 
-        await ShareCommand.run();
+        await ShareCommand.run([]);
 
         expect(spyConnect).toHaveBeenCalledWith({
             addr: 4003,
