e5821cc03f6ebe4603ab37b00c3b691137dbb100
Edgar Goetzendorff <hello@dated.fun>
2019-09-25 16:50:51 +0200

    docs(crypto): improve calculateExpiration docs

--- packages/crypto/src/transactions/types/transaction.ts
+++ packages/crypto/src/transactions/types/transaction.ts
@@ -104,7 +104,8 @@ export abstract class Transaction implements ITransaction {
      * H or any higher block.
      * If the user did not specify an expiration height when creating the transaction then
      * we calculate one from the timestamp of the transaction creation and the configured
-     * maximum transaction age.
+     * maximum transaction age (for v1 transactions). v2 transactions have a dedicated
+     * expiration property.
      * @return number expiration height or null if the transaction does not expire
      */
     public calculateExpiration(
