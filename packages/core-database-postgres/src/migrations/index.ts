bd8c3ca21f50ecab805099f95faf797cbfa95f55
Vasil Dimov <vd@FreeBSD.org>
2019-10-09 13:57:37 +0200

    chore(core-database-postgres): load migration file names automatically

--- packages/core-database-postgres/src/migrations/index.ts
+++ packages/core-database-postgres/src/migrations/index.ts
@@ -1,24 +1,7 @@
 import { loadQueryFile } from "../utils";
+import { readdirSync } from "fs";
 
-export const migrations = [
-    loadQueryFile(__dirname, "./20180304100000-create-migrations-table.sql"),
-    loadQueryFile(__dirname, "./20180305100000-create-wallets-table.sql"),
-    loadQueryFile(__dirname, "./20180305200000-create-rounds-table.sql"),
-    loadQueryFile(__dirname, "./20180305300000-create-blocks-table.sql"),
-    loadQueryFile(__dirname, "./20180305400000-create-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20181129400000-add-block_id-index-to-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20181204100000-add-generator_public_key-index-to-blocks-table.sql"),
-    loadQueryFile(__dirname, "./20181204200000-add-timestamp-index-to-blocks-table.sql"),
-    loadQueryFile(__dirname, "./20181204300000-add-sender_public_key-index-to-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20181204400000-add-recipient_id-index-to-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20190307000000-drop-wallets-table.sql"),
-    loadQueryFile(__dirname, "./20190606000000-add-block-id-foreign-key-on-transactions.sql"),
-    loadQueryFile(__dirname, "./20190619000000-drop-id-column-from-rounds-table.sql"),
-    loadQueryFile(__dirname, "./20190626000000-enforce-chained-blocks.sql"),
-    loadQueryFile(__dirname, "./20190718000000-check_previous_block-add-schema.sql"),
-    loadQueryFile(__dirname, "./20190803000000-add-type_group-column-to-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20190806000000-add-nonce-column-to-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20190905000000-change-set_row_nonce-to-use-max-nonce.sql"),
-    loadQueryFile(__dirname, "./20190917000000-add-asset-column-to-transactions-table.sql"),
-    loadQueryFile(__dirname, "./20191003000000-migrate-vendor-field-hex.sql"),
-];
+export const migrations = readdirSync(__dirname)
+    .filter(name => name.substr(-4).toLowerCase() === '.sql')
+    .sort()
+    .map(name => loadQueryFile(__dirname, name));
