3bcc8fae31ee0955ed74f8ab6389dfacf053df2d
Brian Faust <faustbrian@users.noreply.github.com>
2019-10-17 03:48:42 +0300

    fix(core-api): add missing offset handling to /api/peers (#3075)

--- packages/core-api/src/handlers/peers/controller.ts
+++ packages/core-api/src/handlers/peers/controller.ts
@@ -1,6 +1,7 @@
 import { P2P } from "@arkecosystem/core-interfaces";
 import Boom from "@hapi/boom";
 import Hapi from "@hapi/hapi";
+import { get } from "dottie";
 import semver from "semver";
 import { Controller } from "../shared/controller";
 
@@ -15,7 +16,17 @@ export class PeersController extends Controller {
 
         const count: number = result.length;
 
-        result = result.slice(0, +request.query.limit || 100);
+        const limit: number = +request.query.limit || 100;
+
+        let offset: number = +get(request.query, "offset", 0);
+
+        if (offset <= 0 && +request.query.page > 1) {
+            offset = (+request.query.page - 1) * limit;
+        }
+
+        if (Number.isNaN(offset)) {
+            offset = 0;
+        }
 
         const orderBy: string = request.query.orderBy as string;
         if (orderBy) {
@@ -28,6 +39,8 @@ export class PeersController extends Controller {
                         : result.sort((a, b) => semver.rcompare(a[order[0]], b[order[0]]));
             }
         }
+
+        result = result.slice(offset, offset + limit);
 
         return super.toPagination({ rows: result, count }, "peer");
     }
