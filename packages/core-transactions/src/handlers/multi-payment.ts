6bf64d6c8f4247c73289f5e38e692dc2fdcf6969
Brian Faust <hello@basecode.sh>
2019-10-19 19:13:37 +0300

    fix: use strict comparison to decide if a transaction should be enabled

--- packages/core-transactions/src/handlers/multi-payment.ts
+++ packages/core-transactions/src/handlers/multi-payment.ts
@@ -35,7 +35,7 @@ export class MultiPaymentTransactionHandler extends TransactionHandler {
     }
 
     public async isActivated(): Promise<boolean> {
-        return !!Managers.configManager.getMilestone().aip11;
+        return Managers.configManager.getMilestone().aip11 === true;
     }
 
     public async throwIfCannotBeApplied(
