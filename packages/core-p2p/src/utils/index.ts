c0123cea2126bd190de7a1f35ef5644c3c8a2b49
Vasil Dimov <vd@FreeBSD.org>
2019-10-29 14:32:28 +0100

    feat(core-p2p): implement throttling on outgoing p2p communication
    Use the rate limiter in reverse to stop ourselves from sending too
    many requests to a given peer that could possibly trigger their
    protection and result in banning us.
    
    We use the default configuration for rate limits. So this will work as
    long as the peer is using the same software and has not changed his
    config or has made it more liberal. If the peer has made his limits
    more restricted, then we could still trigger their protection.
    
    A more robust solution would be to negotiate the rate limits at peer
    handshake and then obey different limits for each peer.

--- packages/core-p2p/src/utils/index.ts
+++ packages/core-p2p/src/utils/index.ts
@@ -1,5 +1,6 @@
 export { checkDNS } from "./check-dns";
 export { checkNTP } from "./check-ntp";
+export { buildRateLimiter } from "./build-rate-limiter";
 export { isWhitelisted } from "./is-whitelisted";
 export { socketEmit } from "./socket";
 export { validateJSON } from "./validate-json";
