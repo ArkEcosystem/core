c36152ca461090e1be456a5f0502b8c87fa266e0
Lemii <peter.spaargaren@gmail.com>
2019-11-04 10:33:24 +0100

    refactor: update tests to comply with new validation

--- __tests__/functional/transaction-forging/bridgechain-registration.test.ts
+++ __tests__/functional/transaction-forging/bridgechain-registration.test.ts
@@ -42,7 +42,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject2",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[0])
@@ -51,7 +51,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration2 = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject2",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[0])
@@ -69,7 +69,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "4e07408562bedb8b60ce05c1decfe3ad16b72230967de01f640b7e4729b49fce",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[0])
@@ -85,7 +85,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "\u0008mybridgechain",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "4b227777d4dd1fc61c6f884f48641d02b4d121d3fd328cb08b5531fcacdabf8a",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[0])
@@ -101,7 +101,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "mybridgech@in",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "ef2d127de37b942baad06145e54b0c619a1f22327b2ebbcfbec78f5564afe39d",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[0])
@@ -126,7 +126,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "e7f6c011776e8db7cd330b54174fd76f7d0216b612387a5ffcfb81e6f0919683",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[0])
@@ -154,7 +154,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject",
                 seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "7902699be42c8a8e46fbbb4501726517e86b22c56a189f7625a6da49081b2451",
                 bridgechainRepository: "repository.com/myorg/myrepo",
             })
                 .withPassphrase(secrets[5])
@@ -207,7 +207,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject",
                 seedNodes: ["2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "2c624232cdd221771294dfbb310aca000a0df6ac8b66b696d90ef06fdefb64a3",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withPassphrase(passphrase)
@@ -279,7 +279,7 @@ describe("Transaction Forging - Bridgechain registration", () => {
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
                 name: "cryptoProject",
                 seedNodes: ["2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
-                genesisHash: "127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935",
+                genesisHash: "19581e27de7ced00ff1ce50b2047e7a567c76b1cbaebabe5ef03f7c3017bb5b7",
                 bridgechainRepository: "http://www.repository.com/myorg/myrepo",
             })
                 .withSenderPublicKey(multiSigPublicKey)
