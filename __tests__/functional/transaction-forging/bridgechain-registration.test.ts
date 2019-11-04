ec4e27497248f4819a5058ca6f49d1a0c6645b6b
Lemii <peter.spaargaren@gmail.com>
2019-11-04 10:45:44 +0100

    test: add test for duplicate genesisHash

--- __tests__/functional/transaction-forging/bridgechain-registration.test.ts
+++ __tests__/functional/transaction-forging/bridgechain-registration.test.ts
@@ -96,6 +96,33 @@ describe("Transaction Forging - Bridgechain registration", () => {
             await expect(bridgechainRegistration.id).not.toBeForged();
         });
 
+        it("should reject bridgechain registration, because bridgechain with same genesisHash is already registered [Signed with 1 Passphrase]", async () => {
+            // Registering a bridgechain
+            const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
+                name: "MyBridgechain",
+                seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
+                genesisHash: "83cf8b609de60036a8277bd0e96135751bbc07eb234256d4b65b893360651bf2",
+                bridgechainRepository: "http://www.repository.com/myorg/myrepo",
+            })
+                .withPassphrase(secrets[0])
+                .createOne();
+
+            const bridgechainRegistration2 = TransactionFactory.bridgechainRegistration({
+                name: "MyOtherBridgechain",
+                seedNodes: ["1.2.3.4", "2001:0db8:85a3:0000:0000:8a2e:0370:7334"],
+                genesisHash: "83cf8b609de60036a8277bd0e96135751bbc07eb234256d4b65b893360651bf2",
+                bridgechainRepository: "http://www.repository.com/myorg/myrepo",
+            })
+                .withPassphrase(secrets[0])
+                .withNonce(bridgechainRegistration.nonce.plus(1))
+                .createOne();
+
+            await expect([bridgechainRegistration, bridgechainRegistration2]).not.toBeAllAccepted();
+            await support.snoozeForBlock(1);
+            await expect(bridgechainRegistration.id).toBeForged();
+            await expect(bridgechainRegistration2.id).not.toBeForged();
+        });
+
         it("should reject bridgechain registration, because bridgechain name contains disallowed characters [Signed with 1 Passphrase]", async () => {
             // Bridgechain registration
             const bridgechainRegistration = TransactionFactory.bridgechainRegistration({
