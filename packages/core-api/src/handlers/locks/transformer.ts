899100e8f3b246022a96487e4518efccf07dd9c3
Edgar Goetzendorff <hello@dated.fun>
2019-11-06 15:27:56 +0100

    refactor: pass expiration type and value to calculator

--- packages/core-api/src/handlers/locks/transformer.ts
+++ packages/core-api/src/handlers/locks/transformer.ts
@@ -1,12 +1,14 @@
 import { formatTimestamp } from "@arkecosystem/core-utils";
 import { expirationCalculator } from "@arkecosystem/core-utils";
-import { Interfaces } from "@arkecosystem/crypto";
 
-export const transformLock = (lock: Interfaces.IHtlcLock) => {
+export const transformLock = lock => {
     return {
         ...lock,
         amount: lock.amount.toFixed(),
         timestamp: formatTimestamp(lock.timestamp),
-        isExpired: expirationCalculator.calculateLockExpirationStatus(lock.expiration),
+        isExpired: expirationCalculator.calculateLockExpirationStatus({
+            type: lock.expirationType,
+            value: lock.expirationValue,
+        }),
     };
 };
