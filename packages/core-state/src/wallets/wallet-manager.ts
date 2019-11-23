7809ca77c59dc6be9d0b99227d44d912dc71af29
Dmitry Tyshchenko <rainydio@gmail.com>
2019-11-23 14:19:00 +0200

    Remove unused import

--- packages/core-state/src/wallets/wallet-manager.ts
+++ packages/core-state/src/wallets/wallet-manager.ts
@@ -1,6 +1,6 @@
 import { app } from "@arkecosystem/core-container";
 import { Logger, Shared, State } from "@arkecosystem/core-interfaces";
-import { Handlers, Interfaces as TransactionInterfaces } from "@arkecosystem/core-transactions";
+import { Handlers } from "@arkecosystem/core-transactions";
 import { Enums, Identities, Interfaces, Utils } from "@arkecosystem/crypto";
 import pluralize from "pluralize";
 import { WalletIndexAlreadyRegisteredError, WalletIndexNotFoundError } from "./errors";
