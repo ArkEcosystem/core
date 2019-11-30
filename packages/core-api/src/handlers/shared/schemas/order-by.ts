b95f3a38fc136165b88b7927cd56271343b98151
Edgar Goetzendorff <hello@dated.fun>
2019-11-30 16:35:27 +0100

    refactor: return more verbose error message if validation fails

--- packages/core-api/src/handlers/shared/schemas/order-by.ts
+++ packages/core-api/src/handlers/shared/schemas/order-by.ts
@@ -6,52 +6,62 @@ const customJoi = Joi.extend(joi => ({
     coerce: (value, state, options) => (value && value.split ? value.split(":") : undefined),
 }));
 
+const validIteratees = [
+    "address",
+    "amount",
+    "approval",
+    "balance",
+    "blockId",
+    "confirmations",
+    "expirationType",
+    "expirationValue",
+    "fee",
+    "forgedFees",
+    "forgedRewards",
+    "forgedTotal",
+    "generatorPublicKey",
+    "height",
+    "id",
+    "isDelegate",
+    "isResigned",
+    "latency",
+    "lockedBalance",
+    "nonce",
+    "producedBlocks",
+    "publicKey",
+    "rank",
+    "recipient",
+    "sender",
+    "senderPublicKey",
+    "signature",
+    "signSignature",
+    "timestamp",
+    "transactions",
+    "type",
+    "typeGroup",
+    "username",
+    "vendorField",
+    "version",
+    "voteBalance",
+    "votes",
+];
+
+const validDirections = ["asc", "desc"];
+
 export const orderBy = customJoi
     .orderBy()
     .length(2)
     .items(
         Joi.string()
-            .valid(
-                "address",
-                "amount",
-                "approval",
-                "balance",
-                "blockId",
-                "confirmations",
-                "expirationType",
-                "expirationValue",
-                "fee",
-                "forgedFees",
-                "forgedRewards",
-                "forgedTotal",
-                "generatorPublicKey",
-                "height",
-                "id",
-                "isDelegate",
-                "isResigned",
-                "latency",
-                "lockedBalance",
-                "nonce",
-                "producedBlocks",
-                "publicKey",
-                "rank",
-                "recipient",
-                "sender",
-                "senderPublicKey",
-                "signature",
-                "signSignature",
-                "timestamp",
-                "transactions",
-                "type",
-                "typeGroup",
-                "username",
-                "vendorField",
-                "version",
-                "voteBalance",
-                "votes",
-            )
+            .valid(...validIteratees)
             .required(),
         Joi.string()
-            .valid("asc", "desc")
+            .valid(...validDirections)
             .required(),
+    )
+    .error(
+        () =>
+            `"orderBy" must have the following format <iteratee>:<direction>, where <iteratee> is one of: ${validIteratees.join(
+                ", ",
+            )} and <direction> one of: ${validDirections.join(", ")}`,
     );
