e246093fdf1f89ec65324fe44b88366d33dd699e
supaiku <1311798+supaiku0@users.noreply.github.com>
2019-12-20 06:38:38 +0100

    refactor: enable htlc types via dedicated milestone (#3353)

--- packages/core-transactions/src/handlers/htlc-lock.ts
+++ packages/core-transactions/src/handlers/htlc-lock.ts
@@ -52,7 +52,8 @@ export class HtlcLockTransactionHandler extends TransactionHandler {
     }
 
     public async isActivated(): Promise<boolean> {
-        return Managers.configManager.getMilestone().aip11 === true;
+        const milestone = Managers.configManager.getMilestone();
+        return milestone.aip11 === true && milestone.htlcEnabled === true;
     }
 
     public async throwIfCannotBeApplied(
@@ -138,11 +139,11 @@ export class HtlcLockTransactionHandler extends TransactionHandler {
         transaction: Interfaces.ITransaction,
         walletManager: State.IWalletManager,
         // tslint:disable-next-line: no-empty
-    ): Promise<void> {}
+    ): Promise<void> { }
 
     public async revertForRecipient(
         transaction: Interfaces.ITransaction,
         walletManager: State.IWalletManager,
         // tslint:disable-next-line: no-empty
-    ): Promise<void> {}
+    ): Promise<void> { }
 }
