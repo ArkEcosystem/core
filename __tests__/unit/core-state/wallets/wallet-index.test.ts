ee33c4b6d65d3fd49130d011951b342da64b90a0
Bertie <berniesnell@gmail.com>
2020-02-13 14:38:25 +0000

    test(core-state): add further tests to wallet-indexer

--- __tests__/unit/core-state/wallets/wallet-index.test.ts
+++ __tests__/unit/core-state/wallets/wallet-index.test.ts
@@ -1,32 +1,96 @@
-// import { State } from "@arkecosystem/core-interfaces";
-// import { Wallet } from "../../../../packages/core-state/src/wallets";
-// import { WalletIndex } from "../../../../packages/core-state/src/wallets/wallet-index";
-// import wallets from "../__fixtures__/wallets.json";
+import { Contracts, Container, Services } from "@arkecosystem/core-kernel";
+import { Wallets } from "@arkecosystem/core-state";
 
-// const walletData1 = wallets[0];
+import { WalletIndex } from "../../../../packages/core-state/src/wallets/wallet-index";
+import wallets from "../__fixtures__/wallets.json";
+import { Sandbox } from "@packages/core-test-framework/src";
 
-// describe("WalletIndex", () => {
-//     it("should be ok", () => {
-//         const wallet = new Wallet(walletData1.address);
-//         const walletIndex = new WalletIndex((index: State.IWalletIndex, wallet: State.IWallet) => {
-//             index.set(wallet.address, wallet);
-//         });
+const walletData1 = wallets[0];
 
-//         expect(walletIndex.has(walletData1.address)).toBeFalse();
+let sandbox: Sandbox;
 
-//         walletIndex.index(wallet);
-//         expect(walletIndex.has(walletData1.address)).toBeTrue();
+beforeAll(() => {
+    sandbox = new Sandbox();
 
-//         walletIndex.forget(walletData1.address);
-//         expect(walletIndex.has(walletData1.address)).toBeFalse();
+    sandbox.app
+        .bind<Services.Attributes.AttributeSet>(Container.Identifiers.WalletAttributes)
+        .to(Services.Attributes.AttributeSet)
+        .inSingletonScope();
+});
 
-//         walletIndex.set(walletData1.address, wallet);
-//         expect(walletIndex.get(walletData1.address)).toBe(wallet);
-//         expect(walletIndex.has(walletData1.address)).toBeTrue();
+const createWallet = (address: string): Contracts.State.Wallet =>
+    new Wallets.Wallet(
+        address,
+        new Services.Attributes.AttributeMap(
+            sandbox.app.get<Services.Attributes.AttributeSet>(Container.Identifiers.WalletAttributes),
+        ),
+    );
 
-//         expect(walletIndex.values()).toContain(wallet);
+describe("WalletIndex", () => {
+    it("should index and forget wallets", () => {
+        const wallet = createWallet(walletData1.address);
+        const walletIndex = new WalletIndex((index, wallet) => {
+            index.set(wallet.address, wallet);
+        });
 
-//         walletIndex.clear();
-//         expect(walletIndex.has(walletData1.address)).toBeFalse();
-//     });
-// });
+        expect(walletIndex.has(walletData1.address)).toBeFalse();
+
+        walletIndex.index(wallet);
+        expect(walletIndex.has(walletData1.address)).toBeTrue();
+
+        walletIndex.forget(walletData1.address);
+        expect(walletIndex.has(walletData1.address)).toBeFalse();
+    });
+
+    it("should set and get addresses", () => {
+        const wallet = createWallet(walletData1.address);
+        const walletIndex = new WalletIndex((index, wallet) => {
+            index.set(wallet.address, wallet);
+        });
+
+        expect(walletIndex.has(walletData1.address)).toBeFalse();
+
+        walletIndex.index(wallet);
+        walletIndex.set(walletData1.address, wallet);
+
+        expect(walletIndex.get(walletData1.address)).toBe(wallet);
+        expect(walletIndex.has(walletData1.address)).toBeTrue();
+
+        expect(walletIndex.values()).toContain(wallet);
+
+        walletIndex.clear();
+        expect(walletIndex.has(walletData1.address)).toBeFalse();
+    })
+    
+    it("should be cloneable", () => {
+        const walletIndex = new WalletIndex((index, wallet) => {
+            index.set(wallet.address, wallet);
+        });
+
+        const clonedWalletIndex = walletIndex.clone();
+        expect(walletIndex).toEqual(clonedWalletIndex);
+    });
+
+    it("should return entries", () => {
+        const wallet = createWallet(walletData1.address);
+        const walletIndex = new WalletIndex((index, wallet) => {
+            index.set(wallet.address, wallet);
+        });
+
+        walletIndex.index(wallet);
+        const entries = walletIndex.entries();
+        expect(entries.length).toEqual(1);
+        expect(entries[0][0]).toEqual(entries[0][1].address);
+        expect(entries[0][0]).toEqual(walletData1.address);
+    });
+
+    it("should return keys", () => {
+        const wallet = createWallet(walletData1.address);
+        const walletIndex = new WalletIndex((index, wallet) => {
+            index.set(wallet.address, wallet);
+        });
+
+        walletIndex.index(wallet);
+        expect(walletIndex.keys()).toContain(walletData1.address);
+    });
+});
