756c7b9026c75a78db53f1e50f15cb6671a4a350
Bertie <berniesnell@gmail.com>
2020-02-14 12:53:19 +0000

    test(core-state): remove unused config and fixtures since now using factory

--- packages/crypto/src/types.ts
+++ packages/crypto/src/types.ts
@@ -3,7 +3,6 @@ import * as networks from "./networks";
 export type NetworkType =
     | typeof networks.mainnet.network
     | typeof networks.devnet.network
-    | typeof networks.testnet.network
-    | typeof networks.unitnet.network;
+    | typeof networks.testnet.network;
 
 export type NetworkName = keyof typeof networks;
