241083a1c60585fe6bc8f3b4ac4fd4a7ea692045
Bertie <berniesnell@gmail.com>
2020-02-17 14:40:29 +0000

    test(core-state): (re)include wallet indexer tests

--- __tests__/unit/core-state/wallets/wallet-index.test.ts
+++ __tests__/unit/core-state/wallets/wallet-index.test.ts
@@ -26,7 +26,7 @@ const createWallet = (address: string): Contracts.State.Wallet =>
         ),
     );
 
-xdescribe("WalletIndex", () => {
+describe("WalletIndex", () => {
     it("should index and forget wallets", () => {
         const wallet = createWallet(walletData1.address);
         const walletIndex = new WalletIndex((index, wallet) => {
