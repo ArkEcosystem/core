d021eea491600e3509808bb94eb7e2eebf0579a1
Dmitry Tyshchenko <rainydio@gmail.com>
2020-02-20 05:10:51 +0200

    Simpler mocks

--- packages/core-p2p/src/transaction-broadcaster.test.ts
+++ packages/core-p2p/src/transaction-broadcaster.test.ts
@@ -4,53 +4,56 @@ import { Interfaces } from "@arkecosystem/crypto";
 import { TransactionBroadcaster } from "./transaction-broadcaster";
 
 describe("TransactionBroadcaster", () => {
-    let container: Container.Container;
-    let logger: { warning: jest.Mock; debug: jest.Mock };
-    let configuration: { getRequired: jest.Mock };
-    let storage: { getPeers: jest.Mock };
-    let communicator: { postTransactions: jest.Mock };
+    describe("broadcastTransactions", () => {
+        const logger = { warning: jest.fn(), debug: jest.fn() };
+        const configuration = { getRequired: jest.fn() };
+        const storage = { getPeers: jest.fn() };
+        const communicator = { postTransactions: jest.fn() };
+        const container = new Container.Container();
 
-    beforeEach(() => {
-        logger = { warning: jest.fn(), debug: jest.fn() };
-        configuration = { getRequired: jest.fn() };
-        storage = { getPeers: jest.fn() };
-        communicator = { postTransactions: jest.fn() };
+        beforeEach(() => {
+            logger.warning.mockReset();
+            logger.debug.mockReset();
+            configuration.getRequired.mockReset();
+            storage.getPeers.mockReset();
+            communicator.postTransactions.mockReset();
 
-        container = new Container.Container();
-        container.bind(Container.Identifiers.LogService).toConstantValue(logger);
-        container.bind(Container.Identifiers.PluginConfiguration).toConstantValue(configuration);
-        container.bind(Container.Identifiers.PeerStorage).toConstantValue(storage);
-        container.bind(Container.Identifiers.PeerCommunicator).toConstantValue(communicator);
-    });
+            container.unbindAll();
+            container.bind(Container.Identifiers.LogService).toConstantValue(logger);
+            container.bind(Container.Identifiers.PluginConfiguration).toConstantValue(configuration);
+            container.bind(Container.Identifiers.PeerStorage).toConstantValue(storage);
+            container.bind(Container.Identifiers.PeerCommunicator).toConstantValue(communicator);
+        });
 
-    it("should warn when attempting to broadcast empty array", async () => {
-        const broadcaster = container.resolve(TransactionBroadcaster);
+        it("should warn when attempting to broadcast empty array", async () => {
+            const broadcaster = container.resolve(TransactionBroadcaster);
 
-        await broadcaster.broadcastTransactions([]);
+            await broadcaster.broadcastTransactions([]);
 
-        expect(logger.warning).toBeCalledWith("Broadcasting 0 transactions");
-        expect(configuration.getRequired).not.toBeCalled();
-        expect(storage.getPeers).not.toBeCalled();
-        expect(communicator.postTransactions).not.toBeCalled();
-    });
+            expect(logger.warning).toBeCalledWith("Broadcasting 0 transactions");
+            expect(configuration.getRequired).not.toBeCalled();
+            expect(storage.getPeers).not.toBeCalled();
+            expect(communicator.postTransactions).not.toBeCalled();
+        });
 
-    it("should broadcast transaction to peers", async () => {
-        const peers = [{}, {}, {}];
-        configuration.getRequired.mockReturnValue(3);
-        storage.getPeers.mockReturnValue(peers);
-        const broadcaster = container.resolve(TransactionBroadcaster);
-        const jsons = [{}];
-        const transactions = ([
-            { toJson: jest.fn().mockReturnValue(jsons[0]) },
-        ] as unknown) as Interfaces.ITransaction[];
+        it("should broadcast transaction to peers", async () => {
+            const peers = [{}, {}, {}];
+            configuration.getRequired.mockReturnValue(3);
+            storage.getPeers.mockReturnValue(peers);
+            const broadcaster = container.resolve(TransactionBroadcaster);
+            const jsons = [{}];
+            const transactions = ([
+                { toJson: jest.fn().mockReturnValue(jsons[0]) },
+            ] as unknown) as Interfaces.ITransaction[];
 
-        await broadcaster.broadcastTransactions(transactions);
+            await broadcaster.broadcastTransactions(transactions);
 
-        expect(configuration.getRequired).toBeCalledWith("maxPeersBroadcast");
-        expect(storage.getPeers).toBeCalled();
-        expect(logger.debug).toBeCalledWith("Broadcasting 1 transaction to 3 peers");
-        expect(transactions[0].toJson).toBeCalled();
-        expect(communicator.postTransactions).toBeCalledWith(peers[0], jsons);
-        expect(communicator.postTransactions).toBeCalledWith(peers[1], jsons);
+            expect(configuration.getRequired).toBeCalledWith("maxPeersBroadcast");
+            expect(storage.getPeers).toBeCalled();
+            expect(logger.debug).toBeCalledWith("Broadcasting 1 transaction to 3 peers");
+            expect(transactions[0].toJson).toBeCalled();
+            expect(communicator.postTransactions).toBeCalledWith(peers[0], jsons);
+            expect(communicator.postTransactions).toBeCalledWith(peers[1], jsons);
+        });
     });
 });
--- packages/core-transaction-pool/src/processor.test.ts
+++ packages/core-transaction-pool/src/processor.test.ts
@@ -3,44 +3,52 @@ import { Interfaces, Transactions } from "@arkecosystem/crypto";
 
 import { Processor } from "./processor";
 
-jest.mock("@arkecosystem/crypto/dist/transactions/factory");
+jest.mock("@arkecosystem/crypto");
 
 describe("Processor", () => {
-    let container: Container.Container;
-    let logger: { warning: jest.Mock };
-    let pool: { addTransaction: jest.Mock };
-    let dynamicFeeMatcher: { canEnterPool: jest.Mock; canBroadcast: jest.Mock };
-    let transactionBroadcaster: { broadcastTransactions: jest.Mock };
+    describe("process", () => {
+        const logger = { warning: jest.fn() };
+        const pool = { addTransaction: jest.fn() };
+        const dynamicFeeMatcher = { canEnterPool: jest.fn(), canBroadcast: jest.fn() };
+        const transactionBroadcaster = { broadcastTransactions: jest.fn() };
+        const container = new Container.Container();
 
-    beforeEach(() => {
-        logger = { warning: jest.fn() };
-        pool = { addTransaction: jest.fn() };
-        dynamicFeeMatcher = { canEnterPool: jest.fn(), canBroadcast: jest.fn() };
-        transactionBroadcaster = { broadcastTransactions: jest.fn() };
+        beforeEach(() => {
+            (Transactions.TransactionFactory.fromData as jest.Mock).mockReset();
 
-        container = new Container.Container();
-        container.bind(Container.Identifiers.LogService).toConstantValue(logger);
-        container.bind(Container.Identifiers.TransactionPoolService).toConstantValue(pool);
-        container.bind(Container.Identifiers.TransactionPoolDynamicFeeMatcher).toConstantValue(dynamicFeeMatcher);
-        container.bind(Container.Identifiers.PeerTransactionBroadcaster).toConstantValue(transactionBroadcaster);
-    });
+            logger.warning.mockReset();
+            pool.addTransaction.mockReset();
+            dynamicFeeMatcher.canEnterPool.mockReset();
+            dynamicFeeMatcher.canBroadcast.mockReset();
+            transactionBroadcaster.broadcastTransactions.mockReset();
 
-    it("should add eligible transactions to pool", async () => {
-        const processor = container.resolve(Processor);
-        const fromData = Transactions.TransactionFactory.fromData as jest.Mock;
-        fromData.mockImplementation(d => ({ id: d.id }));
-        dynamicFeeMatcher.canEnterPool.mockReturnValueOnce(true).mockReturnValueOnce(false);
-        const data = ([{ id: "id_eligible" }, { id: "id_non_eligible" }] as unknown) as Interfaces.ITransactionData[];
+            container.unbindAll();
+            container.bind(Container.Identifiers.LogService).toConstantValue(logger);
+            container.bind(Container.Identifiers.TransactionPoolService).toConstantValue(pool);
+            container.bind(Container.Identifiers.TransactionPoolDynamicFeeMatcher).toConstantValue(dynamicFeeMatcher);
+            container.bind(Container.Identifiers.PeerTransactionBroadcaster).toConstantValue(transactionBroadcaster);
+        });
 
-        await processor.process(data);
+        it("should add eligible transactions to pool", async () => {
+            (Transactions.TransactionFactory.fromData as jest.Mock).mockImplementation(d => ({ id: d.id }));
 
-        expect(processor.accept).toEqual(["id_eligible"]);
-        expect(processor.invalid).toEqual(["id_non_eligible"]);
-        expect(processor.errors).toEqual({
-            id_non_eligible: {
-                type: "ERR_LOW_FEE",
-                message: "Transaction id_non_eligible fee is to low to include in pool",
-            },
+            const processor = container.resolve(Processor);
+            dynamicFeeMatcher.canEnterPool.mockReturnValueOnce(true).mockReturnValueOnce(false);
+            const data = ([
+                { id: "id_eligible" },
+                { id: "id_non_eligible" },
+            ] as unknown) as Interfaces.ITransactionData[];
+
+            await processor.process(data);
+
+            expect(processor.accept).toEqual(["id_eligible"]);
+            expect(processor.invalid).toEqual(["id_non_eligible"]);
+            expect(processor.errors).toEqual({
+                id_non_eligible: {
+                    type: "ERR_LOW_FEE",
+                    message: "Transaction id_non_eligible fee is to low to include in pool",
+                },
+            });
         });
     });
 });
