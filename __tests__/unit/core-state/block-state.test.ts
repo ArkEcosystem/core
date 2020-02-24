78a3c8ec002c888b07e7aef34116f7658c6b7eca
Bertie <berniesnell@gmail.com>
2020-02-24 12:02:28 +0000

    test(core-state): throws error when reverting

--- __tests__/unit/core-state/block-state.test.ts
+++ __tests__/unit/core-state/block-state.test.ts
@@ -239,7 +239,7 @@ describe("BlockState", () => {
     });
 
     describe("when 1 transaction fails while applying it", () => {
-        it.only("should revert sequentially (from last to first) all the transactions of the block", async () => {
+        it("should revert sequentially (from last to first) all the transactions of the block", async () => {
             // @ts-ignore
             jest.spyOn(blockState, "applyTransaction").mockImplementation(tx => {
                 if (tx === blocks[0].transactions[2]) {
@@ -264,6 +264,21 @@ describe("BlockState", () => {
                         blocks[0].transactions[i],
                     );
                 }
+            }
+        });
+
+        it("throws the Error", async () => {
+            blockState.applyTransaction = jest.fn(tx => {
+                throw new Error("Fake error");
+            });
+
+            try {
+                await blockState.applyBlock(blocks[0]);
+
+                expect(undefined).toBe("this should fail if no error is thrown");
+            } catch (error) {
+                expect(error).toBeInstanceOf(Error);
+                expect(error.message).toBe("Fake error");
             }
         });
     });
