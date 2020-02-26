325fdb9f0a2cc0967cbd924e52965f8bdac25787
Bertie <berniesnell@gmail.com>
2020-02-26 21:17:04 +0000

    test(core-state): test that state builder doesn't fail for certain whitelisted scenarios

--- __tests__/unit/core-state/state-builder.test.ts
+++ __tests__/unit/core-state/state-builder.test.ts
@@ -4,6 +4,8 @@ import { setUp, setUpDefaults } from "./setup";
 import { StateBuilder } from "@arkecosystem/core-state/src/state-builder";
 import { Enums, Utils } from "@arkecosystem/core-kernel";
 import { WalletRepository } from "@arkecosystem/core-state/src/wallets";
+import { Sandbox } from "@packages/core-test-framework/src";
+import { Managers } from "@arkecosystem/crypto";
 
 let stateBuilder: StateBuilder;
 let getBlockRewardsSpy: jest.SpyInstance;
@@ -12,6 +14,8 @@ let getRegisteredHandlersSpy: jest.SpyInstance;
 let dispatchSpy: jest.SpyInstance;
 const generatorKey = setUpDefaults.getBlockRewards.generatorPublicKey;
 const senderKey = setUpDefaults.getSentTransaction.senderPublicKey;
+
+let sandbox: Sandbox;
 
 let loggerWarningSpy: jest.SpyInstance;
 
@@ -22,6 +26,7 @@ beforeAll(async () => {
 
     walletRepo = initialEnv.walletRepo;
     stateBuilder = initialEnv.stateBuilder;
+    sandbox = initialEnv.sandbox;
 
     getBlockRewardsSpy = initialEnv.spies.getBlockRewardsSpy;
     getSentTransactionSpy = initialEnv.spies.getSentTransactionSpy;
@@ -34,6 +39,7 @@ describe("StateBuilder", () => {
 
     beforeEach(() => {
         jest.clearAllMocks();
+        walletRepo.reset();
     });
 
     it("should call block repository to get intial block rewards", async () => {
@@ -79,12 +85,73 @@ describe("StateBuilder", () => {
     });
 
     // TODO: this tests fails, but presumably should pass?
-    it("should exit app if any wallet balance is negative", async () => {
+    it("should exit app if any wallet balance is negative and not whitelisted", async () => {
         const wallet = walletRepo.findByPublicKey(senderKey);
         wallet.balance = Utils.BigNumber.make(-80000);
         wallet.publicKey = senderKey;
 
         walletRepo.reindex(wallet);
+
+        await stateBuilder.run();
+
+        expect(loggerWarningSpy).toHaveBeenCalledWith("Wallet ATtEq2tqNumWgR9q9zF6FjGp34Mp5JpKGp has a negative balance of '-80000'");
+    });
+
+    it("should not fail for negative genesis wallet balances", async () => {
+        const genesisPublicKeys: string[] = Managers.configManager
+            .get("genesisBlock.transactions")
+            .reduce((acc, curr) => [...acc, curr.senderPublicKey], []);
+        const wallet = walletRepo.findByPublicKey(genesisPublicKeys[0]);
+        wallet.balance = Utils.BigNumber.make(-80000);
+        wallet.publicKey = genesisPublicKeys[0];
+
+        walletRepo.reindex(wallet);
+
+        expect.assertions(2);
+        await stateBuilder.run().then(() => expect("not called").toEqual("not called"));
+        expect(loggerWarningSpy).not.toHaveBeenCalled();
+    });
+
+    it("should not fail if the publicKey is whitelisted", async () => {
+        const wallet = walletRepo.findByPublicKey(senderKey);
+        wallet.nonce = Utils.BigNumber.make(-80000)
+        const allowedWalletNegativeBalance = Utils.BigNumber.make(-50000);
+        wallet.balance = allowedWalletNegativeBalance;
+        wallet.publicKey = senderKey;
+        
+        const balance: Record<string, Record<string, string>> = {
+            [senderKey]: {
+                [wallet.nonce.toString()]: allowedWalletNegativeBalance.toString()
+            }
+        }
+
+        sandbox.app.config(
+            "exceptions.negativeBalances",
+            balance,
+        );
+
+        expect.assertions(2);
+        await stateBuilder.run().then(() => expect("not called").toEqual("not called"));
+        expect(loggerWarningSpy).not.toHaveBeenCalled();
+    });
+
+    // TODO: check that this should be passing?
+    it("should fail if the whitelisted key doesn't have the allowed negative balance", async () => {
+        const wallet = walletRepo.findByPublicKey(senderKey);
+        wallet.nonce = Utils.BigNumber.make(-80000)
+        wallet.balance = Utils.BigNumber.make(-50000);
+        wallet.publicKey = senderKey;
+        
+        const balance: Record<string, Record<string, string>> = {
+            [senderKey]: {
+                [wallet.nonce.toString()]: Utils.BigNumber.make(-80000).toString()
+            }
+        }
+
+        sandbox.app.config(
+            "exceptions.negativeBalances",
+            balance,
+        );
 
         await stateBuilder.run();
 
