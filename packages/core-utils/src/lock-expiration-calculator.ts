fc67202065dfdf754cc10ced2751d58fe28780d3
Brian Faust <hello@basecode.sh>
2020-02-28 06:57:13 +0200

    fix: calculateLockExpirationStatus method signature

--- packages/core-utils/src/lock-expiration-calculator.ts
+++ packages/core-utils/src/lock-expiration-calculator.ts
@@ -1,16 +1,8 @@
-import { app } from "@arkecosystem/core-container";
-import { State } from "@arkecosystem/core-interfaces";
 import { Enums, Interfaces } from "@arkecosystem/crypto";
 
-export const calculateLockExpirationStatus = (expiration: Interfaces.IHtlcExpiration): boolean => {
-    const lastBlock: Interfaces.IBlock = app
-        .resolvePlugin<State.IStateService>("state")
-        .getStore()
-        .getLastBlock();
-
-    return (
-        (expiration.type === Enums.HtlcLockExpirationType.EpochTimestamp &&
-            expiration.value <= lastBlock.data.timestamp) ||
-        (expiration.type === Enums.HtlcLockExpirationType.BlockHeight && expiration.value <= lastBlock.data.height)
-    );
-};
+export const calculateLockExpirationStatus = (
+    lastBlock: Interfaces.IBlock,
+    expiration: Interfaces.IHtlcExpiration,
+): boolean =>
+    (expiration.type === Enums.HtlcLockExpirationType.EpochTimestamp && expiration.value <= lastBlock.data.timestamp) ||
+    (expiration.type === Enums.HtlcLockExpirationType.BlockHeight && expiration.value <= lastBlock.data.height);
