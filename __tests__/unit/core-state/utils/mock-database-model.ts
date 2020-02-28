5cab169a0a24c76bf97b6620d6301e61949ff562
Bertie <berniesnell@gmail.com>
2020-02-28 17:50:38 +0000

    test(core-state): add tests for search-parameter-converter

---
+++ __tests__/unit/core-state/utils/mock-database-model.ts
@@ -0,0 +1,48 @@
+import { Contracts } from "@packages/core-kernel";
+
+export class MockDatabaseModel { // TODO: formerly implemented: Contracts.Database.IModel - does this type exist yet?
+    public getName(): string {
+        return this.constructor.name;
+    }
+
+    public getTable(): string {
+        return "test";
+    }
+
+    public query(): any {
+        return;
+    }
+
+    public getColumnSet(): any {
+        return;
+    }
+    
+    public getSearchableFields() { // TODO: formerly returned: Database.ISearchableField[] - does this type exist yet?
+        return [
+            {
+                fieldName: "id",
+                supportedOperators: [Contracts.Database.SearchOperator.OP_EQ],
+            },
+            {
+                fieldName: "timestamp",
+                supportedOperators: [Contracts.Database.SearchOperator.OP_GTE, Contracts.Database.SearchOperator.OP_LTE],
+            },
+            {
+                fieldName: "sentence",
+                supportedOperators: [Contracts.Database.SearchOperator.OP_EQ, Contracts.Database.SearchOperator.OP_LIKE],
+            },
+            {
+                fieldName: "basket",
+                supportedOperators: [Contracts.Database.SearchOperator.OP_IN],
+            },
+            {
+                fieldName: "range",
+                supportedOperators: [
+                    Contracts.Database.SearchOperator.OP_EQ,
+                    Contracts.Database.SearchOperator.OP_GTE,
+                    Contracts.Database.SearchOperator.OP_LTE,
+                ],
+            },
+        ];
+    }
+}
