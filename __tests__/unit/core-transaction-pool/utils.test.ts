ad5765c56e519b851b67b46861b0f84449a134d2
Dmitry Tyshchenko <rainydio@gmail.com>
2020-03-05 19:11:46 +0200

    Cover utils

--- __tests__/unit/core-transaction-pool/utils.test.ts
+++ __tests__/unit/core-transaction-pool/utils.test.ts
@@ -1,7 +1,7 @@
 import { createLock, IteratorMany } from "../../../packages/core-transaction-pool/src/utils";
 
 describe("IteratorMany", () => {
-    it("should select next item based on comparator", () => {
+    it("should choose next item based on comparator", () => {
         const iter1 = [1, 5, 3, 9, 0][Symbol.iterator]();
         const iter2 = [8, 2, 7, 4, 6][Symbol.iterator]();
         const iteratorMany = new IteratorMany([iter1, iter2], (a, b) => a - b);
@@ -40,6 +40,21 @@ describe("createLock", () => {
             await new Promise(resolve => setTimeout(resolve, 100));
             return ++counter;
         });
+        const promise2 = lock(async () => {
+            return ++counter;
+        });
+        const results = await Promise.all([promise1, promise2]);
+
+        expect(results).toStrictEqual([1, 2]);
+    });
+
+    it("should synchronize parallel async executions even if error was thrown", async () => {
+        let counter = 0;
+        const lock = createLock();
+
+        const promise1 = lock(async () => {
+            await new Promise((_, reject) => setTimeout(() => reject(++counter), 100));
+        }).catch(value => value);
         const promise2 = lock(async () => {
             return ++counter;
         });
