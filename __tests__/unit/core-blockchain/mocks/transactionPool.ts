2a0250dddabd8032db064d522d141049895b4d0b
Air1 <erwann@ark.io>
2020-03-06 11:30:20 +0400

    test: fix mock for block handler unit test

--- __tests__/unit/core-blockchain/mocks/transactionPool.ts
+++ __tests__/unit/core-blockchain/mocks/transactionPool.ts
@@ -2,4 +2,8 @@ export const transactionPool = {
     buildWallets: () => undefined,
     acceptChainedBlock: () => undefined,
     removeTransactionsById: () => undefined,
+    flush: () => undefined,
+    walletManager: {
+        reset: () => undefined,
+    },
 };
