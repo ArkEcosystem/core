1199c8b18c90a6461b8a60683dc80dad3dfad1e5
Bertie <berniesnell@gmail.com>
2020-03-10 11:41:04 +0000

    test(core-state): search respository should allow orderBy params

--- __tests__/unit/core-state/wallets/utils/search-entires.test.ts
+++ __tests__/unit/core-state/wallets/utils/search-entires.test.ts
@@ -2,6 +2,7 @@ import "jest-extended";
 
 import { Container, Services } from "@packages/core-kernel";
 import { searchEntries } from "@packages/core-state/src/wallets/utils/search-entries";
+import { Utils } from "@packages/crypto/src";
 
 import { FixtureGenerator } from "../../__utils__/fixture-generator";
 import { setUp } from "../../setup";
@@ -57,5 +58,36 @@ describe("searchEntries", () => {
         const result = searchEntries({ addresses, address }, query, wallets, defaultOrder);
 
         expect(result.count).toEqual(0);
+    });
+
+    it("should accept orderBy parameter also", () => {
+        const wallets = fixtureGenerator.generateFullWallets();
+        for (let i = 0; i < wallets.length; i++) {
+            const wallet = wallets[i];
+            if (i < 17) {
+                wallet.setAttribute("delegate.voteBalance", Utils.BigNumber.make(12));
+            } else if (i < 29) {
+                wallet.setAttribute("delegate.voteBalance", Utils.BigNumber.make(17));
+            }
+        }
+
+        const params = {
+            orderBy: "voteBalance",
+            voteBalance: {
+                from: 11,
+                to: 18,
+            }
+        };
+        
+        const query = {
+            exact: [ 'publicKey', 'secondPublicKey', 'username', 'vote' ],
+            between: [ 'balance', 'voteBalance', 'lockedBalance' ],
+            in: [ 'address' ]
+        };
+        const defaultOrder = [ 'balance', 'desc' ];
+
+        const result = searchEntries(params, query, wallets, defaultOrder);
+
+        expect(result.count).toEqual(29);
     });
 });
