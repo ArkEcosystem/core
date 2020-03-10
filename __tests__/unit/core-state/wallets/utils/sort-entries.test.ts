724f6a2c30ecadd9c2aaa082c77ef4ae43be5e14
Bertie <berniesnell@gmail.com>
2020-03-10 13:48:43 +0000

    test(core-state): sort entries asc and desc

--- __tests__/unit/core-state/wallets/utils/sort-entries.test.ts
+++ __tests__/unit/core-state/wallets/utils/sort-entries.test.ts
@@ -2,6 +2,7 @@ import "jest-extended";
 
 import { Container, Services } from "@packages/core-kernel";
 import { sortEntries } from "@packages/core-state/src/wallets/utils/sort-entries";
+import { Utils } from "@packages/crypto/src";
 
 import { FixtureGenerator } from "../../__utils__/fixture-generator";
 import { setUp } from "../../setup";
@@ -29,7 +30,11 @@ const moveIndexToFrontofArray = (index, array) =>
         .concat(array.slice(0, index))
         .concat(array.slice(index + 1));
 
+const moveIndexToBackOfArray = (index, array) => array.slice(0, index).concat(array.slice(index + 1)).concat(array
+    .slice(index, index + 1));
+
 describe("sortEntries", () => {
+
     it("should sort entries using an iteratee", () => {
         const entries = fixtureGenerator.generateFullWallets();
         const collectThird = entry => entry !== entries[3];
@@ -37,5 +42,32 @@ describe("sortEntries", () => {
         const actual = sortEntries({ orderBy: [collectThird, "asc"] }, entries, entries[0]);
 
         expect(actual).toEqual(thirdNowAtStart);
+    });
+
+    it("should use default value if there is no orderBy set - asc", () => {
+
+        const wallets = fixtureGenerator.generateFullWallets();
+        wallets[3].setAttribute("delegate", {
+            balance: Utils.BigNumber.make(500),
+
+        });
+    
+        const actual = sortEntries({}, wallets, ["balance", "asc"]); 
+        const thirdNowAtBack = moveIndexToBackOfArray(3, wallets);
+
+        expect(actual).toEqual(thirdNowAtBack);
+    });
+
+    it("should use default value if there is no orderBy set - desc", () => {
+        const wallets = fixtureGenerator.generateFullWallets();
+        wallets[3].setAttribute("delegate", {
+            balance: Utils.BigNumber.make(500),
+
+        });
+
+        const actual = sortEntries({}, wallets, ["balance", "desc"]);;
+        const thirdNowAtFront = moveIndexToFrontofArray(3, wallets);
+
+        expect(actual).toEqual(thirdNowAtFront);
     });
 });
