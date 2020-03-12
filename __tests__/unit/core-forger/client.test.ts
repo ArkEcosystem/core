65005b3d89f59c4e43c36c41130854641b1256da
Bertie <berniesnell@gmail.com>
2020-03-12 12:52:06 +0000

    test(core-forger): broadcast block on Client

--- __tests__/unit/core-forger/client.test.ts
+++ __tests__/unit/core-forger/client.test.ts
@@ -29,6 +29,7 @@ describe("Client", () => {
     let spySocketDisconnect;
     let spySocketOn;
     let spySocketCluster;
+    let spyEmit;
 
     let mockHosts;
     let client: Client;
@@ -37,16 +38,17 @@ describe("Client", () => {
         client = app.resolve<Client>(Client);
         spySocketOn = jest.fn();
         spySocketDisconnect = jest.fn();
+        spyEmit = jest.fn((__, data, cb) => cb(undefined, data));
         // @ts-ignore
-        spySocketCluster = jest.spyOn(socketCluster, "create").mockImplementation(() => ({
-            // @ts-ignore
-            on: spySocketOn,
-            disconnect: spySocketDisconnect,
-        }));
+        spySocketCluster = jest.spyOn(socketCluster, "create").mockImplementation(() => mockHosts[0].socket);
 
         mockHosts = [
             {
-                socket: {},
+                socket: {
+                    on: spySocketOn,
+                    disconnect: spySocketDisconnect,
+                    emit: spyEmit,
+                },
                 hostname: "mock-1",
             },
             {
@@ -118,16 +120,77 @@ describe("Client", () => {
     });
 
     describe("broadcastBlock", () => {
-        it("should log broadcast as debug message", () => {
+        it("should log broadcast as debug message", async () => {
             client.register(mockHosts);
 
-            client.broadcastBlock(forgedBlockWithTransactions);
+            await client.broadcastBlock(forgedBlockWithTransactions);
             expect(logger.debug).toHaveBeenCalledWith(
                 `Broadcasting block ${forgedBlockWithTransactions.data.height.toLocaleString()} (${
                     forgedBlockWithTransactions.data.id
                 }) with ${forgedBlockWithTransactions.data.numberOfTransactions} transactions to ${
                     mockHosts[0].hostname
                 }`,
+            );
+        });
+
+        it("should not broadcast block when there is an issue with socket", async () => {
+            client.register(mockHosts);
+
+            expect(await client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+            expect(logger.error).toHaveBeenCalledWith(
+                `Broadcast block failed: Request to ${mockHosts[0].hostname}:${mockHosts[0].port}<p2p.peer.postBlock> failed, because of 'socket.getState is not a function'.`,
+            );
+        });
+
+        // TODO: Fix: state shared between tests
+        it.skip("should not broadcast block when there the socket is closed", async () => {
+            client.register(mockHosts);
+
+            mockHosts[0].socket = {
+                getState: () => true,
+                OPEN: false,
+                emit: spyEmit,
+            };
+
+            expect(await client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+            expect(spyEmit).not.toHaveBeenCalled();
+            expect(logger.error).toHaveBeenCalledWith(
+                `Broadcast block failed: Request to ${mockHosts[0].hostname}:${
+                    mockHosts[0].port
+                }<p2p.peer.postBlock> failed, because of 'Peer ${
+                    mockHosts[0].hostname
+                } socket is not connected. State: ${mockHosts[0].socket.getState()}'.`,
+            );
+        });
+
+        it("should broadcast valid blocks without error", async () => {
+            client.register(mockHosts);
+
+            mockHosts[0].socket = {
+                getState: () => true,
+                OPEN: true,
+                emit: spyEmit,
+            };
+
+            expect(await client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+            expect(spyEmit).toHaveBeenCalledWith("p2p.peer.postBlock", expect.anything(), expect.anything());
+            expect(logger.error).not.toHaveBeenCalled();
+        });
+
+        it("should not broadcast blocks on socketEmit error", async () => {
+            client.register(mockHosts);
+
+            spyEmit.mockImplementation((__, data, cb) => cb("Error", data));
+
+            mockHosts[0].socket = {
+                getState: () => true,
+                OPEN: true,
+                emit: spyEmit,
+            };
+
+            expect(await client.broadcastBlock(forgedBlockWithTransactions)).toResolve();
+            expect(logger.error).toHaveBeenCalledWith(
+                `Broadcast block failed: Request to ${mockHosts[0].hostname}:${mockHosts[0].port}<p2p.peer.postBlock> failed, because of 'undefined'.`,
             );
         });
     });
