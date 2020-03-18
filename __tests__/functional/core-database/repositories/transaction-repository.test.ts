1f8c60fe0764ae476bca628ac7ab32a1c152edb4
Dmitry Tyshchenko <rainydio@gmail.com>
2020-03-18 18:04:56 +0200

    Few more TransactionRepository tests

--- __tests__/functional/core-database/repositories/transaction-repository.test.ts
+++ __tests__/functional/core-database/repositories/transaction-repository.test.ts
@@ -1,10 +1,11 @@
-import { Blocks, Managers, Utils } from "@arkecosystem/crypto";
+import { Blocks, Enums, Identities, Managers, Transactions, Utils } from "@arkecosystem/crypto";
 import { Connection } from "typeorm";
 import { getCustomRepository } from "typeorm";
 
 import { clearCoreDatabase, getCoreDatabaseConnection } from "../__support__";
 import { BlockRepository } from "../../../../packages/core-database/src/repositories/block-repository";
 import { TransactionRepository } from "../../../../packages/core-database/src/repositories/transaction-repository";
+import { BIP39 } from "../../../../packages/core-forger/src/methods/bip39";
 
 let connection: Connection | undefined;
 
@@ -16,7 +17,43 @@ beforeEach(async () => {
     await clearCoreDatabase(connection);
 });
 
+const transaction1 = Transactions.BuilderFactory.transfer()
+    .version(1)
+    .amount("100")
+    .recipientId(Identities.Address.fromPassphrase("recipient's secret"))
+    .nonce("1")
+    .fee("100")
+    .sign("sender's secret")
+    .build();
+const transaction2 = Transactions.BuilderFactory.transfer()
+    .version(1)
+    .amount("100")
+    .recipientId(Identities.Address.fromPassphrase("recipient's secret"))
+    .nonce("2")
+    .fee("200")
+    .sign("sender's secret")
+    .build();
+const transaction3 = Transactions.BuilderFactory.transfer()
+    .version(1)
+    .amount("100")
+    .recipientId(Identities.Address.fromPassphrase("recipient's secret"))
+    .nonce("3")
+    .fee("300")
+    .sign("sender's secret")
+    .build();
+
+const bip39 = new BIP39("generator's secret");
 const block1 = Blocks.BlockFactory.fromJson(Managers.configManager.get("genesisBlock"));
+const block2 = bip39.forge([transaction1.data], {
+    timestamp: block1.data.timestamp + 60,
+    previousBlock: block1.data,
+    reward: new Utils.BigNumber("100"),
+});
+const block3 = bip39.forge([transaction2.data, transaction3.data], {
+    timestamp: block2.data.timestamp + 120,
+    previousBlock: block2.data,
+    reward: new Utils.BigNumber("100"),
+});
 
 describe("TransactionRepository.findByBlockIds", () => {
     it("should find transactions by block id", async () => {
@@ -60,5 +97,50 @@ describe("TransactionRepository.getStatistics", () => {
         expect(statistics.totalFee).toBe(
             block1.transactions.reduce((s, t) => s.plus(t.data.fee), new Utils.BigNumber(0)).toString(),
         );
+    });
+});
+
+describe("TransactionRepository.getFeeStatistics", () => {
+    it("should return fee statistics", async () => {
+        const blockRepository = getCustomRepository(BlockRepository);
+        const transactionRepository = getCustomRepository(TransactionRepository);
+        await blockRepository.saveBlocks([block1, block2, block3]);
+        const feeStatistics = await transactionRepository.getFeeStatistics(365 * 25, 0);
+        expect(feeStatistics).toStrictEqual([
+            {
+                typeGroup: Enums.TransactionTypeGroup.Core,
+                type: Enums.TransactionType.DelegateRegistration,
+                avg: "0",
+                max: "0",
+                min: "0",
+                sum: "0",
+            },
+            {
+                typeGroup: Enums.TransactionTypeGroup.Core,
+                type: Enums.TransactionType.Transfer,
+                avg: "150",
+                max: "300",
+                min: "0",
+                sum: "600",
+            },
+        ]);
+    });
+});
+
+describe("TransactionRepository.getSentTransactions", () => {
+    it("should return nonce, amount, fee grouped by sender", async () => {
+        const blockRepository = getCustomRepository(BlockRepository);
+        const transactionRepository = getCustomRepository(TransactionRepository);
+        await blockRepository.saveBlocks([block1, block2, block3]);
+        const sentTransactions = await transactionRepository.getSentTransactions();
+        const senderTransaction = sentTransactions.find(
+            t => t.senderPublicKey === Identities.PublicKey.fromPassphrase("sender's secret"),
+        );
+        expect(senderTransaction).toStrictEqual({
+            senderPublicKey: Identities.PublicKey.fromPassphrase("sender's secret"),
+            nonce: "3",
+            amount: "300",
+            fee: "600",
+        });
     });
 });
