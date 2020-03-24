383379cf97f3cbb3460eccc51dca7f8c0038a5f3
Bertie <berniesnell@gmail.com>
2020-03-24 16:45:52 +0000

    test(core): add tests for uninstall command

--- __tests__/unit/core/commands/uninstall.test.ts
+++ __tests__/unit/core/commands/uninstall.test.ts
@@ -1,10 +1,10 @@
 import { Console } from "@arkecosystem/core-test-framework";
-import { Command } from "@packages/core/src/commands/snapshot-verify";
+import { Command } from "@packages/core/src/commands/uninstall";
 
 let cli;
 beforeEach(() => (cli = new Console()));
 
-describe("SnapshotVerifyCommand", () => {
+describe("UninstallCommand", () => {
     it("should throw since the command is not implemented", async () => {
         await expect(cli.execute(Command)).rejects.toThrow("[ERROR] This command has not been implemented.");
     });
