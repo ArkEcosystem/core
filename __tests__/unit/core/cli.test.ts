3a0d549bf904cfb73753e1cc032f275c86a1b80c
Bertie <berniesnell@gmail.com>
2020-03-25 12:12:56 +0000

    test(core): test all CLI paths

--- __tests__/unit/core/cli.test.ts
+++ __tests__/unit/core/cli.test.ts
@@ -3,6 +3,8 @@ import "jest-extended";
 import { CommandLineInterface } from "@packages/core/src/cli";
 import prompts from "prompts";
 
+afterEach(() => jest.resetAllMocks());
+
 describe("CLI", () => {
     it("should run successfully using valid commands", async () => {
         const cli = new CommandLineInterface(["help"]);
@@ -21,5 +23,30 @@ describe("CLI", () => {
 
         expect(message).toContain(`is not a ark command.`);
         expect(mockExit).toHaveBeenCalled();
+    });
+
+    it("should exit when the command doesn't have a valid signature", async () => {
+        // @ts-ignore
+        const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {});
+        const cli = new CommandLineInterface(["--nope"]);
+        await expect(cli.execute("./packages/core/dist")).toReject();
+        expect(mockExit).toHaveBeenCalled();
+    });
+
+    it("should exit when a valid command appears with the help flag", async () => {
+        // @ts-ignore
+        const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {});
+        const cli = new CommandLineInterface(["update", "--help"]);
+        await expect(cli.execute("./packages/core/dist")).toResolve();
+        expect(mockExit).toHaveBeenCalled();
+    });
+
+    it("should execute a suggested command", async () => {
+        // @ts-ignore
+        const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {});
+        const cli = new CommandLineInterface(["hello"]);
+        prompts.inject([true]);
+        await expect(cli.execute("./packages/core/dist")).toResolve();
+        expect(mockExit).not.toHaveBeenCalled();
     });
 });
