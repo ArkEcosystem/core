bb69c7008ae9458789d4c0df87036ffa1bd3f84f
Bertie <berniesnell@gmail.com>
2020-03-31 11:03:02 +0100

    feat(core-kernel): dynamic block times (#3627)

---
+++ packages/core-kernel/src/utils/block-time-calculator.ts
@@ -0,0 +1,44 @@
+import { Managers } from "@arkecosystem/crypto";
+
+export const isNewBlockTime = (height: number): boolean => {
+    if (height === 1) return true;
+
+    const milestones = Managers.configManager.get("milestones");
+
+    let milestone;
+
+    for (let i = milestones.length - 1; i >= 0; i--) {
+        const temp = milestones[i];
+
+        if (temp.height > height) {
+            continue;
+        }
+
+        if (!milestone || temp.blocktime === milestone.blocktime) {
+            if (temp.blocktime) {
+                milestone = temp;
+            }
+        } else {
+            break;
+        }
+    }
+
+    if (!milestone) return false;
+
+    return height - milestone.height === 0;
+};
+
+export const calculateBlockTime = (height: number): number => {
+    const milestones = Managers.configManager.get("milestones");
+
+    for (let i = milestones.length - 1; i >= 0; i--) {
+        const milestone = milestones[i];
+        if (milestone.height <= height) {
+            if (milestone.blocktime) {
+                return milestone.blocktime;
+            }
+        }
+    }
+
+    throw new Error(`No milestones specifying any height were found`);
+};
