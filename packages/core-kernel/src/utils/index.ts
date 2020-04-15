77b1372f0d104749f0472f5d2c18310e0e181de0
Bertie <berniesnell@gmail.com>
2020-04-15 16:18:14 +0100

    refactor(core-kernel): fix imports in Utils

--- packages/core-kernel/src/utils/index.ts
+++ packages/core-kernel/src/utils/index.ts
@@ -5,7 +5,6 @@ import { formatTimestamp } from "./format-timestamp";
 import { getBlockTimeLookup } from "./get-blocktime-lookup";
 import { getBlockNotChainedErrorMessage, isBlockChained } from "./is-block-chained";
 import { isWhitelisted } from "./is-whitelisted";
-import { mapHeightToMilestoneSpanTimestamp } from "./map-height-to-milestone-span-timestamp";
 import { calculateRound, isNewRound } from "./round-calculator";
 import { calculate } from "./supply-calculator";
 import * as Plugins from "./transform-plugins";
@@ -18,6 +17,6 @@ export const delegateCalculator = { calculateApproval, calculateForgedTotal };
 export const expirationCalculator = { calculateTransactionExpiration, calculateLockExpirationStatus };
 export const roundCalculator = { calculateRound, isNewRound };
 export const supplyCalculator = { calculate };
-export const forgingInfoCalculator = { calculateForgingInfo, mapHeightToMilestoneSpanTimestamp, getBlockTimeLookup };
+export const forgingInfoCalculator = { calculateForgingInfo, getBlockTimeLookup };
 
 export { formatTimestamp, isBlockChained, getBlockNotChainedErrorMessage, isWhitelisted, Plugins };
