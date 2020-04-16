18cb94ab4af5a8731787f77e32728eb6cc3c8e98
Bertie <berniesnell@gmail.com>
2020-04-16 17:01:45 +0100

    test(core-kernel): add tests to check that next forger is calculated correctly when slots are missed

--- __tests__/unit/core-kernel/utils/calculate-forging-info.test.ts
+++ __tests__/unit/core-kernel/utils/calculate-forging-info.test.ts
@@ -613,4 +613,200 @@ describe("calculateForgingInfo", () => {
             calculateForgingInfo(84, 18, Utils.roundCalculator.calculateRound(18), mockGetBlockTimeLookup).nextForger,
         ).toEqual(1);
     });
+
+    it("should calculate the currentForger index correctly for dynamic block times, changing max delegate numbers and missed slots", async () => {
+        const milestones = [
+            { height: 1, blocktime: 4, activeDelegates: 4 },
+            { height: 4, blocktime: 3 },
+            { height: 10, blocktime: 5, activeDelegates: 5 },
+        ];
+
+        const config = { ...devnet, milestones };
+        // @ts-ignore
+        jest.spyOn(configManager, "validateMilestones").mockReturnValue(true);
+        // @ts-ignore
+        jest.spyOn(Managers.configManager, "validateMilestones").mockReturnValue(true);
+        configManager.setConfig(config);
+        Managers.configManager.setConfig(config);
+
+        const mockGetBlockTimeLookup = (height: number) => {
+            console.log("Looking up height: ", height);
+            switch (height) {
+                case 1:
+                    return 0;
+                case 3:
+                    return 8;
+                case 9:
+                    return 42;
+                default:
+                    throw new Error(`Test scenarios should not hit this line`);
+            }
+        };
+
+        expect(calculateForgingInfo(0, 1, Utils.roundCalculator.calculateRound(1), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 0,
+                canForge: true,
+            }),
+        );
+
+        expect(calculateForgingInfo(3, 1, Utils.roundCalculator.calculateRound(1), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 0,
+                canForge: false,
+            }),
+        );
+
+        expect(calculateForgingInfo(4, 2, Utils.roundCalculator.calculateRound(1), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 1,
+                nextForger: 2,
+                blockTimestamp: 4,
+                canForge: true,
+            }),
+        );
+
+        expect(calculateForgingInfo(8, 3, Utils.roundCalculator.calculateRound(1), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 2,
+                nextForger: 3,
+                blockTimestamp: 8,
+                canForge: true,
+            }),
+        );
+
+        expect(calculateForgingInfo(12, 4, Utils.roundCalculator.calculateRound(1), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 3,
+                nextForger: 0,
+                blockTimestamp: 12,
+                canForge: true,
+            }),
+        );
+
+        expect(calculateForgingInfo(16, 4, Utils.roundCalculator.calculateRound(2), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 15,
+                canForge: false,
+            }),
+        );
+
+        expect(calculateForgingInfo(18, 4, Utils.roundCalculator.calculateRound(2), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 1,
+                nextForger: 2,
+                blockTimestamp: 18,
+            }),
+        );
+
+        expect(calculateForgingInfo(21, 5, Utils.roundCalculator.calculateRound(2), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 2,
+                nextForger: 3,
+                blockTimestamp: 21,
+            }),
+        );
+
+        expect(calculateForgingInfo(24, 5, Utils.roundCalculator.calculateRound(2), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 3,
+                nextForger: 0,
+                blockTimestamp: 24,
+            }),
+        );
+
+        expect(calculateForgingInfo(27, 6, Utils.roundCalculator.calculateRound(3), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 27,
+            }),
+        );
+
+        expect(calculateForgingInfo(30, 6, Utils.roundCalculator.calculateRound(3), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 1,
+                nextForger: 2,
+                blockTimestamp: 30,
+            }),
+        );
+
+        expect(calculateForgingInfo(33, 7, Utils.roundCalculator.calculateRound(3), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 2,
+                nextForger: 3,
+                blockTimestamp: 33,
+            }),
+        );
+
+        expect(calculateForgingInfo(36, 8, Utils.roundCalculator.calculateRound(3), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 3,
+                nextForger: 0,
+                blockTimestamp: 36,
+            }),
+        );
+
+        expect(calculateForgingInfo(39, 9, Utils.roundCalculator.calculateRound(4), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 39,
+            }),
+        );
+
+        expect(calculateForgingInfo(42, 9, Utils.roundCalculator.calculateRound(4), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 1,
+                nextForger: 2,
+                blockTimestamp: 42,
+            }),
+        );
+
+        expect(calculateForgingInfo(45, 10, Utils.roundCalculator.calculateRound(4), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 2,
+                nextForger: 3,
+                blockTimestamp: 45,
+            }),
+        );
+
+        expect(calculateForgingInfo(50, 11, Utils.roundCalculator.calculateRound(4), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 3,
+                nextForger: 0,
+                blockTimestamp: 50,
+            }),
+        );
+
+        expect(calculateForgingInfo(55, 12, Utils.roundCalculator.calculateRound(5), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 55,
+            }),
+        );
+
+        expect(calculateForgingInfo(100, 19, Utils.roundCalculator.calculateRound(6), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 4,
+                nextForger: 0,
+                blockTimestamp: 100,
+            }),
+        );
+
+        expect(calculateForgingInfo(105, 19, Utils.roundCalculator.calculateRound(7), mockGetBlockTimeLookup)).toEqual(
+            expect.objectContaining({
+                currentForger: 0,
+                nextForger: 1,
+                blockTimestamp: 105,
+            }),
+        );
+    });
 });
