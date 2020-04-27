0af0f36f9b1782ebe99611d78519e837f5ed08e9
Bertie <berniesnell@gmail.com>
2020-04-27 10:48:55 +0100

    refactor(crypto): make the crypto manager type generic, so it can be used with custom blocks

--- packages/crypto/src/manager.ts
+++ packages/crypto/src/manager.ts
@@ -6,7 +6,7 @@ import { libraryDefaults } from "./managers/defaults";
 import { LibraryManager } from "./managers/library-manager";
 import { NetworkName } from "./types";
 
-export class Crypto {
+export class Crypto<T> {
     public identities: Identities;
     public libraryManager: LibraryManager;
     public heightTracker: HeightTracker;
@@ -19,7 +19,7 @@ export class Crypto {
      * @param network
      * @param libraries
      */
-    public constructor(network: NetworkConfig, private libraries: Libraries) {
+    public constructor(network: NetworkConfig<T>, private libraries: Libraries) {
         this.libraries = { ...libraryDefaults, ...libraries };
         this.heightTracker = new HeightTracker();
         this.configManager = new ConfigManager(network);
@@ -34,15 +34,15 @@ export class Crypto {
         this.identities = new Identities(this.libraryManager, network.network);
     }
 
-    public static createFromConfig(config: NetworkConfig, libraries = libraryDefaults): Crypto {
+    public static createFromConfig<T>(config: NetworkConfig<T>, libraries = libraryDefaults): Crypto<T> {
         return new Crypto(config, libraries);
     }
 
-    public static createFromPreset(name: NetworkName, libraries = libraryDefaults): Crypto {
+    public static createFromPreset<T>(name: NetworkName, libraries = libraryDefaults): Crypto<T> {
         return new Crypto(NetworkManager.findByName(name), libraries);
     }
 
-    public static getPresets(): Record<NetworkName, NetworkConfig> {
+    public static getPresets<T>(): Record<NetworkName, NetworkConfig<T>> {
         return NetworkManager.all();
     }
 }
