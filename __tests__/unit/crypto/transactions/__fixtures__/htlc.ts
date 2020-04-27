29b6cda4f5a7fc1af75b2ee2075e51513cca8ec8
Bertie <berniesnell@gmail.com>
2020-04-27 12:36:13 +0100

    test(crypto): remove tests associated with blocks, transactions and validation

--- __tests__/unit/crypto/transactions/__fixtures__/htlc.ts
+++
@@ -1,3 +0,0 @@
-export const htlcSecretHex = "c27f1ce845d8c29eebc9006be932b604fd06755521b1a8b0be4204c65377151a";
-// SHA256(c2 7f ...) = 9c 1a ...
-export const htlcSecretHashHex = "9c1a3815d49e0c9f78b872bfb017e825ea2db708158b70815526a830c85912b4";
