2eb45fe15dbab39f42fb3f77a94009a9879ffc85
Bertie <berniesnell@gmail.com>
2020-04-28 17:03:55 +0100

    test(crypto): add tests for heightTracker

---
+++ __tests__/unit/crypto/managers/height-tracker.test.ts
@@ -0,0 +1,18 @@
+import "jest-extended";
+
+import { CryptoManager } from "@packages/crypto/src";
+
+let heightTracker;
+
+beforeAll(() => {
+    const devnetCrypto = CryptoManager.createFromPreset("devnet");
+    heightTracker = devnetCrypto.heightTracker;
+});
+
+describe("HeightTracker", () => {
+    it("should set and get the height correctly ", () => {
+        heightTracker.setHeight(21600);
+
+        expect(heightTracker.getHeight()).toEqual(21600);
+    });
+});
