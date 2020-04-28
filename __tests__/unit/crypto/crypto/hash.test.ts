ea4e7e6adbb9a667f226fc5bb402ce9b4a2108b2
Bertie <berniesnell@gmail.com>
2020-04-28 13:34:39 +0100

    test(crypto): fix hash algorithms test by writing previous transaction data to buffer

--- __tests__/unit/crypto/crypto/hash.test.ts
+++ __tests__/unit/crypto/crypto/hash.test.ts
@@ -1,36 +1,25 @@
 import "jest-extended";
 
-import { Factories, Generators } from "@packages/core-test-framework/src";
-import { TransactionFactory } from "@packages/core-test-framework/src/utils/transaction-factory";
+import { CryptoManager } from "@packages/crypto/src";
 
-import { Hash } from "../../../../packages/crypto/src/crypto";
-import { configManager } from "../../../../packages/crypto/src/managers";
-import { Utils as TransactionUtils } from "../../../../packages/crypto/src/transactions";
+import { identity } from "../fixtures/identities";
 
-const transaction = TransactionFactory.initialize()
-    .transfer("AJWRd23HNEhPLkK1ymMnwnDBX2a7QBZqff", 1000)
-    .withVersion(2)
-    .withFee(2000)
-    .withPassphrase("secret")
-    .createOne();
-
-let identity;
+let Hash;
+let transactionHash;
 
 beforeEach(() => {
-    // todo: completely wrap this into a function to hide the generation and setting of the config?
-    const config = Generators.generateCryptoConfigRaw();
-    configManager.setConfig(config);
-
-    identity = Factories.factory("Identity").withOptions({ passphrase: "this is a top secret passphrase" }).make();
+    const crypto = CryptoManager.createFromPreset("testnet");
+    Hash = crypto.libraryManager.Crypto.Hash;
+    const hex = "27f68f1e62b9e6e3bc13b7113488f1e27263a4e47e7d9c7acd9c9af67d7fa11c";
+    transactionHash = Buffer.from(hex, "hex");
 });
 
 describe("Hash", () => {
     describe("ECDSA", () => {
         it("should sign the data and verify it [String]", () => {
-            const hash: Buffer = TransactionUtils.toHash(transaction);
-            const signature: string = Hash.signECDSA(hash, identity.keys);
+            const signature: string = Hash.signECDSA(transactionHash, identity.keys);
 
-            expect(Hash.verifyECDSA(hash, signature, identity.publicKey)).toBeTrue();
+            expect(Hash.verifyECDSA(transactionHash, signature, identity.publicKey)).toBeTrue();
 
             expect(signature).toEqual(
                 "30450221008682af02d5f3c6302af14f3239a997022f69c28a5e3282603d5f25912ccd3b47022023cec266362f5bb91e6a2f2fcb62f4c61829dfd7a096432ff8b4a54a83577444",
@@ -38,11 +27,14 @@ describe("Hash", () => {
         });
 
         it("should sign the data and verify it [Buffer]", () => {
-            const hash: Buffer = TransactionUtils.toHash(transaction);
-            const signature: string = Hash.signECDSA(hash, identity.keys);
+            const signature: string = Hash.signECDSA(transactionHash, identity.keys);
 
             expect(
-                Hash.verifyECDSA(hash, Buffer.from(signature, "hex"), Buffer.from(identity.publicKey, "hex")),
+                Hash.verifyECDSA(
+                    transactionHash,
+                    Buffer.from(signature, "hex"),
+                    Buffer.from(identity.publicKey, "hex"),
+                ),
             ).toBeTrue();
 
             expect(signature).toEqual(
@@ -53,10 +45,9 @@ describe("Hash", () => {
 
     describe("schnorr", () => {
         it("should sign the data and verify it [String]", () => {
-            const hash: Buffer = TransactionUtils.toHash(transaction);
-            const signature: string = Hash.signSchnorr(hash, identity.keys);
+            const signature: string = Hash.signSchnorr(transactionHash, identity.keys);
 
-            expect(Hash.verifySchnorr(hash, signature, identity.publicKey)).toBeTrue();
+            expect(Hash.verifySchnorr(transactionHash, signature, identity.publicKey)).toBeTrue();
 
             expect(signature).toEqual(
                 "dd78ce399058357fc3ca881d38e54efc1b6841719106aa55fb186186fa0f3330bc37c8cb2bd8e48d272f1f9532df89a6b5f69945c56d05947bd3186e872db99a",
@@ -64,11 +55,14 @@ describe("Hash", () => {
         });
 
         it("should sign the data and verify it [Buffer]", () => {
-            const hash: Buffer = TransactionUtils.toHash(transaction);
-            const signature: string = Hash.signSchnorr(hash, identity.keys);
+            const signature: string = Hash.signSchnorr(transactionHash, identity.keys);
 
             expect(
-                Hash.verifySchnorr(hash, Buffer.from(signature, "hex"), Buffer.from(identity.publicKey, "hex")),
+                Hash.verifySchnorr(
+                    transactionHash,
+                    Buffer.from(signature, "hex"),
+                    Buffer.from(identity.publicKey, "hex"),
+                ),
             ).toBeTrue();
 
             expect(signature).toEqual(
