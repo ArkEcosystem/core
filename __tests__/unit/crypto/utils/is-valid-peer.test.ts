8f5ba70b51b250404a48e9b0001974909f56b64d
Bertie <berniesnell@gmail.com>
2020-04-28 16:25:01 +0100

    test(crypto): fix isValidPeer tests

--- __tests__/unit/crypto/utils/is-valid-peer.test.ts
+++ __tests__/unit/crypto/utils/is-valid-peer.test.ts
@@ -1,35 +1,41 @@
 import "jest-extended";
 
+import { CryptoManager } from "@packages/crypto/src";
 import os from "os";
 
-import { isValidPeer } from "../../../../packages/crypto/src/utils/is-valid-peer";
+let Utils;
+
+beforeAll(() => {
+    const crypto = CryptoManager.createFromPreset("devnet");
+    Utils = crypto.libraryManager.Utils;
+});
 
 describe("isValidPeer", () => {
     it("should not be ok for 127.0.0.1", () => {
-        expect(isValidPeer({ ip: "127.0.0.1" })).toBeFalse();
+        expect(Utils.isValidPeer({ ip: "127.0.0.1" })).toBeFalse();
     });
 
     it("should not be ok for ::ffff:127.0.0.1", () => {
         const peer = { ip: "::ffff:127.0.0.1" };
-        expect(isValidPeer(peer)).toBeFalse();
+        expect(Utils.isValidPeer(peer)).toBeFalse();
     });
 
     it("should not be ok for 0.0.0.0", () => {
-        expect(isValidPeer({ ip: "0.0.0.0" })).toBeFalse();
+        expect(Utils.isValidPeer({ ip: "0.0.0.0" })).toBeFalse();
     });
 
     it("should not be ok for ::1", () => {
         const peer = { ip: "::1" };
-        expect(isValidPeer(peer)).toBeFalse();
+        expect(Utils.isValidPeer(peer)).toBeFalse();
     });
 
     it("should not be ok for 2130706433", () => {
         const peer = { ip: "2130706433" };
-        expect(isValidPeer(peer)).toBeFalse();
+        expect(Utils.isValidPeer(peer)).toBeFalse();
     });
 
     it("should not be ok for garbage", () => {
-        expect(isValidPeer({ ip: "garbage" })).toBeFalse();
+        expect(Utils.isValidPeer({ ip: "garbage" })).toBeFalse();
     });
 
     it("should not be ok for LAN addresses", () => {
@@ -42,14 +48,14 @@ describe("isValidPeer", () => {
         }
 
         for (const ipAddress of addresses) {
-            expect(isValidPeer({ ip: ipAddress })).toBeFalse();
+            expect(Utils.isValidPeer({ ip: ipAddress })).toBeFalse();
         }
     });
 
     it("should be ok", () => {
-        expect(isValidPeer({ ip: "192.168.178.0" })).toBeTrue();
-        expect(isValidPeer({ ip: "5.196.105.32" })).toBeTrue();
-        expect(isValidPeer({ ip: "5.196.105.32" })).toBeTrue();
-        expect(isValidPeer({ ip: "5.196.105.32" })).toBeTrue();
+        expect(Utils.isValidPeer({ ip: "192.168.178.0" })).toBeTrue();
+        expect(Utils.isValidPeer({ ip: "5.196.105.32" })).toBeTrue();
+        expect(Utils.isValidPeer({ ip: "5.196.105.32" })).toBeTrue();
+        expect(Utils.isValidPeer({ ip: "5.196.105.32" })).toBeTrue();
     });
 });
