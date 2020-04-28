a9ae0c6d228af1aa36b7c8bee1f82b08dcdae0bb
Bertie <berniesnell@gmail.com>
2020-04-28 16:24:40 +0100

    test(crypto): consolidate Message tests into one place

--- __tests__/unit/crypto/identities/message.test.ts
+++ __tests__/unit/crypto/identities/message.test.ts
@@ -2,7 +2,7 @@ import "jest-extended";
 
 import { CryptoManager } from "@packages/crypto/src";
 
-import { identity, signedMessageEntries } from "../fixtures/identities";
+import { fixture, identity, signedMessageEntries } from "../fixtures/identities";
 
 let Message;
 let crypto;
@@ -17,6 +17,15 @@ describe("Message", () => {
         it("should sign a message", () => {
             // @ts-ignore
             expect(Message.sign("test", identity.passphrase)).toContainAllEntries(signedMessageEntries);
+        });
+
+        it("should be ok", () => {
+            const actual = Message.sign(fixture.data.message, fixture.passphrase);
+
+            expect(actual).toHaveProperty("publicKey");
+            expect(actual).toHaveProperty("signature");
+            expect(actual).toHaveProperty("message");
+            expect(Message.verify(actual)).toBeTrue();
         });
     });
 
@@ -35,6 +44,10 @@ describe("Message", () => {
     });
 
     describe("verify", () => {
+        it("should be ok", () => {
+            expect(Message.verify(fixture.data)).toBeTrue();
+        });
+
         it("should verify a signed message", () => {
             const signedMessage = Message.sign("test", identity.passphrase);
             expect(Message.verify(signedMessage)).toBe(true);
