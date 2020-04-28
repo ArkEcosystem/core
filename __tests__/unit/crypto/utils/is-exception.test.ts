31f2ed456614b57faf3cd6820d2a07785715e2d8
Bertie <berniesnell@gmail.com>
2020-04-28 16:23:50 +0100

    test(crypto): fix is-exception tests, using actual data from networks

--- __tests__/unit/crypto/utils/is-exception.test.ts
+++ __tests__/unit/crypto/utils/is-exception.test.ts
@@ -10,8 +10,12 @@ beforeAll(() => {
 });
 
 describe("IsException", () => {
-    it("should return true", () => {
-        expect(Utils.isException("1")).toBeTrue();
+    it("should return true for block exception", () => {
+        expect(Utils.isException("15895730198424359628")).toBeTrue();
+    });
+
+    it("should return true for transaction exception", () => {
+        expect(Utils.isException("76bd168e57a4431a64617c4e7864df1e0be89831eabaa230e37643efae2def6f")).toBeTrue();
     });
 
     it("should return false", () => {
