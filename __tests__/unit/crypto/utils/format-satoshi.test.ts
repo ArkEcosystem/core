81c5bc7e5d6a386024e3ccab9fc63244a229a84d
Bertie <berniesnell@gmail.com>
2020-04-28 15:55:49 +0100

    test(crypto): test format satoshi method

--- __tests__/unit/crypto/utils/format-satoshi.test.ts
+++ __tests__/unit/crypto/utils/format-satoshi.test.ts
@@ -1,14 +1,24 @@
 import "jest-extended";
 
+import { CryptoManager } from "@packages/crypto/src";
+
 import { SATOSHI } from "../../../../packages/crypto/src/constants";
-import { BigNumber, formatSatoshi } from "../../../../packages/crypto/src/utils";
+
+let BigNumber;
+let Utils;
+
+beforeAll(() => {
+    const crypto = CryptoManager.createFromPreset("devnet");
+    BigNumber = crypto.libraryManager.libraries.BigNumber;
+    Utils = crypto.libraryManager.Utils;
+});
 
 describe("Format Satoshi", () => {
     it("should format satoshis", () => {
-        expect(formatSatoshi(BigNumber.make(SATOSHI))).toBe("1 DѦ");
-        expect(formatSatoshi(BigNumber.make(0.1 * SATOSHI))).toBe("0.1 DѦ");
-        expect(formatSatoshi(BigNumber.make((0.1 * SATOSHI).toString()))).toBe("0.1 DѦ");
-        expect(formatSatoshi(BigNumber.make(10))).toBe("0.0000001 DѦ");
-        expect(formatSatoshi(BigNumber.make(SATOSHI + 10012))).toBe("1.00010012 DѦ");
+        expect(Utils.formatSatoshi(BigNumber.make(SATOSHI))).toBe("1 DѦ");
+        expect(Utils.formatSatoshi(BigNumber.make(0.1 * SATOSHI))).toBe("0.1 DѦ");
+        expect(Utils.formatSatoshi(BigNumber.make((0.1 * SATOSHI).toString()))).toBe("0.1 DѦ");
+        expect(Utils.formatSatoshi(BigNumber.make(10))).toBe("0.0000001 DѦ");
+        expect(Utils.formatSatoshi(BigNumber.make(SATOSHI + 10012))).toBe("1.00010012 DѦ");
     });
 });
