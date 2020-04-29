e5afa320d512bb94a8b11e54471e3a685d96e45b
Bertie <berniesnell@gmail.com>
2020-04-29 12:00:28 +0100

    refactor(crypto): remove errors that relate to transactions and blocks

--- packages/crypto/src/errors.ts
+++ packages/crypto/src/errors.ts
@@ -70,106 +70,6 @@ export class AddressNetworkError extends CryptoError {
     }
 }
 
-export class TransactionTypeError extends CryptoError {
-    public constructor(given: string) {
-        super(`Type ${given} not supported.`);
-    }
-}
-
-export class InvalidTransactionBytesError extends CryptoError {
-    public constructor(message: string) {
-        super(`Failed to deserialize transaction, encountered invalid bytes: ${message}`);
-    }
-}
-
-export class TransactionSchemaError extends CryptoError {
-    public constructor(what: string) {
-        super(what);
-    }
-}
-
-export class TransactionVersionError extends CryptoError {
-    public constructor(given: number) {
-        super(`Version ${given} not supported.`);
-    }
-}
-
-export class UnkownTransactionError extends CryptoError {
-    public constructor(given: string) {
-        super(`Unknown transaction type: ${given}`);
-    }
-}
-
-export class TransactionAlreadyRegisteredError extends CryptoError {
-    public constructor(name: string) {
-        super(`Transaction type ${name} is already registered.`);
-    }
-}
-
-export class TransactionKeyAlreadyRegisteredError extends CryptoError {
-    public constructor(name: string) {
-        super(`Transaction key ${name} is already registered.`);
-    }
-}
-
-export class TransactionVersionAlreadyRegisteredError extends CryptoError {
-    public constructor(name: string, version: number) {
-        super(`Transaction type ${name} is already registered in version ${version}.`);
-    }
-}
-
-export class CoreTransactionTypeGroupImmutableError extends CryptoError {
-    public constructor() {
-        super(`The Core transaction type group is immutable.`);
-    }
-}
-
-export class MissingMilestoneFeeError extends CryptoError {
-    public constructor(name: string) {
-        super(`Missing milestone fee for '${name}'.`);
-    }
-}
-
-export class MaximumPaymentCountExceededError extends CryptoError {
-    public constructor(limit: number) {
-        super(`Number of payments exceeded the allowed maximum of ${limit}.`);
-    }
-}
-
-export class MinimumPaymentCountSubceededError extends CryptoError {
-    public constructor() {
-        super(`Number of payments subceeded the required minimum of 2.`);
-    }
-}
-
-export class VendorFieldLengthExceededError extends CryptoError {
-    public constructor(limit: number) {
-        super(`Length of vendor field exceeded the allowed maximum ${limit}.`);
-    }
-}
-
-export class MissingTransactionSignatureError extends CryptoError {
-    public constructor() {
-        super(`Expected the transaction to be signed.`);
-    }
-}
-
-export class BlockSchemaError extends CryptoError {
-    public constructor(height: number, what: string) {
-        super(`Height (${height}): ${what}`);
-    }
-}
-
-export class PreviousBlockIdFormatError extends CryptoError {
-    public constructor(thisBlockHeight: number, previousBlockId: string) {
-        super(
-            `The config denotes that the block at height ${thisBlockHeight - 1} ` +
-                `must use full SHA256 block id, but the next block (at ${thisBlockHeight}) ` +
-                `contains previous block id "${previousBlockId}"`,
-        );
-    }
-}
-
 export class InvalidMilestoneConfigurationError extends CryptoError {
     public constructor(message: string) {
         super(message);
@@ -179,11 +79,5 @@ export class InvalidMilestoneConfigurationError extends CryptoError {
 export class InvalidMultiSignatureAssetError extends CryptoError {
     public constructor() {
         super(`The multi signature asset is invalid.`);
-    }
-}
-
-export class DuplicateParticipantInMultiSignatureError extends CryptoError {
-    public constructor() {
-        super(`Invalid multi signature, because duplicate participant found.`);
     }
 }
