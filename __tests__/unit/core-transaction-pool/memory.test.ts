416eaa147af31f819bc814b71f3ff4ff922427db
Air1 <erwann@ark.io>
2020-04-30 18:42:07 +0400

    test(unit): added tx pool memory tests

--- __tests__/unit/core-transaction-pool/memory.test.ts
+++ __tests__/unit/core-transaction-pool/memory.test.ts
@@ -8,18 +8,51 @@ Managers.configManager.getMilestone().aip11 = true;
 
 describe("Memory", () => {
     describe("getLowestFeeLastNonce", () => {
-        it("should get the lowest fee which also is the last nonce from the sender", () => {
+        it("should get the lowest fee which also is the last nonce from the sender - when a 'last nonce tx' is among the 100 lowest fee txs", () => {
             const txs = [];
             let lastNonceLowestFeeTx;
-            for (let i = 0; i < 20; i++) {
-                for (let nonce = 1; nonce <= 5; nonce++) {
+            for (let i = 0; i < 100; i++) {
+                for (let nonce = 1; nonce <= 10; nonce++) {
                     const passphrase = `random ${i}`;
                     const address = "AWLzbT4z7KntQ3D9LTz7ukPHyXy6mNy2YQ";
                     const transaction = Transactions.BuilderFactory.transfer()
                         .nonce(nonce.toString())
                         .recipientId(address)
                         .amount("1")
-                        .fee(Math.floor(Math.random() * 100000).toString()) //
+                        .fee((1e6 - 1000 * nonce - i).toString()) // the last nonce tx will be the lowest fee
+                        .sign(passphrase)
+                        .build();
+
+                    txs.push(transaction);
+                }
+                const lastNonceTx = txs[txs.length - 1];
+                lastNonceLowestFeeTx = lastNonceLowestFeeTx
+                    ? lastNonceTx.data.fee.isLessThan(lastNonceLowestFeeTx.data.fee)
+                        ? lastNonceTx
+                        : lastNonceLowestFeeTx
+                    : lastNonceTx;
+            }
+
+            const memory = new Memory(120);
+            for (const tx of txs) {
+                memory.remember(tx);
+            }
+
+            expect(memory.getLowestFeeLastNonce()).toEqual(lastNonceLowestFeeTx);
+        });
+
+        it("should get the lowest fee which also is the last nonce from the sender - when the 100 lowest fees are not a 'last nonce tx'", () => {
+            const txs = [];
+            let lastNonceLowestFeeTx;
+            for (let i = 0; i < 100; i++) {
+                for (let nonce = 1; nonce <= 10; nonce++) {
+                    const passphrase = `random ${i}`;
+                    const address = "AWLzbT4z7KntQ3D9LTz7ukPHyXy6mNy2YQ";
+                    const transaction = Transactions.BuilderFactory.transfer()
+                        .nonce(nonce.toString())
+                        .recipientId(address)
+                        .amount("1")
+                        .fee((1000 * nonce + i).toString()) // the last nonce tx will be the biggest fee
                         .sign(passphrase)
                         .build();
 
