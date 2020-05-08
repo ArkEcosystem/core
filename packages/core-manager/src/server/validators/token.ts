81ebb94960e84fdada0ec3cd1302fa1ff54a793e
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2020-05-08 14:20:14 +0200

    feat(core-manager) implement token authentication (#3688)

---
+++ packages/core-manager/src/server/validators/token.ts
@@ -0,0 +1,16 @@
+import { Container, Providers } from "@arkecosystem/core-kernel";
+import { Authentication } from "../../contracts";
+
+@Container.injectable()
+export class SimpleTokenValidator implements Authentication.TokenValidator {
+    @Container.inject(Container.Identifiers.PluginConfiguration)
+    @Container.tagged("plugin", "@arkecosystem/core-manager")
+    private readonly configuration!: Providers.PluginConfiguration;
+
+    public async validate(token: string): Promise<boolean> {
+        // ts-ignore
+        const pluginsConfiguration = this.configuration.get("plugins") as any;
+
+        return token === pluginsConfiguration.tokenAuthentication.token;
+    }
+}
