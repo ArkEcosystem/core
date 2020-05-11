66ec52e3b74f9988b0f059d4e95341764954a574
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-05-11 12:26:06 +0200

    core(core-manager): fix lint problems

--- packages/core-manager/src/actions/info-core-version.ts
+++ packages/core-manager/src/actions/info-core-version.ts
@@ -4,10 +4,10 @@ import latestVersion from "latest-version"
 
 @Container.injectable()
 export class Action implements Actions.Action {
+    public name = "info.coreVersion";
+
     @Container.inject(Container.Identifiers.Application)
     private readonly app!: Application;
-
-    public name = "info.coreVersion";
 
     public async execute(params: object): Promise<any> {
         return {
