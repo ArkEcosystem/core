f31109fc669b004a3ad23ed71016a04b940856d4
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2020-05-15 07:40:43 +0200

    feat(core-manager): implement `process.list` action (#3701)
    * chore(core): implement manager start and stop commands
    
    * test(core): test manager start and stop commands
    
    * chore(core-manager): implements process-list action
    
    * test(core-manager): test process-list action
    
    * chore(core-manager): process memory in KB

--- __tests__/unit/core-manager/actions/info-core-status.test.ts
+++ __tests__/unit/core-manager/actions/info-core-status.test.ts
@@ -1,10 +1,10 @@
 import "jest-extended";
 
-import { Sandbox } from "@packages/core-test-framework";
 import { ProcessState } from "@packages/core-cli/src/contracts";
 import { Action } from "@packages/core-manager/src/actions/info-core-status";
 import { Identifiers } from "@packages/core-manager/src/ioc";
 import { HttpClient } from "@packages/core-manager/src/utils";
+import { Sandbox } from "@packages/core-test-framework";
 
 let sandbox: Sandbox;
 let action: Action;
@@ -14,19 +14,15 @@ let mockCli;
 beforeEach(() => {
     mockCli = {
         get: jest.fn().mockReturnValue({
-            status: jest.fn().mockReturnValue(ProcessState.Online)
-        })
-    }
+            status: jest.fn().mockReturnValue(ProcessState.Online),
+        }),
+    };
 
-    HttpClient.prototype.get = jest.fn().mockReturnValue(
-        {
-            data: {
-                syncing: true
-            }
-        }
-    )
-
-
+    HttpClient.prototype.get = jest.fn().mockReturnValue({
+        data: {
+            syncing: true,
+        },
+    });
 
     sandbox = new Sandbox();
 
@@ -36,62 +32,62 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-    delete process.env.CORE_API_DISABLED
+    delete process.env.CORE_API_DISABLED;
     jest.clearAllMocks();
-})
+});
 
 describe("Info:CoreStatus", () => {
     it("should have name", () => {
         expect(action.name).toEqual("info.coreStatus");
-    })
+    });
 
     it("should return status and syncing using HTTP", async () => {
-        let promise = action.execute({});
+        const promise = action.execute({});
 
         await expect(promise).toResolve();
 
-        let result = await promise;
+        const result = await promise;
 
-        expect(result).toEqual({ processStatus: "online", syncing: true })
+        expect(result).toEqual({ processStatus: "online", syncing: true });
     });
 
     it("should return status and syncing using HTTPS", async () => {
         process.env.CORE_API_DISABLED = "true";
 
-        let promise = action.execute({});
+        const promise = action.execute({});
 
         await expect(promise).toResolve();
 
-        let result = await promise;
+        const result = await promise;
 
-        expect(result).toEqual({ processStatus: "online", syncing: true })
+        expect(result).toEqual({ processStatus: "online", syncing: true });
     });
 
     it("should return syncing = false if error on request", async () => {
         HttpClient.prototype.get = jest.fn().mockImplementation(async () => {
             throw new Error();
-        })
+        });
 
-        let promise = action.execute({});
+        const promise = action.execute({});
 
         await expect(promise).toResolve();
 
-        let result = await promise;
+        const result = await promise;
 
-        expect(result).toEqual({ processStatus: "online", syncing: undefined })
+        expect(result).toEqual({ processStatus: "online", syncing: undefined });
     });
 
     it("should return status undefined if process status is undefined", async () => {
         mockCli.get = jest.fn().mockReturnValue({
-            status: jest.fn().mockReturnValue(undefined)
-        })
+            status: jest.fn().mockReturnValue(undefined),
+        });
 
-        let promise = action.execute({});
+        const promise = action.execute({});
 
         await expect(promise).toResolve();
 
-        let result = await promise;
+        const result = await promise;
 
-        expect(result).toEqual({ processStatus: "undefined", syncing: true })
+        expect(result).toEqual({ processStatus: "undefined", syncing: true });
     });
 });
---
+++ __tests__/unit/core-manager/actions/process-list.test.ts
@@ -0,0 +1,69 @@
+import "jest-extended";
+
+import { ProcessState } from "@packages/core-cli/src/contracts";
+import { Action } from "@packages/core-manager/src/actions/process-list";
+import { Identifiers } from "@packages/core-manager/src/ioc";
+import { Sandbox } from "@packages/core-test-framework";
+
+let sandbox: Sandbox;
+let action: Action;
+
+let mockCli;
+
+beforeEach(() => {
+    mockCli = {
+        get: jest.fn().mockReturnValue({
+            list: jest.fn().mockReturnValue([
+                {
+                    pid: 13477,
+                    name: "ark-manager",
+                    pm_id: 0,
+                    pm2_env: {},
+                    monit: {
+                        memory: 98283520,
+                        cpu: 0.1,
+                    },
+                },
+            ]),
+            status: jest.fn().mockReturnValue(ProcessState.Online),
+        }),
+    };
+
+    sandbox = new Sandbox();
+
+    sandbox.app.bind(Identifiers.CLI).toConstantValue(mockCli);
+
+    action = sandbox.app.resolve(Action);
+});
+
+afterEach(() => {
+    delete process.env.CORE_API_DISABLED;
+    jest.clearAllMocks();
+});
+
+describe("Process:List", () => {
+    it("should have name", () => {
+        expect(action.name).toEqual("process.list");
+    });
+
+    it("should return process list", async () => {
+        const promise = action.execute({});
+
+        await expect(promise).toResolve();
+
+        const result = await promise;
+
+        expect(result).toEqual([
+            {
+                pid: 13477,
+                name: "ark-manager",
+                pm_id: 0,
+                monit: {
+                    memory: 95980,
+                    cpu: 0.1,
+                },
+                status: "online",
+            },
+        ]);
+    });
+});
