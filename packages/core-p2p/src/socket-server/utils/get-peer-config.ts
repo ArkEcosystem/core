b8d68b676b73dbc11396200ae764aff4a73aaf40
Air1 <erwann@ark.io>
2020-05-29 15:12:11 +0400

    syntax: lint

--- packages/core-p2p/src/socket-server/utils/get-peer-config.ts
+++ packages/core-p2p/src/socket-server/utils/get-peer-config.ts
@@ -3,26 +3,30 @@ import { Managers } from "@arkecosystem/crypto";
 
 type PluginConfig = { package: string; options: any };
 
-export const getPeerConfig = (app: Contracts.Kernel.Application): Contracts.P2P.PeerConfig => {
-    return {
-        version: app.version(),
-        network: {
-            version: Managers.configManager.get("network.pubKeyHash"),
-            name: Managers.configManager.get("network.name"),
-            nethash: Managers.configManager.get("network.nethash"),
-            explorer: Managers.configManager.get("network.client.explorer"),
-            token: {
-                name: Managers.configManager.get("network.client.token"),
-                symbol: Managers.configManager.get("network.client.symbol"),
-            },
-        },
-        plugins: transformPlugins(
-            getPluginsConfig(
-                app.get<Services.Config.ConfigRepository>(Container.Identifiers.ConfigRepository).get("app.plugins"),
-                app
-            )
-        ),
-    };
+const transformPlugins = (plugins: PluginConfig[]): Contracts.P2P.PeerPlugins => {
+    const result: Contracts.P2P.PeerPlugins = {};
+
+    for (const pluginConfig of plugins) {
+        const name = pluginConfig.package;
+        const options = pluginConfig.options?.server?.http
+            || pluginConfig.options?.server?.https
+            || pluginConfig.options?.server
+            || pluginConfig.options
+            || {}; // lots of options for server configuration... TODO review see if it can be cleaner
+
+        const port = Number(options.port);
+
+        if (isNaN(port) || name.includes("core-p2p")) {
+            continue;
+        }
+
+        result[name] = {
+            enabled: true, // default to true because "enabled" flag is in different place based on which plugin
+            port,
+        };
+    }
+
+    return result;
 };
 
 const getPluginsConfig = (plugins: PluginConfig[], app: Contracts.Kernel.Application) => {
@@ -61,28 +65,24 @@ const getPluginsConfig = (plugins: PluginConfig[], app: Contracts.Kernel.Applica
     });
 }
 
-const transformPlugins = (plugins: PluginConfig[]): Contracts.P2P.PeerPlugins => {
-    const result: Contracts.P2P.PeerPlugins = {};
-
-    for (const pluginConfig of plugins) {
-        const name = pluginConfig.package;
-        const options = pluginConfig.options?.server?.http
-            || pluginConfig.options?.server?.https
-            || pluginConfig.options?.server
-            || pluginConfig.options
-            || {}; // lots of options for server configuration... TODO review see if it can be cleaner
-
-        const port = Number(options.port);
-
-        if (isNaN(port) || name.includes("core-p2p")) {
-            continue;
-        }
-
-        result[name] = {
-            enabled: true, // default to true because "enabled" flag is in different place based on which plugin
-            port,
-        };
-    }
-
-    return result;
+export const getPeerConfig = (app: Contracts.Kernel.Application): Contracts.P2P.PeerConfig => {
+    return {
+        version: app.version(),
+        network: {
+            version: Managers.configManager.get("network.pubKeyHash"),
+            name: Managers.configManager.get("network.name"),
+            nethash: Managers.configManager.get("network.nethash"),
+            explorer: Managers.configManager.get("network.client.explorer"),
+            token: {
+                name: Managers.configManager.get("network.client.token"),
+                symbol: Managers.configManager.get("network.client.symbol"),
+            },
+        },
+        plugins: transformPlugins(
+            getPluginsConfig(
+                app.get<Services.Config.ConfigRepository>(Container.Identifiers.ConfigRepository).get("app.plugins"),
+                app
+            )
+        ),
+    };
 };
