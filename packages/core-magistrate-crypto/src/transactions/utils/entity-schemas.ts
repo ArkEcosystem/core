b03fe0f1cf1e63a2432c154caa05d8964e659ac0
Air1 <erwann@ark.io>
2020-06-02 14:55:07 +0400

    refactor: entity strings max length 250

--- packages/core-magistrate-crypto/src/transactions/utils/entity-schemas.ts
+++ packages/core-magistrate-crypto/src/transactions/utils/entity-schemas.ts
@@ -3,22 +3,28 @@ export const update = {
     additionalProperties: false,
     properties: {
         description: {
-            allOf: [
-                { type: "string", pattern: "^[a-zA-Z0-9]+(( - |[ ._-])[a-zA-Z0-9]+)*[.]?$" },
-                { minLength: 1, maxLength: 140 },
-            ]
+            type: "string",
+            pattern: "^[a-zA-Z0-9]+(( - |[ ._-])[a-zA-Z0-9]+)*[.]?$",
+            minLength: 1,
+            maxLength: 250,
         },
-        website: { type: "string", format: "uri" },
+        website: { type: "string", format: "uri", minLength: 1, maxLength: 250 },
         images: {
             type: "array",
-            items: { type: "string", format: "uri", pattern: "https:\/\/flickr.com\/.*" },
+            items: { type: "string", format: "uri", pattern: "https:\/\/flickr.com\/.*", minLength: 1, maxLength: 250 },
             minItems: 1,
             maxItems: 10,
             uniqueItems: true,
         },
         videos: {
             type: "array",
-            items: { type: "string", format: "uri", pattern: "(https:\/\/youtube.com\/.*)|(https:\/\/vimeo.com\/.*)" },
+            items: {
+                type: "string",
+                format: "uri",
+                pattern: "(https:\/\/youtube.com\/.*)|(https:\/\/vimeo.com\/.*)",
+                minLength: 1,
+                maxLength: 250,
+            },
             minItems: 1,
             maxItems: 10,
             uniqueItems: true,
@@ -27,19 +33,19 @@ export const update = {
             type: "object",
             additionalProperties: false,
             properties: {
-                github: { type: "string", format: "uri", pattern: "https:\/\/github.com\/.*" },
-                gitlab: { type: "string", format: "uri", pattern: "https:\/\/gitlab.com\/.*" },
-                bitbucket: { type: "string", format: "uri", pattern: "https:\/\/bitbucket.org\/.*" },
-                npmjs: { type: "string", format: "uri", pattern: "https:\/\/npmjs.com\/.*" },
+                github: { type: "string", format: "uri", pattern: "https:\/\/github.com\/.*", minLength: 1, maxLength: 250 },
+                gitlab: { type: "string", format: "uri", pattern: "https:\/\/gitlab.com\/.*", minLength: 1, maxLength: 250 },
+                bitbucket: { type: "string", format: "uri", pattern: "https:\/\/bitbucket.org\/.*", minLength: 1, maxLength: 250 },
+                npmjs: { type: "string", format: "uri", pattern: "https:\/\/npmjs.com\/.*", minLength: 1, maxLength: 250 },
             }
         },
         socialMedia: {
             type: "object",
             additionalProperties: false,
             properties: {
-                facebook: { type: "string", format: "uri", pattern: "https:\/\/facebook.com\/.*" },
-                twitter: { type: "string", format: "uri", pattern: "https:\/\/twitter.com\/.*" },
-                linkedin: { type: "string", format: "uri", pattern: "https:\/\/linkedin.com\/.*" },
+                facebook: { type: "string", format: "uri", pattern: "https:\/\/facebook.com\/.*", minLength: 1, maxLength: 250 },
+                twitter: { type: "string", format: "uri", pattern: "https:\/\/twitter.com\/.*", minLength: 1, maxLength: 250 },
+                linkedin: { type: "string", format: "uri", pattern: "https:\/\/linkedin.com\/.*", minLength: 1, maxLength: 250 },
             }
         }
     }
