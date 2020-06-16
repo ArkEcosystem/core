79a443f0cce0d9e80594d7cc6528b1b745bae3b2
Dmitry Tyshchenko <rainydio@gmail.com>
2020-06-16 07:06:55 +0300

    Improve lock test

--- __tests__/unit/core-kernel/utils/lock.test.ts
+++ __tests__/unit/core-kernel/utils/lock.test.ts
@@ -13,15 +13,10 @@ describe("Lock", () => {
         };
 
         const lock = new Lock();
-        const exclusive1 = lock.runExclusive(fn);
-        const exclusive2 = lock.runExclusive(fn);
-        const exclusive3 = lock.runExclusive(fn);
-
+        const promises = [lock.runExclusive(fn), lock.runExclusive(fn), lock.runExclusive(fn)];
         resolve();
 
-        const results = await Promise.all([exclusive1, exclusive2, exclusive3]);
-
-        expect(results).toEqual([1, 2, 3]);
+        expect(await Promise.all(promises)).toEqual([1, 2, 3]);
     });
 
     it("should run non-exclusive executions in parallel", async () => {
@@ -36,15 +31,10 @@ describe("Lock", () => {
         };
 
         const lock = new Lock();
-        const nonExclusive1 = lock.runNonExclusive(fn);
-        const nonExclusive2 = lock.runNonExclusive(fn);
-        const nonExclusive3 = lock.runNonExclusive(fn);
-
+        const promises = [lock.runNonExclusive(fn), lock.runNonExclusive(fn), lock.runNonExclusive(fn)];
         resolve();
 
-        const results = await Promise.all([nonExclusive1, nonExclusive2, nonExclusive3]);
-
-        expect(results).toEqual([3, 3, 3]);
+        expect(await Promise.all(promises)).toEqual([3, 3, 3]);
     });
 
     it("should run exclusive execution after non-exclusive had finished", async () => {
@@ -59,14 +49,27 @@ describe("Lock", () => {
         };
 
         const lock = new Lock();
-        const nonExclusive1 = lock.runNonExclusive(fn);
-        const nonExclusive2 = lock.runNonExclusive(fn);
-        const exclusive1 = lock.runExclusive(fn);
-
+        const promises = [lock.runNonExclusive(fn), lock.runNonExclusive(fn), lock.runExclusive(fn)];
         resolve();
 
-        const results = await Promise.all([nonExclusive1, nonExclusive2, exclusive1]);
+        expect(await Promise.all(promises)).toEqual([2, 2, 3]);
+    });
 
-        expect(results).toEqual([2, 2, 3]);
+    it("should run non-exclusive execution after exclusive had finished", async () => {
+        let resolve: () => void;
+        const promise = new Promise((r) => (resolve = r));
+
+        let executions = 0;
+        const fn = async () => {
+            executions++;
+            await promise;
+            return executions;
+        };
+
+        const lock = new Lock();
+        const promises = [lock.runExclusive(fn), lock.runNonExclusive(fn), lock.runNonExclusive(fn)];
+        resolve();
+
+        expect(await Promise.all(promises)).toEqual([1, 3, 3]);
     });
 });
