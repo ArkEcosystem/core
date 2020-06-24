2848c6837cd42af067b201f833aab2d519a097c3
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-06-24 13:34:07 +0200

    chore(core-manager): filter events with watchers settings

--- packages/core-manager/src/defaults.ts
+++ packages/core-manager/src/defaults.ts
@@ -5,15 +5,14 @@ export const defaults = {
         storage: `${process.env.CORE_PATH_DATA}/events.sqlite`,
         watch: {
             blocks: !process.env.CORE_WATCH_BLOCKS_DISABLED,
-            transactions: !process.env.CORE_WATCH_TRANSACTIONS_DISABLED,
-            wallets: !process.env.CORE_WATCH_WALLETS_DISABLED,
-            rounds: !process.env.CORE_WATCH_ROUNDS_DISABLED,
             errors: !process.env.CORE_WATCH_ERRORS_DISABLED,
             logs: !process.env.CORE_WATCH_LOGS_DISABLED,
             queries: !process.env.CORE_WATCH_QUERIES_DISABLED,
-            jobs: !process.env.CORE_WATCH_JOBS_DISABLED,
             queues: !process.env.CORE_WATCH_QUEUES_DISABLED,
+            rounds: !process.env.CORE_WATCH_ROUNDS_DISABLED,
             schedules: !process.env.CORE_WATCH_SCHEDULES_DISABLED,
+            transactions: !process.env.CORE_WATCH_TRANSACTIONS_DISABLED,
+            wallets: !process.env.CORE_WATCH_WALLETS_DISABLED,
             webhooks: !process.env.CORE_WATCH_WEBHOOKS_DISABLED,
         },
     },
@@ -29,6 +28,7 @@ export const defaults = {
             host: process.env.CORE_MONITOR_SSL_HOST || "0.0.0.0",
             port: process.env.CORE_MONITOR_SSL_PORT || 8445,
             tls: {
+                // TODO: Take from CORE_API_SSL_KEY if not provided
                 key: process.env.CORE_MONITOR_SSL_KEY,
                 cert: process.env.CORE_MONITOR_SSL_CERT,
             },
--- packages/core-manager/src/listener.ts
+++ packages/core-manager/src/listener.ts
@@ -1,9 +1,13 @@
-import { Container, Contracts } from "@arkecosystem/core-kernel";
+import { Container, Contracts, Providers } from "@arkecosystem/core-kernel";
 
 import { DatabaseService } from "./database-service";
 
 @Container.injectable()
 export class Listener {
+    @Container.inject(Container.Identifiers.PluginConfiguration)
+    @Container.tagged("plugin", "@arkecosystem/core-manager")
+    private readonly configuration!: Providers.PluginConfiguration;
+
     @Container.inject(Container.Identifiers.EventDispatcherService)
     private readonly eventDispatcher!: Contracts.Kernel.EventDispatcher;
 
@@ -19,6 +23,37 @@ export class Listener {
     }
 
     private handleEvents(data: { name: Contracts.Kernel.EventName; data: any }) {
-        this.databaseService.addEvent(data.name.toString(), data.data);
+        if (this.canAddEvent(data.name.toString())) {
+            this.databaseService.addEvent(data.name.toString(), data.data);
+        }
+    }
+
+    private canAddEvent(name: string): boolean {
+        if (name.startsWith("block") && !this.configuration.getRequired<boolean>("watcher.watch.blocks")) {
+            return false;
+        }
+        if (name.startsWith("log.error") && !this.configuration.getRequired<boolean>("watcher.watch.errors")) {
+            return false;
+        }
+        if (name.startsWith("queue") && !this.configuration.getRequired<boolean>("watcher.watch.queues")) {
+            return false;
+        }
+        if (name.startsWith("round") && !this.configuration.getRequired<boolean>("watcher.watch.rounds")) {
+            return false;
+        }
+        if (name.startsWith("schedule") && !this.configuration.getRequired<boolean>("watcher.watch.schedule")) {
+            return false;
+        }
+        if (name.startsWith("transaction") && !this.configuration.getRequired<boolean>("watcher.watch.transactions")) {
+            return false;
+        }
+        if (name.startsWith("wallet") && !this.configuration.getRequired<boolean>("watcher.watch.wallets")) {
+            return false;
+        }
+        if (name.startsWith("webhooks") && !this.configuration.getRequired<boolean>("watcher.watch.webhooks")) {
+            return false;
+        }
+
+        return true;
     }
 }
