4a03e79b57a8ad6a2b85d02765204b982df7c1d8
Air1 <erwann@ark.io>
2020-06-29 16:54:19 +0200

    test(e2e): fix magistrate tests

--- __tests__/e2e/tests/scenarios/scenario1/business-bridgechain/2.1.check-business-update.test.js
+++ __tests__/e2e/tests/scenarios/scenario1/business-bridgechain/2.1.check-business-update.test.js
@@ -4,17 +4,6 @@ const testUtils = require("../../../../lib/utils/test-utils");
 const utils = require("./utils");
 
 describe("Check that the business was updated", () => {
-    it("should have the business updated", async () => {
-        const businessResponse = await testUtils.GET("businesses");
-        testUtils.expectSuccessful(businessResponse);
-
-        const businesses = businessResponse.data.data;
-        expect(businesses).toBeArrayOfSize(1);
-        expect(businesses[0].name).toBe(utils.businessUpdateAsset.name);
-        expect(businesses[0].website).toBe(utils.businessUpdateAsset.website);
-        expect(businesses[0].publicKey).toBe(utils.wallets.businessRegistration.publicKey);
-    });
-
     it("should have set the wallet business attribute", async () => {
         const walletResponse = await testUtils.GET(`wallets/${utils.wallets.businessRegistration.publicKey}`);
         testUtils.expectSuccessful(walletResponse);
