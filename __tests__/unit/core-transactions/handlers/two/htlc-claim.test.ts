5006f265579f35328582b83c8b798a857df73e7a
Dmitry Tyshchenko <rainydio@gmail.com>
2020-06-30 12:50:46 +0300

    Rollback htlc claim test

--- __tests__/unit/core-transactions/handlers/two/htlc-claim.test.ts
+++ __tests__/unit/core-transactions/handlers/two/htlc-claim.test.ts
@@ -52,19 +52,13 @@ const mockGetLastBlock = jest.fn();
 StateStore.prototype.getLastBlock = mockGetLastBlock;
 mockGetLastBlock.mockReturnValue({ data: mockLastBlockData });
 
-const transactionHistoryService = {
-    streamManyByCriteria: jest.fn(),
-};
-
 beforeEach(() => {
-    transactionHistoryService.streamManyByCriteria.mockReset();
-
     const config = Generators.generateCryptoConfigRaw();
     configManager.setConfig(config);
     Managers.configManager.setConfig(config);
 
     app = initApp();
-    app.bind(Identifiers.TransactionHistoryService).toConstantValue(transactionHistoryService);
+    app.bind(Identifiers.TransactionHistoryService).toConstantValue(null);
 
     walletRepository = app.get<Wallets.WalletRepository>(Identifiers.WalletRepository);
 
@@ -199,11 +193,8 @@ describe("Htlc claim", () => {
 
         describe("bootstrap", () => {
             it("should resolve", async () => {
-                transactionHistoryService.streamManyByCriteria.mockImplementationOnce(async (_, cb: Function) => {
-                    cb(htlcLockTransaction);
-                });
+                Mocks.TransactionRepository.setTransactions([Mapper.mapTransactionToModel(htlcLockTransaction)]);
                 await expect(handler.bootstrap()).toResolve();
-                // TODO: assert wallet repository
             });
         });
 
