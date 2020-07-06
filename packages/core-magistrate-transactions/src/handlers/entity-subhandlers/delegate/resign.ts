8b22912227b41ecf5ff764839d3dd04ad7bda4df
Air1 <erwann@ark.io>
2020-07-06 17:27:44 +0200

    feat: aip36 delegate entity

---
+++ packages/core-magistrate-transactions/src/handlers/entity-subhandlers/delegate/resign.ts
@@ -0,0 +1,3 @@
+import { EntityResignSubHandler } from "../resign";
+
+export class DelegateResignSubHandler extends EntityResignSubHandler {}
---
+++ packages/core-magistrate-transactions/src/handlers/entity-subhandlers/delegate/update.ts
@@ -0,0 +1,3 @@
+import { EntityUpdateSubHandler } from "../update";
+
+export class DelegateUpdateSubHandler extends EntityUpdateSubHandler {}
