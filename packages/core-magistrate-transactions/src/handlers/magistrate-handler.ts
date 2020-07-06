1e563962137c95e6967a62702b6f98154b71cdc2
Bertie <berniesnell@gmail.com>
2020-07-06 13:35:28 +0100

    fix(core-magistrate-transactions): remove dynamic fee from magistrate handler

--- packages/core-magistrate-transactions/src/handlers/magistrate-handler.ts
+++ packages/core-magistrate-transactions/src/handlers/magistrate-handler.ts
@@ -2,16 +2,10 @@ import { Contracts } from "@arkecosystem/core-kernel";
 import { Handlers } from "@arkecosystem/core-transactions";
 import { Interfaces as CryptoInterfaces, Managers, Utils } from "@arkecosystem/crypto";
 
-import { StaticFeeMismatchError } from "../errors";
-
 export abstract class MagistrateTransactionHandler extends Handlers.TransactionHandler {
     public async isActivated(): Promise<boolean> {
         const milestone = Managers.configManager.getMilestone();
         return milestone.aip11 === true && !milestone.aip36;
-    }
-
-    public dynamicFee({ height }: Contracts.Shared.DynamicFeeContext): Utils.BigNumber {
-        return this.getConstructor().staticFee({ height });
     }
 
     public async throwIfCannotBeApplied(
@@ -20,11 +14,6 @@ export abstract class MagistrateTransactionHandler extends Handlers.TransactionH
     ): Promise<void> {
         if (Utils.isException(transaction.data.id)) {
             return;
-        }
-
-        const staticFee: Utils.BigNumber = this.getConstructor().staticFee();
-        if (!transaction.data.fee.isEqualTo(staticFee)) {
-            throw new StaticFeeMismatchError(staticFee.toFixed());
         }
 
         return super.throwIfCannotBeApplied(transaction, wallet);
