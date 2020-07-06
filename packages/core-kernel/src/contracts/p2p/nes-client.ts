3a2ed5f5f84d096189e8fa3e01627c5dee7d25ec
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-07-06 12:45:05 +0000

    refactor(core-p2p): cleanup

--- packages/core-kernel/src/contracts/p2p/nes-client.ts
+++ packages/core-kernel/src/contracts/p2p/nes-client.ts
@@ -1,11 +1,5 @@
 export interface Client {
     connect(options: any): Promise<any>;
-    // overrideReconnectionAuth(auth: any): boolean;s
-    // reauthenticate(auth: any): Promise<any>;
     disconnect(): Promise<any>;
     request(options: any): Promise<any>;
-    // message(message: any): Promise<any>;
-    // subscriptions(): string[];
-    // subscribe(path: any, handler: any): Promise<any>;
-    // unsubscribe(path: any, handler: any): Promise<any>;
 }
