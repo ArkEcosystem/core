7c07d3c193bce872d0fcd1e6fa3db982ff943730
Seba2321 <sebastijan.kuzner@outlook.com>
2020-07-17 12:41:53 +0200

    chore(core-kernel): variable rename

--- packages/core-kernel/src/utils/is-blacklisted.ts
+++ packages/core-kernel/src/utils/is-blacklisted.ts
@@ -1,12 +1,12 @@
 import nm from "nanomatch";
 
 // todo: review the implementation
-export const isBlacklisted = (whitelist: string[], remoteAddress: string): boolean => {
-    if (!Array.isArray(whitelist) || !whitelist.length) {
+export const isBlacklisted = (blacklist: string[], remoteAddress: string): boolean => {
+    if (!Array.isArray(blacklist) || !blacklist.length) {
         return false;
     }
 
-    for (const ip of whitelist) {
+    for (const ip of blacklist) {
         try {
             if (nm.isMatch(remoteAddress, ip)) {
                 return true;
