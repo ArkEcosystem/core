7f9226b07f627a11998c02f49ed46d419c4e6793
Dmitry Tyshchenko <rainydio@gmail.com>
2020-08-06 06:41:05 +0300

    Remove identifiers

--- packages/core-transactions/src/identifiers.ts
+++
@@ -1,4 +0,0 @@
-export const Identifiers = {
-    DelegateSearchService: Symbol.for("Transactions<DelegateSearchService>"),
-    HtlcLockSearchService: Symbol.for("Transactions<HtlcLockSearchService>"),
-};
--- packages/core-transactions/src/index.ts
+++ packages/core-transactions/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./service-provider";
-export * from "./identifiers";
 
 export * as Errors from "./errors";
 export * as Handlers from "./handlers";
