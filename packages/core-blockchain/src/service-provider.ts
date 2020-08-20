11ad2a71e84581945466c7cab945a05c1176b1fe
Dmitry Tyshchenko <rainydio@gmail.com>
2020-08-20 07:35:02 +0300

    Initialize blockchain when resolved first time

--- packages/core-blockchain/src/service-provider.ts
+++ packages/core-blockchain/src/service-provider.ts
@@ -4,31 +4,22 @@ import { ProcessBlockAction } from "./actions";
 import { Blockchain } from "./blockchain";
 import { BlockProcessor } from "./processor";
 import { StateMachine } from "./state-machine";
-import { blockchainMachine } from "./state-machine/machine";
 
 export class ServiceProvider extends Providers.ServiceProvider {
     public async register(): Promise<void> {
         this.app.bind(Container.Identifiers.StateMachine).to(StateMachine).inSingletonScope();
-
-        const blockchain: Blockchain = this.app.resolve<Blockchain>(Blockchain);
-
-        this.app.bind(Container.Identifiers.BlockchainService).toConstantValue(blockchain);
-
+        this.app.bind(Container.Identifiers.BlockchainService).to(Blockchain).inSingletonScope();
         this.app.bind(Container.Identifiers.BlockProcessor).to(BlockProcessor).inSingletonScope();
-
-        blockchain.initialize(this.config().all()); // ? why it isn't in boot?
-
-        this.app.get<Contracts.State.StateStore>(Container.Identifiers.StateStore).reset(blockchainMachine);
 
         this.registerActions();
     }
 
     public async boot(): Promise<void> {
-        await this.app.get<Blockchain>(Container.Identifiers.BlockchainService).boot();
+        await this.app.get<Contracts.Blockchain.Blockchain>(Container.Identifiers.BlockchainService).boot();
     }
 
     public async dispose(): Promise<void> {
-        await this.app.get<Blockchain>(Container.Identifiers.BlockchainService).dispose();
+        await this.app.get<Contracts.Blockchain.Blockchain>(Container.Identifiers.BlockchainService).dispose();
     }
 
     public async bootWhen(): Promise<boolean> {
