089e5b401471ab7280ebb0ef2c6b15fdc126595b
Brian Faust <hello@basecode.sh>
2020-08-25 10:41:20 +0300

    wip

--- packages/core-transactions/src/errors.ts
+++ packages/core-transactions/src/errors.ts
@@ -80,6 +80,24 @@ export class UnexpectedMultiSignatureError extends TransactionError {
     }
 }
 
+export class MissingMultiSignatureOnSenderError extends TransactionError {
+    constructor() {
+        super(`Failed to apply transaction, because sender does not have a multi signature.`);
+    }
+}
+
+export class InvalidMultiSignaturesError extends TransactionError {
+    constructor() {
+        super(`Failed to apply transaction, because multi signatures are invalid.`);
+    }
+}
+
+export class UnsupportedMultiSignatureTransactionError extends TransactionError {
+    constructor() {
+        super(`Failed to apply transaction, because the transaction does not support multi signatures.`);
+    }
+}
+
 export class InvalidSecondSignatureError extends TransactionError {
     constructor() {
         super(`Failed to apply transaction, because the second signature could not be verified.`);
@@ -178,6 +196,12 @@ export class InvalidMultiSignatureError extends TransactionError {
 export class LegacyMultiSignatureError extends TransactionError {
     constructor() {
         super(`Failed to apply transaction, because legacy multi signature is no longer supported.`);
+    }
+}
+
+export class LegacyMultiSignatureRegistrationError extends TransactionError {
+    constructor() {
+        super(`Failed to apply transaction, because legacy multi signature registrations are no longer supported.`);
     }
 }
 
