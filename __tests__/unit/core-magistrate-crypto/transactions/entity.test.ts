e5ffec391cc467e5ef4d673012be745c20d9ca75
Seba2321 <sebastijan.kuzner@outlook.com>
2020-08-26 16:16:53 +0200

    test(core-magistrate-crypto): increase transactions coverage

--- __tests__/unit/core-magistrate-crypto/transactions/entity.test.ts
+++ __tests__/unit/core-magistrate-crypto/transactions/entity.test.ts
@@ -1,15 +1,15 @@
 import "jest-extended";
 
+import { Interfaces } from "@arkecosystem/core-magistrate-crypto";
 import { EntityBuilder } from "@arkecosystem/core-magistrate-crypto/src/builders";
 import { EntityTransaction } from "@arkecosystem/core-magistrate-crypto/src/transactions";
 import { Managers, Transactions, Validation } from "@arkecosystem/crypto";
 
+import { generateAssets } from "../fixtures/entity/assets/generate";
+import { invalidRegisters, validRegisters } from "../fixtures/entity/schemas/register";
+import { invalidResigns, validResigns } from "../fixtures/entity/schemas/resign";
+import { invalidUpdates, validUpdates } from "../fixtures/entity/schemas/update";
 import { checkCommonFields } from "../helper";
-import { generateAssets, generateSpecialAssets } from "../fixtures/entity/assets/generate";
-import { validRegisters, invalidRegisters } from "../fixtures/entity/schemas/register";
-import { validResigns, invalidResigns } from "../fixtures/entity/schemas/resign";
-import { validUpdates, invalidUpdates } from "../fixtures/entity/schemas/update";
-import { Interfaces } from "@arkecosystem/core-magistrate-crypto";
 
 let builder: EntityBuilder;
 
@@ -25,18 +25,13 @@ describe("Entity transaction", () => {
 
     describe("Ser/deser", () => {
         const entityAssets = generateAssets();
-        const entitySpecialAssets = generateSpecialAssets();
 
-        it.each([entityAssets])("should ser/deserialize giving back original fields", (asset) => {
-            const mockVerifySchema = jest.spyOn(Transactions.Verifier, "verifySchema").mockImplementation(
-                (data) => ({ value: data, error: undefined })
-            );
+        it.each(entityAssets)("should ser/deserialize giving back original fields", (asset) => {
+            const mockVerifySchema = jest
+                .spyOn(Transactions.Verifier, "verifySchema")
+                .mockImplementation((data) => ({ value: data, error: undefined }));
 
-            const entity = builder
-                .network(23)
-                .asset(asset)
-                .sign("passphrase")
-                .getStruct();
+            const entity = builder.network(23).asset(asset).sign("passphrase").getStruct();
 
             const serialized = Transactions.TransactionFactory.fromData(entity).serialized.toString("hex");
             const deserialized = Transactions.Deserializer.deserialize(serialized);
@@ -45,21 +40,6 @@ describe("Entity transaction", () => {
             expect(deserialized.data.asset).toEqual(entity.asset);
 
             mockVerifySchema.mockRestore();
-        });
-
-        it.each([entitySpecialAssets])
-        ("should ser/deserialize giving back original fields", ([assetToSerialize, expectedDeserialized]) => {
-            const entity = builder
-                .network(23)
-                .asset(assetToSerialize)
-                .sign("passphrase")
-                .getStruct();
-
-            const serialized = Transactions.TransactionFactory.fromData(entity).serialized.toString("hex");
-            const deserialized = Transactions.Deserializer.deserialize(serialized);
-
-            checkCommonFields(deserialized, entity);
-            expect(deserialized.data.asset).toEqual(expectedDeserialized);
         });
     });
 
@@ -70,21 +50,28 @@ describe("Entity transaction", () => {
             transactionSchema = EntityTransaction.getSchema();
         });
 
-        it.each([...validRegisters, ...validResigns, ...validUpdates].map(asset => [asset]))
-        ("should not give any validation error", (asset: Interfaces.IEntityAsset) => {
-            const entityRegistration = builder.asset(asset).sign("passphrase");
+        it.each([...validRegisters, ...validResigns, ...validUpdates].map((asset) => [asset]))(
+            "should not give any validation error",
+            (asset: Interfaces.IEntityAsset) => {
+                const entityRegistration = builder.asset(asset).sign("passphrase");
 
-            const { error, value } = Validation.validator.validate(transactionSchema, entityRegistration.getStruct());
-            expect(error).toBeUndefined();
-            expect(value.asset).toEqual(asset);
-        });
+                const { error, value } = Validation.validator.validate(
+                    transactionSchema,
+                    entityRegistration.getStruct(),
+                );
+                expect(error).toBeUndefined();
+                expect(value.asset).toEqual(asset);
+            },
+        );
 
-        it.each([...invalidRegisters, ...invalidResigns, ...invalidUpdates].map(asset => [asset]))
-        ("should give validation error", (asset: Interfaces.IEntityAsset) => {
-            const entityRegistration = builder.asset(asset).sign("passphrase");
+        it.each([...invalidRegisters, ...invalidResigns, ...invalidUpdates].map((asset) => [asset]))(
+            "should give validation error",
+            (asset: Interfaces.IEntityAsset) => {
+                const entityRegistration = builder.asset(asset).sign("passphrase");
 
-            const { error } = Validation.validator.validate(transactionSchema, entityRegistration.getStruct());
-            expect(error).not.toBeUndefined();
-        });
+                const { error } = Validation.validator.validate(transactionSchema, entityRegistration.getStruct());
+                expect(error).not.toBeUndefined();
+            },
+        );
     });
 });
