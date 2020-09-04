4fe542aef59f8c91b2c8c36ef4ce368123fc31ff
Dmitry Tyshchenko <rainydio@gmail.com>
2020-09-04 14:49:22 +0300

    Final few pagination service tests

--- __tests__/unit/core-kernel/services/search/pagination-service.test.ts
+++ __tests__/unit/core-kernel/services/search/pagination-service.test.ts
@@ -4,7 +4,34 @@ import { Utils } from "@arkecosystem/crypto";
 
 const container = new Container.Container();
 
+describe("PaginationService.getEmptyPage", () => {
+    it("should return empty page", () => {
+        const paginationService = container.resolve(PaginationService);
+        const emptyPage = paginationService.getEmptyPage();
+
+        expect(emptyPage).toEqual({
+            results: [],
+            totalCount: 0,
+            meta: { totalCountIsEstimate: false },
+        });
+    });
+});
+
 describe("PaginationService.getPage", () => {
+    it("should leave items order intact when sorting isn't provided", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items = [{ v: 1 }, { v: 3 }, {}, { v: 2 }];
+        const sorting = [];
+        const resultsPage = paginationService.getPage(pagination, sorting, items);
+
+        expect(resultsPage).toEqual({
+            results: [{ v: 1 }, { v: 3 }, {}, { v: 2 }],
+            totalCount: 4,
+            meta: { totalCountIsEstimate: false },
+        });
+    });
+
     it("should return items with undefined properties at the end", () => {
         const paginationService = container.resolve(PaginationService);
         const pagination = { offset: 0, limit: 100 };
@@ -61,7 +88,57 @@ describe("PaginationService.getPage", () => {
         });
     });
 
-    it("should sort using second sorting instruction when first properties are equal", () => {
+    it("should sort using second sorting instruction when first properties are equal booleans", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items = [
+            { a: false, b: 200 },
+            { a: false, b: 101 },
+            { a: true, b: 100 },
+        ];
+        const sorting = [
+            { property: "a", direction: "asc" as const },
+            { property: "b", direction: "asc" as const },
+        ];
+        const resultsPage = paginationService.getPage(pagination, sorting, items);
+
+        expect(resultsPage).toEqual({
+            results: [
+                { a: false, b: 101 },
+                { a: false, b: 200 },
+                { a: true, b: 100 },
+            ],
+            totalCount: 3,
+            meta: { totalCountIsEstimate: false },
+        });
+    });
+
+    it("should sort using second sorting instruction when first properties are equal strings", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items = [
+            { a: "a", b: 200 },
+            { a: "a", b: 101 },
+            { a: "b", b: 100 },
+        ];
+        const sorting = [
+            { property: "a", direction: "asc" as const },
+            { property: "b", direction: "asc" as const },
+        ];
+        const resultsPage = paginationService.getPage(pagination, sorting, items);
+
+        expect(resultsPage).toEqual({
+            results: [
+                { a: "a", b: 101 },
+                { a: "a", b: 200 },
+                { a: "b", b: 100 },
+            ],
+            totalCount: 3,
+            meta: { totalCountIsEstimate: false },
+        });
+    });
+
+    it("should sort using second sorting instruction when first properties are equal numbers", () => {
         const paginationService = container.resolve(PaginationService);
         const pagination = { offset: 0, limit: 100 };
         const items = [
@@ -86,6 +163,56 @@ describe("PaginationService.getPage", () => {
         });
     });
 
+    it("should sort using second sorting instruction when first properties are equal bigints", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items = [
+            { a: BigInt(1), b: 200 },
+            { a: BigInt(1), b: 101 },
+            { a: BigInt(2), b: 100 },
+        ];
+        const sorting = [
+            { property: "a", direction: "asc" as const },
+            { property: "b", direction: "asc" as const },
+        ];
+        const resultsPage = paginationService.getPage(pagination, sorting, items);
+
+        expect(resultsPage).toEqual({
+            results: [
+                { a: BigInt(1), b: 101 },
+                { a: BigInt(1), b: 200 },
+                { a: BigInt(2), b: 100 },
+            ],
+            totalCount: 3,
+            meta: { totalCountIsEstimate: false },
+        });
+    });
+
+    it("should sort using second sorting instruction when first properties are equal Utils.BigNumber instances", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items = [
+            { a: Utils.BigNumber.make("1"), b: 200 },
+            { a: Utils.BigNumber.make("1"), b: 101 },
+            { a: Utils.BigNumber.make("2"), b: 100 },
+        ];
+        const sorting = [
+            { property: "a", direction: "asc" as const },
+            { property: "b", direction: "asc" as const },
+        ];
+        const resultsPage = paginationService.getPage(pagination, sorting, items);
+
+        expect(resultsPage).toEqual({
+            results: [
+                { a: Utils.BigNumber.make("1"), b: 101 },
+                { a: Utils.BigNumber.make("1"), b: 200 },
+                { a: Utils.BigNumber.make("2"), b: 100 },
+            ],
+            totalCount: 3,
+            meta: { totalCountIsEstimate: false },
+        });
+    });
+
     it("should sort by Utils.BigNumber property", () => {
         const paginationService = container.resolve(PaginationService);
         const pagination = { offset: 0, limit: 100 };
@@ -98,5 +225,25 @@ describe("PaginationService.getPage", () => {
             totalCount: 3,
             meta: { totalCountIsEstimate: false },
         });
+    });
+
+    it("should throw when sorting over property with union type", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items: { v: number | string }[] = [{ v: 1 }, { v: "2" }];
+        const sorting = [{ property: "v", direction: "asc" as const }];
+
+        expect(() => paginationService.getPage(pagination, sorting, items)).toThrowError(
+            "Mismatched types 'string' and 'number' at 'v'",
+        );
+    });
+
+    it("should throw when sorting over property with invalid type", () => {
+        const paginationService = container.resolve(PaginationService);
+        const pagination = { offset: 0, limit: 100 };
+        const items = [{ v: Symbol() }, { v: Symbol() }];
+        const sorting = [{ property: "v", direction: "asc" as const }];
+
+        expect(() => paginationService.getPage(pagination, sorting, items)).toThrowError("Unexpected type at 'v'");
     });
 });
