773eb8ce7ba23964ed5f516067a1c154993380a0
air1one <36802613+air1one@users.noreply.github.com>
2020-09-18 17:38:37 +0200

    chore(magistrate-api): extend /node/fees and /transactions/fees with entities static fees (#4047)

--- packages/core-magistrate-api/src/service-provider.ts
+++ packages/core-magistrate-api/src/service-provider.ts
@@ -4,6 +4,7 @@ import { Providers } from "@arkecosystem/core-kernel";
 import Handlers from "./handlers";
 import { Identifiers } from "./identifiers";
 import { EntitySearchService } from "./services";
+import { Enums } from "@arkecosystem/core-magistrate-crypto";
 
 /**
  * @export
@@ -20,11 +21,71 @@ export class ServiceProvider extends Providers.ServiceProvider {
 
         for (const identifier of [ApiIdentifiers.HTTP, ApiIdentifiers.HTTPS]) {
             if (this.app.isBound<Server>(identifier)) {
-                await this.app.get<Server>(identifier).register({
+                const server: Server = this.app.get<Server>(identifier);
+
+                await server.register({
                     plugin: Handlers,
                     routes: { prefix: "/api" }, // todo: add magistrate prefix?
                 });
+
+                this.extendApiNodeFees(server);
+                this.extendApiTransactionsFees(server);
             }
         }
+    }
+
+    private extendApiNodeFees(server: Server): void {
+        const nodeFeesRoute = server.getRoute("GET", "/api/node/fees");
+        const originalNodeFeesHandler = nodeFeesRoute.settings.handler;
+        nodeFeesRoute.settings.handler = async (request) => {
+            const originalResponse = await originalNodeFeesHandler(request);
+            
+            return {
+                ...originalResponse,
+                data: {
+                    ...originalResponse.data,
+                    "2": {
+                        entityRegistration: {
+                            avg: Enums.MagistrateTransactionStaticFees.EntityRegister,
+                            max: Enums.MagistrateTransactionStaticFees.EntityRegister,
+                            min: Enums.MagistrateTransactionStaticFees.EntityRegister,
+                            sum: "0",
+                        },
+                        entityResignation: {
+                            avg: Enums.MagistrateTransactionStaticFees.EntityResign,
+                            max: Enums.MagistrateTransactionStaticFees.EntityResign,
+                            min: Enums.MagistrateTransactionStaticFees.EntityResign,
+                            sum: "0",
+                        },
+                        entityUpdate: {
+                            avg: Enums.MagistrateTransactionStaticFees.EntityUpdate,
+                            max: Enums.MagistrateTransactionStaticFees.EntityUpdate,
+                            min: Enums.MagistrateTransactionStaticFees.EntityUpdate,
+                            sum: "0",
+                        },
+                    },
+                }
+            };
+        };
+    }
+
+    private extendApiTransactionsFees(server: Server): void {
+        const transactionsFeesRoute = server.getRoute("GET", "/api/transactions/fees");
+        const originalTransactionsFeesHandler = transactionsFeesRoute.settings.handler;
+        transactionsFeesRoute.settings.handler = async (request) => {
+            const originalResponse = await originalTransactionsFeesHandler(request);
+            
+            return {
+                ...originalResponse,
+                data: {
+                    ...originalResponse.data,
+                    "2": {
+                        entityRegistration: Enums.MagistrateTransactionStaticFees.EntityRegister,
+                        entityResignation: Enums.MagistrateTransactionStaticFees.EntityResign,
+                        entityUpdate: Enums.MagistrateTransactionStaticFees.EntityUpdate,
+                    },
+                }
+            };
+        };
     }
 }
