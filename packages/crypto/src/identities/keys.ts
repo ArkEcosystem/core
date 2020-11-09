e174ab49044014c9545680c8422ff9eb3f1dff74
Brian Faust <hello@basecode.sh>
2020-11-09 07:42:03 +0200

    wip

--- packages/crypto/src/identities/keys.ts
+++ packages/crypto/src/identities/keys.ts
@@ -1,46 +1,19 @@
-import { secp256k1 } from "bcrypto";
-import wif from "wif";
+import { Keys as Identity } from "@arkecosystem/crypto-identities";
 
-import { HashAlgorithms } from "../crypto";
-import { NetworkVersionError } from "../errors";
 import { IKeyPair } from "../interfaces";
 import { Network } from "../interfaces/networks";
-import { configManager } from "../managers";
+import { getWifFromNetwork } from "./helpers";
 
 export class Keys {
     public static fromPassphrase(passphrase: string, compressed = true): IKeyPair {
-        return Keys.fromPrivateKey(HashAlgorithms.sha256(Buffer.from(passphrase, "utf8")), compressed);
+        return Identity.fromPassphrase(passphrase, compressed);
     }
 
     public static fromPrivateKey(privateKey: Buffer | string, compressed = true): IKeyPair {
-        privateKey = privateKey instanceof Buffer ? privateKey : Buffer.from(privateKey, "hex");
-
-        return {
-            publicKey: secp256k1.publicKeyCreate(privateKey, compressed).toString("hex"),
-            privateKey: privateKey.toString("hex"),
-            compressed,
-        };
+        return Identity.fromPrivateKey(privateKey, compressed);
     }
 
     public static fromWIF(wifKey: string, network?: Network): IKeyPair {
-        if (!network) {
-            network = configManager.get("network");
-        }
-
-        if (!network) {
-            throw new Error();
-        }
-
-        const { version, compressed, privateKey } = wif.decode(wifKey, network.wif);
-
-        if (version !== network.wif) {
-            throw new NetworkVersionError(network.wif, version);
-        }
-
-        return {
-            publicKey: secp256k1.publicKeyCreate(privateKey, compressed).toString("hex"),
-            privateKey: privateKey.toString("hex"),
-            compressed,
-        };
+        return Identity.fromWIF(wifKey, { wif: getWifFromNetwork(network) });
     }
 }
