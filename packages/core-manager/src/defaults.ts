96531feb2351f45c94a609e11412b6f979f12640
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-11 11:14:50 +0100

    chore(core-manager): comment default access data

--- packages/core-manager/src/defaults.ts
+++ packages/core-manager/src/defaults.ts
@@ -28,7 +28,6 @@ export const defaults = {
             host: process.env.CORE_MONITOR_SSL_HOST || "0.0.0.0",
             port: process.env.CORE_MONITOR_SSL_PORT || 8445,
             tls: {
-                // TODO: Take from CORE_API_SSL_KEY if not provided
                 key: process.env.CORE_MONITOR_SSL_KEY,
                 cert: process.env.CORE_MONITOR_SSL_CERT,
             },
@@ -37,18 +36,21 @@ export const defaults = {
     plugins: {
         whitelist: ["*"],
         tokenAuthentication: {
-            enabled: false,
-            token: "secret_token",
+            enabled: true,
+            // Secret access token
+            // token: "secret_token",
         },
         basicAuthentication: {
             enabled: false,
+            // Aragon2Id secret key
             secret: "secret",
             users: [
-                {
-                    username: "username",
-                    password:
-                        "$argon2id$v=19$m=4096,t=3,p=1$NiGA5Cy5vFWTxhBaZMG/3Q$TwEFlzTuIB0fDy+qozEas+GzEiBcLRkm5F+/ClVRCDY",
-                },
+                // Basic Auth User definition with Aragon2Id
+                // {
+                //     username: "username",
+                //     password:
+                //         "$argon2id$v=19$m=4096,t=3,p=1$NiGA5Cy5vFWTxhBaZMG/3Q$TwEFlzTuIB0fDy+qozEas+GzEiBcLRkm5F+/ClVRCDY",
+                // },
             ],
         },
     },
