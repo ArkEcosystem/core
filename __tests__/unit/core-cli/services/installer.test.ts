45336f5e83d03e7c9b4dc89a4b445bd96f7e76c2
Dmitry Tyshchenko <rainydio@gmail.com>
2020-11-12 04:56:28 +0200

    Add installer tests

--- __tests__/unit/core-cli/services/installer.test.ts
+++ __tests__/unit/core-cli/services/installer.test.ts
@@ -22,6 +22,8 @@ afterEach(() => jest.resetAllMocks());
 
 describe("Installer.install", () => {
     it("should install latest package when tag isn't provided", () => {
+        jest.spyOn(installer, "installPeerDependencies").mockReturnValueOnce(undefined);
+
         const spySync: jest.SpyInstance = jest.spyOn(execa, "sync").mockReturnValue({
             stdout: "stdout",
             exitCode: 0,
@@ -33,6 +35,8 @@ describe("Installer.install", () => {
     });
 
     it("should install specific package when tag is provided", () => {
+        jest.spyOn(installer, "installPeerDependencies").mockReturnValueOnce(undefined);
+
         const spySync: jest.SpyInstance = jest.spyOn(execa, "sync").mockReturnValue({
             stdout: "stdout",
             exitCode: 0,
@@ -44,11 +48,67 @@ describe("Installer.install", () => {
     });
 
     it("should throw when exit code isn't 0", () => {
+        jest.spyOn(installer, "installPeerDependencies").mockReturnValueOnce(undefined);
+
         const spySync: jest.SpyInstance = jest.spyOn(execa, "sync").mockReturnValue({
             stderr: "stderr",
             exitCode: 1,
         });
 
         expect(() => installer.install("@arkecosystem/core")).toThrow("stderr");
+
+        expect(spySync).toHaveBeenCalledWith("yarn global add @arkecosystem/core@latest", { shell: true });
     });
 });
+
+describe("Installer.installPeerDependencies", () => {
+    it("should install each peer dependency", () => {
+        const spyInstallVersion: jest.SpyInstance = jest.spyOn(installer, "installVersion").mockReturnValue(undefined);
+
+        const spySync: jest.SpyInstance = jest.spyOn(execa, "sync").mockReturnValue({
+            stdout: JSON.stringify({ pm2: "4.5.0", somepkg: "^1.0.0" }),
+            exitCode: 0,
+        });
+
+        installer.installPeerDependencies("@arkecosystem/core", "3.0.0");
+
+        expect(spySync).toHaveBeenCalledWith("yarn info @arkecosystem/core@3.0.0 peerDependencies --json", {
+            shell: true,
+        });
+
+        expect(spyInstallVersion).toHaveBeenCalledWith("pm2", "4.5.0");
+        expect(spyInstallVersion).toHaveBeenCalledWith("somepkg", "^1.0.0");
+    });
+
+    it("should not install peer dependencies when there aren't any", () => {
+        const spyInstallVersion: jest.SpyInstance = jest.spyOn(installer, "installVersion").mockReturnValue(undefined);
+
+        const spySync: jest.SpyInstance = jest.spyOn(execa, "sync").mockReturnValue({
+            stdout: JSON.stringify(null),
+            exitCode: 0,
+        });
+
+        installer.installPeerDependencies("@arkecosystem/core", "3.0.0");
+
+        expect(spySync).toHaveBeenCalledWith("yarn info @arkecosystem/core@3.0.0 peerDependencies --json", {
+            shell: true,
+        });
+
+        expect(spyInstallVersion).not.toHaveBeenCalled();
+    });
+
+    it("should throw error when yarn command fails", () => {
+        const spySync: jest.SpyInstance = jest.spyOn(execa, "sync").mockReturnValue({
+            stderr: "stderr",
+            exitCode: 1,
+        });
+
+        expect(() => installer.installPeerDependencies("@arkecosystem/core")).toThrow("stderr");
+
+        expect(spySync).toHaveBeenCalledWith("yarn info @arkecosystem/core@latest peerDependencies --json", {
+            shell: true,
+        });
+    });
+});
+
+describe("Installer.installVersion", () => {});
