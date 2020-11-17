e8a56cba802236acc828a2531f34ae3098c439b6
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-17 13:58:20 +0100

    chore(core-manager): getPlugins returns json object

--- packages/core-manager/src/actions/configuration-get-plugins.ts
+++ packages/core-manager/src/actions/configuration-get-plugins.ts
@@ -1,4 +1,5 @@
-import { Application, Container, Contracts } from "@arkecosystem/core-kernel";
+import { Application, Container } from "@arkecosystem/core-kernel";
+import { readJSONSync } from "fs-extra";
 
 import { Actions } from "../contracts";
 
@@ -7,16 +8,9 @@ export class Action implements Actions.Action {
     @Container.inject(Container.Identifiers.Application)
     private readonly app!: Application;
 
-    @Container.inject(Container.Identifiers.FilesystemService)
-    private readonly filesystem!: Contracts.Kernel.Filesystem;
-
     public name = "configuration.getPlugins";
 
     public async execute(params: object): Promise<any> {
-        return await this.getPluginFile();
-    }
-
-    public async getPluginFile(): Promise<string> {
-        return (await this.filesystem.get(this.app.configPath("app.json"))).toString();
+        return readJSONSync(this.app.configPath("app.json"));
     }
 }
