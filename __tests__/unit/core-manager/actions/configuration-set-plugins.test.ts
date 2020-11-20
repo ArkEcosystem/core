e211fe94d6a50ba64881db98ddf248ede5d611a5
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2020-11-20 06:01:10 +0100

    test(core-manager): increase test coverage (#4182)

--- __tests__/unit/core-manager/actions/configuration-set-plugins.test.ts
+++ __tests__/unit/core-manager/actions/configuration-set-plugins.test.ts
@@ -35,6 +35,13 @@ describe("Configuration:SetPlugins", () => {
         expect(writeJSONSync).toHaveBeenCalledWith("/path/to/file", params, { spaces: 4 });
     });
 
+    it("should throw error - content cannot be resolved", async () => {
+        const params = "invalid_params";
+        await expect(action.execute(params)).rejects.toThrow("Content cannot be resolved");
+
+        expect(writeJSONSync).not.toHaveBeenCalled();
+    });
+
     it("should throw error - plugins keys are missing", async () => {
         const params = { core: {} };
         await expect(action.execute(params)).rejects.toThrow("core plugins array is missing");
@@ -42,7 +49,7 @@ describe("Configuration:SetPlugins", () => {
         expect(writeJSONSync).not.toHaveBeenCalled();
     });
 
-    it("should throw error - plugin is not an abject", async () => {
+    it("should throw error - plugin is not a string", async () => {
         const params = { core: { plugins: [{ package: 123 }] } };
         await expect(action.execute(params)).rejects.toThrow("Package is not a string");
 
