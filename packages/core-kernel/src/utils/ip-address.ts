60735a5bc22e93160adef28fefc74808ea749985
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-24 17:19:08 +0100

    chore(core-kernel): normalize ip address

--- packages/core-kernel/src/utils/ip-address.ts
+++ packages/core-kernel/src/utils/ip-address.ts
@@ -2,8 +2,22 @@ import ipaddr from "ipaddr.js";
 
 export const isIPv6Address = (ip: string) => {
     try {
-        return ipaddr.parse(ip.replace("[", "").replace("]", "")).kind() === "ipv6";
+        return ipaddr.parse(clean(ip)).kind() === "ipv6";
     } catch {}
 
     return false;
+};
+
+export const normalizeAddress = (ip: string) => {
+    ip = clean(ip);
+
+    if (isIPv6Address(ip)) {
+        return `[${ip}]`;
+    }
+
+    return ip;
+};
+
+const clean = (ip: string) => {
+    return ip.replace("[", "").replace("]", "");
 };
