db0f1994822c615089d8a8e27334acc065f04def
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-25 16:00:18 +0100

    refactor(core-kernel): export cleanAddress

--- packages/core-kernel/src/utils/ip-address.ts
+++ packages/core-kernel/src/utils/ip-address.ts
@@ -1,19 +1,19 @@
 import ipaddr from "ipaddr.js";
 
 export const isValidAddress = (ip: string) => {
-    return ipaddr.isValid(clean(ip));
+    return ipaddr.isValid(cleanAddress(ip));
 };
 
 export const isIPv6Address = (ip: string) => {
     try {
-        return ipaddr.parse(clean(ip)).kind() === "ipv6";
+        return ipaddr.parse(cleanAddress(ip)).kind() === "ipv6";
     } catch {}
 
     return false;
 };
 
 export const normalizeAddress = (ip: string) => {
-    ip = clean(ip);
+    ip = cleanAddress(ip);
 
     if (isIPv6Address(ip)) {
         return `[${ip}]`;
@@ -22,6 +22,6 @@ export const normalizeAddress = (ip: string) => {
     return ip;
 };
 
-const clean = (ip: string) => {
+export const cleanAddress = (ip: string) => {
     return ip.replace("[", "").replace("]", "");
 };
