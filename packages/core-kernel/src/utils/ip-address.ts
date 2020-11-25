a776999aac4bae05d8ddd875e7f264365a19a870
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2020-11-25 06:38:57 +0100

    feat(core-kernel): ip address utils (#4195)

---
+++ packages/core-kernel/src/utils/ip-address.ts
@@ -0,0 +1,27 @@
+import ipaddr from "ipaddr.js";
+
+export const isValidAddress = (ip: string) => {
+    return ipaddr.isValid(clean(ip));
+};
+
+export const isIPv6Address = (ip: string) => {
+    try {
+        return ipaddr.parse(clean(ip)).kind() === "ipv6";
+    } catch {}
+
+    return false;
+};
+
+export const normalizeAddress = (ip: string) => {
+    ip = clean(ip);
+
+    if (isIPv6Address(ip)) {
+        return `[${ip}]`;
+    }
+
+    return ip;
+};
+
+const clean = (ip: string) => {
+    return ip.replace("[", "").replace("]", "");
+};
