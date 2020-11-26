fe53f1212b6ecfa68eba039ae7b7583677f86c23
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-26 19:29:42 +0100

    feat(core-manager): get cpu, mem, filesystem data

--- packages/core-manager/src/actions/info-resources.ts
+++ packages/core-manager/src/actions/info-resources.ts
@@ -1,4 +1,5 @@
 import { Container } from "@arkecosystem/core-kernel";
+import si from "systeminformation";
 
 import { Actions } from "../contracts";
 
@@ -6,12 +7,53 @@ import { Actions } from "../contracts";
 export class Action implements Actions.Action {
     public name = "info.resources";
 
-    public schema = {
-        type: "object",
-        properties: {},
-    };
+    public async execute(params: any): Promise<any> {
+        return {
+            cpu: await this.prepareCpuData(),
+            mem: await this.prepareMemData(),
+            filesystems: await this.prepareFilesystemsData(),
+        };
+    }
 
-    public async execute(params: { showAllDisks?: boolean }): Promise<any> {
-        return {};
+    private async prepareCpuData(): Promise<any> {
+        const cpuLoad = await si.currentLoad();
+
+        return {
+            total: 100,
+            used: cpuLoad.currentload,
+            available: 100 - cpuLoad.currentload,
+        };
+    }
+
+    private async prepareMemData(): Promise<any> {
+        const mem = await si.mem();
+
+        return {
+            total: this.convert(mem.total),
+            used: this.convert(mem.used),
+            available: this.convert(mem.free),
+        };
+    }
+
+    private async prepareFilesystemsData(): Promise<any> {
+        const fs = await si.fsSize();
+
+        const result: any[] = [];
+
+        for (const disk of fs) {
+            result.push({
+                filesystem: disk.fs,
+                total: this.convert(disk.size),
+                used: this.convert(disk.used),
+                available: this.convert(disk.size - disk.used),
+                mountpoint: disk.mount,
+            });
+        }
+
+        return result;
+    }
+
+    private convert(sizeInBytes: number): number {
+        return sizeInBytes / 1024;
     }
 }
