aa852daffcb2ac89b960b6126bf0e3dc919c3a51
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-26 19:56:38 +0100

    test(core-manager): test info.resources action

--- __tests__/unit/core-manager/actions/info-resources.test.ts
+++ __tests__/unit/core-manager/actions/info-resources.test.ts
@@ -16,4 +16,22 @@ describe("Info:Resources", () => {
     it("should have name", () => {
         expect(action.name).toEqual("info.resources");
     });
+
+    it("should return cpu, mem, filesystem data", async () => {
+        const result = await action.execute({});
+
+        expect(result.cpu.total).toEqual(100);
+        expect(result.cpu.total - (result.cpu.used + result.cpu.available)).toEqual(0);
+
+        expect(typeof result.mem.total).toEqual("number");
+        expect(result.mem.total - (result.mem.used + result.mem.available)).toEqual(0);
+
+        expect(result.filesystems.length).toBeGreaterThan(1);
+        result.filesystems.forEach((fs) => {
+            expect(typeof fs.mountpoint).toEqual("string");
+            expect(typeof fs.filesystem).toEqual("string");
+            expect(typeof fs.total).toEqual("number");
+            expect(fs.total - (fs.used + fs.available)).toEqual(0);
+        });
+    });
 });
