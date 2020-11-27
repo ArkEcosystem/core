1f4af8ae70a72721134295edd2053b35e0d7ffc2
sebastijankuzner <sebastijan.kuzner@outlook.com>
2020-11-27 18:51:33 +0100

    test(core-manager): test primary, autoincrement, nullable, default

--- __tests__/unit/core-manager/database/database-service.test.ts
+++ __tests__/unit/core-manager/database/database-service.test.ts
@@ -26,6 +26,35 @@ const schema: Schema = {
                 },
             ],
         },
+        {
+            name: "table_2",
+            columns: [
+                {
+                    name: "id",
+                    type: "integer",
+                    primary: true,
+                    autoincrement: true,
+                },
+                {
+                    name: "column_1",
+                    type: "varchar(255)",
+                },
+                {
+                    name: "column_2",
+                    type: "varchar(255)",
+                },
+                {
+                    name: "column_3",
+                    type: "varchar(255)",
+                    nullable: true,
+                },
+                {
+                    name: "timestamp",
+                    type: "datetime",
+                    default: "CURRENT_TIMESTAMP",
+                },
+            ],
+        },
     ],
 };
 
@@ -43,29 +72,82 @@ describe("DatabaseService", () => {
             expect(spyOnExec).toHaveBeenCalledTimes(1);
             expect(spyOnExec).toHaveBeenCalledWith(
                 "PRAGMA journal_mode = WAL;\n" +
-                    "CREATE TABLE IF NOT EXISTS table_1 (column_1 VARCHAR(255) NOT NULL);\n",
+                    "CREATE TABLE IF NOT EXISTS table_1 (column_1 VARCHAR(255) NOT NULL);\n" +
+                    "CREATE TABLE IF NOT EXISTS table_2 (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, column_1 VARCHAR(255) NOT NULL, column_2 VARCHAR(255) NOT NULL, column_3 VARCHAR(255), timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);\n",
             );
         });
     });
 
     describe("Add", () => {
-        it("should add data to table", () => {
+        beforeEach(() => {
             database = new DatabaseService(storagePath, schema);
             database.boot();
             expect(existsSync(storagePath)).toBeTrue();
+        });
 
+        it("should add data to table_1", () => {
             database.add("table_1", {
                 column_1: "string content",
             });
 
             const result = database.getAll("table_1");
-
             expect(result.length).toEqual(1);
             expect(result).toEqual([
                 {
                     column_1: "string content",
                 },
             ]);
+        });
+
+        it("should add data to table_2", () => {
+            database.add("table_2", {
+                column_1: "content 1",
+                column_2: "content 2",
+            });
+
+            const result = database.getAll("table_2");
+            expect(result.length).toEqual(1);
+            expect(result[0]).toEqual({
+                id: 1,
+                column_1: "content 1",
+                column_2: "content 2",
+                column_3: null,
+                timestamp: expect.toBeString(),
+            });
+        });
+
+        it("should throw if table does not exist", () => {
+            expect(() => {
+                database.add("table_x", {
+                    column_1: "string content",
+                });
+            }).toThrow("Table table_x does not exists.");
+        });
+
+        it("should throw if required column is missing", () => {
+            expect(() => {
+                database.add("table_2", {
+                    column_1: "content 1",
+                });
+            }).toThrow("NOT NULL constraint failed: table_2.column_2");
+        });
+
+        it("should throw if data is empty", () => {
+            expect(() => {
+                database.add("table_1", {});
+            }).toThrow();
+        });
+
+        it("should throw if data is null", () => {
+            expect(() => {
+                database.add("table_1", null);
+            }).toThrow();
+        });
+
+        it("should throw if data is undefined", () => {
+            expect(() => {
+                database.add("table_1", undefined);
+            }).toThrow();
         });
     });
 
