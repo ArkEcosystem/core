7076346e5bb992a9e9a3df0166cd354050497184
Dmitry Tyshchenko <rainydio@gmail.com>
2020-11-27 14:41:18 +0200

    Remove console.log

--- packages/core-forger/src/forger-service.ts
+++ packages/core-forger/src/forger-service.ts
@@ -116,7 +116,6 @@ export class ForgerService {
             AppUtils.assert.defined<Contracts.P2P.CurrentRound>(this.round);
             timeout = Math.max(0, this.getRoundRemainingSlotTime(this.round));
         } catch (error) {
-            console.error(error.stack);
             this.logger.warning("Waiting for a responsive host");
         } finally {
             this.checkLater(timeout);
