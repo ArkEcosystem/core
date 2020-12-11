4c79c455a80d6245cb0e6ad33c2a062e627fbbb2
Air1 <erwann@ark.io>
2020-12-11 16:08:44 +0100

    fix(core-p2p): limit peers returned by getPeers

--- packages/core-p2p/src/constants.ts
+++ packages/core-p2p/src/constants.ts
@@ -3,4 +3,5 @@ export const constants = {
     DEFAULT_MAX_PAYLOAD: 20 * 1024 * 1024, // default maxPayload value on the server WS socket
     DEFAULT_MAX_PAYLOAD_CLIENT: 100 * 1024, // default maxPayload value on the client WS socket
     KILOBYTE: 1024,
+    MAX_PEERS_GETPEERS: 2000,
 };
