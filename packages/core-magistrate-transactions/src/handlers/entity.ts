612c35b691c4d1f18af95a8f2a5fde747760d04a
Dmitry Tyshchenko <rainydio@gmail.com>
2020-12-16 15:19:42 +0200

    Add missing await that is causing unhandled rejection

--- packages/core-magistrate-transactions/src/handlers/entity.ts
+++ packages/core-magistrate-transactions/src/handlers/entity.ts
@@ -74,7 +74,7 @@ export class EntityTransactionHandler extends Handlers.TransactionHandler {
             throw new StaticFeeMismatchError(staticFee.toFixed());
         }
 
-        super.throwIfCannotBeApplied(transaction, wallet);
+        await super.throwIfCannotBeApplied(transaction, wallet);
 
         KernelUtils.assert.defined<object>(transaction.data.asset);
 
