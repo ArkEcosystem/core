a70a48370deb3b38cb6636f7e417a847a624e84f
Air1 <erwann@ark.io>
2020-12-17 18:24:43 +0100

    chore: maxBytes and validation schema

--- packages/core-p2p/src/socket-server/schemas/transactions.ts
+++ packages/core-p2p/src/socket-server/schemas/transactions.ts
@@ -1,9 +1,17 @@
 import Joi from "@hapi/joi";
+import { Providers, Container, Contracts } from "@arkecosystem/core-kernel";
 import { headers } from "./shared";
 
 export const transactionsSchemas = {
-    postTransactions: Joi.object({
-        transactions: Joi.array().items(Joi.binary()),
+    createPostTransactionsSchema: (app: Contracts.Kernel.Application) => Joi.object({
+        transactions: Joi.array().items(Joi.binary()).max(
+            app.getTagged<Providers.PluginConfiguration>(
+                Container.Identifiers.PluginConfiguration,
+                "plugin",
+                "@arkecosystem/core-transaction-pool",
+            )
+            .getOptional<number>("maxTransactionsPerRequest", 40)
+        ),
         headers,
-    }),
+    })
 };
