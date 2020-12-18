a49fb040ee95451edb8f43f8d5f78de8986ae7e5
Dmitry Tyshchenko <rainydio@gmail.com>
2020-12-18 11:19:35 +0200

    refactor(core-magistrate-transactions): remove unused entity subhandlers (#4233)

--- packages/core-magistrate-transactions/src/handlers/entity-subhandlers/bridgechain/index.ts
+++
@@ -1,9 +0,0 @@
-import { BridgechainRegisterSubHandler } from "./register";
-import { BridgechainResignSubHandler } from "./resign";
-import { BridgechainUpdateSubHandler } from "./update";
-
-export default {
-    BridgechainRegisterSubHandler,
-    BridgechainResignSubHandler,
-    BridgechainUpdateSubHandler,
-};
