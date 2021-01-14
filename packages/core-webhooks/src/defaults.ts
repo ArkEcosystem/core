3170aa013f5ba20c63ca7341229532a2845c1add
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-01-14 16:31:57 +0100

    chore(core-webhooks): parseInt from numeric env variables

--- packages/core-webhooks/src/defaults.ts
+++ packages/core-webhooks/src/defaults.ts
@@ -3,9 +3,9 @@ export const defaults = {
     server: {
         http: {
             host: process.env.CORE_WEBHOOKS_HOST || "0.0.0.0",
-            port: process.env.CORE_WEBHOOKS_PORT || 4004,
+            port: parseInt(process.env.CORE_WEBHOOKS_PORT!) || 4004,
         },
         whitelist: ["127.0.0.1", "::ffff:127.0.0.1"],
     },
-    timeout: process.env.CORE_WEBHOOKS_TIMEOUT || 1500,
+    timeout: parseInt(process.env.CORE_WEBHOOKS_TIMEOUT!) || 1500,
 };
