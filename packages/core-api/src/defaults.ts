13c35a2adffdc8d5dbf85c4660be6093607571be
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-01-14 12:16:35 +0100

    chore(core-api): rate limit 100 req/min

--- packages/core-api/src/defaults.ts
+++ packages/core-api/src/defaults.ts
@@ -24,7 +24,7 @@ export const defaults = {
         },
         rateLimit: {
             enabled: !process.env.CORE_API_RATE_LIMIT_DISABLED,
-            points: process.env.CORE_API_RATE_LIMIT_USER_LIMIT || 300,
+            points: process.env.CORE_API_RATE_LIMIT_USER_LIMIT || 100,
             duration: process.env.CORE_API_RATE_LIMIT_USER_EXPIRES || 60, // Sec
             whitelist: process.env.CORE_API_RATE_LIMIT_WHITELIST
                 ? process.env.CORE_API_RATE_LIMIT_WHITELIST.split(",")
