b4f0ad58cca2df2ff5496c9ba5ccce380e6c29fa
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-01-14 16:26:13 +0100

    chore(core-forger): parseInt from numeric env variables

--- packages/core-forger/src/defaults.ts
+++ packages/core-forger/src/defaults.ts
@@ -2,7 +2,7 @@ export const defaults = {
     hosts: [
         {
             hostname: "127.0.0.1",
-            port: process.env.CORE_P2P_PORT || 4000,
+            port: parseInt(process.env.CORE_P2P_PORT!) || 4000,
         },
     ],
     tracker: false,
