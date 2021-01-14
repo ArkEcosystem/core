fbcd2abba3a14ba7ded2a79a388b215ccef909ab
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-01-14 16:24:44 +0100

    chore(core-database): parseInt from numeric env variables

--- packages/core-database/src/defaults.ts
+++ packages/core-database/src/defaults.ts
@@ -2,7 +2,7 @@ export const defaults = {
     connection: {
         type: "postgres",
         host: process.env.CORE_DB_HOST || "localhost",
-        port: process.env.CORE_DB_PORT || 5432,
+        port: parseInt(process.env.CORE_DB_PORT!) || 5432,
         database: process.env.CORE_DB_DATABASE || `${process.env.CORE_TOKEN}_${process.env.CORE_NETWORK_NAME}`,
         username: process.env.CORE_DB_USERNAME || process.env.CORE_TOKEN,
         password: process.env.CORE_DB_PASSWORD || "password",
