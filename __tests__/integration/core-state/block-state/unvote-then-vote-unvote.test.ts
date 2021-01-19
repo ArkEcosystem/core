4e08827791a9455bafe5b76c08e83aecd2950282
Dmitry Tyshchenko <rainydio@gmail.com>
2021-01-19 03:18:50 +0200

    Merge tests into a single one

--- __tests__/integration/core-state/block-state/unvote-then-vote-unvote.test.ts
+++ __tests__/integration/core-state/block-state/unvote-then-vote-unvote.test.ts
@@ -1,97 +1,82 @@
 import { Application, Container, Contracts } from "@arkecosystem/core-kernel";
-import { Utils, Transactions, Interfaces } from "@arkecosystem/crypto";
+import { Utils, Transactions } from "@arkecosystem/crypto";
 import { delegates } from "@arkecosystem/core-test-framework";
 import { BIP39 } from "../../../../packages/core-forger/src/methods/bip39";
 
 import { setUp, tearDown } from "../__support__/setup";
 
 let app: Application;
-let stateStore: Contracts.State.StateStore;
-let blockState: Contracts.State.BlockState;
-let walletRepository: Contracts.State.WalletRepository;
-
-let block1: Interfaces.IBlock;
-let block2: Interfaces.IBlock;
-let block3: Interfaces.IBlock;
 
 beforeAll(async () => {
     app = await setUp();
-
-    stateStore = app.get<Contracts.State.StateStore>(Container.Identifiers.StateStore);
-    blockState = app.getTagged<Contracts.State.BlockState>(Container.Identifiers.BlockState, "state", "blockchain");
-    walletRepository = app.getTagged<Contracts.State.WalletRepository>(
-        Container.Identifiers.WalletRepository,
-        "state",
-        "blockchain",
-    );
-
-    const unvoteTransaction = Transactions.BuilderFactory.vote()
-        .votesAsset([`-${delegates[10].publicKey}`])
-        .nonce("3")
-        .fee("100")
-        .sign(delegates[10].passphrase)
-        .build();
-
-    const voteUnvoteTransaction = Transactions.BuilderFactory.vote()
-        .votesAsset([`+${delegates[10].publicKey}`, `-${delegates[10].publicKey}`])
-        .nonce("4")
-        .fee("100")
-        .sign(delegates[10].passphrase)
-        .build();
-
-    const bip39 = new BIP39(delegates[0].passphrase);
-
-    block1 = stateStore.getLastBlock();
-
-    block2 = bip39.forge([unvoteTransaction.data], {
-        timestamp: block1.data.timestamp + 60,
-        previousBlock: block1.data,
-        reward: Utils.BigNumber.make("100"),
-    });
-
-    block3 = bip39.forge([voteUnvoteTransaction.data], {
-        timestamp: block2.data.timestamp + 60,
-        previousBlock: block2.data,
-        reward: Utils.BigNumber.make("100"),
-    });
 });
 
 afterAll(async () => {
     await tearDown();
 });
 
-describe("BlockState handling unvote, vote+unvote transactions", () => {
-    it("should apply block#2 containing single unvote transaction", async () => {
-        const delegate10 = walletRepository.findByPublicKey(delegates[10].publicKey);
+test("BlockState handling unvote, vote+unvote transactions", async () => {
+    const stateStore = app.get<Contracts.State.StateStore>(Container.Identifiers.StateStore);
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
+    const blockState = app.getTagged<Contracts.State.BlockState>(
+        Container.Identifiers.BlockState,
+        "state",
+        "blockchain",
+    );
 
-        await blockState.applyBlock(block2);
+    const walletRepository = app.getTagged<Contracts.State.WalletRepository>(
+        Container.Identifiers.WalletRepository,
+        "state",
+        "blockchain",
+    );
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("0");
+    const unvoteTransaction = Transactions.BuilderFactory.vote()
+        .votesAsset([`-${delegates[2].publicKey}`])
+        .nonce("3")
+        .fee("100")
+        .sign(delegates[2].passphrase)
+        .build();
+
+    const voteUnvoteTransaction = Transactions.BuilderFactory.vote()
+        .votesAsset([`+${delegates[2].publicKey}`, `-${delegates[2].publicKey}`])
+        .nonce("4")
+        .fee("100")
+        .sign(delegates[2].passphrase)
+        .build();
+
+    const bip39 = new BIP39(delegates[1].passphrase);
+
+    const block1 = stateStore.getLastBlock();
+
+    const block2 = bip39.forge([unvoteTransaction.data], {
+        timestamp: block1.data.timestamp + 60,
+        previousBlock: block1.data,
+        reward: Utils.BigNumber.make("100"),
     });
 
-    it("should apply block#3 containing single vote+unvote transaction", async () => {
-        const delegate10 = walletRepository.findByPublicKey(delegates[10].publicKey);
-
-        await blockState.applyBlock(block3);
-
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("0");
+    const block3 = bip39.forge([voteUnvoteTransaction.data], {
+        timestamp: block2.data.timestamp + 60,
+        previousBlock: block2.data,
+        reward: Utils.BigNumber.make("100"),
     });
 
-    it("should revert block#3 containing single vote+unvote transaction", async () => {
-        const delegate10 = walletRepository.findByPublicKey(delegates[10].publicKey);
+    const delegate2 = walletRepository.findByPublicKey(delegates[2].publicKey);
 
-        await blockState.revertBlock(block3);
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("0");
-    });
+    await blockState.applyBlock(block2);
 
-    it("should revert block#2 containing single unvote transaction", async () => {
-        const delegate10 = walletRepository.findByPublicKey(delegates[10].publicKey);
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("0");
 
-        await blockState.revertBlock(block2);
+    await blockState.applyBlock(block3);
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
-    });
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("0");
+
+    await blockState.revertBlock(block3);
+
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("0");
+
+    await blockState.revertBlock(block2);
+
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
 });
--- __tests__/integration/core-state/block-state/uvote-vote.test.ts
+++ __tests__/integration/core-state/block-state/uvote-vote.test.ts
@@ -1,67 +1,61 @@
 import { Application, Container, Contracts } from "@arkecosystem/core-kernel";
-import { Utils, Transactions, Interfaces } from "@arkecosystem/crypto";
+import { Utils, Transactions } from "@arkecosystem/crypto";
 import { delegates } from "@arkecosystem/core-test-framework";
 import { BIP39 } from "../../../../packages/core-forger/src/methods/bip39";
 
 import { setUp, tearDown } from "../__support__/setup";
 
 let app: Application;
-let stateStore: Contracts.State.StateStore;
-let blockState: Contracts.State.BlockState;
-let walletRepository: Contracts.State.WalletRepository;
-
-let block1: Interfaces.IBlock;
-let block2: Interfaces.IBlock;
 
 beforeAll(async () => {
     app = await setUp();
-
-    stateStore = app.get<Contracts.State.StateStore>(Container.Identifiers.StateStore);
-    blockState = app.getTagged<Contracts.State.BlockState>(Container.Identifiers.BlockState, "state", "blockchain");
-    walletRepository = app.getTagged<Contracts.State.WalletRepository>(
-        Container.Identifiers.WalletRepository,
-        "state",
-        "blockchain",
-    );
-
-    const unvoteVoteTransaction = Transactions.BuilderFactory.vote()
-        .votesAsset([`-${delegates[10].publicKey}`, `+${delegates[10].publicKey}`])
-        .nonce("3")
-        .fee("100")
-        .sign(delegates[10].passphrase)
-        .build();
-
-    const bip39 = new BIP39(delegates[0].passphrase);
-
-    block1 = stateStore.getLastBlock();
-
-    block2 = bip39.forge([unvoteVoteTransaction.data], {
-        timestamp: block1.data.timestamp + 60,
-        previousBlock: block1.data,
-        reward: Utils.BigNumber.make("100"),
-    });
 });
 
 afterAll(async () => {
     await tearDown();
 });
 
-describe("BlockState handling unvote+vote transaction", () => {
-    it("should apply block#2 containing single unvote+vote transaction", async () => {
-        const delegate10 = walletRepository.findByPublicKey(delegates[10].publicKey);
+test("BlockState handling unvote+vote transaction", async () => {
+    const stateStore = app.get<Contracts.State.StateStore>(Container.Identifiers.StateStore);
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
+    const blockState = app.getTagged<Contracts.State.BlockState>(
+        Container.Identifiers.BlockState,
+        "state",
+        "blockchain",
+    );
 
-        await blockState.applyBlock(block2);
+    const walletRepository = app.getTagged<Contracts.State.WalletRepository>(
+        Container.Identifiers.WalletRepository,
+        "state",
+        "blockchain",
+    );
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("299999999999900");
+    const unvoteVoteTransaction = Transactions.BuilderFactory.vote()
+        .votesAsset([`-${delegates[2].publicKey}`, `+${delegates[2].publicKey}`])
+        .nonce("3")
+        .fee("100")
+        .sign(delegates[2].passphrase)
+        .build();
+
+    const bip39 = new BIP39(delegates[1].passphrase);
+
+    const block1 = stateStore.getLastBlock();
+
+    const block2 = bip39.forge([unvoteVoteTransaction.data], {
+        timestamp: block1.data.timestamp + 60,
+        previousBlock: block1.data,
+        reward: Utils.BigNumber.make("100"),
     });
 
-    it("should revert block#2 containing single unvote+vote transaction", async () => {
-        const delegate10 = walletRepository.findByPublicKey(delegates[10].publicKey);
+    const delegate2 = walletRepository.findByPublicKey(delegates[2].publicKey);
 
-        await blockState.revertBlock(block2);
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
 
-        expect(delegate10.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
-    });
+    await blockState.applyBlock(block2);
+
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("299999999999900");
+
+    await blockState.revertBlock(block2);
+
+    expect(delegate2.getAttribute("delegate.voteBalance").toFixed()).toBe("300000000000000");
 });
