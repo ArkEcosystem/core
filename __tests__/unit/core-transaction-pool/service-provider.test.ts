1efaac5a0789173cd2552e7ebca370d4c3424b93
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-01-20 11:12:12 +0100

    test(core-transaction-pool): test ENV variables

--- __tests__/unit/core-transaction-pool/service-provider.test.ts
+++ __tests__/unit/core-transaction-pool/service-provider.test.ts
@@ -104,5 +104,121 @@ describe("ServiceProvider", () => {
                 expect(item.packageName).toBeString();
             });
         });
+
+        describe("process.env.CORE_TRANSACTION_POOL_DISABLED", () => {
+            it("should return true when process.env.CORE_TRANSACTION_POOL_DISABLED is undefined", async () => {
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeUndefined();
+                expect(result.value.enabled).toBeTrue();
+            });
+
+            it("should return false when process.env.CORE_TRANSACTION_POOL_DISABLED is present", async () => {
+                process.env.CORE_TRANSACTION_POOL_DISABLED = "true";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeUndefined();
+                expect(result.value.enabled).toBeFalse();
+            });
+        });
+
+        describe("process.env.CORE_PATH_DATA", () => {
+            it("should return path containing process.env.CORE_PATH_DATA", async () => {
+                process.env.CORE_PATH_DATA = "dummy/path";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeUndefined();
+                expect(result.value.storage).toEqual("dummy/path/transaction-pool.sqlite");
+            });
+        });
+
+        describe("process.env.CORE_MAX_TRANSACTIONS_IN_POOL", () => {
+            it("should parse process.env.CORE_MAX_TRANSACTIONS_IN_POOL", async () => {
+                process.env.CORE_MAX_TRANSACTIONS_IN_POOL = "4000";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeUndefined();
+                expect(result.value.maxTransactionsInPool).toEqual(4000);
+            });
+
+            it("should throw if process.env.CORE_MAX_TRANSACTIONS_IN_POOL is not number", async () => {
+                process.env.CORE_MAX_TRANSACTIONS_IN_POOL = "false";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeDefined();
+                expect(result.error!.message).toEqual('"maxTransactionsInPool" must be a number');
+            });
+        });
+
+        describe("process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER", () => {
+            it("should parse process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER", async () => {
+                process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER = "4000";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeUndefined();
+                expect(result.value.maxTransactionsPerSender).toEqual(4000);
+            });
+
+            it("should throw if process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER is not number", async () => {
+                process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER = "false";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeDefined();
+                expect(result.error!.message).toEqual('"maxTransactionsPerSender" must be a number');
+            });
+        });
+
+        describe("process.env.CORE_TRANSACTION_POOL_MAX_PER_REQUEST", () => {
+            it("should parse process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER", async () => {
+                process.env.CORE_TRANSACTION_POOL_MAX_PER_REQUEST = "4000";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeUndefined();
+                expect(result.value.maxTransactionsPerRequest).toEqual(4000);
+            });
+
+            it("should throw if process.env.CORE_TRANSACTION_POOL_MAX_PER_REQUEST is not number", async () => {
+                process.env.CORE_TRANSACTION_POOL_MAX_PER_REQUEST = "false";
+
+                jest.resetModules();
+                const result = (serviceProvider.configSchema() as AnySchema).validate(
+                    (await import("@packages/core-transaction-pool/src/defaults")).defaults,
+                );
+
+                expect(result.error).toBeDefined();
+                expect(result.error!.message).toEqual('"maxTransactionsPerRequest" must be a number');
+            });
+        });
     });
 });
