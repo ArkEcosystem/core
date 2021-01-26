6d86afc96cb6e20dcb52341a4d20cf1be85c1a2b
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2021-01-26 03:02:56 +0100

    chore(core-logger-pino): implements service provider schema (#4283)

--- packages/core-logger-pino/src/service-provider.ts
+++ packages/core-logger-pino/src/service-provider.ts
@@ -1,4 +1,5 @@
 import { Container, Contracts, Providers, Services } from "@arkecosystem/core-kernel";
+import Joi from "joi";
 
 import { PinoLogger } from "./driver";
 
@@ -18,5 +19,17 @@ export class ServiceProvider extends Providers.ServiceProvider {
     public async dispose(): Promise<void> {
         // todo: destroy console stream
         // todo: destroy file stream
+    }
+
+    public configSchema(): object {
+        return Joi.object({
+            levels: Joi.object({
+                console: Joi.string().required(),
+                file: Joi.string().required(),
+            }).required(),
+            fileRotator: Joi.object({
+                interval: Joi.string().required(),
+            }).required(),
+        });
     }
 }
