31adc4b836b22d32cffcc99cd770950f245bee09
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-02-04 16:52:27 +0100

    fix(core-manager): handle stream error on zip generator

--- packages/core-manager/src/workers/generate-log-zip.ts
+++ packages/core-manager/src/workers/generate-log-zip.ts
@@ -1,27 +1,47 @@
 import archiver from "archiver";
-import { createWriteStream, ensureDirSync, renameSync } from "fs-extra";
+import { createWriteStream, ensureDirSync, removeSync, renameSync } from "fs-extra";
 import { dirname, parse } from "path";
-import { Writable, Readable } from "stream";
+import { pipeline, Readable, Writable } from "stream";
 
 import { GenerateLog } from "./generate-log";
 import { LogTransformStream } from "./log-transform-stream";
 
 export class GenerateLogZip extends GenerateLog {
     public async execute(): Promise<void> {
-        const readStream = Readable.from(this.database.getAllIterator("logs", this.options.query), {objectMode: true});
         const writeStream = this.prepareOutputStream();
 
         const archive = archiver("zip", {
             zlib: { level: 9 },
         });
-        archive.pipe(writeStream);
 
-        archive.append(
-            readStream.pipe(
-                new LogTransformStream(),
-            ),
-            { name: parse(this.options.logFileName).name + ".log" },
+        const handleError = (err) => {
+            archive.abort();
+            writeStream.destroy();
+            removeSync(this.getTempFilePath());
+
+            throw err;
+        };
+
+        archive.on("error", function (err) {
+            handleError(err);
+        });
+
+        const readStream = pipeline(
+            Readable.from(this.database.getAllIterator("logs", this.options.query), {
+                objectMode: true,
+            }),
+            new LogTransformStream(),
+            (err) => {
+                if (err) {
+                    handleError(err);
+                }
+            },
         );
+
+        archive.pipe(writeStream);
+        archive.append(readStream, {
+            name: parse(this.options.logFileName).name + ".log",
+        });
 
         archive.finalize();
 
