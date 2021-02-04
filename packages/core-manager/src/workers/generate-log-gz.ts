41db08cc4bd5dc552e430c186992c85a05b66c2c
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-02-04 17:31:23 +0100

    chore(core-manager): use promise

--- packages/core-manager/src/workers/generate-log-gz.ts
+++ packages/core-manager/src/workers/generate-log-gz.ts
@@ -6,23 +6,26 @@ import { LogTransformStream } from "./log-transform-stream";
 
 export class GenerateLogGz extends GenerateLog {
     public async execute(): Promise<void> {
-        const writeStream = this.prepareOutputStream();
+        await new Promise((resolve, reject) => {
+            const writeStream = this.prepareOutputStream();
 
-        pipeline(
-            Readable.from(this.database.getAllIterator("logs", this.options.query), { objectMode: true }),
-            new LogTransformStream(),
-            zlib.createGzip(),
-            writeStream,
-            (err) => {
-                if (err) {
-                    writeStream.destroy();
-                    this.removeTempFiles();
+            pipeline(
+                Readable.from(this.database.getAllIterator("logs", this.options.query), { objectMode: true }),
+                new LogTransformStream(),
+                zlib.createGzip(),
+                writeStream,
+                (err) => {
+                    if (err) {
+                        writeStream.destroy();
+                        this.removeTempFiles();
 
-                    throw err;
-                } else {
-                    this.moveArchive();
-                }
-            },
-        );
+                        reject(err);
+                    } else {
+                        this.moveArchive();
+                        resolve();
+                    }
+                },
+            );
+        });
     }
 }
