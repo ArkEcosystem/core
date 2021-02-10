1be81e0d2a2e9bdec8deed88c00e904fdc641f8f
Dmitry Tyshchenko <rainydio@gmail.com>
2021-02-10 15:47:08 +0200

    Unit-testable rate limit plugin

--- packages/core-p2p/src/socket-server/plugins/rate-limit.ts
+++ packages/core-p2p/src/socket-server/plugins/rate-limit.ts
@@ -13,25 +13,33 @@ export class RateLimitPlugin {
     @Container.inject(Container.Identifiers.PeerRateLimiter)
     private readonly rateLimiter!: Contracts.P2P.PeerRateLimiter;
 
-    public register(server) {
-        const allRoutesConfigByPath = {
+    private readonly endpointByPath: Map<string, string> = new Map<string, string>();
+
+    @Container.postConstruct()
+    public initialize(): void {
+        const configs = {
             ...this.app.resolve(InternalRoute).getRoutesConfigByPath(),
             ...this.app.resolve(PeerRoute).getRoutesConfigByPath(),
             ...this.app.resolve(BlocksRoute).getRoutesConfigByPath(),
             ...this.app.resolve(TransactionsRoute).getRoutesConfigByPath(),
         };
 
-        server.ext({
-            type: "onPreAuth",
-            method: async (request, h) => {
-                const endpoint = allRoutesConfigByPath[request.path].id;
+        for (const [path, config] of Object.entries(configs)) {
+            this.endpointByPath.set(path, config.id);
+        }
+    }
 
-                if (await this.rateLimiter.consumeIncoming(request.info.remoteAddress, endpoint)) {
-                    return h.continue;
-                } else {
-                    return Boom.tooManyRequests("Rate limit exceeded");
-                }
-            },
-        });
+    public register(server) {
+        server.ext({ type: "onPreAuth", method: this.onPreAuth.bind(this) });
+    }
+
+    public async onPreAuth(request, h) {
+        const endpoint: string | undefined = this.endpointByPath.get(request.path);
+
+        if (await this.rateLimiter.consumeIncoming(request.info.remoteAddress, endpoint)) {
+            return h.continue;
+        } else {
+            return Boom.tooManyRequests("Rate limit exceeded");
+        }
     }
 }
