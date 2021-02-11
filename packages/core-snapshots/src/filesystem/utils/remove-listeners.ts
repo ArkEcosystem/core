a1b30257d2c40f39957ed735b5ef4d94f79ff979
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-02-11 16:11:37 +0100

    refactor(core-snapshot): add types

--- packages/core-snapshots/src/filesystem/utils/remove-listeners.ts
+++ packages/core-snapshots/src/filesystem/utils/remove-listeners.ts
@@ -1,6 +1,6 @@
 import { Stream } from "../../contracts";
 
-export const removeListeners = (emitter: NodeJS.EventEmitter, eventListenerPairs: Stream.EventListenerPair[]) => {
+export const removeListeners = (emitter: NodeJS.EventEmitter, eventListenerPairs: Stream.EventListenerPair[]): void => {
     for (const eventListenerPair of eventListenerPairs) {
         emitter.removeListener(eventListenerPair.event, eventListenerPair.listener);
     }
