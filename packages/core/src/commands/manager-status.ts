10e3cb547366d8f641f78efd21059aaaf8f6a67a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-02-15 11:59:18 +0100

    feat(core): implement manager:status command

---
+++ packages/core/src/commands/manager-status.ts
@@ -0,0 +1,46 @@
+import { Commands, Container } from "@arkecosystem/core-cli";
+import Joi from "joi";
+
+/**
+ * @export
+ * @class Command
+ * @extends {Commands.Command}
+ */
+@Container.injectable()
+export class Command extends Commands.Command {
+    /**
+     * The console command signature.
+     *
+     * @type {string}
+     * @memberof Command
+     */
+    public signature: string = "manager:status";
+
+    /**
+     * The console command description.
+     *
+     * @type {string}
+     * @memberof Command
+     */
+    public description: string = "Display the status of the Manager process.";
+
+    /**
+     * Configure the console command.
+     *
+     * @returns {void}
+     * @memberof Command
+     */
+    public configure(): void {
+        this.definition.setFlag("token", "The name of the token.", Joi.string().default("ark"));
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @returns {Promise<void>}
+     * @memberof Command
+     */
+    public async execute(): Promise<void> {
+        this.app.get<any>(Container.Identifiers.ProcessFactory)(this.getFlag("token"), "manager").status();
+    }
+}
