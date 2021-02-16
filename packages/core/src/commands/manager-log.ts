89213147c74a40e9890f19c4a14efc837dd535fc
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2021-02-16 09:45:41 +0100

    feat(core): implement `manager:status`, `manager:log` & `manager:restart` cli commands (#4319)
    * feat(core): implement manager:status command
    
    * feat(core): implement manager:log command
    
    * test(core): manager:log & manager:status command
    
    * feat(core): implement manager:restart command
    
    * test(core): manager:restart command
    
    Co-authored-by: air1one <36802613+air1one@users.noreply.github.com>

---
+++ packages/core/src/commands/manager-log.ts
@@ -0,0 +1,51 @@
+import { Commands, Container } from "@arkecosystem/core-cli";
+import Joi from "joi";
+
+/**
+ * @export
+ * @class Command
+ * @extends {Commands.Command}
+ */
+@Container.injectable()
+export class Command extends Commands.Command {
+    /**
+     * The console command signature.
+     *
+     * @type {string}
+     * @memberof Command
+     */
+    public signature: string = "manager:log";
+
+    /**
+     * The console command description.
+     *
+     * @type {string}
+     * @memberof Command
+     */
+    public description: string = "Display the Manager process log.";
+
+    /**
+     * Configure the console command.
+     *
+     * @returns {void}
+     * @memberof Command
+     */
+    public configure(): void {
+        this.definition
+            .setFlag("token", "The name of the token.", Joi.string().default("ark"))
+            .setFlag("error", "Only display the error output.", Joi.boolean())
+            .setFlag("lines", "The number of lines to output.", Joi.number().default(15));
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @returns {Promise<void>}
+     * @memberof Command
+     */
+    public async execute(): Promise<void> {
+        await this.app
+            .get<any>(Container.Identifiers.ProcessFactory)(this.getFlag("token"), "manager")
+            .log(this.getFlag("error"), this.getFlag("lines"));
+    }
+}
