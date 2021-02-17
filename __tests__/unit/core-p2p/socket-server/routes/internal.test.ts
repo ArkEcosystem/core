9df4dda3b6b4125a1f635f959f3d640e3ba3d14c
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-02-17 13:23:45 +0100

    test(core-p2p): internal routes

--- __tests__/unit/core-p2p/socket-server/routes/internal.test.ts
+++ __tests__/unit/core-p2p/socket-server/routes/internal.test.ts
@@ -33,6 +33,7 @@ describe("InternalRoute", () => {
                 payload: {
                     maxBytes: config.maxBytes,
                 },
+                isInternal: true,
             },
         }));
 
