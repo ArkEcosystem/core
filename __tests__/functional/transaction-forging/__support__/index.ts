9f846bbf0663e53a33d006cfd3ebb47868aa3a70
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-03-09 13:27:11 +0100

    test(functional): remove round generation on setUp

--- __tests__/functional/transaction-forging/__support__/index.ts
+++ __tests__/functional/transaction-forging/__support__/index.ts
@@ -1,14 +1,11 @@
 import "jest-extended";
 
 import { Container, Contracts } from "@arkecosystem/core-kernel";
-import { Identities, Managers, Utils } from "@arkecosystem/crypto";
-import secrets from "@packages/core-test-framework/src/internal/passphrases.json";
+import { Managers } from "@arkecosystem/crypto";
 import delay from "delay";
 
 jest.setTimeout(1200000);
 
-import { DatabaseService } from "@arkecosystem/core-database";
-import { DatabaseInteraction } from "@arkecosystem/core-state";
 import { StateBuilder } from "@arkecosystem/core-state/src/state-builder";
 import { Sandbox } from "@packages/core-test-framework/src";
 
@@ -27,6 +24,7 @@ export const setUp = async (): Promise<Contracts.Kernel.Application> => {
             list: [{ ip: "127.0.0.1", port: 4000 }],
         },
     });
+
     await sandbox.boot(async ({ app }) => {
         await app.bootstrap({
             flags: {
@@ -62,35 +60,6 @@ export const setUp = async (): Promise<Contracts.Kernel.Application> => {
 
         Managers.configManager.getMilestone().aip11 = true;
         Managers.configManager.getMilestone().htlcEnabled = true;
-
-        const databaseService = app.get<DatabaseService>(Container.Identifiers.DatabaseService);
-        const walletRepository = app.getTagged<Contracts.State.WalletRepository>(
-            Container.Identifiers.WalletRepository,
-            "state",
-            "blockchain",
-        );
-
-        await databaseService.saveRound(
-            secrets.map((secret, i) => {
-                const wallet = walletRepository.findByPublicKey(Identities.PublicKey.fromPassphrase(secret));
-
-                wallet.setAttribute("delegate", {
-                    username: `genesis_${i + 1}`,
-                    voteBalance: Utils.BigNumber.make("300000000000000"),
-                    forgedFees: Utils.BigNumber.ZERO,
-                    forgedRewards: Utils.BigNumber.ZERO,
-                    producedBlocks: 0,
-                    round: 1,
-                    rank: undefined,
-                });
-
-                return wallet;
-            }),
-        );
-
-        const databaseInteraction = app.get<DatabaseInteraction>(Container.Identifiers.DatabaseInteraction);
-
-        await (databaseInteraction as any).initializeActiveDelegates(1);
     });
 
     return sandbox.app;
@@ -116,18 +85,27 @@ export const tearDown = async (): Promise<void> => {
             balance: wallet.balance,
             nonce: wallet.nonce,
             attributes: walletAttributes,
-        }
+        };
     };
-    const sortWallets = (a: Contracts.State.Wallet, b: Contracts.State.Wallet) => a.publicKey!.localeCompare(b.publicKey!);
+    const sortWallets = (a: Contracts.State.Wallet, b: Contracts.State.Wallet) =>
+        a.publicKey!.localeCompare(b.publicKey!);
 
-    const allByPublicKey = walletRepository.allByPublicKey().map(w => w.clone()).sort(sortWallets).map(mapWallets);
+    const allByPublicKey = walletRepository
+        .allByPublicKey()
+        .map((w) => w.clone())
+        .sort(sortWallets)
+        .map(mapWallets);
 
     walletRepository.reset();
 
     await sandbox.app.resolve<StateBuilder>(StateBuilder).run();
     await delay(2000); // if there is an issue with state builder, we wait a bit to be sure to catch it in the logs
 
-    const allByPublicKeyBootstrapped = walletRepository.allByPublicKey().map(w => w.clone()).sort(sortWallets).map(mapWallets);
+    const allByPublicKeyBootstrapped = walletRepository
+        .allByPublicKey()
+        .map((w) => w.clone())
+        .sort(sortWallets)
+        .map(mapWallets);
     expect(allByPublicKeyBootstrapped).toEqual(allByPublicKey);
 };
 
