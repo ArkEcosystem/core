590d0b847d54a418c91c43cdd293742de45a712f
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-04-01 17:44:20 +0200

    chore(core-state): wallet-repository init

--- packages/core-state/src/wallets/wallet-repository-clone.ts
+++ packages/core-state/src/wallets/wallet-repository-clone.ts
@@ -11,21 +11,7 @@ export class WalletRepositoryClone extends WalletRepository {
     @Container.postConstruct()
     public initialize(): void {
         super.initialize();
-
-        for (const wallet of this.blockchainWalletRepository.allByAddress()) {
-            this.cloneWallet(this.blockchainWalletRepository, wallet);
-        }
     }
 
-    public index(wallets: Contracts.State.Wallet | Contracts.State.Wallet[]): void {
-        for (const wallet of Array.isArray(wallets) ? wallets : [wallets]) {
-            if (this.blockchainWalletRepository.hasByAddress(wallet.address)) {
-                if (this.blockchainWalletRepository.findByAddress(wallet.address) === wallet) {
-                    throw new Error("Can't index state=blockchain wallet");
-                }
-            }
-        }
 
-        super.index(wallets);
-    }
 }
