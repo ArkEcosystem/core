ec40f49d3a96f3a706a1bd8f55769e4eb12530cb
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-04-13 12:49:41 +0200

    chore(core-database): update wallet changes

--- packages/core-database/src/repositories/round-repository.ts
+++ packages/core-database/src/repositories/round-repository.ts
@@ -25,7 +25,7 @@ export class RoundRepository extends Repository<Round> {
     public async save(delegates: readonly Contracts.State.Wallet[]): Promise<never> {
         const round: { publicKey: string; balance: Utils.BigNumber; round: number }[] = delegates.map(
             (delegate: Contracts.State.Wallet) => ({
-                publicKey: delegate.publicKey!,
+                publicKey: delegate.getPublicKey()!,
                 balance: delegate.getAttribute("delegate.voteBalance"),
                 round: delegate.getAttribute("delegate.round"),
             }),
