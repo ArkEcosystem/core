4ef37cf50e6c19b0e828a83444643107ee39cf9b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-06-01 15:11:42 +0200

    chore(core-blockchain): getBlocks from databaseInterceptor

--- packages/core-blockchain/src/processor/handlers/exception-handler.ts
+++ packages/core-blockchain/src/processor/handlers/exception-handler.ts
@@ -1,4 +1,4 @@
-import { DatabaseService } from "@arkecosystem/core-database";
+import { DatabaseInterceptor } from "@arkecosystem/core-state";
 import { Container, Contracts, Utils } from "@arkecosystem/core-kernel";
 import { Interfaces } from "@arkecosystem/crypto";
 
@@ -17,8 +17,8 @@ export class ExceptionHandler implements BlockHandler {
     @Container.inject(Container.Identifiers.LogService)
     private readonly logger!: Contracts.Kernel.Logger;
 
-    @Container.inject(Container.Identifiers.DatabaseService)
-    private readonly database!: DatabaseService;
+    @Container.inject(Container.Identifiers.DatabaseInterceptor)
+    private readonly databaseInterceptor!: DatabaseInterceptor;
 
     public async execute(block: Interfaces.IBlock): Promise<BlockProcessorResult> {
         Utils.assert.defined<string>(block.data.id);
@@ -26,7 +26,7 @@ export class ExceptionHandler implements BlockHandler {
         const id: string = block.data.id;
 
         // Ensure the block has not been forged yet, as an exceptional block bypasses all other checks.
-        const forgedBlock: Interfaces.IBlock | undefined = await this.database.getBlock(id);
+        const forgedBlock: Interfaces.IBlock | undefined = await this.databaseInterceptor.getBlock(id);
 
         if (forgedBlock) {
             this.blockchain.resetLastDownloadedBlock();
