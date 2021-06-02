145b3f31fa6897bd3dbfd8ac92e70390d3ae39b7
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-06-02 14:35:59 +0200

    chore(core-state): getLastStoredBlochHeight is always defined

--- packages/core-state/src/stores/state.ts
+++ packages/core-state/src/stores/state.ts
@@ -20,7 +20,7 @@ export class StateStore implements Contracts.State.StateStore {
     private blockchain: any = {};
     private genesisBlock?: Interfaces.IBlock;
     private lastDownloadedBlock?: Interfaces.IBlockData;
-    private lastStoredBlockHeight?: number;
+    private lastStoredBlockHeight: number = 1;
     private blockPing?: Contracts.State.BlockPing;
     private started = false;
     private forkedBlock?: Interfaces.IBlock;
@@ -70,7 +70,7 @@ export class StateStore implements Contracts.State.StateStore {
         this.lastDownloadedBlock = block;
     }
 
-    public getLastStoredBlockHeight(): number | undefined {
+    public getLastStoredBlockHeight(): number {
         return this.lastStoredBlockHeight;
     }
 
