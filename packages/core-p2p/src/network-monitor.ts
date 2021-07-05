f1efff9d75736bf7ef6dcdf18c03836bd098dc85
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-07-05 11:34:50 +0200

    fix(core-p2p): parse body if type is string

--- packages/core-p2p/src/network-monitor.ts
+++ packages/core-p2p/src/network-monitor.ts
@@ -618,7 +618,7 @@ export class NetworkMonitor implements Contracts.P2P.NetworkMonitor {
             // URL...
             this.logger.debug(`GET ${url}`);
             const { data } = await Utils.http.get(url);
-            return data;
+            return typeof data === "object" ? data : JSON.parse(data);
         }
 
         return [];
