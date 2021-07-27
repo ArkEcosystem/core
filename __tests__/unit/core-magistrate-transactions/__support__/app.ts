55ef976137f5a0055f946d38d6a3464f5f2075f9
Dmitry Tyshchenko <rainydio@gmail.com>
2021-07-27 21:48:54 +0300

    Fiix magistrate-transactions unit tests

--- __tests__/unit/core-magistrate-transactions/__support__/app.ts
+++ __tests__/unit/core-magistrate-transactions/__support__/app.ts
@@ -38,6 +38,7 @@ import { TransactionHandlerProvider } from "@packages/core-transactions/src/hand
 import { TransactionHandlerRegistry } from "@packages/core-transactions/src/handlers/handler-registry";
 import { Identities, Utils } from "@packages/crypto";
 import { IMultiSignatureAsset } from "@packages/crypto/src/interfaces";
+import { TransactionHandlerConstructorsBinding } from "@packages/core-transactions/src/service-provider";
 
 const logger = {
     notice: jest.fn(),
@@ -166,6 +167,7 @@ export const initApp = (): Application => {
     app.bind(Identifiers.TransactionHandler).to(Two.HtlcClaimTransactionHandler);
     app.bind(Identifiers.TransactionHandler).to(Two.HtlcRefundTransactionHandler);
 
+    app.bind(Identifiers.TransactionHandlerConstructors).toDynamicValue(TransactionHandlerConstructorsBinding);
     app.bind(Identifiers.TransactionHandlerProvider).to(TransactionHandlerProvider).inSingletonScope();
     app.bind(Identifiers.TransactionHandlerRegistry).to(TransactionHandlerRegistry).inSingletonScope();
 
