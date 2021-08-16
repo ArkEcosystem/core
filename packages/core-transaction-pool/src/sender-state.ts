96da4d0b3f0498fe3e20906290ee277d52017728
Dmitry Tyshchenko <rainydio@gmail.com>
2021-08-16 16:09:26 +0300

    Revert sender state changes

--- packages/core-transaction-pool/src/sender-state.ts
+++ packages/core-transaction-pool/src/sender-state.ts
@@ -35,7 +35,7 @@ export class SenderState implements Contracts.TransactionPool.SenderState {
 
     public async apply(transaction: Interfaces.ITransaction): Promise<void> {
         const maxTransactionBytes: number = this.configuration.getRequired<number>("maxTransactionBytes");
-        if (transaction.serialized.length > maxTransactionBytes) {
+        if (JSON.stringify(transaction.data).length > maxTransactionBytes) {
             throw new TransactionExceedsMaximumByteSizeError(transaction, maxTransactionBytes);
         }
 
