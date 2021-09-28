8660ec55c24ceb82135daa6524b5876f17da6cf2
Dmitry Tyshchenko <rainydio@gmail.com>
2021-09-28 11:52:50 +0300

    Create pool processor extension point

--- packages/core-kernel/src/contracts/transaction-pool/processor.ts
+++ packages/core-kernel/src/contracts/transaction-pool/processor.ts
@@ -5,14 +5,22 @@ export type ProcessorError = {
     message: string;
 };
 
+export type ProcessorResult = {
+    accept: string[];
+    broadcast: string[];
+    invalid: string[];
+    excess: string[];
+    errors?: { [id: string]: ProcessorError };
+};
+
+export interface ProcessorExtension {
+    throwIfCannotBroadcast(transaction: Interfaces.ITransaction): Promise<void>;
+}
+
+export interface ProcessorDynamicFeeExtension extends ProcessorExtension {}
+
 export interface Processor {
-    process(data: Interfaces.ITransactionData[] | Buffer[]): Promise<{
-        accept: string[],
-        broadcast: string[],
-        invalid: string[],
-        excess: string[],
-        errors?: { [id: string]: ProcessorError },
-    }>;
+    process(data: Interfaces.ITransactionData[] | Buffer[]): Promise<ProcessorResult>;
 }
 
 export type ProcessorFactory = () => Processor;
