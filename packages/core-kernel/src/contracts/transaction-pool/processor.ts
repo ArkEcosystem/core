8e9474e4e63368d7a959809cf427c92bb102e29f
Dmitry Tyshchenko <rainydio@gmail.com>
2021-10-05 05:54:05 +0300

    feat(core-transaction-pool): processor extension point (#4522)

--- packages/core-kernel/src/contracts/transaction-pool/processor.ts
+++ packages/core-kernel/src/contracts/transaction-pool/processor.ts
@@ -1,18 +1,28 @@
 import { Interfaces } from "@arkecosystem/crypto";
+import { injectable } from "../../ioc";
 
 export type ProcessorError = {
     type: string;
     message: string;
 };
 
+export type ProcessorResult = {
+    accept: string[];
+    broadcast: string[];
+    invalid: string[];
+    excess: string[];
+    errors?: { [id: string]: ProcessorError };
+};
+
+@injectable()
+export abstract class ProcessorExtension {
+    public async throwIfCannotBroadcast(transaction: Interfaces.ITransaction): Promise<void> {
+        // override me
+    }
+}
+
 export interface Processor {
-    process(data: Interfaces.ITransactionData[] | Buffer[]): Promise<{
-        accept: string[],
-        broadcast: string[],
-        invalid: string[],
-        excess: string[],
-        errors?: { [id: string]: ProcessorError },
-    }>;
+    process(data: Interfaces.ITransactionData[] | Buffer[]): Promise<ProcessorResult>;
 }
 
 export type ProcessorFactory = () => Processor;
