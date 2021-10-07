9403cfca7abcbaf8457c10dc38680a5ab91b12a1
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-10-07 17:56:06 +0200

    chore(core-p2p): implements get & remove

--- packages/core-p2p/src/proposal-storage.ts
+++ packages/core-p2p/src/proposal-storage.ts
@@ -7,6 +7,8 @@ export class ProposalStorage {
     private database!: BetterSqlite3.Database;
     private addProposalStmt!: BetterSqlite3.Statement<{ height: number; data: any }>;
     private hasProposalStmt!: BetterSqlite3.Statement<{ height: number }>;
+    private getProposalStmt!: BetterSqlite3.Statement<{ height: number }>;
+    private removeProposalStmt!: BetterSqlite3.Statement<{ height: number }>;
 
     public boot(): void {
         const filename = `${process.env.CORE_PATH_DATA}/proposal.sqlite`;
@@ -25,7 +27,7 @@ export class ProposalStorage {
                 data               JSON         NOT NULL
             );
 
-            CREATE INDEX IF NOT EXISTS ${table}_height ON ${table} (height);
+            CREATE UNIQUE INDEX IF NOT EXISTS ${table}_height ON ${table} (height);
         `);
 
         this.addProposalStmt = this.database.prepare(`INSERT INTO ${table} (height, data) VALUES (:height, :data)`);
@@ -33,6 +35,10 @@ export class ProposalStorage {
         this.hasProposalStmt = this.database
             .prepare(`SELECT COUNT(*) FROM ${table} WHERE height = :height`)
             .pluck(true);
+
+        this.getProposalStmt = this.database.prepare(`SELECT data FROM ${table} WHERE height = :height`);
+
+        this.removeProposalStmt = this.database.prepare(`DELETE FROM ${table} WHERE height = :height`);
     }
 
     public addProposal(proposal: { height: number }): void {
@@ -41,5 +47,19 @@ export class ProposalStorage {
 
     public hasProposal(height: number): boolean {
         return !!this.hasProposalStmt.get({ height });
+    }
+
+    public getProposal(height: number): any {
+        const result = this.getProposalStmt.get({ height });
+
+        if (result) {
+            return JSON.parse(result.data);
+        }
+
+        return undefined;
+    }
+
+    public removeProposal(height: number): void {
+        this.removeProposalStmt.run({ height });
     }
 }
