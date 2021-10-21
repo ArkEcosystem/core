837c0053b8533ade810e8a7070d0af442b4143b6
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-10-21 15:28:22 +0200

    test(core-cli): PluginManager install

--- __tests__/unit/core-cli/services/plugin-manager.test.ts
+++ __tests__/unit/core-cli/services/plugin-manager.test.ts
@@ -3,6 +3,31 @@ import { PluginManager } from "@packages/core-cli/src/services";
 import { join } from "path";
 import { setGracefulCleanup } from "tmp";
 
+const packageName = "dummyPackageName";
+const install = jest.fn();
+const exists = jest.fn().mockReturnValue(false);
+
+jest.mock("@packages/core-cli/src/services/source-providers/npm", () => ({
+    NPM: jest.fn().mockImplementation(() => ({
+        exists,
+        install,
+    })),
+}));
+
+jest.mock("@packages/core-cli/src/services/source-providers/git", () => ({
+    Git: jest.fn().mockImplementation(() => ({
+        exists,
+        install,
+    })),
+}));
+
+jest.mock("@packages/core-cli/src/services/source-providers/file", () => ({
+    File: jest.fn().mockImplementation(() => ({
+        exists,
+        install,
+    })),
+}));
+
 let cli;
 let pluginManager;
 
@@ -41,6 +66,35 @@ describe("DiscoverPlugins", () => {
             const plugins = await pluginManager.list("ark", "undefined");
 
             expect(plugins).toEqual([]);
+        });
+    });
+
+    describe("install", () => {
+        it("should throw an error when package doesn't exist", async () => {
+            const errorMessage = `The given package [${packageName}] is neither a git nor a npm package.`;
+            await expect(pluginManager.install("ark", "testnet", packageName)).rejects.toThrow(errorMessage);
+
+            expect(exists).toHaveBeenCalledWith(packageName, undefined);
+            expect(install).not.toHaveBeenCalled();
+        });
+
+        it("should call install on existing packages", async () => {
+            exists.mockReturnValue(true);
+
+            await expect(pluginManager.install("ark", "testnet", packageName)).toResolve();
+
+            expect(exists).toHaveBeenCalledWith(packageName, undefined);
+            expect(install).toHaveBeenCalledWith(packageName, undefined);
+        });
+
+        it("should call install on existing packages with version", async () => {
+            exists.mockReturnValue(true);
+
+            const version = "3.0.0";
+            await expect(pluginManager.install("ark", "testnet", packageName, version)).toResolve();
+
+            expect(exists).toHaveBeenCalledWith(packageName, version);
+            expect(install).toHaveBeenCalledWith(packageName, version);
         });
     });
 });
