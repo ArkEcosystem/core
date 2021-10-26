915fe5a91b583436b30c434940979ffdfc3e2694
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-10-26 17:26:58 +0200

    fix(core-p2p): mapAddr

--- packages/core-p2p/src/socket-server/utils/map-addr.ts
+++ packages/core-p2p/src/socket-server/utils/map-addr.ts
@@ -1,4 +1,3 @@
-import { parse } from "ipaddr.js";
+import * as ipAddr from "ipaddr.js";
 
-// TODO: Check why process from ipaddr.js doesn't work
-export const mapAddr = (addr: string) => parse(addr).toString();
+export const mapAddr = (addr: string) => ipAddr.process(addr).toString();
