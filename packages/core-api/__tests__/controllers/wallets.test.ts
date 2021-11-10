93346f96066e7f3d92561e32714ff90f60943b51
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-11-10 18:17:42 +0100

    test(core-api): wallets.test.ts

--- packages/core-api/__tests__/controllers/wallets.test.ts
+++ packages/core-api/__tests__/controllers/wallets.test.ts
@@ -1,9 +1,9 @@
+import { Boom } from "@hapi/boom";
 import { LockSearchService, Resources, WalletSearchService } from "@packages/core-api";
 import { WalletsController } from "@packages/core-api/src/controllers/wallets";
 import { Identifiers } from "@packages/core-api/src/identifiers";
 import { Application, Container, Contracts, Providers, Services } from "@packages/core-kernel";
 import { Enums, Utils } from "@packages/crypto";
-import { Boom } from "@hapi/boom";
 
 const jestfn = <T extends (...args: unknown[]) => unknown>(
     implementation?: (...args: Parameters<T>) => ReturnType<T>,
@@ -36,6 +36,8 @@ const transactionHistoryService = {
 const paginationService = {
     getEmptyPage: jestfn<Services.Search.PaginationService["getEmptyPage"]>(),
 };
+
+const h: any = {};
 
 const container = new Container.Container();
 container.bind(Container.Identifiers.Application).toConstantValue(app);
@@ -92,14 +94,17 @@ describe("WalletsController", () => {
             walletSearchService.getWalletsPage.mockReturnValueOnce(walletsPage);
 
             const walletsController = container.resolve(WalletsController);
-            const result = walletsController.index({
-                query: {
-                    page: 1,
-                    limit: 100,
-                    orderBy: ["balance:desc", "address:asc"],
-                    address: "ATrkBiUXGDKduaSjqez2Ar7T9rQW6cnaeu",
-                },
-            });
+            const result = walletsController.index(
+                {
+                    query: {
+                        page: 1,
+                        limit: 100,
+                        orderBy: ["balance:desc", "address:asc"],
+                        address: "ATrkBiUXGDKduaSjqez2Ar7T9rQW6cnaeu",
+                    },
+                } as any,
+                h,
+            );
 
             expect(walletSearchService.getWalletsPage).toBeCalledWith(
                 { offset: 0, limit: 100 },
@@ -123,14 +128,17 @@ describe("WalletsController", () => {
             walletSearchService.getWalletsPage.mockReturnValueOnce(walletsPage);
 
             const walletsController = container.resolve(WalletsController);
-            const result = walletsController.top({
-                query: {
-                    page: 1,
-                    limit: 100,
-                    orderBy: ["balance:desc", "address:asc"],
-                    address: "ATrkBiUXGDKduaSjqez2Ar7T9rQW6cnaeu",
-                },
-            });
+            const result = walletsController.top(
+                {
+                    query: {
+                        page: 1,
+                        limit: 100,
+                        orderBy: ["balance:desc", "address:asc"],
+                        address: "ATrkBiUXGDKduaSjqez2Ar7T9rQW6cnaeu",
+                    },
+                } as any,
+                h,
+            );
 
             expect(walletSearchService.getWalletsPage).toBeCalledWith(
                 { offset: 0, limit: 100 },
@@ -147,11 +155,14 @@ describe("WalletsController", () => {
             walletSearchService.getWallet.mockReturnValueOnce(walletResource1);
 
             const walletsController = container.resolve(WalletsController);
-            const result = walletsController.show({
-                params: {
-                    id: walletResource1.address,
-                },
-            });
+            const result = walletsController.show(
+                {
+                    params: {
+                        id: walletResource1.address,
+                    },
+                } as any,
+                h,
+            );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.address);
             expect(result).toEqual({ data: walletResource1 });
@@ -161,11 +172,14 @@ describe("WalletsController", () => {
             walletSearchService.getWallet.mockReturnValueOnce(undefined);
 
             const walletsController = container.resolve(WalletsController);
-            const result = walletsController.show({
-                params: {
-                    id: "non-existing-wallet-id",
-                },
-            });
+            const result = walletsController.show(
+                {
+                    params: {
+                        id: "non-existing-wallet-id",
+                    },
+                } as any,
+                h,
+            );
 
             expect(walletSearchService.getWallet).toBeCalledWith("non-existing-wallet-id");
             expect(result).toBeInstanceOf(Boom);
@@ -184,17 +198,20 @@ describe("WalletsController", () => {
             lockSearchService.getWalletLocksPage.mockReturnValueOnce(locksPage);
 
             const walletsController = container.resolve(WalletsController);
-            const result = walletsController.locks({
-                params: {
-                    id: walletResource1.publicKey,
-                },
-                query: {
-                    page: 1,
-                    limit: 100,
-                    orderBy: ["timestamp.unix:desc"],
-                    isExpired: false,
-                },
-            });
+            const result = walletsController.locks(
+                {
+                    params: {
+                        id: walletResource1.publicKey,
+                    },
+                    query: {
+                        page: 1,
+                        limit: 100,
+                        orderBy: ["timestamp.unix:desc"],
+                        isExpired: false,
+                    },
+                } as any,
+                h,
+            );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
 
@@ -212,11 +229,14 @@ describe("WalletsController", () => {
             walletSearchService.getWallet.mockReturnValueOnce(undefined);
 
             const walletsController = container.resolve(WalletsController);
-            const result = walletsController.locks({
-                params: {
-                    id: "non-existing-wallet-id",
-                },
-            });
+            const result = walletsController.locks(
+                {
+                    params: {
+                        id: "non-existing-wallet-id",
+                    },
+                } as any,
+                h,
+            );
 
             expect(walletSearchService.getWallet).toBeCalledWith("non-existing-wallet-id");
             expect(result).toBeInstanceOf(Boom);
@@ -256,8 +276,8 @@ describe("WalletsController", () => {
                         transform: false,
                         type: Enums.TransactionType.MultiPayment,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -320,8 +340,8 @@ describe("WalletsController", () => {
                         transform: true,
                         type: Enums.TransactionType.MultiPayment,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -360,8 +380,8 @@ describe("WalletsController", () => {
                     params: {
                         id: "non-existing-wallet-id",
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith("non-existing-wallet-id");
@@ -402,8 +422,8 @@ describe("WalletsController", () => {
                         transform: false,
                         type: Enums.TransactionType.MultiPayment,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -466,8 +486,8 @@ describe("WalletsController", () => {
                         transform: true,
                         type: Enums.TransactionType.MultiPayment,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -506,8 +526,8 @@ describe("WalletsController", () => {
                     params: {
                         id: "non-existing-wallet-id",
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith("non-existing-wallet-id");
@@ -526,8 +546,8 @@ describe("WalletsController", () => {
                     params: {
                         id: walletResource2Cold.address,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource2Cold.address);
@@ -569,8 +589,8 @@ describe("WalletsController", () => {
                         transform: false,
                         type: Enums.TransactionType.MultiPayment,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -633,8 +653,8 @@ describe("WalletsController", () => {
                         transform: true,
                         type: Enums.TransactionType.MultiPayment,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -673,8 +693,8 @@ describe("WalletsController", () => {
                     params: {
                         id: "non-existing-wallet-id",
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith("non-existing-wallet-id");
@@ -714,8 +734,8 @@ describe("WalletsController", () => {
                         orderBy: "amount:desc",
                         transform: false,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -778,8 +798,8 @@ describe("WalletsController", () => {
                         orderBy: "amount:desc",
                         transform: true,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource1.publicKey);
@@ -819,8 +839,8 @@ describe("WalletsController", () => {
                     params: {
                         id: "non-existing-wallet-id",
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith("non-existing-wallet-id");
@@ -843,8 +863,8 @@ describe("WalletsController", () => {
                     params: {
                         id: walletResource2Cold.address,
                     },
-                },
-                undefined,
+                } as any,
+                h,
             );
 
             expect(walletSearchService.getWallet).toBeCalledWith(walletResource2Cold.address);
