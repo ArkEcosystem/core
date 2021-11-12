0c972eb5ee25094412d077cd4dc6d173e63648c5
sebastijankuzner <sebastijan.kuzner@outlook.com>
2021-11-12 10:25:35 +0100

    test(integration): use correct payload type

--- __tests__/integration/core-test-framework/utils/__support__/echo-controller.ts
+++ __tests__/integration/core-test-framework/utils/__support__/echo-controller.ts
@@ -1,9 +1,13 @@
 import Hapi from "@hapi/hapi";
 import { Container } from "@packages/core-kernel";
+import stream from "stream";
 
 @Container.injectable()
 export class EchoController {
-    public index(request: Hapi.Request): { query: Hapi.RequestQuery; payload: any } {
+    public index(request: Hapi.Request): {
+        query: Hapi.RequestQuery;
+        payload: stream.Readable | Buffer | string | object;
+    } {
         return { query: request.query, payload: request.payload };
     }
 }
