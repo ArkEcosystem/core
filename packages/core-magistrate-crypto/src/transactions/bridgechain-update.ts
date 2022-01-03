22b75ecb883c46db63004cf9f315e29055af8478
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-01-03 14:52:16 +0100

    chore(core-magistrate-crypto): use byteBuffer in BridgechainUpdate

--- packages/core-magistrate-crypto/src/transactions/bridgechain-update.ts
+++ packages/core-magistrate-crypto/src/transactions/bridgechain-update.ts
@@ -1,6 +1,5 @@
 import { Utils as AppUtils } from "@arkecosystem/core-kernel";
 import { Transactions, Utils } from "@arkecosystem/crypto";
-import ByteBuffer from "bytebuffer";
 
 import { MagistrateTransactionGroup, MagistrateTransactionStaticFees, MagistrateTransactionType } from "../enums";
 import { IBridgechainPorts, IBridgechainUpdateAsset } from "../interfaces";
@@ -65,7 +64,7 @@ export class BridgechainUpdateTransaction extends Transactions.Transaction {
         });
     }
 
-    public serialize(): ByteBuffer {
+    public serialize(): Utils.ByteBuffer {
         const { data } = this;
 
         AppUtils.assert.defined<IBridgechainUpdateAsset>(data.asset?.bridgechainUpdate);
@@ -124,90 +123,150 @@ export class BridgechainUpdateTransaction extends Transactions.Transaction {
             bridgechainAssetRepositoryBufferLength += bridgechainAssetRepositoryBuffer.length;
         }
 
-        const buffer: ByteBuffer = new ByteBuffer(
-            32 + // bridgechain id
-                seedNodesBuffersLength +
-                portsBuffersLength +
-                bridgechainRepositoryBufferLength +
-                bridgechainAssetRepositoryBufferLength,
-            true,
+        const buffer = new Utils.ByteBuffer(
+            Buffer.alloc(
+                32 + // bridgechain id
+                    seedNodesBuffersLength +
+                    portsBuffersLength +
+                    bridgechainRepositoryBufferLength +
+                    bridgechainAssetRepositoryBufferLength,
+            ),
         );
 
-        buffer.append(bridgechainUpdateAsset.bridgechainId, "hex");
-        buffer.writeUint8(seedNodesBuffers.length);
+        // buffer.append(bridgechainUpdateAsset.bridgechainId, "hex");
+        buffer.writeBuffer(Buffer.from(bridgechainUpdateAsset.bridgechainId, "hex"));
 
+        // buffer.writeUint8(seedNodesBuffers.length);
+        // for (const seedBuf of seedNodesBuffers) {
+        //     buffer.writeUint8(seedBuf.length);
+        //     buffer.append(seedBuf);
+        // }
+        buffer.writeUInt8(seedNodesBuffers.length);
         for (const seedBuf of seedNodesBuffers) {
-            buffer.writeUint8(seedBuf.length);
-            buffer.append(seedBuf);
+            buffer.writeUInt8(seedBuf.length);
+            buffer.writeBuffer(seedBuf);
         }
 
-        buffer.writeUint8(portsLength);
+        // buffer.writeUint8(portsLength);
+        // for (const [i, nameBuffer] of portNamesBuffers.entries()) {
+        //     buffer.writeUint8(nameBuffer.length);
+        //     buffer.append(nameBuffer);
+        //     buffer.writeUint16(portNumbers[i]);
+        // }
+        buffer.writeUInt8(portsLength);
         for (const [i, nameBuffer] of portNamesBuffers.entries()) {
-            buffer.writeUint8(nameBuffer.length);
-            buffer.append(nameBuffer);
-            buffer.writeUint16(portNumbers[i]);
+            buffer.writeUInt8(nameBuffer.length);
+            buffer.writeBuffer(nameBuffer);
+            buffer.writeUInt16LE(portNumbers[i]);
         }
 
+        // if (bridgechainRepositoryBuffer) {
+        //     buffer.writeUint8(bridgechainRepositoryBuffer.length);
+        //     buffer.append(bridgechainRepositoryBuffer);
+        // } else {
+        //     buffer.writeUint8(0);
+        // }
         if (bridgechainRepositoryBuffer) {
-            buffer.writeUint8(bridgechainRepositoryBuffer.length);
-            buffer.append(bridgechainRepositoryBuffer);
+            buffer.writeUInt8(bridgechainRepositoryBuffer.length);
+            buffer.writeBuffer(bridgechainRepositoryBuffer);
         } else {
-            buffer.writeUint8(0);
+            buffer.writeUInt8(0);
         }
 
+        // if (bridgechainAssetRepositoryBuffer) {
+        //     buffer.writeUint8(bridgechainAssetRepositoryBuffer.length);
+        //     buffer.append(bridgechainAssetRepositoryBuffer);
+        // } else {
+        //     buffer.writeUint8(0);
+        // }
         if (bridgechainAssetRepositoryBuffer) {
-            buffer.writeUint8(bridgechainAssetRepositoryBuffer.length);
-            buffer.append(bridgechainAssetRepositoryBuffer);
+            buffer.writeUInt8(bridgechainAssetRepositoryBuffer.length);
+            buffer.writeBuffer(bridgechainAssetRepositoryBuffer);
         } else {
-            buffer.writeUint8(0);
+            buffer.writeUInt8(0);
         }
 
         return buffer;
     }
 
-    public deserialize(buf: ByteBuffer): void {
+    public deserialize(buf: Utils.ByteBuffer): void {
         const { data } = this;
-        const bridgechainId: string = buf.readBytes(32).toString("hex");
+        // const bridgechainId: string = buf.readBytes(32).toString("hex");
+        const bridgechainId: string = buf.readBuffer(32).toString("hex");
 
         const bridgechainUpdate: IBridgechainUpdateAsset = {
             bridgechainId,
         };
 
-        const seedNodesLength: number = buf.readUint8();
+        // const seedNodesLength: number = buf.readUint8();
+        // if (seedNodesLength) {
+        //     const seedNodes: string[] = [];
+        //
+        //     for (let i = 0; i < seedNodesLength; i++) {
+        //         const ipLength = buf.readUint8();
+        //         const ip = buf.readString(ipLength);
+        //         seedNodes.push(ip);
+        //     }
+        //
+        //     bridgechainUpdate.seedNodes = seedNodes;
+        // }
+        const seedNodesLength: number = buf.readUInt8();
         if (seedNodesLength) {
             const seedNodes: string[] = [];
 
             for (let i = 0; i < seedNodesLength; i++) {
-                const ipLength = buf.readUint8();
-                const ip = buf.readString(ipLength);
+                const ipLength = buf.readUInt8();
+                const ip = buf.readBuffer(ipLength).toString("utf8");
                 seedNodes.push(ip);
             }
 
             bridgechainUpdate.seedNodes = seedNodes;
         }
 
-        const portsLength: number = buf.readUint8();
+        // const portsLength: number = buf.readUint8();
+        // if (portsLength) {
+        //     const ports: IBridgechainPorts = {};
+        //
+        //     for (let i = 0; i < portsLength; i++) {
+        //         const nameLength: number = buf.readUint8();
+        //         const name: string = buf.readString(nameLength);
+        //         const port: number = buf.readUint16();
+        //         ports[name] = port;
+        //     }
+        //
+        //     bridgechainUpdate.ports = ports;
+        // }
+        const portsLength: number = buf.readUInt8();
         if (portsLength) {
             const ports: IBridgechainPorts = {};
 
             for (let i = 0; i < portsLength; i++) {
-                const nameLength: number = buf.readUint8();
-                const name: string = buf.readString(nameLength);
-                const port: number = buf.readUint16();
-                ports[name] = port;
+                const nameLength: number = buf.readUInt8();
+                const name: string = buf.readBuffer(nameLength).toString("utf8");
+                ports[name] = buf.readUInt16LE();
             }
 
             bridgechainUpdate.ports = ports;
         }
 
-        const bridgechainRepositoryLength: number = buf.readUint8();
+        // const bridgechainRepositoryLength: number = buf.readUint8();
+        // if (bridgechainRepositoryLength) {
+        //     bridgechainUpdate.bridgechainRepository = buf.readString(bridgechainRepositoryLength);
+        // }
+        const bridgechainRepositoryLength: number = buf.readUInt8();
         if (bridgechainRepositoryLength) {
-            bridgechainUpdate.bridgechainRepository = buf.readString(bridgechainRepositoryLength);
+            bridgechainUpdate.bridgechainRepository = buf.readBuffer(bridgechainRepositoryLength).toString("utf8");
         }
 
-        const bridgechainAssetRepositoryLength: number = buf.readUint8();
+        // const bridgechainAssetRepositoryLength: number = buf.readUint8();
+        // if (bridgechainAssetRepositoryLength) {
+        //     bridgechainUpdate.bridgechainAssetRepository = buf.readString(bridgechainAssetRepositoryLength);
+        // }
+        const bridgechainAssetRepositoryLength: number = buf.readUInt8();
         if (bridgechainAssetRepositoryLength) {
-            bridgechainUpdate.bridgechainAssetRepository = buf.readString(bridgechainAssetRepositoryLength);
+            bridgechainUpdate.bridgechainAssetRepository = buf
+                .readBuffer(bridgechainAssetRepositoryLength)
+                .toString("utf8");
         }
 
         data.asset = {
