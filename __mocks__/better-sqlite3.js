e8d12a63910eb177ab64b2c3001cc41d865a1df9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-01-26 11:03:47 +0100

    Remove better-sqlite3 mock

--- __mocks__/better-sqlite3.js
+++
@@ -1,7 +0,0 @@
-const BetterSqlite3 = jest.requireActual("better-sqlite3");
-
-module.exports = class BetterSqlite3Mock extends BetterSqlite3 {
-    constructor(path, options = {}) {
-        super(path || ":memory:", options);
-    }
-};
