85b8cee34f1f216d63434988b56a951c2a4b2b07
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2022-06-27 09:58:50 +0200

    refactor(core-p2p): get min peer version from `default.ts` instead of milestones (#4680)
    * Add v4 next into minVersions
    
    * Remove version check from milestones
    
    * Remove 3.0.0-alpha.0

--- packages/core-p2p/src/utils/is-valid-version.ts
+++ packages/core-p2p/src/utils/is-valid-version.ts
@@ -12,25 +12,15 @@ export const isValidVersion = (app: Contracts.Kernel.Application, peer: Contract
         return false;
     }
 
-    let minimumVersions: string[];
-    const milestones: Record<string, any> = Managers.configManager.getMilestone();
-
-    const { p2p } = milestones;
-
-    if (p2p && Array.isArray(p2p.minimumVersions) && p2p.minimumVersions.length > 0) {
-        minimumVersions = p2p.minimumVersions;
-    } else {
-        const configuration = app.getTagged<Providers.PluginConfiguration>(
-            Container.Identifiers.PluginConfiguration,
-            "plugin",
-            "@arkecosystem/core-p2p",
-        );
-        minimumVersions = configuration.getOptional<string[]>("minimumVersions", []);
-    }
+    const configuration = app.getTagged<Providers.PluginConfiguration>(
+        Container.Identifiers.PluginConfiguration,
+        "plugin",
+        "@arkecosystem/core-p2p",
+    );
+    const minimumVersions = configuration.getOptional<string[]>("minimumVersions", []);
 
     const includePrerelease: boolean = Managers.configManager.get("network.name") !== "mainnet";
     return minimumVersions.some((minimumVersion: string) =>
-        // @ts-ignore - check why the peer.version errors even though we exit early
-        semver.satisfies(peer.version, minimumVersion, { includePrerelease }),
+        semver.satisfies(peer.version!, minimumVersion, { includePrerelease }),
     );
 };
