bd37119e4d621d25f88e582d1504df204736ca1f
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2022-08-09 15:47:02 +0200

    fix(core-p2p): node version should pass min version defined in configuration and milestones (#4695)

--- packages/core-p2p/src/utils/is-valid-version.ts
+++ packages/core-p2p/src/utils/is-valid-version.ts
@@ -2,7 +2,6 @@ import { Container, Contracts, Providers } from "@arkecosystem/core-kernel";
 import { Managers } from "@arkecosystem/crypto";
 import semver from "semver";
 
-// todo: review the implementation
 export const isValidVersion = (app: Contracts.Kernel.Application, peer: Contracts.P2P.Peer): boolean => {
     if (!peer.version) {
         return false;
@@ -12,15 +11,44 @@ export const isValidVersion = (app: Contracts.Kernel.Application, peer: Contract
         return false;
     }
 
+    const includePrerelease: boolean = Managers.configManager.get("network.name") !== "mainnet";
+
+    return (
+        isValidConfigVersion(app, peer.version, includePrerelease) &&
+        isValidMilestoneVersion(peer.version, includePrerelease)
+    );
+};
+
+const isValidConfigVersion = (
+    app: Contracts.Kernel.Application,
+    version: string,
+    includePrerelease: boolean,
+): boolean => {
     const configuration = app.getTagged<Providers.PluginConfiguration>(
         Container.Identifiers.PluginConfiguration,
         "plugin",
         "@arkecosystem/core-p2p",
     );
+
     const minimumVersions = configuration.getOptional<string[]>("minimumVersions", []);
 
-    const includePrerelease: boolean = Managers.configManager.get("network.name") !== "mainnet";
-    return minimumVersions.some((minimumVersion: string) =>
-        semver.satisfies(peer.version!, minimumVersion, { includePrerelease }),
-    );
+    if (minimumVersions.length > 0) {
+        return minimumVersions.some((minimumVersion: string) =>
+            semver.satisfies(version, minimumVersion, { includePrerelease }),
+        );
+    }
+
+    return true;
+};
+
+const isValidMilestoneVersion = (version: string, includePrerelease: boolean): boolean => {
+    const { p2p } = Managers.configManager.getMilestone();
+
+    if (p2p && Array.isArray(p2p.minimumVersions) && p2p.minimumVersions.length > 0) {
+        return p2p.minimumVersions.some((minimumVersion: string) =>
+            semver.satisfies(version, minimumVersion, { includePrerelease }),
+        );
+    }
+
+    return true;
 };
