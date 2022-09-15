653d6cf0f791742da87ebe8f28e628c3ac909b9f
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-09-15 17:40:46 +0200

    Use LRU-cache

--- packages/core-transactions/src/cache/second-signature-verification.ts
+++ packages/core-transactions/src/cache/second-signature-verification.ts
@@ -1,39 +1,27 @@
 import { Container } from "@arkecosystem/core-kernel";
 import { Interfaces, Transactions } from "@arkecosystem/crypto";
+import LRUCache from "lru-cache";
 
 @Container.injectable()
 export class SecondSignatureVerificationCache {
-    private results: Map<string, boolean> = new Map();
+    private lruCache = new LRUCache<string, boolean>({ max: 2 });
 
     public verifySecondSignature(transaction: Interfaces.ITransactionData, publicKey: string): boolean {
-        if (this.has(transaction, publicKey)) {
-            return this.get(transaction, publicKey);
+        const key = this.getKey(transaction, publicKey);
+
+        if (this.lruCache.has(key)) {
+            return this.lruCache.get(key)!;
         }
 
         const result = Transactions.Verifier.verifySecondSignature(transaction, publicKey);
 
-        this.set(transaction, publicKey, result);
+        this.lruCache.set(key, result);
 
         return result;
     }
 
-    private set(transaction: Interfaces.ITransactionData, publicKey: string, result: boolean): void {
-        if (transaction.id) {
-            this.results.set(this.getKey(transaction, publicKey), result);
-        }
-    }
-
-    private has(transaction: Interfaces.ITransactionData, publicKey: string): boolean {
-        return this.results.has(this.getKey(transaction, publicKey));
-    }
-
-    private get(transaction: Interfaces.ITransactionData, publicKey: string): boolean {
-        const result = this.results.get(this.getKey(transaction, publicKey));
-        if (result === undefined) {
-            throw new Error("Key is not defined");
-        }
-
-        return result;
+    public clear() {
+        this.lruCache.clear();
     }
 
     private getKey(transaction: Interfaces.ITransactionData, publicKey: string): string {
