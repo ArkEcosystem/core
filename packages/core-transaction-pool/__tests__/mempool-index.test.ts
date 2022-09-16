5c683f5f3ed0cbfcb7a219c807cfb6c272b82cea
sebastijankuzner <sebastijan.kuzner@outlook.com>
2022-09-16 11:13:30 +0200

    Add mempool index

---
+++ packages/core-transaction-pool/__tests__/mempool-index.test.ts
@@ -0,0 +1,49 @@
+import "jest-extended";
+
+import { Interfaces } from "@arkecosystem/crypto";
+
+import { MempoolIndex } from "../../../packages/core-transaction-pool/src/mempool-index";
+
+describe("MempoolIndex", () => {
+    let index: MempoolIndex;
+    let transaction: Interfaces.ITransaction;
+
+    beforeEach(() => {
+        index = new MempoolIndex();
+        transaction = {} as Interfaces.ITransaction;
+    });
+
+    it("should set key", () => {
+        const key = "key";
+
+        expect(index.has(key)).toBeFalse();
+
+        index.set(key, transaction);
+
+        expect(index.has(key)).toBeTrue();
+    });
+
+    it("should forget key", () => {
+        const key = "key";
+
+        index.set(key, transaction);
+
+        expect(index.has(key)).toBeTrue();
+
+        index.forget(key);
+
+        expect(index.has(key)).toBeFalse();
+    });
+
+    it("should clear all keys", () => {
+        const key = "key";
+
+        index.set(key, transaction);
+
+        expect(index.has(key)).toBeTrue();
+
+        index.clear();
+
+        expect(index.has(key)).toBeFalse();
+    });
+});
