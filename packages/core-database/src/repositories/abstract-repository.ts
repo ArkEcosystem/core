1d042a6b6138b28d85a95a99d15485e5f524a80a
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2023-02-24 22:01:40 +0100

    revert: "fix(core-database): query optimizer workaround" (#4759)
    Revert "fix(core-database): query optimizer workaround (#4726)"
    
    This reverts commit 2e1e824f49e0c1c965fed8c4a34d60bc605dbe56.

--- packages/core-database/src/repositories/abstract-repository.ts
+++ packages/core-database/src/repositories/abstract-repository.ts
@@ -142,13 +142,7 @@ export abstract class AbstractRepository<TEntity extends ObjectLiteral> extends 
 
     private addOrderBy(queryBuilder: SelectQueryBuilder<TEntity>, sorting: Contracts.Search.Sorting): void {
         if (sorting.length) {
-            let column = this.queryHelper.getColumnName(this.metadata, sorting[0].property);
-
-            // Forces PostgreSQL query optimizer to take faster route
-            if (this.metadata.name === "Transaction" && column === "timestamp") {
-                column = `${column}+0`;
-            }
-
+            const column = this.queryHelper.getColumnName(this.metadata, sorting[0].property);
             queryBuilder.orderBy(column, sorting[0].direction === "desc" ? "DESC" : "ASC");
 
             for (const item of sorting.slice(1)) {
