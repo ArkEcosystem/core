e2aa7154f5782c3c435856535de00d9be84bcab1
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2024-01-29 18:39:36 +0100

    feat(core-blockchain): increase block download frequency using fastSync option  (#4805)
    * Fast sync default
    
    * Use value
    
    * fix tests

--- packages/core-blockchain/src/defaults.ts
+++ packages/core-blockchain/src/defaults.ts
@@ -1,4 +1,5 @@
 export const defaults = {
+    fastSync: !!process.env.CORE_BLOCKCHAIN_FAST_SYNC, // Improves sync rate for readonly nodes, that have a p2p port closed to public. Such node doesn't broadcast data
     databaseRollback: {
         maxBlockRewind: 10000,
         steps: 1000,
