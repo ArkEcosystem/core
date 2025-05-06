83238cc86c9ed80847f7523cd1fb2baaa8279714
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-05-06 17:12:42 +0000

    Add extra tests

--- __tests__/functional/transaction-forging/bls-public-key-registration.test.ts
+++ __tests__/functional/transaction-forging/bls-public-key-registration.test.ts
@@ -67,6 +67,120 @@ describe("Transaction Forging - Bls Public Key Registration", () => {
             await expect(transactionsRegisterSecond.id).toBeForged();
             await expect(transactionsRegisterSecond).blsPublicKeyRegistered();
         });
+
+        it("should reject if bls key is already registered", async () => {
+            // Prepare a fresh wallet for the tests
+            const passphrase = generateMnemonic();
+
+            // Initial Funds
+            const initialFunds = TransactionFactory.initialize(app)
+                .transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e8)
+                .withPassphrase(genesisPassphrase)
+                .createOne();
+
+            await expect(initialFunds).toBeAccepted();
+            await snoozeForBlock(1);
+            await expect(initialFunds.id).toBeForged();
+
+            // Register a delegate
+            const transactionsRegister = TransactionFactory.initialize(app)
+                .delegateRegistration()
+                .withPassphrase(passphrase)
+                .createOne();
+
+            await expect(transactionsRegister).toBeAccepted();
+            await snoozeForBlock(1);
+            await expect(transactionsRegister.id).toBeForged();
+
+            // Register bls public key
+            const transactionsRegisterFirst = TransactionFactory.initialize(app)
+                .blsPublicKeyRegistration({
+                    newBlsPublicKey: "b".repeat(96),
+                })
+                .withPassphrase(passphrase)
+                .createOne();
+
+            await expect(transactionsRegisterFirst).toBeRejected();
+            await snoozeForBlock(1);
+            await expect(transactionsRegisterFirst.id).not.toBeForged();
+            await expect(transactionsRegisterFirst).not.blsPublicKeyRegistered();
+        });
+
+        it("should reject if delegate is resiged", async () => {
+            // Prepare a fresh wallet for the tests
+            const passphrase = generateMnemonic();
+
+            // Initial Funds
+            const initialFunds = TransactionFactory.initialize(app)
+                .transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e8)
+                .withPassphrase(genesisPassphrase)
+                .createOne();
+
+            await expect(initialFunds).toBeAccepted();
+            await snoozeForBlock(1);
+            await expect(initialFunds.id).toBeForged();
+
+            // Register a delegate
+            const transactionsRegister = TransactionFactory.initialize(app)
+                .delegateRegistration()
+                .withPassphrase(passphrase)
+                .createOne();
+
+            await expect(transactionsRegister).toBeAccepted();
+            await snoozeForBlock(1);
+            await expect(transactionsRegister.id).toBeForged();
+
+            // Resign a delegate
+            const transactionsResign = TransactionFactory.initialize(app)
+                .delegateResignation()
+                .withPassphrase(passphrase)
+                .createOne();
+
+            await expect(transactionsResign).toBeAccepted();
+            await snoozeForBlock(1);
+            await expect(transactionsResign.id).toBeForged();
+
+            // Register bls public key
+            const transactionsRegisterFirst = TransactionFactory.initialize(app)
+                .blsPublicKeyRegistration({
+                    newBlsPublicKey: "f".repeat(96),
+                })
+                .withPassphrase(passphrase)
+                .createOne();
+
+            await expect(transactionsRegisterFirst).toBeRejected();
+            await snoozeForBlock(1);
+            await expect(transactionsRegisterFirst.id).not.toBeForged();
+            await expect(transactionsRegisterFirst).not.blsPublicKeyRegistered();
+        });
+
+        it("should reject if not from delegate", async () => {
+            // Prepare a fresh wallet for the tests
+            const passphrase = generateMnemonic();
+
+            // Initial Funds
+            const initialFunds = TransactionFactory.initialize(app)
+                .transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e8)
+                .withPassphrase(genesisPassphrase)
+                .createOne();
+
+            await expect(initialFunds).toBeAccepted();
+            await snoozeForBlock(1);
+            await expect(initialFunds.id).toBeForged();
+
+            // Register bls public key
+            const transactionsRegisterFirst = TransactionFactory.initialize(app)
+                .blsPublicKeyRegistration({
+                    newBlsPublicKey: "g".repeat(96),
+                })
+                .withPassphrase(passphrase)
+                .createOne();
+
+            await expect(transactionsRegisterFirst).toBeRejected();
+            await snoozeForBlock(1);
+            await expect(transactionsRegisterFirst.id).not.toBeForged();
+            await expect(transactionsRegisterFirst).not.blsPublicKeyRegistered();
+        });
     });
 
     describe("Signed with 2 Passphases", () => {
