1cf6702ad8dc8b879d0dacc73c19be405f0a6773
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-05-13 13:23:06 +0000

    feat(crypto-transactions): disable sending from and receiving on MultiSignature wallets (#4865)
    * Add errors
    
    * Check sender
    
    * Yarn.lock
    
    * Disable multi signature sending/receiving
    
    * Rename milestones
    
    * Update crypto networks
    
    * Update crypto generator
    
    * Test transfers
    
    * Test multi-payment
    
    * Test remaining
    
    * Update crypto network
    
    * Remove commented line
    
    Co-authored-by: ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>

--- packages/core-transactions/src/handlers/two/multi-payment.ts
+++ packages/core-transactions/src/handlers/two/multi-payment.ts
@@ -1,7 +1,7 @@
 import { Container, Contracts, Utils as AppUtils } from "@arkecosystem/core-kernel";
 import { Interfaces, Managers, Transactions, Utils } from "@arkecosystem/crypto";
 
-import { InsufficientBalanceError } from "../../errors";
+import { InsufficientBalanceError, DisabledMultiSignatureReceiving } from "../../errors";
 import { TransactionHandler, TransactionHandlerConstructor } from "../transaction";
 
 @Container.injectable()
@@ -56,6 +56,16 @@ export class MultiPaymentTransactionHandler extends TransactionHandler {
 
         if (wallet.getBalance().minus(totalPaymentsAmount).minus(transaction.data.fee).isNegative()) {
             throw new InsufficientBalanceError();
+        }
+
+        const milestone = Managers.configManager.getMilestone();
+        if (milestone.multiSignatureReceivingEnabled !== true) {
+            for (const payment of payments) {
+                const recipient: Contracts.State.Wallet = this.walletRepository.findByAddress(payment.recipientId);
+                if (recipient.hasMultiSignature()) {
+                    throw new DisabledMultiSignatureReceiving();
+                }
+            }
         }
 
         return super.throwIfCannotBeApplied(transaction, wallet);
